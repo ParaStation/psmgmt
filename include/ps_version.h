93d46dda606a589a7088b5e814626814acd6ee45
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-04-23 14:05:16 +0000

    *** empty log message ***
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@30 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/ps_version.h
+++ include/ps_version.h
@@ -1,6 +1,6 @@
 /* Generated File! Dont edit */
-#define PSM_BUILD "#504"
-#define PSM_COMPILE_DATE "Fri Apr 20 21:15:32 CEST 2001"
+#define PSM_BUILD "#515"
+#define PSM_COMPILE_DATE "Mon Apr 23 15:10:21 CEST 2001"
 #define PSM_COMPILE_BY "hauke"
 #define PSM_COMPILE_HOST "Phoenix"
 #define PSM_COMPILE_DIR "psmlib"
