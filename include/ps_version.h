07f94c92475aab464c090548bd8f4a0955002941
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-07-27 17:04:50 +0000

    new build with mcp,sis and jm
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@239 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/ps_version.h
+++ include/ps_version.h
@@ -1,6 +1,6 @@
 /* Generated File! Dont edit */
-#define PSM_BUILD "#1221"
-#define PSM_COMPILE_DATE "Fri Jul 20 15:42:37 CEST 2001"
+#define PSM_BUILD "#1441"
+#define PSM_COMPILE_DATE "Fri Jul 27 18:37:54 CEST 2001"
 #define PSM_COMPILE_BY "hauke"
-#define PSM_COMPILE_HOST "europa"
+#define PSM_COMPILE_HOST "Phoenix"
 #define PSM_COMPILE_DIR "psmlib"
