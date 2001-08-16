4b864416e606ebb6d990ebd943f7109b2171406e
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-08-16 17:45:37 +0000

    new PORTHASHFUNCTION
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@304 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/ps_version.h
+++ include/ps_version.h
@@ -1,6 +1,6 @@
 /* Generated File! Dont edit */
-#define PSM_BUILD "#1840"
-#define PSM_COMPILE_DATE "Wed Aug 15 19:37:44 CEST 2001"
+#define PSM_BUILD "#1848"
+#define PSM_COMPILE_DATE "Thu Aug 16 19:20:22 CEST 2001"
 #define PSM_COMPILE_BY "hauke"
 #define PSM_COMPILE_HOST "Phoenix"
 #define PSM_COMPILE_DIR "psmlib"
