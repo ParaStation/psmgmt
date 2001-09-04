450d26cad061c87eea2969bfcb7870af55a79aa4
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-09-04 16:25:26 +0000

    update
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@381 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/ps_version.h
+++ include/ps_version.h
@@ -1,6 +1,6 @@
 /* Generated File! Dont edit */
-#define PSM_BUILD "#2172"
-#define PSM_COMPILE_DATE "Mon Sep 3 16:55:05 CEST 2001"
+#define PSM_BUILD "#2204"
+#define PSM_COMPILE_DATE "Tue Sep 4 17:38:05 CEST 2001"
 #define PSM_COMPILE_BY "hauke"
 #define PSM_COMPILE_HOST "Phoenix"
 #define PSM_COMPILE_DIR "psmlib"
