a645523e4d1d2455f934f3ec4566ed7ce1143a8f
cvs-partec <cvs-partec@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-09-06 15:16:42 +0000

    psistatus ('status net') removed, countstatus ('status count') implemented.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@388 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/info.h
+++ include/info.h
@@ -1,28 +1,15 @@
 #ifndef __info_h_
 #define __info_h_
 
-typedef struct _INFO_STATUS{
-    int myid, nrofnodes, speed;
-    int aos_err, da_err, nack_err, crc_err, rsa_err;
-    int sbuf, maxsbuf, irbuf, maxirbuf, rbuf, maxrbuf;
-} INFO_STATUS;
-
-typedef struct _INFO_COUNT{
-    int t[8];
-    int c[8];
-} INFO_COUNT;
-
 int INFO_request_rdpstatus(int nodeno,void* buffer, int size);
 
 int INFO_request_hoststatus(void* buffer, int size);
 
 int INFO_request_host(unsigned int addr);
 
-int INFO_request_countstatus(int nodeno);
+int INFO_request_countstatus(int nodeno, int header);
 
 int INFO_request_tasklist(int nodeno);
-
-int INFO_request_psistatus(int nodeno);
 
 enum{
     INFO_UID = 0x01,        /* get the uid of the task */
@@ -32,9 +19,5 @@ enum{
 };
 
 long INFO_request_taskinfo(long tid,long what);
-
-void INFO_printCount(INFO_COUNT info);
-
-void INFO_printStatus(INFO_STATUS info);
 
 #endif /* __info_h_ */
