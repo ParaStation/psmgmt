876192b88a49d7d215f04f9a69f0cfb5d218b22e
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-10-10 11:36:09 +0000

    Add forwarder-related stuff. Cleanup.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@487 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/logger.h
+++ include/logger.h
@@ -4,44 +4,39 @@
 #include <sys/types.h>
 #include "psitask.h"
 
-extern int PrependSource;
-
-extern fd_set LOGGERmyfds;
-extern int LOGGERnoclients;
+struct LOGGERclient_t{
+    long id;
+    int std;
+};
 
 /*************************************************************
  * int
- * LOGGERredirect_std(u_long port)
+ * LOGGERredirect_std(int port)
  * 
  * redirect stdout/stderr to sockets connected to port
+ *
  * RETURN 0 success
  *        -1 error errno is set.
  */
 int LOGGERredirect_std(unsigned int node, int port, PStask_t* task);
 
 /*********************************************************************
- * LOGGERgetparentsock(int listensock)
+ * int LOGGERspawnforwarder(unsigned int logger_node, int logger_port)
  *
- * waits util the parent connects to this socket.
- * This socket is needed for (logger<->parent) control
+ * spawns a forwarder. the forwarder will create a channel to the logger
+ * listening at logger_node on logger_port.
+ *
+ * RETURN the portno of the forwarder
  */
-int LOGGERgetparentsock(int listensock);
+int LOGGERspawnforwarder(unsigned int logger_node, int logger_port);
 
 /*********************************************************************
- * LOGGERloop(LOGGERlisten,LOGGERparent)
+ * int LOGGERspawnlogger()
  *
- * does all the logging work. The connection to the parent is already
- * installed. Now childs can connect and log out via the logger.
- */
-int LOGGERloop(int LOGGERlisten,int LOGGERparent);
-
-/*********************************************************************
- * long
- * LOGGERspawnlogger()
+ * spawns a logger.
  *
- * spawns a logger and creates a channel to it.
  * RETURN the portno of the logger
  */
-long LOGGERspawnlogger();
+int LOGGERspawnlogger(void);
 
 #endif
