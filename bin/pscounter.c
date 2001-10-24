fda6e2bb188c5e079b764196288378dcf665f2a8
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-10-24 15:48:47 +0000

    Add differential counter
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@530 34291a86-62e8-0310-8159-f060f8a2ee4e

--- bin/pscounter.c
+++ bin/pscounter.c
@@ -2,6 +2,7 @@
 /* 2001-09-04 (c) ParTec AG, Jens Hauke */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
@@ -17,34 +18,70 @@
 int main(int argc, char **argv)
 {
     PSHALInfoCounter_t *ic;
+    PSHALInfoCounter_t ic2;
     int i;
+    int loop;
+    int time=0;
+    int cont=0;
     
     if (PSHALStartUp(0)){
 	perror("PSHALStartUp");
 	exit(1);
     }
 
-    ic=PSHALSYSGetInfoCounter();
-    if ( !ic ){
-	perror("GetInfoCounter");
-	exit(1);
+    if (argc>1){
+	time=atoi(argv[1]);
+	printf("Counter / %d seconds\n",time);
+	cont=1;
+	if (!time){
+	    printf("Use: %s [delay [count]]\n",argv[0]);
+	    exit(1);
+	}
+    }
+    if (argc>2){
+	cont=atoi(argv[2]);
     }
 
-    /* Print Header */
-    for (i=0;i<ic->n;i++){
-	printf("%8s ",ic->counter[i].name);
-    }
-    printf("\n");
 
-    /* Print Values */
-    for (i=0;i<ic->n;i++){
-	char ch[10];
-	/* calc column size from name length */
-	sprintf(ch,"%%%du ",MAX(strlen(ic->counter[i].name),8));
-	printf(ch,ic->counter[i].value);
+    for (loop=0;loop<=cont;loop++){
+	ic=PSHALSYSGetInfoCounter();
+	if ( !ic ){
+	    perror("GetInfoCounter");
+	    exit(1);
+	}
+	if (loop==0){
+	    /* Print Header */
+	    for (i=0;i<ic->n;i++){
+		printf("%8s ",ic->counter[i].name);
+	    }
+	    printf("\n");
+	}
+	if (!time){
+	    /* Print Values */
+	    for (i=0;i<ic->n;i++){
+		char ch[10];
+		/* calc column size from name length */
+		sprintf(ch,"%%%du ",MAX(strlen(ic->counter[i].name),8));
+		printf(ch,ic->counter[i].value);
+	    }
+	    printf("\n");
+	}else{
+	    if (loop>0){
+		/* Print Delta Values */
+		for (i=0;i<ic->n;i++){
+		    char ch[10];
+		    /* calc column size from name length */
+		    sprintf(ch,"%%%du ",MAX(strlen(ic->counter[i].name),8));
+		    printf(ch,ic->counter[i].value-ic2.counter[i].value);
+		}
+		printf("\n");
+	    }
+	}
+	if (time){
+	    sleep(time);
+	    ic2= *ic;
+	}
     }
-    printf("\n");
-
     return 0;
 }
 
