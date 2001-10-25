acafa13205edcbd621a7bfb6788840ae8760a856
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-10-25 17:27:32 +0000

    Update. Add a new Sendbuffer State (CSENDRUN) and the handling for the new state. This fix the "early free Sendbuffer" on resend.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@536 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/ps_version.h
+++ include/ps_version.h
@@ -1,7 +1,7 @@
 /* Generated File! Dont edit */
-#define PSM_BUILD "#2582"
-#define PSM_COMPILE_DATE "Fri Oct 12 13:24:23 CEST 2001"
+#define PSM_BUILD "#2669"
+#define PSM_COMPILE_DATE "Thu Oct 25 18:41:51 CEST 2001"
 #define PSM_COMPILE_BY "hauke"
-#define PSM_COMPILE_HOST "Phoenix"
+#define PSM_COMPILE_HOST "io"
 #define PSM_COMPILE_DIR "psmlib"
 #define PSM_COMPILE_COM ""
