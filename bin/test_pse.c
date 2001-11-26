c9ecaf5b3b908f8f0b287a0fcc4673860e613272
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-11-26 11:08:10 +0000

    test pse
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@547 34291a86-62e8-0310-8159-f060f8a2ee4e

---
+++ bin/test_pse.c
@@ -0,0 +1,107 @@
+/*
+ * 2001-09-17  ParTec AG , Jens Hauke
+ *
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <syslog.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+#include "ps_types.h"
+#include "pshal.h"
+#include "psport.h"
+#include "pse.h"
+#include <signal.h>
+#include "arg.h"
+#include <sys/time.h>
+
+int arg_np;
+int arg_cnt;
+
+
+void run(int argc,char **argv,int np)
+{
+    int mapnode;
+    int mapport;
+    int rank;
+
+    mapnode=0;
+    mapport=0;
+    rank =0;
+    
+    PSEinit(np,argc,argv,&mapnode,&mapport,&rank);
+//    sleep(rank/30);
+    printf("node: %d port: %d rank: %d\n",mapnode,mapport,rank  );
+    sleep(10);
+    PSEfinalize();
+}
+
+
+
+
+
+int main(int argc, char **argv)
+{
+
+    //printf(__DATE__" "__TIME__"\n");
+
+    if (arg_parse(argc, argv,
+		  "", "Usage: %s [options]", argv[0],
+		  "-np %d",&arg_np," ",
+		  "-cnt %d",&arg_cnt," ",
+		  0) < 0){
+        exit(1);
+    }
+
+    if (arg_np <= 0) {
+	fprintf(stderr,"missing arg -np\n");
+	exit(1);
+    }
+
+    run(argc,argv,arg_np);
+    return 0;
+}
+
+
+
+
+/*
+ * Local Variables:
+ *  compile-command: "make test_pse"
+ * End
+ */
+
+/*
+ssh io "cdl psm;cd tools;make test_nodes";cdl psm;scp -C tools/alpha_Linux/test_nodes alice:
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
