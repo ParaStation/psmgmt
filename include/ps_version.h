cea380e11e651456dafbcfc34489280e6b3ff086
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-12-13 18:17:10 +0000

    completely rewrite of pci mapping. Unified for 2.2.x and 2.4.x Alpha and Intel. Fixed memory management (walking through pgd,pmd,pte) for HighMemory Support (4 and 64 GByte Intel). Code cleanups...
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@550 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/ps_version.h
+++ include/ps_version.h
@@ -1,7 +1,7 @@
 /* Generated File! Dont edit */
-#define PSM_BUILD "#2669"
-#define PSM_COMPILE_DATE "Thu Oct 25 18:41:51 CEST 2001"
+#define PSM_BUILD "#2883"
+#define PSM_COMPILE_DATE "Thu Dec 13 19:00:57 CET 2001"
 #define PSM_COMPILE_BY "hauke"
-#define PSM_COMPILE_HOST "io"
+#define PSM_COMPILE_HOST "Phoenix"
 #define PSM_COMPILE_DIR "psmlib"
-#define PSM_COMPILE_COM ""
+#define PSM_COMPILE_COM "(develop)"
--- include/psm_module.h
+++ include/psm_module.h
@@ -1,6 +1,6 @@
 
 /*************************************************************fdb*
- * $Id: psm_module.h,v 1.2 2001/08/30 17:32:03 hauke Exp $
+ * $Id: psm_module.h,v 1.3 2001/12/13 18:17:09 hauke Exp $
  * structures and prototypes of psm_module.c
  *
  *************************************************************fde*/
@@ -47,6 +47,28 @@ struct psm_mcpif {
 
 
 extern int psm_node_id;
+
+/*
+ * Zero copy statistics
+ */
+#define PSM_ZC_SEND_OK			0
+#define PSM_ZC_SEND_MLOCK_FAIL		1
+#define PSM_ZC_SEND_VIRT2BUS_FAIL	2
+#define PSM_ZC_SEND_DISABLED		3
+#define PSM_ZC_RECV_OK			0
+#define PSM_ZC_RECV_MLOCK_FAIL		1
+#define PSM_ZC_RECV_VIRT2BUS_FAIL	2
+#define PSM_ZC_RECV_DISABLED		3
+
+#define PSM_ZC_SEND_INC(name)  psm_zc_send_cnt[PSM_ZC_SEND_##name]++
+#define PSM_ZC_RECV_INC(name)  psm_zc_recv_cnt[PSM_ZC_SEND_##name]++
+extern int psm_zc_send_cnt[4];
+extern int psm_zc_recv_cnt[4];
+
+/*
+ * Enable/disable zero copy
+ */
+extern int zc;
 
 
 int psm_ioctl(int kern,psm_context_t context, unsigned int cmd, unsigned long arg);
