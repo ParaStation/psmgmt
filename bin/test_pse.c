e34111d7c907aa6dc43286088d5fb9d343720c23
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2001-12-21 20:12:59 +0000

    more info
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@559 34291a86-62e8-0310-8159-f060f8a2ee4e

--- bin/test_pse.c
+++ bin/test_pse.c
@@ -29,15 +29,17 @@ void run(int argc,char **argv,int np)
     int mapnode;
     int mapport;
     int rank;
-
+    char name[100];
     mapnode=0;
     mapport=0;
     rank =0;
     
     PSEinit(np,argc,argv,&mapnode,&mapport,&rank);
 //    sleep(rank/30);
-    printf("node: %d port: %d rank: %d\n",mapnode,mapport,rank  );
-    sleep(10);
+    gethostname(name,sizeof(name)-1);
+
+    printf("node: %d port: %d rank: %d host:%s\n",mapnode,mapport,rank,name  );
+    sleep(3);
     PSEfinalize();
 }
 
