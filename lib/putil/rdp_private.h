998024cee6a6edb958f25dd9418766d14905f979
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-01-15 11:44:14 +0000

    Forward declarations moved from rdp.c to rdp_private.c Sometimes, all functions will be documented inside this file...
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@618 34291a86-62e8-0310-8159-f060f8a2ee4e

---
+++ lib/putil/rdp_private.h
@@ -0,0 +1,48 @@
+/*
+ *               ParaStation3
+ * rdp_private.h
+ *
+ * Copyright (C) ParTec AG Karlsruhe
+ * All rights reserved.
+ *
+ * $Id: rdp_private.h,v 1.1 2002/01/15 11:44:14 eicker Exp $ 
+ *
+ */
+/**
+ * \file
+ * rdp_private: Reliable Datagram Protocol for ParaStation daemon
+ *              Private functions and definitions
+ *
+ * $Id: rdp_private.h,v 1.1 2002/01/15 11:44:14 eicker Exp $ 
+ *
+ * \author
+ * Norbert Eicker <eicker@par-tec.com>
+ *
+ */
+#ifndef __RDP_PRIVATE_H
+#define __RDP_PRIVATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#if 0
+} // <- just for emacs indentation
+#endif
+#endif
+
+static void resendMsgs(int node);
+
+static void closeConnection(int node);
+
+static void handleMCAST(void);
+
+static void MCAST_PING(RDPState state);
+
+static void initMCAST(int group, unsigned short port);
+
+static char *CIstate(RDPState state);
+
+#ifdef __cplusplus
+}/* extern "C" */
+#endif
+
+#endif /* __RDP_H */
