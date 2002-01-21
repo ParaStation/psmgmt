74f7dd5e612a11344cc24cccd551145f2cbfd7ff
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-01-21 16:12:20 +0000

    Problems with output of 'status count' if nodes are down fixed.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@644 34291a86-62e8-0310-8159-f060f8a2ee4e

--- bin/admin/psiadmin.c
+++ bin/admin/psiadmin.c
@@ -5,11 +5,11 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: psiadmin.c,v 1.20 2002/01/21 10:18:12 eicker Exp $
+ * $Id: psiadmin.c,v 1.21 2002/01/21 16:12:20 eicker Exp $
  *
  */
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
-static char vcid[] __attribute__(( unused )) = "$Id: psiadmin.c,v 1.20 2002/01/21 10:18:12 eicker Exp $";
+static char vcid[] __attribute__(( unused )) = "$Id: psiadmin.c,v 1.21 2002/01/21 16:12:20 eicker Exp $";
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
 #include <stdlib.h>
@@ -40,7 +40,7 @@ void *yy_scan_string(char *line);
 void yyparse(void);
 void yy_delete_buffer(void *line_state);
 
-static char psiadmversion[] = "$Revision: 1.20 $";
+static char psiadmversion[] = "$Revision: 1.21 $";
 static int  DoRestart = 1;
 
 int PSIADM_LookUpNodeName(char* hostname)
@@ -126,25 +126,26 @@ void PSIADM_CountStat(int first, int last)
     first = (first==ALLNODES) ? 0 : first;
     last  = (last==ALLNODES) ? PSI_nrofnodes : last+1;
 
+    /* Get header info from own daemon */
+    INFO_request_countstatus(PSI_myid, &ic, sizeof(ic));
+    printf("%6s ", "NODE");
+    for (j=0 ; j<ic.n; j++){
+	printf("%8s ",ic.counter[j].name);
+    }
+    printf("\n");
+
     for (i = first; i < last; i++) {
-	INFO_request_countstatus(i, &ic, sizeof(ic));
-	if (i==first){
-	    /* first node with header */
-	    printf("%6s ", "NODE");
-	    for (j=0 ; j<ic.n; j++){
-		printf("%8s ",ic.counter[j].name);
+	printf("%6u ", i); fflush(stdout);
+
+	if (INFO_request_countstatus(i, &ic, sizeof(ic)) != -1) {
+	    for (j=0; j<ic.n; j++){
+		char ch[10];
+		/* calc column size from name length */
+		sprintf(ch,"%%%du ",(int) MAX(strlen(ic.counter[j].name),8));
+		printf(ch, ic.counter[j].value);
 	    }
 	    printf("\n");
 	}
-
-	printf("%6u ", i);
-	for (j=0; j<ic.n; j++){
-	    char ch[10];
-	    /* calc column size from name length */
-	    sprintf(ch,"%%%du ",(int) MAX(strlen(ic.counter[j].name),8));
-	    printf(ch, ic.counter[j].value);
-	}
-	printf("\n");
     }
 
     return;
