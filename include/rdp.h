e0d0b26f369dcd0e6664069853d18883bbacf106
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-01-30 16:43:21 +0000

    New naming convention. More docu added. Some cleanup.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@679 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/rdp.h
+++ include/rdp.h
@@ -5,14 +5,14 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: rdp.h,v 1.6 2002/01/23 11:25:49 eicker Exp $
+ * $Id: rdp.h,v 1.7 2002/01/30 16:43:21 eicker Exp $
  *
  */
 /**
  * @file
  * rdp: Reliable Datagram Protocol for ParaStation daemon
  *
- * $Id: rdp.h,v 1.6 2002/01/23 11:25:49 eicker Exp $
+ * $Id: rdp.h,v 1.7 2002/01/30 16:43:21 eicker Exp $
  *
  * @author
  * Norbert Eicker <eicker@par-tec.com>
@@ -28,39 +28,11 @@ extern "C" {
 #endif
 #endif
 
-typedef enum {
-    CLOSED=0x1,
-    SYN_SENT=0x2,
-    SYN_RECVD=0x3,
-    ACTIVE=0x4
-} RDPState;
-
-typedef struct RDPLoad_ {
-  double	load[3];	/* Load parameters of that node */
-} RDPLoad;
-
-typedef struct RDP_ConInfo_ {
-  RDPState	state;
-  RDPLoad	load;
-  int	 	misscounter;
-} RDP_ConInfo;
-
-#define T_INFO		0x01
-#define T_CLOSE		0x02
-#define T_LIC		0x10
-#define T_KILL		0x20
-
-typedef struct Mmsg_ {
-  short         node;           /* Sender ID */
-  short         type;
-  RDPState      state;
-  RDPLoad       load;
-} Mmsg;   
-
-typedef struct RDPDeadbuf_{
-  int dst;
-  void *buf;
-  int buflen;
+/** @todo Create docu */
+typedef struct {
+    int dst;
+    void *buf;
+    int buflen;
 } RDPDeadbuf;
 
 
@@ -68,19 +40,12 @@ typedef struct RDPDeadbuf_{
 #define RDP_LOST_CONNECTION	0x2	/* buf == nodeno */
 #define RDP_PKT_UNDELIVERABLE	0x3	/* buf == (dst,*buf,buflen) */
 
-#define RDP_LIC_LOST		0x10	/* buf == sinaddr */ 
-#define RDP_LIC_SHUTDOWN	0x20	/* buf == reason */ 
-
-#define LIC_LOST_CONECTION	0x1
-#define LIC_KILL_MSG		0x2
-
 /**
- * @brief Initializes the RDP and MCast modules.
+ * @brief Initializes the RDP module.
  *
- * Initializes the RDP and MCast machinery for @a nodes nodes.
+ * Initializes the RDP machinery for @a nodes nodes.
  *
  * @param nodes Number of nodes to handle.
- * @param mgroup The MultiCast group to use.
  * @param usesyslog If true, all error-messages are printed via syslog().
  * @param hosts An array of size @a nodes containing the IP-addresses of the
  * participating nodes in network-byteorder.
@@ -90,46 +55,23 @@ typedef struct RDPDeadbuf_{
  * @return On success, the filedescriptor of the RDP socket is returned.
  * On error, exit() is called within this function.
  */
-int initRDP(int nodes, int mgroup, int usesyslog, unsigned int hosts[],
+int initRDP(int nodes, int usesyslog, unsigned int hosts[],
 	    void (*func)(int, void*));
-
-/**
- * @brief Initializes the MCast module.
- *
- * Initializes the MCast machinery for @a nodes nodes.
- *
- * @param nodes Number of nodes to handle.
- * @param mgroup The MultiCast group to use.
- * @param usesyslog If true, all error-messages are printed via syslog().
- * @param hosts An array of size @a nodes containing the IP-addresses of the
- * participating nodes in network-byteorder.
- * @param func Pointer to a callback-function. This function is called if
- * something exceptional happens. If NULL, no callbacks will be done.
- *
- * @return On success, the filedescriptor of the MCast socket is returned.
- * On error, exit() is called within this function.
- */
-int initRDPMCAST(int nodes, int mgroup, int usesyslog, unsigned int hosts[],
-		 void (*func)(int, void*));
 
 /*
  * Shutdown RDP
  */
 void exitRDP(void);
 
-/*
+/**
+ * @brief Send a RDP packet.
+ *
  * Sent a msg[buf:len] to node <node> reliable
  */
 int Rsendto(int node, void *buf, int len);
 
-/*
- * RDP select() call
- */
-int Rselect(int n, fd_set  *readfds,  fd_set  *writefds, fd_set *exceptfds, struct timeval *timeout);
-int Mselect(int n, fd_set  *readfds,  fd_set  *writefds, fd_set *exceptfds, struct timeval *timeout);
-
-/*
- * receive a RDP packet 
+/**
+ * @brief Receive a RDP packet.
  * Parameters:	node: source node of msg (O)
  *              msg:  pointer to msg buffer (O)
  *              len:  max lenght of buffer (I)
@@ -137,37 +79,49 @@ int Mselect(int n, fd_set  *readfds,  fd_set  *writefds, fd_set *exceptfds, stru
  */
 int Rrecvfrom(int *node, void *msg, int len);
 
-/*
- * Get DebugLevel
+/**
+ * @brief Query the debug-level.
+ *
+ * Get the debug-level of the RDP module.
+ *
+ * @return The actual debug-level is returned.
+ *
+ * @see setDebugLevelRDP()
  */
-int getRDPDebugLevel(void);
+int getDebugLevelRDP(void);
 
-/*
- * Set DebugLevel
+/**
+ * @brief Set the debug-level.
+ *
+ * Set the debug-level of the RDP module. Posible values are:
+ *  - 0: Critical errors (usually exit)
+ *  - 1: .... @todo More levels to add.
+ *
+ * @param level The debug-level to set
+ *
+ * @return No return value.
+ *
+ * @see getDebugLevelRDP()
  */
-void setRDPDebugLevel(int level);
+void setDebugLevelRDP(int level);
 
-/*
- * Set Log Msg
+/**
+ * @brief Get status info.
+ *
+ * Get status information from the RDP module concerning the connection to
+ * node @a node. The result is returned in @a string and can be directly
+ * put out via printf() and friends.
+ *
+ * @param node The node, which is joined via the connection, the status
+ * information is retrieved from.
+ * @param string The string to which the status information is written.
+ * @param len The length of @a string.
+ *
+ * @return No return value.
+ *
+ * @see printf(3)
  */
-void setRDPLogMsg(int level);
-
-/*
- * Get Dead Limit
- */
-int getRDPDeadLimit(void);
-
-/*
- * Set Dead Limit
- */
-void setRDPDeadLimit(int limit);
-
-/*
- * Get Info for node n
- */
-void getRDPInfo(int n, RDP_ConInfo *info);
-
-void getRDPStateInfo(int n, char *s, size_t len);
+void getStateInfoRDP(int node, char *string, size_t len);
 
 #ifdef __cplusplus
 }/* extern "C" */
