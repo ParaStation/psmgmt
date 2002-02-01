4163d0b9943c7a15877e14dc02f120744188a2f6
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-02-01 16:37:33 +0000

    Don't put out all the spaces.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@706 34291a86-62e8-0310-8159-f060f8a2ee4e

--- lib/psi/psp.c
+++ lib/psi/psp.c
@@ -44,86 +44,85 @@ extern struct PSPctrlmessages_t PSPctrlmessages[];
 
 struct PSPctrlmessages_t PSPctrlmessages[]=
 {
-   { PSP_CD_CLIENTCONNECT      ,"PSP_CD_CLIENTCONNECT      "},
-   { PSP_CD_CLIENTESTABLISHED  ,"PSP_CD_CLIENTESTABLISHED  "},
-   { PSP_CD_CLIENTREFUSED      ,"PSP_CD_CLIENTREFUSED      "},
-   { PSP_CD_OLDVERSION         ,"PSP_CD_OLDVERSION         "},
-   { PSP_CD_NOSPACE            ,"PSP_CD_NOSPACE            "}, 
-   { PSP_CD_REMOTECONNECT      ,"PSP_CD_REMOTECONNECT      "},
-   { PSP_CD_UIDLIMIT           ,"PSP_CD_UIDLIMIT           "},
-   { PSP_CD_PROCLIMIT          ,"PSP_CD_PROCLIMIT          "}, 
-   { PSP_DD_SETOPTION          ,"PSP_DD_SETOPTION          "}, 
-   { PSP_DD_GETOPTION          ,"PSP_DD_GETOPTION          "}, 
-   { PSP_DD_CONTACTNODE        ,"PSP_DD_CONTACTNODE        "},
+   { PSP_CD_CLIENTCONNECT      ,"PSP_CD_CLIENTCONNECT"      },
+   { PSP_CD_CLIENTESTABLISHED  ,"PSP_CD_CLIENTESTABLISHED"  },
+   { PSP_CD_CLIENTREFUSED      ,"PSP_CD_CLIENTREFUSED"      },
+   { PSP_CD_OLDVERSION         ,"PSP_CD_OLDVERSION"         },
+   { PSP_CD_NOSPACE            ,"PSP_CD_NOSPACE"            }, 
+   { PSP_CD_REMOTECONNECT      ,"PSP_CD_REMOTECONNECT"      },
+   { PSP_CD_UIDLIMIT           ,"PSP_CD_UIDLIMIT"           },
+   { PSP_CD_PROCLIMIT          ,"PSP_CD_PROCLIMIT"          }, 
+   { PSP_DD_SETOPTION          ,"PSP_DD_SETOPTION"          }, 
+   { PSP_DD_GETOPTION          ,"PSP_DD_GETOPTION"          }, 
+   { PSP_DD_CONTACTNODE        ,"PSP_DD_CONTACTNODE"        },
    
-   { PSP_CD_TASKINFO           ,"PSP_CD_TASKINFO           "}, 
-   { PSP_CD_TASKINFOEND        ,"PSP_CD_TASKINFOEND        "}, 
-   { PSP_CD_TASKLISTREQUEST    ,"PSP_CD_TASKLISTREQUEST    "}, 
-   { PSP_CD_TASKINFOREQUEST    ,"PSP_CD_TASKINFOREQUEST    "},
-   { PSP_CD_HOSTSTATUSREQUEST  ,"PSP_CD_HOSTSTATUSREQUEST  "},
-   { PSP_CD_HOSTSTATUSRESPONSE ,"PSP_CD_HOSTSTATUSRESPONSE "},
+   { PSP_CD_TASKINFO           ,"PSP_CD_TASKINFO"           }, 
+   { PSP_CD_TASKINFOEND        ,"PSP_CD_TASKINFOEND"        }, 
+   { PSP_CD_TASKLISTREQUEST    ,"PSP_CD_TASKLISTREQUEST"    }, 
+   { PSP_CD_TASKINFOREQUEST    ,"PSP_CD_TASKINFOREQUEST"    },
+   { PSP_CD_HOSTSTATUSREQUEST  ,"PSP_CD_HOSTSTATUSREQUEST"  },
+   { PSP_CD_HOSTSTATUSRESPONSE ,"PSP_CD_HOSTSTATUSRESPONSE" },
 
-   { PSP_CD_HOSTREQUEST        ,"PSP_CD_HOSTREQUEST        "},
-   { PSP_CD_HOSTRESPONSE       ,"PSP_CD_HOSTRESPONSE       "},
+   { PSP_CD_HOSTREQUEST        ,"PSP_CD_HOSTREQUEST"        },
+   { PSP_CD_HOSTRESPONSE       ,"PSP_CD_HOSTRESPONSE"       },
 
-   { PSP_CD_LOADREQ            ,"PSP_CD_LOADREQ            "},
-   { PSP_CD_LOADRES            ,"PSP_CD_LOADRES            "},
+   { PSP_CD_LOADREQ            ,"PSP_CD_LOADREQ"            },
+   { PSP_CD_LOADRES            ,"PSP_CD_LOADRES"            },
 
-   { PSP_CD_COUNTSTATUSREQUEST ,"PSP_CD_COUNTSTATUSREQUEST "},
+   { PSP_CD_COUNTSTATUSREQUEST ,"PSP_CD_COUNTSTATUSREQUEST" },
    { PSP_CD_COUNTSTATUSRESPONSE,"PSP_CD_COUNTSTATUSRESPONSE"},
 
-   { PSP_CD_RDPSTATUSREQUEST   ,"PSP_CD_RDPSTATUSREQUEST   "},  
-   { PSP_CD_RDPSTATUSRESPONSE  ,"PSP_CD_RDPSTATUSRESPONSE  "},  
+   { PSP_CD_RDPSTATUSREQUEST   ,"PSP_CD_RDPSTATUSREQUEST"   },  
+   { PSP_CD_RDPSTATUSRESPONSE  ,"PSP_CD_RDPSTATUSRESPONSE"  },  
 
-   { PSP_CD_MCASTSTATUSREQUEST ,"PSP_CD_MCASTSTATUSREQUEST "},  
+   { PSP_CD_MCASTSTATUSREQUEST ,"PSP_CD_MCASTSTATUSREQUEST" },  
    { PSP_CD_MCASTSTATUSRESPONSE,"PSP_CD_MCASTSTATUSRESPONSE"},  
 
-   { PSP_DD_SPAWNREQUEST       ,"PSP_DD_SPAWNREQUEST       "}, 
-   { PSP_DD_SPAWNSUCCESS       ,"PSP_DD_SPAWNSUCCESS       "}, 
-   { PSP_DD_SPAWNFAILED        ,"PSP_DD_SPAWNFAILED        "}, 
+   { PSP_DD_SPAWNREQUEST       ,"PSP_DD_SPAWNREQUEST"       }, 
+   { PSP_DD_SPAWNSUCCESS       ,"PSP_DD_SPAWNSUCCESS"       }, 
+   { PSP_DD_SPAWNFAILED        ,"PSP_DD_SPAWNFAILED"        }, 
    
-   { PSP_DD_TASKKILL           ,"PSP_DD_TASKKILL           "}, 
-   { PSP_DD_NOTIFYDEAD         ,"PSP_DD_NOTIFYDEAD         "},
-   { PSP_DD_RELEASE            ,"PSP_DD_RELEASE            "},
-   { PSP_DD_WHODIED            ,"PSP_DD_WHODIED            "},
-   { PSP_DD_NOTIFYDEADRES      ,"PSP_DD_NOTIFYDEADRES      "},
-   { PSP_DD_RELEASERES         ,"PSP_DD_RELEASERES         "},
+   { PSP_DD_TASKKILL           ,"PSP_DD_TASKKILL"           }, 
+   { PSP_DD_NOTIFYDEAD         ,"PSP_DD_NOTIFYDEAD"         },
+   { PSP_DD_RELEASE            ,"PSP_DD_RELEASE"            },
+   { PSP_DD_WHODIED            ,"PSP_DD_WHODIED"            },
+   { PSP_DD_NOTIFYDEADRES      ,"PSP_DD_NOTIFYDEADRES"      },
+   { PSP_DD_RELEASERES         ,"PSP_DD_RELEASERES"         },
 
-   { PSP_DD_SYSTEMERROR        ,"PSP_DD_SYSTEMERROR        "}, 
-   { PSP_DD_STATENOCONNECT     ,"PSP_DD_STATENOCONNECT     "},
+   { PSP_DD_SYSTEMERROR        ,"PSP_DD_SYSTEMERROR"        }, 
+   { PSP_DD_STATENOCONNECT     ,"PSP_DD_STATENOCONNECT"     },
 
-   { PSP_DD_RESET              ,"PSP_DD_RESET              "},
-   { PSP_DD_RESET_START_REQ    ,"PSP_DD_RESET_START_REQ    "},
-   { PSP_DD_RESET_START_RESP   ,"PSP_DD_RESET_START_RESP   "},
-   { PSP_DD_RESET_DORESET      ,"PSP_DD_RESET_DORESET      "},
-   { PSP_DD_RESET_DONE         ,"PSP_DD_RESET_DONE         "},
-   { PSP_DD_RESET_ABORT        ,"PSP_DD_RESET_ABORT        "},
-   { PSP_DD_RESET_OK           ,"PSP_DD_RESET_OK           "},
+   { PSP_DD_RESET              ,"PSP_DD_RESET"              },
+   { PSP_DD_RESET_START_REQ    ,"PSP_DD_RESET_START_REQ"    },
+   { PSP_DD_RESET_START_RESP   ,"PSP_DD_RESET_START_RESP"   },
+   { PSP_DD_RESET_DORESET      ,"PSP_DD_RESET_DORESET"      },
+   { PSP_DD_RESET_DONE         ,"PSP_DD_RESET_DONE"         },
+   { PSP_DD_RESET_ABORT        ,"PSP_DD_RESET_ABORT"        },
+   { PSP_DD_RESET_OK           ,"PSP_DD_RESET_OK"           },
 
-   { PSP_CD_RESET              ,"PSP_CD_RESET              "},
-   { PSP_CD_RESET_START_REQ    ,"PSP_CD_RESET_START_REQ    "},
-   { PSP_CD_RESET_START_RESP   ,"PSP_CD_RESET_START_RESP   "},
-   { PSP_CD_RESET_DORESET      ,"PSP_CD_RESET_DORESET      "},
-   { PSP_CD_RESET_DONE         ,"PSP_CD_RESET_DONE         "},
-   { PSP_CD_RESET_ABORT        ,"PSP_CD_RESET_ABORT        "},
-   { PSP_CD_RESET_OK           ,"PSP_CD_RESET_OK           "},
+   { PSP_CD_RESET              ,"PSP_CD_RESET"              },
+   { PSP_CD_RESET_START_REQ    ,"PSP_CD_RESET_START_REQ"    },
+   { PSP_CD_RESET_START_RESP   ,"PSP_CD_RESET_START_RESP"   },
+   { PSP_CD_RESET_DORESET      ,"PSP_CD_RESET_DORESET"      },
+   { PSP_CD_RESET_DONE         ,"PSP_CD_RESET_DONE"         },
+   { PSP_CD_RESET_ABORT        ,"PSP_CD_RESET_ABORT"        },
+   { PSP_CD_RESET_OK           ,"PSP_CD_RESET_OK"           },
 
-   { PSP_DD_DAEMONCONNECT      ,"PSP_DD_DAEMONCONNECT      "}, 
-   { PSP_DD_DAEMONESTABLISHED  ,"PSP_DD_DAEMONESTABLISHED  "}, 
-   { PSP_DD_DAEMONSTOP         ,"PSP_DD_DAEMONSTOP         "}, 
-   { PSP_CD_DAEMONSTOP         ,"PSP_CD_DAEMONSTOP         "}, 
+   { PSP_DD_DAEMONCONNECT      ,"PSP_DD_DAEMONCONNECT"      }, 
+   { PSP_DD_DAEMONESTABLISHED  ,"PSP_DD_DAEMONESTABLISHED"  }, 
+   { PSP_DD_DAEMONSTOP         ,"PSP_DD_DAEMONSTOP"         }, 
+   { PSP_CD_DAEMONSTOP         ,"PSP_CD_DAEMONSTOP"         }, 
 
-   { PSP_DD_NEWPROCESS         ,"PSP_DD_NEWPROCESS         "},
-   { PSP_DD_DELETEPROCESS      ,"PSP_DD_DELETEPROCESS      "}, 
-   { PSP_DD_CHILDDEAD          ,"PSP_DD_CHILDDEAD          "},
+   { PSP_DD_NEWPROCESS         ,"PSP_DD_NEWPROCESS"         },
+   { PSP_DD_DELETEPROCESS      ,"PSP_DD_DELETEPROCESS"      }, 
+   { PSP_DD_CHILDDEAD          ,"PSP_DD_CHILDDEAD"          },
 
    {0,NULL}
 };
 
 char PSPctrlmsgtxt[80];
 
-char*
-PSPctrlmsg(int msgtype)
+char* PSPctrlmsg(int msgtype)
 {
     int i=0;
     while ((PSPctrlmessages[i].id!=0)            /* end symbol */
