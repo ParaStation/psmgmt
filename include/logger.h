93cd012286286367c7e40c78c4d719113f310a4d
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-02-11 12:43:13 +0000

    *** empty log message ***
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@755 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/logger.h
+++ include/logger.h
@@ -5,14 +5,14 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: logger.h,v 1.6 2002/02/08 17:19:29 hauke Exp $
+ * $Id: logger.h,v 1.7 2002/02/11 12:43:13 eicker Exp $
  *
  */
 /**
  * @file
  * User-functions for interaction with the ParaStation Logger.
  *
- * $Id: logger.h,v 1.6 2002/02/08 17:19:29 hauke Exp $
+ * $Id: logger.h,v 1.7 2002/02/11 12:43:13 eicker Exp $
  *
  * @author
  * Norbert Eicker <eicker@par-tec.com>
@@ -36,21 +36,28 @@ extern int stdin_fileno_backup;
 extern int stdout_fileno_backup;
 extern int stderr_fileno_backup;
 
-/*********************************************************************
- * void LOGGERspawnforwarder(unsigned int logger_node, int logger_port)
+/**
+ * @brief Spans a forwarder.
  *
- * spawns a forwarder connected with 2 pipes and redirects stdout and
- * stderr to this pipes. stdout and stderr are backed up for later reuse
- *
+ * @todo
  * Spawns a forwarder and redirect stdin/stdout/stderr to forwarder.
  * stdin,stdout and stderr are backed up for later reuse.
  * The forwarder will create a channel to the logger listening
  * at logger_node on logger_port. rank is the rank of the spawned task.
  * if tty != 0 create an pty for stdin and stdout.
  *
- * RETURN nothing
+ * spawns a forwarder connected with 2 pipes and redirects stdout and
+ * stderr to this pipes. stdout and stderr are backed up for later reuse
+ *
+ * @param logger_node IP-address of the node where the logger listens.
+ * @param logger_port Port the logger is listening on.
+ * @param rank @todo
+ * @param tty
+ *
+ * @return No return value.
  */
-void LOGGERspawnforwarder(unsigned int logger_node, int logger_port,int rank,int tty);
+void LOGGERspawnforwarder(unsigned int logger_node, int logger_port,
+			  int rank, int tty);
 
 /*********************************************************************
  * int LOGGERspawnlogger()
