0f34013e37ba1886c97e0b83b1f284dd38b005c1
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-02-11 13:27:04 +0000

    Cosmetical changes.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@761 34291a86-62e8-0310-8159-f060f8a2ee4e

--- bin/logger/psiforwarder.c
+++ bin/logger/psiforwarder.c
@@ -5,21 +5,21 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: psiforwarder.c,v 1.11 2002/02/08 21:05:34 hauke Exp $
+ * $Id: psiforwarder.c,v 1.12 2002/02/11 13:27:04 eicker Exp $
  *
  */
 /**
  * @file
  * psiforwarder: Forwarding-daemon for ParaStation I/O forwarding facility
  *
- * $Id: psiforwarder.c,v 1.11 2002/02/08 21:05:34 hauke Exp $
+ * $Id: psiforwarder.c,v 1.12 2002/02/11 13:27:04 eicker Exp $
  *
  * @author
  * Norbert Eicker <eicker@par-tec.com>
  *
  */
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
-static char vcid[] __attribute__(( unused )) = "$Id: psiforwarder.c,v 1.11 2002/02/08 21:05:34 hauke Exp $";
+static char vcid[] __attribute__(( unused )) = "$Id: psiforwarder.c,v 1.12 2002/02/11 13:27:04 eicker Exp $";
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
 #include <stdio.h>
@@ -68,14 +68,13 @@ void closelog(void)
     writelog(loggersock, FINALIZE, id, NULL, 0);
 
     readlog(loggersock, (FLBufferMsg_t *) &msg);
-    if(msg.type != EXIT){
+    if (msg.type != EXIT) {
 	/* Protocol messed up. Hopefully we still can log. */
 	printlog(loggersock, STDERR, id,
 		 "PSIForwarder: PANIC!! Protocol messed up!\n");
     }
 
-    if(loggersock>0)
-	close(loggersock);
+    if (loggersock>0) close(loggersock);
 }
 
 /**
@@ -96,7 +95,7 @@ int loggerconnect(unsigned int node, int port)
     struct sockaddr_in sa;	/* socket address */
     FLBufferMsg_t msg;
 
-    if((loggersock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP))<0){
+    if ((loggersock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
 	return(-1);
     }
 
@@ -105,12 +104,12 @@ int loggerconnect(unsigned int node, int port)
     sa.sin_addr.s_addr = node;
     sa.sin_port = htons(port);
 
-    if((connect(loggersock,(struct sockaddr *)&sa, sizeof(sa)))<0){
+    if ((connect(loggersock,(struct sockaddr *)&sa, sizeof(sa))) < 0) {
 	return(-1);
     }
 
     readlog(loggersock, &msg);
-    if(msg.header.type != INITIALIZE){
+    if (msg.header.type != INITIALIZE) {
 	/* Protocol messed up. Hopefully we can log anyhow. */
 	printlog(loggersock, STDERR, id,
 		 "PSIForwarder: PANIC!! Protocol messed up!\n");
@@ -141,7 +140,7 @@ void CheckFileTable(fd_set* openfds)
     char *errtxt, buf[80];
 
     for(fd=0;fd<FD_SETSIZE;){
-	if(FD_ISSET(fd,openfds)){
+	if (FD_ISSET(fd,openfds)) {
 	    memset(&rfds, 0, sizeof(rfds));
 	    FD_SET(fd,&rfds);
 
@@ -193,16 +192,16 @@ void CheckFileTable(fd_set* openfds)
     }
 }
 
-int writeall(int fd,void *buf,int count)
+int writeall(int fd, void *buf, int count)
 {
     int len;
-    int c=count;
+    int c = count;
 
     while (c>0){
-	len = write(fd,buf,c);
+	len = write(fd, buf, c);
 	if (len<0) return -1;
-	c-=len;
-	((char*)buf)+=len;
+	c -= len;
+	((char*)buf) += len;
     }
     return count;
 }
@@ -214,15 +213,15 @@ void read_from_logger(int logfd, int stdinport)
     int ret;
 
     ret = readlog(logfd, &msg);
-    if ( ret > 0){
+    if (ret > 0) {
 	if (msg.header.type == STDIN){
-	    if (verbose){
+	    if (verbose) {
 		snprintf(obuf, sizeof(obuf),
 			 "PSIforwarder: receive %d byte for STDIN\n",
 			    msg.header.len - sizeof(msg.header) );
 		printlog(loggersock, STDERR, id, obuf);
 	    }
-	    writeall(stdinport, msg.buf,msg.header.len - sizeof(msg.header)); 
+	    writeall(stdinport, msg.buf, msg.header.len - sizeof(msg.header)); 
 	}else{
 	    /* unexpected message. Ignore. */
 	}
@@ -252,7 +251,7 @@ void loop(int stdoutport, int stderrport)
     int n;                       /* number of bytes received */
     FLMsg_msg_t type;
 
-    if(verbose){
+    if (verbose) {
 	snprintf(obuf, sizeof(obuf), "PSIforwarder: stdout=%d stderr=%d\n",
 		 stdoutport, stderrport);
 	printlog(loggersock, STDERR, id, obuf);
@@ -269,7 +268,7 @@ void loop(int stdoutport, int stderrport)
     while (noclients > 0) {
 	memcpy(&afds, &myfds, sizeof(afds));
 	atv = mytv;
-	if(select(FD_SETSIZE, &afds, NULL, NULL, &atv) < 0){
+	if (select(FD_SETSIZE, &afds, NULL, NULL, &atv) < 0) {
 	    snprintf(obuf, sizeof(obuf), "PSIforwarder: error on select(%d):"
 		     " %s\n", errno, strerror(errno));
 	    printlog(loggersock, STDERR, id, obuf);
@@ -279,20 +278,20 @@ void loop(int stdoutport, int stderrport)
 	/*
 	 * check the rest sockets for any outputs
 	 */
-	for(sock=1; sock<FD_SETSIZE; sock++){
-	    if(FD_ISSET(sock, &afds)){ /* socket ready */
-		if(sock==stdoutport){
-		    type=STDOUT;
-		}else if(sock==stderrport){
-		    type=STDERR;
-		}else if(sock==loggersock){
+	for (sock=1; sock<FD_SETSIZE; sock++) {
+	    if (FD_ISSET(sock, &afds)) { /* socket ready */
+		if (sock==loggersock) {
 		    /* Read new input */
-		    if (read_from_logger(loggersock,stdoutport) <= 0){
+		    if (read_from_logger(loggersock,stdoutport) <= 0) {
 			/* connection to logger broken */
 			exit(1);
 		    }
 		    continue;
-		}else{
+		} else if (sock==stdoutport) {
+		    type=STDOUT;
+		} else if (sock==stderrport) {
+		    type=STDERR;
+		} else {
 		    snprintf(obuf, sizeof(obuf),
 			     "PSIforwarder: PANIC: sock %d, which is neither"
 			     " stdout (%d) nor stderr (%d) is active!!\n",
@@ -304,15 +303,15 @@ void loop(int stdoutport, int stderrport)
 		}
 
 		n = read(sock, buf, sizeof(buf));
-		if(verbose){
+		if (verbose) {
 		    snprintf(obuf, sizeof(obuf),
 			     "PSIforwarder: got %d bytes on sock %d\n",
 			     n, sock);
 		    printlog(loggersock, STDERR, id, obuf);
 		}
-		if(n==0){
+		if (n==0) {
 		    /* socket closed */
-		    if(verbose){
+		    if (verbose) {
 			snprintf(obuf, sizeof(obuf),
 				 "PSIforwarder: closing %d\n", sock);
 			printlog(loggersock, STDERR, id, obuf);
@@ -320,20 +319,18 @@ void loop(int stdoutport, int stderrport)
 		    close(sock);
 		    FD_CLR(sock,&myfds);
 		    noclients--;
-		    if(verbose){
+		    if (verbose) {
 			snprintf(obuf, sizeof(obuf),
 				 "PSIforwarder: clients left: %d\n",
 				 noclients);
 			printlog(loggersock, STDERR, id, obuf);
 		    }
-		}else if(n<0){
+		} else if (n<0) {
 		    /* ignore the error */
-		    /*
-		      snprintf(obuf, sizeof(obuf),
-		      "PSIforwarder: read():%s\n", strerror(errno));
-		      printlog(loggersock, STDERR, id, obuf);
-		    */
-		}else{
+		    snprintf(obuf, sizeof(obuf),
+			     "PSIforwarder: read():%s\n", strerror(errno));
+		    printlog(loggersock, STDERR, id, obuf);
+		} else {
 		    /* forward it to logger */
 		    writelog(loggersock, type, id, buf, n);
 		}
@@ -373,7 +370,7 @@ int main( int argc, char**argv)
     /* become process group leader */
     setpgid(0,0);
 
-    if(argc<6){
+    if (argc<6) {
 	exit(1);
     }
     sscanf(argv[1], "%u", &logger_node);
@@ -382,7 +379,7 @@ int main( int argc, char**argv)
     sscanf(argv[4], "%d", &stdoutport);
     sscanf(argv[5], "%d", &stderrport);
 
-    if((ret=loggerconnect(logger_node, logger_port)) < 0){
+    if ((ret=loggerconnect(logger_node, logger_port)) < 0) {
 	exit(1);
     }
 
@@ -392,7 +389,7 @@ int main( int argc, char**argv)
     /* call the loop which does all the work */
     loop(stdoutport, stderrport);
 
-    if(verbose){
+    if (verbose) {
 	printlog(loggersock, STDERR, id, "PSIforwarder: Closing log\n");
     }
     closelog();
