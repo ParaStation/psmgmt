04b25bf5f8412f91c66d4a26dc87552cc93d04d6
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-04-22 22:55:03 +0000

    *** empty log message ***
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@954 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/compat_tru64.h
+++ include/compat_tru64.h
@@ -6,6 +6,14 @@
 
 #define __attribute__( name )
 
+#define _DEC_XPG
+/*typedef unsigned long   socklen_t;*/
+
+/* #define _BSD */
+/* Only defined if _BSD is defined */
+extern int      setenv   (const char *, const char *, int);
+extern void     unsetenv (const char *);
+
 #define NO_MACRODOTDOT
 
 
