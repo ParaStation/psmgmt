943d6f631516f33391af4bd1fd53a4f82fa8482a
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-07-11 10:40:45 +0000

    New message-type TASKLIST (former TASKINFO). New message-type TASKINFO (don't mix up with old type). New member in NodelistEntry_t: numCPU. Distinct between normal and total Jobs in NodelistEntry_t. New type for message DDTaskinfoMsg_t. New member in DDConnectMsg_t: numCPU. Some parts marked as obsolete. Some comments modified.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@1185 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/psprotocol.h
+++ include/psprotocol.h
@@ -5,7 +5,7 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: psprotocol.h,v 1.1 2002/07/03 20:12:15 eicker Exp $
+ * $Id: psprotocol.h,v 1.2 2002/07/11 10:40:45 eicker Exp $
  *
  */
 /**
@@ -13,7 +13,7 @@
  * psp: The ParaStation Protocol
  *      Used for daemon-daemon and client-daemon communication.
  *
- * $Id: psprotocol.h,v 1.1 2002/07/03 20:12:15 eicker Exp $
+ * $Id: psprotocol.h,v 1.2 2002/07/11 10:40:45 eicker Exp $
  *
  * @author
  * Norbert Eicker <eicker@par-tec.com>
@@ -29,7 +29,7 @@ extern "C" {
 #endif
 #endif
 
-#define PSprotocolversion  311
+#define PSprotocolversion  312
 
 /** @todo Documentation */
 
@@ -48,10 +48,12 @@ extern "C" {
 #define PSP_DD_GETOPTION           0x000a
 #define PSP_DD_CONTACTNODE         0x000b
 
-#define PSP_CD_TASKINFO            0x0010
-#define PSP_CD_TASKINFOEND         0x0011
-#define PSP_CD_TASKLISTREQUEST     0x0012
+#define PSP_CD_TASKLISTREQUEST     0x0010 /* Obsolete */
+#define PSP_CD_TASKLIST            0x0011 /* Obsolete */
+#define PSP_CD_TASKLISTEND         0x0012 /* Obsolete */
 #define PSP_CD_TASKINFOREQUEST     0x0013
+#define PSP_CD_TASKINFO            0x0014
+#define PSP_CD_TASKINFOEND         0x0015
 
 #define PSP_CD_HOSTREQUEST         0x0018
 #define PSP_CD_HOSTRESPONSE        0x0019
@@ -71,10 +73,11 @@ extern "C" {
 #define PSP_CD_MCASTSTATUSREQUEST  0x0026
 #define PSP_CD_MCASTSTATUSRESPONSE 0x0027
 
-#define PSP_DD_SPAWNREQUEST        0x0030
-#define PSP_DD_SPAWNSUCCESS        0x0031
-#define PSP_DD_SPAWNFAILED         0x0032
-#define PSP_DD_SPAWNFINISH         0x0033
+#define PSP_DD_SPAWNREQUEST        0x0030  /* Request to spawn a process */
+#define PSP_DD_SPAWNSUCCESS        0x0031  /* Reply if spawn was successful */
+#define PSP_DD_SPAWNFAILED         0x0032  /* Reply if spawn failed */
+#define PSP_DD_SPAWNFINISH         0x0033  /* Reply after successfil end of
+					      spawned process */
 
 #define PSP_DD_TASKKILL            0x0040
 #define PSP_DD_NOTIFYDEAD          0x0041
@@ -159,89 +162,102 @@ typedef struct {
     long sender;            /* sender of the message */ 
     long dest;              /* final destination of the message */
     long len;               /* total length of the message */
-}DDMsg_t;
+} DDMsg_t;
 
 /* Load Message */
-typedef struct{
+typedef struct {
     DDMsg_t header;         /* header of the message */
     double load[3];         /* three load values */
-}DDLoadMsg_t;
+} DDLoadMsg_t;
 
 /* Error Message */
-typedef struct{
+typedef struct {
     DDMsg_t header;         /* header of the message */
     int err;                /* error number */
     long request;           /* request which caused the error */
-}DDErrorMsg_t;
+} DDErrorMsg_t;
 
 /* Reset Message */
-typedef struct{
+typedef struct {
     DDMsg_t header;         /* header of the message */
     int first;              /* first node to be reset */
     int last;               /* last node to be reset */
     long action;            /* request which caused the error */
-}DDResetMsg_t;
+} DDResetMsg_t;
 
 /* Contact Node Message */
-typedef struct{
+typedef struct {
     DDMsg_t header;         /* header of the message */
     long partner;           /* node which should be contacted by header.dest */
-}DDContactMsg_t;
+} DDContactMsg_t;
 
 /* Connect Node Message */
-typedef struct{
+typedef struct {
     DDMsg_t header;         /* header of the message */
     unsigned int hwStatus;  /* Flag to show which hw on nodes is active */
-}DDConnectMsg_t;
+    short numCPU;           /* Number of CPUs in that node */
+} DDConnectMsg_t;
 
 /* untyped Buffer Message */
-typedef struct{
+typedef struct {
     DDMsg_t header;         /* header of the message */
     char buf[8000];         /* buffer for Message */
-}DDBufferMsg_t;
+} DDBufferMsg_t;
 
 /* Init Message */
-typedef struct{
+typedef struct {
     DDMsg_t header;         /* header of the message */
     long reason;            /* reason for unaccepted connect */
     long group;             /* process group of the task */
     long version;           /* version of the PS library */
     int nrofnodes;          /* # of nodes */
     int myid;               /* PS id of this node */
-    unsigned int loggernode;/* */
-    short loggerport;       /* */
+    unsigned int loggernode;/* @todo Obsolete as soon as new forwarder works */
+    short loggerport;       /* @todo Obsolete as soon as new forwarder works */
     int rank;               /* rank of client passed by spawn */
     int uid;                /* user id */
     int pid;                /* process id */
     char instdir[80];       /** Installation directory of ParaStation stuff */
     char psidvers[80];      /** CVS version-string of the ParaStation daemon */
-}DDInitMsg_t;
+} DDInitMsg_t;
 
 #define DDOptionMsgMax 16
 
 /* Options Message */
-typedef struct{
+typedef struct {
     DDMsg_t header;         /* header of the message */
     char count;             /* no of options in opt[] */
-    struct{
+    struct {
 	long option;        /* option to be set/requested */
 	long value;         /* value of option to be set */
     }opt[DDOptionMsgMax];
-}DDOptionMsg_t;
+} DDOptionMsg_t;
 
 /* Signal Message */
-typedef struct{
+typedef struct {
     DDMsg_t header;         /* header of the message */
     int signal;             /* signal to be sent */
     int senderuid;          /* uid of the sender task */
 } DDSignalMsg_t;
 
+/* Taskinfo Message */
+typedef struct {
+    DDMsg_t header;         /* header of the message */
+    long tid;               /* ID of the task */
+    long ptid;              /* ID of the parent-task */
+    int uid;                /* user id of the task */
+    long group;             /* process group of the task */
+} DDTaskinfoMsg_t;
+
 /* Array of this struct is returned to NODELIST_REQUEST */
-typedef struct{
-    short up;               /* Flag if nodes is up */
-    unsigned int hwType;    /* HW available on this node */
-    short numJobs;          /* number of jobs (without logger & admin) */
-    float load[3];          /* load on this node */
+typedef struct {
+    short up;               /**< Flag if nodes is up */
+    short numCPU;           /**< Number of CPUs in this node */
+    unsigned int hwType;    /**< HW available on this node */
+    float load[3];          /**< load on this node */
+    short totalJobs;        /**< number of jobs */
+    short normalJobs;       /**< number of "normal" jobs (i.e. without
+			         logger & admin) */
 } NodelistEntry_t;
 
 #ifdef __cplusplus
