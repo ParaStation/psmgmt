da943cb76605cba452a1ac84d92c9f12f3ea9df7
hauke <hauke@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-07-18 19:02:23 +0000

    Now usefull to test MCPkeys only.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@1255 34291a86-62e8-0310-8159-f060f8a2ee4e

--- license/lictest.c
+++ license/lictest.c
@@ -1,92 +1,61 @@
 
-#define UINT32 unsigned int
-#define UINT8  unsigned char
 
 #include <string.h>
 #include <unistd.h>
 #include "stdio.h"
-#include "license.h"
-#include "../include/dump.c"
+#include <netinet/in.h>
+#include "ps_types.h"
+#include "license_priv.h"
 
+void check_mcpkey(char *Key)
+{
+    static pslic_bin_t LicKey;
+    pslic_binpub_t LicPub;
 
+    char *from;
+    char *to;
+    unsigned int i;
+    
+    if (!Key) return;
+    
+    pslic_ascii2bin((unsigned char *) Key,
+		    pslic_blen2alen(sizeof(LicPub))
+		    ,(unsigned char *)&LicPub);
 
+    from=(char*)&(LicPub);
+    to=(char*)(&LicKey);
 
+    for (i=0; i < sizeof(pslic_bin_t); i++) *to++=*from++;
+    
+    pslic_decode(&LicKey,sizeof(LicKey),LIC_KEYMAGIC);
 
+    LicKey.Nodes     = htonl(LicKey.Nodes);
+    LicKey.ValidFrom = htonl(LicKey.ValidFrom);
+    LicKey.ValidTo   = htonl(LicKey.ValidTo);
+    LicKey.Magic     = htonl(LicKey.Magic);
 
-
+    fprintf(stdout, "MCPKey: LicMagic : %s\n",
+	    LicKey.Magic == LIC_KEYMAGIC ? "OK" : "FALSE");
+    fprintf(stdout, "MCPKey: ValidFrom: %d\n", LicKey.ValidFrom);
+    fprintf(stdout, "MCPKey: ValidTo  : %d\n", LicKey.ValidTo);
+    fprintf(stdout, "MCPKey: Nodes    : %d\n", LicKey.Nodes);
+}
 
 int main(int argc, char **argv)
 {
-    char buf[400];
-    char buf2[400];
-    char orig[400];
-    int len;
-    int i;
-
-//      char enc[256];
-    
-//      int i;
-//      for (i=0;i<256;i++){
-//    	enc[ i ]=0;
-//      }
-//      for (i=0;i<64;i++){
-//    	enc[ LICBITPACK_B2A(i)]=i;
-//      }
-//      for (i=0;i<128;i++){
-//    	printf("\\x%02x",enc[i]);
-//      }
-//      printf("\n");
-//      for (i=0;i<128;i++){
-//    	printf("%c",isprint(i)?i:'.');
-//      }
-//      printf("\n");
-//      exit(0);
-
-
-//      for (i=0;i<64;i++){
-//  	printf("%4d %4d - %4d\n",i,LICBITPACK_A2B(LICBITPACK_B2A(i)),LICBITPACK_B2A(i));
-//      }
-    
-    
-    len=read(0,buf,atoi(argv[1]))-1;
-//    len = 40;
-//    strncpy(buf,"sadfgsdfgosgowjerogjweoijgwerg",len);
-    memcpy(orig,buf,100);
-    fprintf(stderr,"len %d\n",len);
-//    write(1,buf,len);
-//    dump(buf,0,len,0,16,"orig");
-    pslic_encode( buf,len,0x51451233);
-
-    pslic_bin2ascii(buf,len,buf2);
-
-    printf("\n[");fflush(stdout);
-    write(1,buf2,pslic_blen2alen(len));
-    printf("]\n");
-
-    memset(buf,0,100);
-
-    pslic_ascii2bin(buf2,pslic_blen2alen(len),buf);
-    
-    pslic_decode( buf,len,0x51451233);
-//    dump(buf,0,len,0,16,"dec");
-    
-//    write(1,buf,len);
-    printf("\n");
-    if (memcmp(buf,orig,len)){
-	printf("Falsch\n");
-	dump(buf,0,len,0,16,"buf");
-	dump(orig,0,len,0,16,"orig");
-    }else
-  	printf("OK\n");
-    
+    if (argc < 2) {
+	fprintf(stderr, "Expect MCPKey as parameter\n");
+    } else {
+	check_mcpkey(argv[1]);
+    }
 	   
     return 0;
 }
 
 
 /*
- * /remove me/Local Variables:
- *  compile-command: "gcc x.c -o x -save-temps &&x"
+ * Local Variables:
+ *  compile-command: "gcc lictest.c -Wall -W -Wno-unused -g -O2 -I../include -o lictest"
  * End:
  *
  */
