fa4068ed30a401ed3aa127d2b6a84155093c50bf
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-07-26 15:34:41 +0000

    LOGGERspawnforwarder() moved into psidspawn.c and updated there. Major cleanup.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@1313 34291a86-62e8-0310-8159-f060f8a2ee4e

--- lib/psi/logger.c
+++ lib/psi/logger.c
@@ -7,39 +7,26 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: logger.c,v 1.19 2002/07/03 20:33:45 eicker Exp $
+ * $Id: logger.c,v 1.20 2002/07/26 15:34:41 eicker Exp $
  *
  */
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
-static char vcid[] __attribute__(( unused )) = "$Id: logger.c,v 1.19 2002/07/03 20:33:45 eicker Exp $";
+static char vcid[] __attribute__(( unused )) = "$Id: logger.c,v 1.20 2002/07/26 15:34:41 eicker Exp $";
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/time.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <pty.h>
-#include <syslog.h>
 
 #include "pscommon.h"
 #include "psi.h"
-//#include "psitask.h"
-//#include "psilog.h"
 
 #include "logger.h"
-
-pid_t logger_pid=0;
-
 
 static int LOGGERexecv( const char *path, char *const argv[])
 {
@@ -47,170 +34,14 @@ static int LOGGERexecv( const char *path, char *const argv[])
     int cnt;
 
     /* Try 5 times with delay 400ms = 2 sec overall */
-    for (cnt=0;cnt<5;cnt++){
+    for (cnt=0; cnt<5; cnt++){
 	ret = execv(path,argv);
 	usleep(1000 * 400);
     }
     return ret;
 }
 
-void LOGGERspawnforwarder(unsigned int logger_node, int logger_port,
-			  int rank, int tty)
-{
-    int pid;
-    int stdoutfds[2], stderrfds[2];
-    char *errtxt;
-
-    /* 
-     * create two pipes for the forwarder to listen to. 
-     */
-    if (tty){
-#if 0
-	/* ToDo: Receive the terminal size of the orginal tty for winsize
-	 * and remove this codepart from the daemon. The forwarder should open
-	 * the pty. The user process should be a child of the forwarder
-	 * (like the sshd). I dont want openpty inside the psi-library
-	 * because the need of -lutil  *jh*
-	 */
-	if (openpty(&stdoutfds[0],&stdoutfds[1],NULL,NULL,NULL)) {
-	    errtxt = strerror(errno);
-	    syslog(LOG_ERR, "LOGGERspawnforwarder(openpty()): [%d] %s", errno,
-		   errtxt?errtxt:"UNKNOWN");
-	    perror("openpty()");
-	    /* fallback to socketpair */
-	    tty = 0;
-	}
-#else
-	tty=0;
-#endif
-    }
-    if (!tty){
-	if (socketpair(PF_UNIX,SOCK_STREAM,0,stdoutfds)) {
-	    errtxt = strerror(errno);
-	    syslog(LOG_ERR, "LOGGERspawnforwarder(pipe(stdout)): [%d] %s",
-		   errno, errtxt?errtxt:"UNKNOWN");
-	    perror("pipe(stdout)");
-	    exit(1);
-	}
-    }
-    if (socketpair(PF_UNIX,SOCK_STREAM,0,stderrfds)) {
-        errtxt = strerror(errno);
-        syslog(LOG_ERR, "LOGGERspawnforwarder(pipe(stderr)): [%d] %s", errno,
-               errtxt?errtxt:"UNKNOWN");
-        perror("pipe(stderr)");
-	exit(1);
-    }
-
-    /* 
-     * fork to forwarder
-     */
-    if((pid = fork())==0){
-	/*
-	 *   F O R W A R D E R 
-	 */
-	int i;
-	char* argv[7];
-	/*
-	 * close all open filedesciptor except my pipes for reading
-	 */
-	for(i=1; i<FD_SETSIZE; i++)
-	    if(i != stdoutfds[0] && i !=stderrfds[0])
-		close(i);
-
-	argv[0] = (char*)malloc(strlen(PSC_lookupInstalldir()) + 20);
-	sprintf(argv[0], "%s/bin/psiforwarder", PSC_lookupInstalldir());
-	argv[1] = (char*)malloc(10);
-	sprintf(argv[1], "%u", logger_node);
-	argv[2] = (char*)malloc(10);
-	sprintf(argv[2], "%d", logger_port);
-	argv[3] = (char*)malloc(10);
-	sprintf(argv[3], "%d", rank);
-	argv[4] = (char*)malloc(10);
-	sprintf(argv[4], "%d", stdoutfds[0]);
-	argv[5] = (char*)malloc(10);
-	sprintf(argv[5], "%d", stderrfds[0]);
-	argv[6] = NULL;
-
-	LOGGERexecv(argv[0], argv);
-
-	/*
-	 * usually never reached, but if execv fails stop the whole porgram
-	 */
-	close(stdoutfds[0]);
-	close(stderrfds[0]);
-
-        errtxt = strerror(errno);
-        syslog(LOG_ERR, "LOGGERspawnforwarder(execv): %s [%d] %s", argv[0],
-	       errno, errtxt?errtxt:"UNKNOWN");
-        perror("fork()");
-	exit(1);
-    }
-    /*
-     * P A R E N T 
-     */
-
-    /*
-     * close the reading pipes.
-     */
-    close(stdoutfds[0]);
-    close(stderrfds[0]);
-
-    /*
-     * check if fork() was successful
-     */
-    if (pid ==-1){
-        char *errtxt;
-
-	close(stdoutfds[1]);
-	close(stderrfds[1]);
-
-	errtxt = strerror(errno);
-        syslog(LOG_ERR, "LOGGERspawnforwarder(fork): [%d] %s", errno,
-               errtxt?errtxt:"UNKNOWN");
-        perror("execv()");
-	exit(1);
-    }
-
-    /*
-     * redirect input and output
-     */
-    dup2(stdoutfds[1], STDIN_FILENO);
-    dup2(stdoutfds[1], STDOUT_FILENO);
-    close(stdoutfds[1]);
-    dup2(stderrfds[1], STDERR_FILENO);
-    close(stderrfds[1]);
-}
-
 static int listenport = -1;
-
-int LOGGERspawnlogger(void)
-{
-    unsigned short portno;
-
-    /*
-     * create a port for the logger to listen to.
-     */
-    portno = LOGGERopenPort();
-
-    /* 
-     * fork to logger
-     */
-    if((logger_pid=fork())==0){
-	/*
-	 *   L O G G E R 
-	 */
-	LOGGERexecLogger();
-    }
-    /*
-     * P A R E N T 
-     */
-    /*
-     * close the socket which is used by logger to listen for new connections.
-     */
-    close(listenport);
-
-    return portno;
-}
 
 unsigned short LOGGERopenPort(void)
 {
@@ -251,7 +82,7 @@ void LOGGERexecLogger(void)
 {
     int i;
     char* argv[4];
-    char *errtxt;
+    char *errstr;
     /*
      * close all open filedesciptor except my std* and the LOGGERSOCK
      */
@@ -280,9 +111,10 @@ void LOGGERexecLogger(void)
     close(PSI_msock);
     close(listenport);
 
-    errtxt = strerror(errno);
-    syslog(LOG_ERR, "LOGGERexecLogger(execv): %s [%d] %s", argv[0],
-	   errno, errtxt?errtxt:"UNKNOWN");
+    errstr = strerror(errno);
+
+    fprintf(stderr, "LOGGERexecLogger(execv): %s [%d] %s", argv[0],
+	    errno, errstr ? errstr : "UNKNOWN");
     perror("execv()");
 
     exit(1);
