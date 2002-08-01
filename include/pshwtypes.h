8cc94619c6c3203741de53da70efd9114ed85e48
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2002-08-01 16:42:40 +0000

    Docu added.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@1339 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/pshwtypes.h
+++ include/pshwtypes.h
@@ -5,14 +5,14 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: pshwtypes.h,v 1.2 2002/07/24 06:24:41 eicker Exp $
+ * $Id: pshwtypes.h,v 1.3 2002/08/01 16:42:40 eicker Exp $
  *
  */
 /**
  * @file
  * ParaStation hardware types.
  *
- * $Id: pshwtypes.h,v 1.2 2002/07/24 06:24:41 eicker Exp $
+ * $Id: pshwtypes.h,v 1.3 2002/08/01 16:42:40 eicker Exp $
  *
  * @author
  * Norbert Eicker <eicker@par-tec.com>
@@ -28,16 +28,36 @@ extern "C" {
 #endif
 #endif
 
-/*----------------------------------------------------------------------*/
-/* types of communication-hardware supported by ParaStation             */
-/*----------------------------------------------------------------------*/
-#define PSHW_ETHERNET            0x0001
-#define PSHW_MYRINET             0x0002
-#define PSHW_GIGAETHERNET        0x0004
+/**
+ * @brief Supported hardware types.
+ *
+ * ParaStation's supported communication hardware types.
+ */
+#define PSHW_ETHERNET            0x0001 /**< TCP/IP over Ethernet.
+					   Used within ParaStation FE. */
+#define PSHW_MYRINET             0x0002 /**< RDP (Reliable Data Protocol) over
+					   Myrinet.
+					   Used within ParaStation 3 and 4. */
+#define PSHW_GIGAETHERNET        0x0004 /**< RDP (Reliable Data Protocol) over
+					   (GigaBit-)Ethernet.
+					   Used within ParaStation 4. */
 
 /**
- * @todo Docu
- */
+ * @brief Get string describing the hardware-type.
+ *
+ * Get a string describing the hardware-type @a hwType. The returned
+ * pointer leads to a static character array that contains the
+ * describtion. Sequent calls of @ref PSHW_printType() will change the
+ * content of this array. Therefor the result is not what you expect
+ * if more then one call of this function is done within the
+ * argument-list of printf(3) and friends.
+ *
+ * @param hwType The hardware-type to describe. This is supposed to be
+ * a bitwise-or of @ref PSWH_ETHERNET, @ref PSHW_MYRINET and @ref
+ * PSHW_GIGAETHERNET or 0.
+ *
+ * @return A pointer to a static character array containing hwType's
+ * description. Do not try to free(2) this array.  */
 char *PSHW_printType(int hwType);
 
 
