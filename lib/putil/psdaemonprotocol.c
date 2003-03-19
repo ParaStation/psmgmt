f3400d8a8ab2519c0fb9958ab19cd800511287f2
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2003-03-19 16:58:53 +0000

    Put the daemon-daemon part of the ParaStation protocol into psdaemonprotocol.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@1990 34291a86-62e8-0310-8159-f060f8a2ee4e

---
+++ lib/putil/psdaemonprotocol.c
@@ -0,0 +1,57 @@
+/*
+ *               ParaStation3
+ * psdaemonprotocol.c
+ *
+ * ParaStation daemon-daemon high-level protocol.
+ *
+ * Copyright (C) ParTec AG Karlsruhe
+ * All rights reserved.
+ *
+ * $Id: psdaemonprotocol.c,v 1.1 2003/03/19 16:58:53 eicker Exp $
+ *
+ */
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+static char vcid[] __attribute__(( unused )) = "$Id: psdaemonprotocol.c,v 1.1 2003/03/19 16:58:53 eicker Exp $";
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#include <stdio.h>
+
+#include "psdaemonprotocol.h"
+
+/*
+ * string identification of message IDs.
+ * Nicer output for errrors and debugging.
+ */
+static struct {
+    long id;
+    char *message;
+} ctrlmessages[] = {
+    { PSP_DD_DAEMONCONNECT    , "PSP_DD_DAEMONCONNECT"    }, 
+    { PSP_DD_DAEMONESTABLISHED, "PSP_DD_DAEMONESTABLISHED"}, 
+    { PSP_DD_DAEMONREFUSED    , "PSP_DD_DAEMONREFUSED"    }, 
+
+    { PSP_DD_CHILDDEAD        , "PSP_DD_CHILDDEAD"        },
+
+    {0,NULL}
+};
+
+char *PSDaemonP_printMsg(int msgtype)
+{
+    static char txt[30];
+    int i = 0;
+
+    if (msgtype < 0x0100) {
+	return PSP_printMsg(msgtype);
+    }
+
+    while (ctrlmessages[i].id && ctrlmessages[i].id != msgtype) {
+	i++;
+    }
+
+    if (ctrlmessages[i].id) {
+	return ctrlmessages[i].message;
+    } else {
+	snprintf(txt, sizeof(txt), "msgtype 0x%x UNKNOWN", msgtype);
+	return txt;
+    }
+}
