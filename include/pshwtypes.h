8568daf9ed421288e44a076699eebb1adc6c1465
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2003-04-11 10:27:04 +0000

    Remove obsolete prototype PSHW_printType() (replaced by INFO_printHWType()).
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@2121 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/pshwtypes.h
+++ include/pshwtypes.h
@@ -5,14 +5,14 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: pshwtypes.h,v 1.6 2003/04/03 14:58:47 eicker Exp $
+ * $Id: pshwtypes.h,v 1.7 2003/04/11 10:27:04 eicker Exp $
  *
  */
 /**
  * @file
  * ParaStation hardware types.
  *
- * $Id: pshwtypes.h,v 1.6 2003/04/03 14:58:47 eicker Exp $
+ * $Id: pshwtypes.h,v 1.7 2003/04/11 10:27:04 eicker Exp $
  *
  * @author
  * Norbert Eicker <eicker@par-tec.com>
@@ -27,25 +27,6 @@ extern "C" {
 } /* <- just for emacs indentation */
 #endif
 #endif
-
-/**
- * @brief Get string describing the hardware-type.
- *
- * Get a string describing the hardware-type @a hwType. The returned
- * pointer leads to a static character array that contains the
- * describtion. Sequent calls of @ref PSHW_printType() will change the
- * content of this array. Therefor the result is not what you expect
- * if more then one call of this function is done within the
- * argument-list of printf(3) and friends.
- *
- * @param hwType The hardware-type to describe. This is supposed to be
- * a bitwise-or of @ref PSHW_ETHERNET, @ref PSHW_MYRINET and @ref
- * PSHW_GIGAETHERNET or 0.
- *
- * @return A pointer to a static character array containing hwType's
- * description. Do not try to free(2) this array.  */
-char *PSHW_printType(int hwType);
-
 
 #define PSHW_NAME_ETHERNET	"ethernet"
 #define PSHW_NAME_MYRINET	"myrinet"
