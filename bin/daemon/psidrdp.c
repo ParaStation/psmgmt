fa03b7b826ca65b3c8b07f72763f222e9dfbf5a2
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2003-07-04 09:49:51 +0000

    RDP wrapper and helper functions. These are needed for RDP flow control.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@2306 34291a86-62e8-0310-8159-f060f8a2ee4e

---
+++ bin/daemon/psidrdp.c
@@ -0,0 +1,152 @@
+/*
+ *               ParaStation
+ * psidrdp.c
+ *
+ * Copyright (C) ParTec AG Karlsruhe
+ * All rights reserved.
+ *
+ * $Id: psidrdp.c,v 1.1 2003/07/04 09:49:51 eicker Exp $
+ *
+ */
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+static char vcid[] __attribute__(( unused )) = "$Id: psidrdp.c,v 1.1 2003/07/04 09:49:51 eicker Exp $";
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "rdp.h"
+#include "psdaemonprotocol.h"
+#include "pscommon.h"
+
+#include "psidutil.h"
+#include "psidmsgbuf.h"
+#include "psidcomm.h"
+
+#include "psidrdp.h"
+
+int RDPSocket = -1;
+
+static msgbuf_t **node_bufs;
+
+static char errtxt[256]; /**< General string to create error messages */
+
+void initRDPMsgs(void)
+{
+    int i;
+
+    node_bufs = malloc(sizeof(msgbuf_t) * PSC_getNrOfNodes());
+    if (!node_bufs) {
+	snprintf(errtxt, sizeof(errtxt), "%s: no memory", __func__);
+	PSID_errlog(errtxt, 0);
+	exit(0);
+    }
+
+    for (i=0; i<PSC_getNrOfNodes(); i++) {
+	node_bufs[i] = NULL;
+    }
+}
+
+void clearRDPMsgs(int node)
+{
+    while (node_bufs[node]) {
+	msgbuf_t *mp = node_bufs[node];
+
+	node_bufs[node] = node_bufs[node]->next;
+	freeMsg(mp);
+    }
+
+}
+
+static int storeMsgRDP(int node, DDMsg_t *msg)
+{
+    msgbuf_t *msgbuf = node_bufs[node];
+
+    if (msgbuf) {
+	/* Search for end of list */
+	while (msgbuf->next) msgbuf = msgbuf->next;
+	msgbuf->next = getMsg();
+	msgbuf = msgbuf->next;
+    } else {
+	msgbuf = node_bufs[node] = getMsg();
+    }
+
+    if (!msgbuf) {
+	errno = ENOMEM;
+	return -1;
+    }
+
+    msgbuf->msg = malloc(msg->len);
+    if (!msgbuf->msg) {
+	errno = ENOMEM;
+	return -1;
+    }
+    memcpy(msgbuf->msg, msg, msg->len);
+
+    msgbuf->offset = 0;
+
+    return 0;
+}
+
+int flushRDPMsgs(int node)
+{
+    if (node<0 || node >= PSC_getNrOfNodes()) {
+	errno = EINVAL;
+	return -1;
+    }
+
+    while (node_bufs[node]) {
+	msgbuf_t *oldmsg = node_bufs[node];
+	DDMsg_t *msg = oldmsg->msg;
+	int sent = Rsendto(PSC_getID(msg->dest), msg, msg->len);
+
+	if (sent<0) return sent;
+
+	node_bufs[node] = oldmsg->next;
+	if (PSC_getPID(oldmsg->msg->sender)) {
+	    DDMsg_t contmsg = { .type = PSP_DD_SENDCONT,
+				.len = sizeof(DDMsg_t),
+				.sender = oldmsg->msg->dest,
+				.dest = oldmsg->msg->sender };
+
+	    if (PSC_getID(contmsg.dest) == PSC_getMyID()) {
+		msg_SENDCONT(&contmsg);
+	    } else {
+		sendMsg(&contmsg);
+	    }
+	}
+	freeMsg(oldmsg);
+    }
+    return 0;
+}
+
+int sendRDP(DDMsg_t *msg)
+{
+    int node = PSC_getID(msg->dest);
+    int ret = 0;
+
+    if (node<0 || node >= PSC_getNrOfNodes()) {
+	errno = EINVAL;
+	return -1;
+    }
+
+    if (node_bufs[node]) flushRDPMsgs(node);
+
+    if (!node_bufs[node]) {
+	ret = Rsendto(node, msg, msg->len);
+    }
+
+    if (node_bufs[node] || (ret==-1 && errno==EAGAIN)) {
+	if (!storeMsgRDP(node, msg)) errno = EWOULDBLOCK;
+    }
+
+    return ret;
+}
+
+int recvRDP(DDMsg_t *msg, size_t size)
+{
+    int fromnode = -1;
+
+    return Rrecvfrom(&fromnode, msg, size);
+}
---
+++ bin/daemon/psidrdp.h
@@ -0,0 +1,122 @@
+/*
+ *               ParaStation
+ * psidrdp.h
+ *
+ * Copyright (C) ParTec AG Karlsruhe
+ * All rights reserved.
+ *
+ * $Id: psidrdp.h,v 1.1 2003/07/04 09:49:51 eicker Exp $
+ *
+ */
+/**
+ * \file
+ * RDP wrapper and helper functions for the ParaStation daemon
+ *
+ * $Id: psidrdp.h,v 1.1 2003/07/04 09:49:51 eicker Exp $
+ *
+ * \author
+ * Norbert Eicker <eicker@par-tec.com>
+ *
+ */
+#ifndef __PSIDRDP_H
+#define __PSIDRDP_H
+
+#include <sys/types.h>
+
+#include "psprotocol.h"
+
+#ifdef __cplusplus
+extern "C" {
+#if 0
+} /* <- just for emacs indentation */
+#endif
+#endif
+
+/** The socket used for all RDP communication */
+extern int RDPSocket;
+
+/**
+ * @brief Initialize RDP message bufferes.
+ *
+ * Initialize some structures used in order to temporarily store RDP
+ * messages that could not yet be delivered to their final
+ * destination.
+ *
+ * @return No return value
+ */
+void initRDPMsgs(void);
+
+/**
+ * @brief Clear RDP message buffers.
+ *
+ * Clear all pending messages to node @a node.
+ *
+ * @param node The unique node ID to which the pending messages should
+ * not be send to.
+ *
+ * @return No return value
+ */
+void clearRDPMsgs(int node);
+
+/**
+ * @brief Flush messages to node.
+ *
+ * Try to send all messages to the node @a node that could not be
+ * delivered in prior calls to sendRDP() or flushRDPMsgs().
+ *
+ * @param node The unique node ID the messages to send are associated
+ * with.
+ *
+ * @return If all pending message were delivered, 0 is returned. Or
+ * -1, if a problem occured.
+ *
+ * @see sendRDP()
+ */
+int flushRDPMsgs(int node);
+
+/**
+ * @brief Send a message via RDP
+ *
+ * Send the message @a msg via RDP to the remote node defined within
+ * the message.
+ *
+ * @param msg Message to be sent. The format of the message has to
+ * follow DDMsg_t and further deduced message types.
+ *
+ * If the message could not be delivered, it will be stored internally
+ * and @a errno will be set to EWOULDBLOCK. Further calls to sendRDP()
+ * resulting in the same unique node ID or to flushRDPMsgs() using
+ * this unique node ID will try to deliver this packet.
+ *
+ * @return On success, the number of bytes sent is returned. If an error
+ * occured, -1 is returned and errno is set appropriately.
+ *
+ * @see Rsendto(), flushRDPMsgs()
+ */
+int sendRDP(DDMsg_t *msg);
+
+/**
+ * @brief Receive a message from RDP
+ *
+ * Receive a message from the RDP file descriptor @ref RDPSocket and
+ * store it to @a msg. At most @a size bytes are read from RDP and
+ * stored to @a msg.
+ *
+ *
+ * @param msg Buffer to store the message in.
+ *
+ * @param size The maximum length of the message, i.e. the size of @a msg.
+ *
+ *
+ * @return On success, the number of bytes received is returned, or -1 if
+ * an error occured. In the latter case errno will be set appropiately.
+ *
+ * @see Rrecvfrom()
+ */
+int recvRDP(DDMsg_t *msg, size_t size);
+
+#ifdef __cplusplus
+}/* extern "C" */
+#endif
+
+#endif /* __PSIDRDP_H */
