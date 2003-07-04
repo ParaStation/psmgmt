fc8185863dda812ca9463e219134e2851d1edf00
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2003-07-04 10:59:53 +0000

    New functions getClientFD(), flushClientMsgs() needed for flow control. Flow control implemented.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@2308 34291a86-62e8-0310-8159-f060f8a2ee4e

--- bin/daemon/psidclient.c
+++ bin/daemon/psidclient.c
@@ -5,14 +5,15 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: psidclient.c,v 1.2 2003/06/27 16:54:51 eicker Exp $
+ * $Id: psidclient.c,v 1.3 2003/07/04 10:59:53 eicker Exp $
  *
  */
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
-static char vcid[] __attribute__(( unused )) = "$Id: psidclient.c,v 1.2 2003/06/27 16:54:51 eicker Exp $";
+static char vcid[] __attribute__(( unused )) = "$Id: psidclient.c,v 1.3 2003/07/04 10:59:53 eicker Exp $";
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
@@ -26,13 +27,11 @@ static char vcid[] __attribute__(( unused )) = "$Id: psidclient.c,v 1.2 2003/06/
 
 #include "psidutil.h"
 #include "psidtask.h"
+#include "psidmsgbuf.h"
 #include "psidcomm.h"
 
 #include "psidclient.h"
 
-/*------------------------------
- * CLIENTS
- */
 /* possible values of clients.flags */
 #define INITIALCONTACT  0x00000001   /* No message yet (only accept()ed) */
 
@@ -40,10 +39,10 @@ static struct {
     long tid;       /**< Clients task ID */
     PStask_t *task; /**< Clients task structure */
     long flags;     /**< Special flags. Up to now only INITIALCONTACT */
+    msgbuf_t *msgs; /**< Chain of undelivered messages */
 } clients[FD_SETSIZE];
 
 static char errtxt[256]; /**< General string to create error messages */
-
 
 void initClients(void)
 {
@@ -53,6 +52,7 @@ void initClients(void)
 	clients[fd].tid = -1;
 	clients[fd].task = NULL;
 	clients[fd].flags = 0;
+	clients[fd].msgs = NULL;
     }
 }
 
@@ -62,6 +62,7 @@ void registerClient(int fd, long tid, PStask_t *task)
     clients[fd].tid = tid;
     clients[fd].task = task;
     clients[fd].flags |= INITIALCONTACT;
+    clients[fd].msgs = NULL;
 }
 
 long getClientTID(int fd)
@@ -74,6 +75,18 @@ PStask_t *getClientTask(int fd)
     return clients[fd].task;
 }
 
+int getClientFD(long tid)
+{
+    int fd;
+
+    for (fd=0; fd<FD_SETSIZE; fd++) {
+	/* find the FD for the dest */
+	if (clients[fd].tid==tid) break;
+    }
+
+    return fd;
+}
+
 void setEstablishedClient(int fd)
 {
     clients[fd].flags &= ~INITIALCONTACT;
@@ -84,10 +97,112 @@ int isEstablishedClient(int fd)
     return !(clients[fd].flags & INITIALCONTACT);
 }
 
-int sendClient(void *amsg)
+static int do_send(int fd, DDMsg_t *msg, int offset)
 {
-    DDMsg_t *msg = (DDMsg_t *)amsg;
-    int fd;
+    int n, i;
+
+    for (n=offset, i=1; (n<msg->len) && (i>0);) {
+	i = send(fd, &(((char*)msg)[n]), msg->len-n, MSG_DONTWAIT);
+	if (i<=0) {
+	    switch (errno) {
+	    case EINTR:
+		break;
+	    case EAGAIN:
+		return n;
+		break;
+	    default:
+	    {
+		char *errstr = strerror(errno);
+
+		snprintf(errtxt, sizeof(errtxt),
+			 "%s(): got error %d on socket %d: %s",
+			 __func__, errno, fd,
+			 errstr ? errstr : "UNKNOWN");
+		PSID_errlog(errtxt, (errno==EPIPE) ? 1 : 0);
+		deleteClient(fd);
+		return i;
+	    }
+	    }
+	} else
+	    n+=i;
+    }
+    return n;
+}
+
+static int storeMsgClient(int fd, DDMsg_t *msg, int offset)
+{
+    msgbuf_t *msgbuf = clients[fd].msgs;
+
+    if (msgbuf) {
+	/* Search for end of list */
+	while (msgbuf->next) msgbuf = msgbuf->next;
+	msgbuf->next = getMsg();
+	msgbuf = msgbuf->next;
+    } else {
+	msgbuf = clients[fd].msgs = getMsg();
+    }
+
+    if (!msgbuf) {
+	errno = ENOMEM;
+	return -1;
+    }
+
+    msgbuf->msg = malloc(msg->len);
+    if (!msgbuf->msg) {
+	errno = ENOMEM;
+	return -1;
+    }
+    memcpy(msgbuf->msg, msg, msg->len);
+
+    msgbuf->offset = offset;
+
+    return 0;
+}
+
+int flushClientMsgs(int fd)
+{
+    if (fd<0 || fd >= FD_SETSIZE) {
+	errno = EINVAL;
+	return -1;
+    }
+
+    while (clients[fd].msgs) {
+	msgbuf_t *oldmsg = clients[fd].msgs;
+	int sent = do_send(fd, oldmsg->msg, oldmsg->offset);
+
+	if (sent<0) return sent;
+	if (sent != oldmsg->msg->len) {
+	    oldmsg->offset = sent;
+	    break;
+	}
+
+	clients[fd].msgs = oldmsg->next;
+	if (PSC_getPID(oldmsg->msg->sender)) {
+	    DDMsg_t contmsg = { .type = PSP_DD_SENDCONT,
+				.len = sizeof(DDMsg_t),
+				.sender = oldmsg->msg->dest,
+				.dest = oldmsg->msg->sender };
+
+	    if (PSC_getID(contmsg.dest) == PSC_getMyID()) {
+		msg_SENDCONT(&contmsg);
+	    } else {
+		sendMsg(&contmsg);
+	    }
+	}
+	freeMsg(oldmsg);
+    }
+
+    if (clients[fd].msgs) {
+	errno = EWOULDBLOCK;
+	return -1;
+    }
+
+    return 0;
+}
+
+int sendClient(DDMsg_t *msg)
+{
+    int fd, sent = 0;
 
     if (PSC_getID(msg->dest)!=PSC_getMyID()) {
 	errno = EHOSTUNREACH;
@@ -95,38 +210,26 @@ int sendClient(void *amsg)
     }
 
     /* my own node */
-    for (fd=0; fd<FD_SETSIZE; fd++) {
-	/* find the FD for the dest */
-	if (clients[fd].tid==msg->dest) break;
-    }
+    fd = getClientFD(msg->dest);
 
     if (fd==FD_SETSIZE) {
 	errno = EHOSTUNREACH;
 	return -1;
     }
 
-    {
-	int n, i;
+    if (clients[fd].msgs) flushClientMsgs(fd);
 
-	for (n=0, i=1; (n<msg->len) && (i>0);) {
-	    i = send(fd, &(((char*)msg)[n]), msg->len-n, 0);
-	    if (i<=0) {
-		if (errno!=EINTR) {
-		    char *errstr = strerror(errno);
-
-		    snprintf(errtxt, sizeof(errtxt),
-			     "%s(): got error %d on socket %d: %s",
-			     __func__, errno, fd,
-			     errstr ? errstr : "UNKNOWN");
-		    PSID_errlog(errtxt, (errno==EPIPE) ? 1 : 0);
-		    deleteClient(fd);
-		    return i;
-		}
-	    } else
-		n+=i;
-	}
-	return n;
+    if (!clients[fd].msgs) {
+	sent = do_send(fd, msg, 0);
     }
+
+    if (sent<0) return sent;
+    if (sent != msg->len) {
+	if (!storeMsgClient(fd, msg, sent)) errno = EWOULDBLOCK;
+	return -1;
+    }
+
+    return sent;
 }
 
 static size_t readall(int fd, void *buf, size_t count)
@@ -228,6 +331,12 @@ void closeConnection(int fd)
     clients[fd].tid = -1;
     if (clients[fd].task) clients[fd].task->fd = -1;
     clients[fd].task = NULL;
+    while (clients[fd].msgs) {
+	msgbuf_t *mp = clients[fd].msgs;
+
+	clients[fd].msgs = clients[fd].msgs->next;
+	freeMsg(mp);
+    }
 
     shutdown(fd, SHUT_RDWR);
     close(fd);
@@ -268,7 +377,7 @@ void deleteClient(int fd)
 	sendMsg(&msg);
     }
 
-    snprintf(errtxt, sizeof(errtxt), "%s(): closing connection to %s",
+    snprintf(errtxt, sizeof(errtxt), "%s: closing connection to %s",
 	     __func__, PSC_printTID(tid));
     PSID_errlog(errtxt, 1);
 
