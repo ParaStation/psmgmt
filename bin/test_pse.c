3ecba788b240e443179919526899bb3e0a98c30f
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2003-07-11 14:39:33 +0000

    Switch to libpopt (libargs doesn't work with gcc-3.3). Adapt to actual libpse. Cleanup.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@2327 34291a86-62e8-0310-8159-f060f8a2ee4e

--- bin/test_pse.c
+++ bin/test_pse.c
@@ -4,60 +4,82 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
-#include <errno.h>
-#include <syslog.h>
-#include <sys/mman.h>
-#include <sys/ioctl.h>
 
-#include "ps_types.h"
-#include "pshal.h"
-#include "psport.h"
+#include <popt.h>
+
 #include "pse.h"
-#include <signal.h>
-#include "arg.h"
-#include <sys/time.h>
 
 int arg_np;
-int arg_cnt;
 
-
-void run(int argc,char **argv,int np)
+void run(int argc, char *argv[], int np)
 {
     int mapnode;
     int mapport;
     int rank;
-    char name[100];
+    char name[256];
     mapnode=0;
     mapport=0;
     rank =0;
-    
-    PSEinit(np,argc,argv,&mapnode,&mapport,&rank);
+
+    PSE_initialize();
+
+    rank = PSE_getRank();
+
+    if (rank == -1){
+	/* I am the logger */
+	/* Set default to none: */
+	setenv("PSI_NODES_SORT","NONE",0);
+	PSE_spawnMaster(argc, argv);
+	/* Never be here ! */
+	exit(1);
+    }
+
+    if (rank==0){
+	/* Master node: Set parameter from rank 0 */
+	PSE_spawnTasks(np-1, mapnode, mapport, argc, argv);
+    }else{
+	/* Client node: Get parameter from rank 0 */
+	mapnode = PSE_getMasterNode();
+	mapport = PSE_getMasterPort();
+    }
+
 //    sleep(rank/30);
     gethostname(name,sizeof(name)-1);
 
-    printf("node: %d port: %d rank: %d host:%s\n",mapnode,mapport,rank,name  );
-    sleep(3);
-    PSEfinalize();
+    printf("node: %d port: %d rank: %d host:%s\n",mapnode,mapport,rank,name);
+    sleep(120);
+
+    PSE_finalize();
 }
 
 
-
-
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+    poptContext optCon;   /* context for parsing command-line options */
+    int rc;
+
+    struct poptOption optionsTable[] = {
+        { "np", '\0', POPT_ARG_INT | POPT_ARGFLAG_ONEDASH,
+	  &arg_np, 0, "number of processes to start", "num"},
+	POPT_AUTOHELP
+	{ NULL, '\0', 0, NULL, 0, NULL, NULL}
+    };
 
     //printf(__DATE__" "__TIME__"\n");
 
-    if (arg_parse(argc, argv,
-		  "", "Usage: %s [options]", argv[0],
-		  "-np %d",&arg_np," ",
-		  "-cnt %d",&arg_cnt," ",
-		  0) < 0){
-        exit(1);
+    optCon = poptGetContext(NULL, argc, (const char **)argv, optionsTable, 0);
+    rc = poptGetNextOpt(optCon);
+
+    if (rc < -1) {
+	/* an error occurred during option processing */
+	poptPrintUsage(optCon, stderr, 0);
+	fprintf(stderr, "%s: %s\n",
+		poptBadOption(optCon, POPT_BADOPTION_NOALIAS),
+		poptStrerror(rc));
+	return 1;
     }
 
     if (arg_np <= 0) {
