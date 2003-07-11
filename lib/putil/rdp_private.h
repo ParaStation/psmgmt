0954e479a4d5fcb07876dda70656a3c8d16200fa
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2003-07-11 14:05:27 +0000

    Some more tuning.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@2324 34291a86-62e8-0310-8159-f060f8a2ee4e

--- lib/putil/rdp_private.h
+++ lib/putil/rdp_private.h
@@ -5,7 +5,7 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: rdp_private.h,v 1.14 2003/07/11 13:42:27 eicker Exp $
+ * $Id: rdp_private.h,v 1.15 2003/07/11 14:05:27 eicker Exp $
  *
  */
 /**
@@ -14,7 +14,7 @@
  *
  * Private functions and definitions
  *
- * $Id: rdp_private.h,v 1.14 2003/07/11 13:42:27 eicker Exp $
+ * $Id: rdp_private.h,v 1.15 2003/07/11 14:05:27 eicker Exp $
  *
  * \author
  * Norbert Eicker <eicker@par-tec.com>
@@ -120,7 +120,7 @@ static int RDPPktLoss = 0;
  * The actual maximum retransmission count. Get/set by
  * getMaxRetransRDP()/setMaxRetransRDP()
  */
-static int RDPMaxRetransCount = 20;
+static int RDPMaxRetransCount = 128;
 
 /**
  * @todo This is not correct!! What happens on overflow?
