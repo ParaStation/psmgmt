4375bdd7b6e702e9d3906a6e06b029dd829a8fea
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2003-07-18 16:34:55 +0000

    Undo remove
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@2363 34291a86-62e8-0310-8159-f060f8a2ee4e

---
+++ include/compat_tru64.h
@@ -0,0 +1,27 @@
+
+
+#ifndef _COMPAT_TRU64_H_
+#define _COMPAT_TRU64_H_
+
+
+#define __attribute__( name )
+
+#ifndef __KERNEL__
+#define _DEC_XPG
+/*typedef unsigned long   socklen_t;*/
+
+/* #define _BSD */
+/* Only defined if _BSD is defined */
+extern int      setenv   (const char *, const char *, int);
+extern void     unsetenv (const char *);
+
+#endif
+
+#define NO_MACRODOTDOT
+
+
+
+#endif /* _COMPAT_TRU64_H_ */
+
+
+
