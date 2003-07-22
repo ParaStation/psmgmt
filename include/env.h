dc63c9fb624c67454e5a810339a503b97f34a5d6
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2003-07-22 18:27:00 +0000

    Documentation added.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@2380 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/env.h
+++ include/env.h
@@ -1,20 +1,23 @@
-/***********************************************************
- *                  ParaStation4
+/*
+ *                  ParaStation
+ * env.h
  *
- *       Copyright (c) 2002 ParTec AG Karlsruhe
- *       All rights reserved.
- ***********************************************************/
+ * Copyright (c) 2002-2003 ParTec AG Karlsruhe
+ * All rights reserved.
+ *
+ * $Id: env.h,v 1.3 2003/07/22 18:26:31 eicker Exp $
+ *
+ */
 /**
- * env.c: Simple environment handling
+ * @file
+ * Simple environment handling
  *
- * $Id: env.h,v 1.2 2003/04/03 14:56:40 eicker Exp $
+ * $Id: env.h,v 1.3 2003/07/22 18:26:31 eicker Exp $
  *
  * @author
- *         Jens Hauke <hauke@par-tec.de>
+ * Jens Hauke <hauke@par-tec.de>
  *
- * @file
- ***********************************************************/
-
+ */
 #ifndef _ENV_H_
 #define _ENV_H_
 
@@ -25,18 +28,129 @@ extern "C" {
 #endif
 #endif
 
+/** The structure holding the environment */
 typedef struct env_fields_s{
-    char **vars;
-    int cnt;
-    int size;
+    char **vars;    /**< Array of variables */
+    int cnt;        /**< Actual size */
+    int size;       /**< Actual maximum size */
 }env_fields_t;
 
+/**
+ * @brief Init an environment
+ *
+ * Initialzie the environment @a env. This mainly resets the contenv
+ * of @a env, i.e. if @a env is initialized more than once this may
+ * lead to a memory leak.
+ *
+ * @param env The environment to initialize.
+ *
+ * @return No return value.
+*/
 void env_init(env_fields_t *env);
+
+/**
+ * @brief Get environment's size.
+ *
+ * Get the actual size, i.e. the number of stored variables, of the
+ * environment @a env.
+ *
+ * @param env The environment the size should be determined from.
+ *
+ * @return The size of the environment is returned.
+ */
 int env_size(env_fields_t *env);
+
+/**
+ * @brief Get a variables index.
+ *
+ * Get the index of the variable with name @a name within the
+ * environment @a env. The returned index is in between 0 and size-1,
+ * where size is the actual size of the environment.
+ *
+ * @param env The environment to search in.
+ *
+ * @param name The name of the variable to search for. It must not
+ * contain the character '='.
+ *
+ * @return On success, i.e. if the variable was found, its index is
+ * returned. Or -1, if it was not found or an error occured.
+ */
 int env_index(env_fields_t *env, const char *name);
-int env_unset(env_fields_t *env, const char *name);
+
+/**
+ * @brief Set a variable.
+ *
+ * Set the variable with name @a name to the value @a val within the
+ * environment @a env.
+ *
+ * If a variable with name @a name is already set, it will be removed
+ * using the env_unset() function before registering the variable to
+ * its new value.
+ *
+ * If the actual maximum size of the environment is not sufficient, it
+ * will be expanded on the fly.
+ *
+ * @param env The environment to act on.
+ *
+ * @param name The name of the variable to set. It must not contain
+ * the character '='.
+ *
+ * @param val The value to be assigned to the variable.
+ *
+ * @return On success, i.e. if the variable could be registered, 0 is
+ * returned. Or -1, if an error occured.
+ */
 int env_set(env_fields_t *env, const char *name, const char *val);
+
+/**
+ * @brief Get a variable.
+ *
+ * Get the value of the variable with name @a name within the
+ * environment @a env. The variable has to be registered to the
+ * environment before using the env_set() function.
+ *
+ * @param env The environment to look at.
+ *
+ * @param name The name of the variable to lookup. It must not contain
+ * the character '='.
+ *
+ * @return On success, i.e. if the variable could be found, a pointer
+ * to the value is returned. Or NULL if an error occured.
+ */
 char *env_get(env_fields_t *env, const char *name);
+
+/**
+ * @brief Remove a variable.
+ *
+ * Remove the variable with name @a name from the environment @a
+ * env. The variable has to be registered to the environment before
+ * using the env_set() function.
+ *
+ * @param env The environment to remove from.
+ *
+ * @param name The name of the variable to remove. It must not contain
+ * the character '='.
+ *
+ * @return On success, i.e. if the variable could be found, 0 is
+ * returned. Or -1 if an error occured.
+ */
+int env_unset(env_fields_t *env, const char *name);
+
+/**
+ * @brief Dump an environment.
+ *
+ * Dump the variable with index @a idx from the environment @a env.
+ * This function is mainly used for putting whole environments into a
+ * processes real environment via successiv calls of putenv().
+ *
+ * @param env The environment to dump from.
+ *
+ * @param idx The index of the variable to dump.
+ *
+ * @return If an environment variable is stored under the index @a
+ * idx, a string of the format 'name=value' is returned. Otherwise
+ * NULL is returned.
+ */
 char *env_dump(env_fields_t *env, int idx);
 
 #ifdef __cplusplus
