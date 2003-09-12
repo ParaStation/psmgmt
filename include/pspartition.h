05d5eddcab1f99d171c57b48a9cb3d24d7f7ee92
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2003-09-12 13:43:12 +0000

    New file: Basic definitions for setting up and managing partitions.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@2505 34291a86-62e8-0310-8159-f060f8a2ee4e

---
+++ include/pspartition.h
@@ -0,0 +1,66 @@
+/*
+ *               ParaStation
+ * pspartition.h
+ *
+ * Copyright (C) ParTec AG Karlsruhe
+ * All rights reserved.
+ *
+ * $Id: pspartition.h,v 1.1 2003/09/12 13:43:12 eicker Exp $
+ *
+ */
+/**
+ * @file
+ * Basic enumerations for partition creation and reservation.
+ *
+ * $Id: pspartition.h,v 1.1 2003/09/12 13:43:12 eicker Exp $
+ *
+ * @author
+ * Norbert Eicker <eicker@par-tec.com>
+ *
+ */
+#ifndef __PSPARTITION_H
+#define __PSPARTITION_H
+
+#ifdef __cplusplus
+extern "C" {
+#if 0
+} /* <- just for emacs indentation */
+#endif
+#endif
+
+/** Various sort modes for partition creation. */
+typedef enum {
+    PART_SORT_NONE,           /**< No sorting at all. */
+    PART_SORT_PROC,           /**< Sort by number of ParaStation processes. */
+    PART_SORT_LOAD_1,         /**< Sort by 1 minute load average. */
+    PART_SORT_LOAD_5,         /**< Sort by 5 minute load average. */
+    PART_SORT_LOAD_15,        /**< Sort by 15 minute load average. */
+    PART_SORT_PROCLOAD,       /**< Sort by load_1 + proc. */
+    PART_SORT_UNKNOWN = 0x0ff /**< Dummy for unknown sort mode. */
+} PSpart_sort_t;
+
+/** Options possibly bound to a partition. */
+typedef enum {
+    PART_OPT_NODEFIRST = 0x0001, /**< Place consecutive processes on different
+				    nodes, if possible. Usually consecutive
+				    processes are place on the same node. */
+    PART_OPT_EXCLUSIVE = 0x0002, /**< Only get exclusive nodes. I.e. no further
+				    processes are allowed on that node. */
+    PART_OPT_OVERBOOK = 0x0004,  /**< Allow more than one process per node.
+				    This involves @ref PART_OPT_EXCLUSIVE
+				    implicitely. */
+    PART_OPT_WAIT = 0x0008,      /**< If not enough nodes are available, wait
+				    for them (batch mode). */
+} PSpart_option_t;
+
+/**
+ * Chunksize for PSP_CD_GETNODES, PSP_CD_CREATEPARTNL,
+ * PSP_DD_GETPARTNL and PSP_DD_PROVIDEPARTNL messages
+ */
+#define GETNODES_CHUNK 512
+
+#ifdef __cplusplus
+}/* extern "C" */
+#endif
+
+#endif  /* __PSPARTITION_H */
