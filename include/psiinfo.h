49ce04db3c0327692f2f025e951265e0e34043ef
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2003-11-26 17:11:49 +0000

    Replace info.c by the new psiinfo.c, the new info retrieval library.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@2703 34291a86-62e8-0310-8159-f060f8a2ee4e

---
+++ include/psiinfo.h
@@ -0,0 +1,364 @@
+/*
+ *               ParaStation
+ * psiinfo.h
+ *
+ * Copyright (C) ParTec AG Karlsruhe
+ * All rights reserved.
+ *
+ * $Id: psiinfo.h,v 1.1 2003/11/26 17:11:49 eicker Exp $
+ *
+ */
+/**
+ * @file
+ * psiinfo: Functions for information retrieving from ParaStation
+ * daemon
+ *
+ * $Id: psiinfo.h,v 1.1 2003/11/26 17:11:49 eicker Exp $
+ *
+ * @author
+ * Norbert Eicker <eicker@par-tec.com>
+ *
+ */
+#ifndef __PSIINFO_H
+#define __PSIINFO_H
+
+#include <stdint.h>
+
+#include "psnodes.h"
+#include "pstask.h"
+#include "psprotocol.h"
+
+#ifdef __cplusplus
+extern "C" {
+#if 0
+} /* <- just for emacs indentation */
+#endif
+#endif
+
+/**
+ * @brief Retrieve integer information.
+ *
+ * Retrieve the integer information of type @a what from node @a node
+ * and store it to @a val. Depending on the type of information
+ * requested, @a param points to further parameter(s) needed in order
+ * to answer the request.
+ *
+ * The possible values for @a what are:
+ *
+ * - PSP_INFO_NODE request a nodes IP address from its ParaStation
+ * ID. @a param points to a datum of type PSnodes_ID_t containing the
+ * ParaStation ID. Although @a val points to a datum of type @c
+ * int32_t, the value returned has to be interpreted as a @c uint32_t.
+ *
+ * - PSP_INFO_NROFNODES request the total number of nodes within the
+ * ParaStation cluster. No further parameters needed.
+ *
+ * - PSP_INFO_HWNUM request the total number of hardware types
+ * configured within the ParaStation configuration file. No further
+ * parameters needed.
+ *
+ * - PSP_INFO_HWINDEX request the index from a hardware's name. @a
+ * param points to a \0 terminated character array containing the name.
+ *
+ * - PSP_INFO_TASKSIZE request the total number of processes within
+ * the actual task. No further parameters needed.
+ *
+ * - PSP_INFO_TASKRANK request the rank of the local process within
+ * the parallel task. The result might be a positiv number or -1,
+ * where the latter denotes the process to be the logger process. No
+ * further parameters needed.
+ *
+ * @param node The ParaStation ID of the node to ask.
+ *
+ * @param what The type of information to request as described above.
+ *
+ * @param param Pointer to further parameters needed in order to
+ * retrieve the requested information.
+ *
+ * @param val Pointer to the datum reserved for the result.
+ *
+ * @param verbose Flag to be more verbose, if something within the
+ * information retrival went wrong.
+ *
+ * @return On success, 0 is returned. Otherwise -1 is returned.
+ */
+int PSI_infoInt(PSnodes_ID_t node, PSP_Info_t what, const void *param,
+		int32_t *val, int verbose);
+
+/**
+ * @brief Retrieve string information.
+ *
+ * Retrieve the string information of type @a what from node @a node
+ * and store it to @a string, which is of size @a size. Depending on
+ * the type of information requested, @a param points to further
+ * parameter(s) needed in order to answer the request.
+ *
+ * The possible values for @a what are:
+ *
+ * -PSP_INFO_COUNTHEADER request the header line of the counter output
+ * for a special hardware on the designated node. @a param points to
+ * the hardware index of the *hardware wanted. The hardware index has
+ * to be stored within a @c *int32_t datum.
+ *
+ * -PSP_INFO_COUNTSTATUS request the actual status line of the counter
+ * output for a special hardware on the designated node. @a param
+ * points to the hardware index of the *hardware wanted. The hardware
+ * index has to be stored within a @c *int32_t datum.
+ *
+ * - PSP_INFO_HWNAME request the name of a special hardware as
+ * configured within the ParaStation configuration file on the
+ * designated node. @a param points to the hardware index of the
+ * *hardware wanted. The hardware index has to be stored within a @c
+ * *int32_t datum.
+ *
+ * - PSP_INFO_RDPSTATUS request the status of the RDP connection from
+ * node @a node to the one designated within the extra parameter. Thus
+ * @a param has to point to a datum of type @c PSnodes_ID_t holding
+ * this connected node.
+ *
+ * - PSP_INFO_MCASTSTATUS request the status of the MCast connection
+ * from node @a node to the one designated within the extra
+ * parameter. Thus @a param has to point to a datum of type @c
+ * PSnodes_ID_t holding this connected node.
+ *
+ * - PSP_INFO_INSTDIR request the ParaStation installation directory
+ * as configured within the configuration file. No further parameters
+ * needed.
+ *
+ * - PSP_INFO_DAEMONVER reqeust the version string of the ParaStation
+ * daemon. No further parameters needed.
+ *
+ *
+ * @param node The ParaStation ID of the node to ask.
+ *
+ * @param what The type of information to request as described above.
+ *
+ * @param param Pointer to further parameters needed in order to
+ * retrieve the requested information.
+ *
+ * @param string Pointer to the character array reserved for the
+ * result.
+ *
+ * @param size The size of @a string.
+ *
+ * @param verbose Flag to be more verbose, if something within the
+ * information retrival went wrong.
+ *
+ * @return On success, 0 is returned. Otherwise -1 is returned.
+ */
+int PSI_infoString(PSnodes_ID_t node, PSP_Info_t what, const void *param,
+		   char *string, size_t size, int verbose);
+
+/**
+ * @brief Retrieve task ID.
+ *
+ * Retrieve the task ID information of type @a what from node @a node
+ * and store it to @a tid. Depending on the type of information
+ * requested, @a param points to further parameter(s) needed in order
+ * to answer the request.
+ *
+ * The possible values for @a what are:
+ *
+ * - PSP_INFO_PARENTTID request the parent task ID of the actual task,
+ * if any. No further parameters needed.
+ *
+ * - PSP_INFO_LOGGERTID request the logger task ID of the actual task,
+ * if any. No further parameters needed.
+ *
+ *
+ * @param node The ParaStation ID of the node to ask.
+ *
+ * @param what The type of information to request as described above.
+ *
+ * @param param Pointer to further parameters needed in order to
+ * retrieve the requested information.
+ *
+ * @param tid Pointer to a datum reserved for the result.
+ *
+ * @param verbose Flag to be more verbose, if something within the
+ * information retrival went wrong.
+ *
+ * @return On success, 0 is returned. Otherwise -1 is returned.
+ */
+int PSI_infoTaskID(PSnodes_ID_t node, PSP_Info_t what, const void *param,
+		   PStask_ID_t *tid, int verbose);
+
+/**
+ * @brief Retrieve ParaStation ID.
+ *
+ * Retrieve the ParaStation ID information of type @a what from node @a node
+ * and store it to @a tid. Depending on the type of information
+ * requested, @a param points to further parameter(s) needed in order
+ * to answer the request.
+ *
+ * The possible values for @a what are:
+ *
+ * - PSP_INFO_RANKID request the ParaStation ID of the node on which
+ * the process with a destinct rank within the actual task will be
+ * running on. @a param has to point to a @c uint32_t datum holding
+ * the requested rank.
+ *
+ * -PSP_INFO_HOST reqeust the ParaStation ID corresponding to an IP
+ * address within the cluster. @a param has to point to the IP address
+ * of interest stored within a @c uint32_t datum.
+ *
+ *
+ * @param node The ParaStation ID of the node to ask.
+ *
+ * @param what The type of information to request as described above.
+ *
+ * @param param Pointer to further parameters needed in order to
+ * retrieve the requested information.
+ *
+ * @param nid Pointer to a datum reserved for the result.
+ *
+ * @param verbose Flag to be more verbose, if something within the
+ * information retrival went wrong.
+ *
+ * @return On success, 0 is returned. Otherwise -1 is returned.
+ */
+int PSI_infoNodeID(PSnodes_ID_t node, PSP_Info_t what, const void *param,
+		   PSnodes_ID_t *nid, int verbose);
+
+/**
+ * @brief
+ *
+ * Retrieve a list of information of type @a what from node @a node
+ * and store it to the buffer @a buf of size @a size. Depending on the
+ * type of information requested, @a param points to further
+ * parameter(s) needed in order to answer the request.
+ *
+ * The possible values for @a what are:
+ *
+ * - PSP_INFO_LIST_HOSTSTATUS requests the status of all nodes within
+ * the ParaStation cluster. A node's status is provided as a @c char
+ * having the value 0 if the node is down or 1 if it's up. No further
+ * parameters needed.
+ *
+ * - PSP_INFO_LIST_VIRTCPUS requests the number of virtual CPUs of all
+ * nodes within the ParaStation cluster. The number of virtual CPUs
+ * includes multiple counts per CPU if Hyper-Threading-Technology on
+ * Intel IA32 CPU is enabled. A node's number of virtual CPUs is
+ * provided as a @c uint16_t. No further parameters needed.
+ *
+ * - PSP_INFO_LIST_PHYSCPUS requests the number of physical CPUs of
+ * all nodes within the ParaStation cluster. The number of physical
+ * CPUs excludes multiple counts per CPU even if
+ * Hyper-Threading-Technology on Intel IA32 CPU is enabled. This is
+ * mainly needed for license control. A node's number of physical CPUs
+ * is provided as a @c uint16_t. No further parameters needed.
+ *
+ * - PSP_INFO_LIST_HWSTATUS requests the hardware status of all nodes
+ * within the ParaStation cluster. A node's hardware status is
+ * provided as a @c uint32_t, where the bit 1<<index represents the
+ * hardware with a certain index to be present. No further parameters
+ * needed.
+ *
+ * - PSP_INFO_LIST_LOAD requests the load of all nodes within the
+ * ParaStation cluster. A node's load is provided as a @c float[3]
+ * array containing the 1 minute, 5 minute and 15 minute averages. No
+ * further parameters needed.
+ *
+ * - PSP_INFO_LIST_ALLJOBS requests the total number of jobs of all
+ * nodes within the ParaStation cluster. A node's total number of jobs
+ * is provided as a @c uint16_t. No further parameters needed.
+ *
+ * - PSP_INFO_LIST_NORMJOBS requests the number of normal jobs of all
+ * nodes within the ParaStation cluster. Normal jobs are those with
+ * task group TG_ANY, i.e. in contrast to the total number of jobs
+ * this one excludes tasks representing admin, logger, forwared
+ * etc. processes. A node's number of normal jobs is provided as a @c
+ * uint16_t. No further parameters needed.
+ *
+ * - PSP_INFO_LIST_ALLTASKS requests a list of all tasks on the
+ * designated node @a node. All the information about a single task is
+ * returned within a @ref PSP_taskInfo_t structure. Upon return @a buf
+ * will hold an array of @ref PSP_taskInfo_t structures. No further
+ * parameters needed.
+ *
+ * - PSP_INFO_LIST_NORMTASKS requests a list of all normal tasks on
+ * the designated node @a node. Normal tasks are those with task group
+ * TG_ANY, i.e. in contrast to all tasks this one excludes tasks
+ * representing admin, logger, forwared etc. processes. All the
+ * information about a single task is returned within a @ref
+ * PSP_taskInfo_t structure. Upon return @a buf will hold an array of
+ * @ref PSP_taskInfo_t structures. No further parameters needed.
+ *
+ *
+ * @param node The ParaStation ID of the node to ask.
+ *
+ * @param what The type of information to request as described above.
+ *
+ * @param param Pointer to further parameters needed in order to
+ * retrieve the requested information.
+ *
+ * @param buf Buffer to store the requested list to.
+ *
+ * @param size Actual size of the buffer @a buf.
+ *
+ * @param verbose Flag to be more verbose, if something within the
+ * information retrival went wrong.
+ *
+ * @return On success, the number of bytes received and stored within
+ * buf is returned. Otherwise -1 is returned.
+ */
+int PSI_infoList(PSnodes_ID_t node, PSP_Info_t what, const void *param,
+		 void *buf, size_t size, int verbose);
+
+/**
+ * @brief Request options
+ *
+ * Request @a num options from node @a node. The options to request
+ * are stored within the array @a option, the corresponding values
+ * will be returned within the @a value array. Up to @ref
+ * DDOptionMsgMax options might be requested within one call to this
+ * function.
+ *
+ * @param node The ParaStation ID of the node to ask.
+ *
+ * @param num The number of option to request.
+ *
+ * @param option Array holding the actual options to request. Size is
+ * @a num.
+ *
+ * @param value Array the requested options are stored to. Size is @a num.
+ *
+ * @param verbose Flag to be more verbose, if something within the
+ * information retrival went wrong.
+ *
+ * @return On success, the number of options received and stored
+ * within value is returned. Otherwise -1 is returned.
+ */
+int PSI_infoOption(PSnodes_ID_t node, int num,
+		   PSP_Option_t option[], PSP_Optval_t value[], int verbose);
+
+/**
+ * @brief Get hardware name.
+ *
+ * Create a string holding the name(s) of the hardware types set
+ * within @a hwType as defined within the ParaStation configuration
+ * file. @a hwType is provided as a bitset, where the bit 1<<index
+ * represents the hardware with a certain index to be present.
+ *
+ * If @a hwType is 0, i.e. no hardware at all is present, 'none' is
+ * returned.
+ *
+ * @param hwType The hardware type for which the actual name should be
+ * requested.
+ *
+ * @return Upon success, i.e. if all hardware names could be resolved,
+ * a pointer to a space separated static string of hardware names is
+ * returned.  Successiv calls to this function might change this
+ * string, i.e. don't count on the content of this string after
+ * further calls to this function. If an error occurred within
+ * information retrival, NULL is returned.
+ */
+char *PSI_printHWType(unsigned int hwType);
+
+
+
+#ifdef __cplusplus
+}/* extern "C" */
+#endif
+
+#endif /* __PSIINFO_H */
---
+++ lib/psi/psiinfo.c
@@ -0,0 +1,530 @@
+/*
+ *               ParaStation
+ * psiinfo.c
+ *
+ * Copyright (C) ParTec AG Karlsruhe
+ * All rights reserved.
+ *
+ * $Id: psiinfo.c,v 1.1 2003/11/26 17:07:23 eicker Exp $
+ *
+ */
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+static char vcid[] __attribute__(( unused )) = "$Id: psiinfo.c,v 1.1 2003/11/26 17:07:23 eicker Exp $";
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <netinet/in.h>
+
+#include "pscommon.h"
+#include "psprotocol.h"
+#include "pstask.h"
+
+#include "psi.h"
+#include "psilog.h"
+
+#include "psiinfo.h"
+
+static char errtxt[128];
+
+/**
+ * @brief Receive and handle info message.
+ *
+ * Receive and handle info messages and store the content into @a
+ * buf. This is a helper function for the @ref PSI_infoInt(), @ref
+ * PSI_infoString(), @ref PSI_infoTaskID(), @ref PSI_infoNodeID() and
+ * @ref PSI_infoList() functions.
+ *
+ * If the size @a size of the buffer @a buf is sufficiently large, the
+ * content of the received indo message will be stored within @a
+ * buf. @a size is set to the number of bytes stored to @a buf.
+ *
+ * The actual type of the info message will be returned.
+ *
+ *
+ * @param buf The buffer to store the content of the info message to.
+ *
+ * @param size The actual size of the buffer @a buf. On return the
+ * number of bytes received from the info message and stored to @a
+ * buf.
+ *
+ * @param verbose Flag to enable more verbose output.
+ *
+ * @return On success the type of the info message received will be
+ * returned. Otherwise PSP_INFO_UNKNOWN is returned.
+ */
+static PSP_Info_t receiveInfo(void *buf, size_t *size, int verbose)
+{
+    DDTypedBufferMsg_t msg;
+    PSP_Info_t ret;
+
+    if (PSI_recvMsg(&msg)<0) {
+	snprintf(errtxt, sizeof(errtxt), "%s: read", __func__);
+	PSI_errexit(errtxt, errno);
+    }
+
+    switch (msg.header.type) {
+    case PSP_CD_INFORESPONSE:
+    {
+	ret = msg.type;
+	switch (msg.type) {
+	case PSP_INFO_LIST_END:
+	    *size = 0;
+	    break;
+	case PSP_INFO_NROFNODES:
+	case PSP_INFO_INSTDIR:
+	case PSP_INFO_DAEMONVER:
+	case PSP_INFO_HOST:
+	case PSP_INFO_NODE:
+	case PSP_INFO_RDPSTATUS:
+	case PSP_INFO_MCASTSTATUS:
+	case PSP_INFO_COUNTHEADER:
+	case PSP_INFO_COUNTSTATUS:
+	case PSP_INFO_HWNUM:
+	case PSP_INFO_HWINDEX:
+	case PSP_INFO_HWNAME:
+	case PSP_INFO_RANKID:
+	case PSP_INFO_TASKSIZE:
+	case PSP_INFO_TASKRANK:
+	case PSP_INFO_PARENTTID:
+	case PSP_INFO_LOGGERTID:
+	case PSP_INFO_LIST_HOSTSTATUS:
+	case PSP_INFO_LIST_VIRTCPUS:
+	case PSP_INFO_LIST_PHYSCPUS:
+	case PSP_INFO_LIST_HWSTATUS:
+	case PSP_INFO_LIST_LOAD:
+	case PSP_INFO_LIST_ALLJOBS:
+	case PSP_INFO_LIST_NORMJOBS:
+	case PSP_INFO_LIST_ALLTASKS:
+	case PSP_INFO_LIST_NORMTASKS:
+	{
+	    size_t s = msg.header.len - sizeof(msg.header) - sizeof(msg.type);
+	    if (!buf) {
+		snprintf(errtxt, sizeof(errtxt),
+			 "%s: No buffer provided.", __func__);
+		PSI_errlog(errtxt, 1);
+		*size = 0;
+		break;
+	    }
+	    if (*size < s) {
+		snprintf(errtxt, sizeof(errtxt),
+			 "%s: buffer to small.", __func__);
+		PSI_errlog(errtxt, 0);
+		*size = 0;
+		break;
+	    }
+	    *size = s;
+	    memcpy(buf, msg.buf, *size);
+	    break;
+	}
+	case PSP_INFO_UNKNOWN:
+	    snprintf(errtxt, sizeof(errtxt),
+		     "%s: daemon does not know info.", __func__);
+	    PSI_errlog(errtxt, 0);
+	    *size = 0;
+	    break;
+	default:
+	    snprintf(errtxt, sizeof(errtxt),
+		     "%s: received unexpected info type '%s'.",
+		     __func__, PSP_printInfo(msg.type));
+	    PSI_errlog(errtxt, 0);
+	    *size = 0;
+	    ret = PSP_INFO_UNKNOWN;
+	}
+	break;
+    }
+    case PSP_CD_ERROR:
+    {
+	char *errstr = strerror(((DDErrorMsg_t*)&msg)->error);
+	snprintf(errtxt, sizeof(errtxt), "%s: error in command %s : %s",
+		 __func__, PSP_printMsg(((DDErrorMsg_t*)&msg)->request),
+		 errstr ? errstr : "UNKNOWN");
+	PSI_errlog(errtxt, verbose ? 0 : 1);
+	*size = 0;
+	ret = PSP_INFO_UNKNOWN;
+	break;
+    }
+    default:
+	snprintf(errtxt, sizeof(errtxt),
+		 "%s: received unexpected msgtype '%s'.",
+		 __func__, PSP_printMsg(msg.header.type));
+	PSI_errlog(errtxt, 0);
+	*size = 0;
+	ret = PSP_INFO_UNKNOWN;
+    }
+
+    return ret;
+}
+
+int PSI_infoInt(PSnodes_ID_t node, PSP_Info_t what, const void *param,
+		int32_t *val, int verbose)
+{
+    DDTypedBufferMsg_t msg = {
+	.header = {
+	    .type = PSP_CD_INFOREQUEST,
+	    .dest = PSC_getTID(node, 0),
+	    .sender = PSC_getMyTID(),
+	    .len = sizeof(msg.header)+sizeof(msg.type) },
+	.type = what,
+	.buf = { 0 } };
+    size_t size = sizeof(*val);
+
+    switch (what) {
+    case PSP_INFO_NODE:
+	if (param) {
+	    *(PSnodes_ID_t *)msg.buf = *(const PSnodes_ID_t *)param;
+	    msg.header.len += sizeof(PSnodes_ID_t);
+	} else {
+	    snprintf(errtxt, sizeof(errtxt), "%s: %s request needs parameter.",
+		     __func__, PSP_printInfo(what));
+	    PSI_errlog(errtxt, 0);
+	    return -1;
+	}
+	break;
+    case PSP_INFO_HWINDEX:
+	if (param) {
+	    strncpy(msg.buf, (const char *)param, sizeof(msg.buf));
+	    msg.buf[sizeof(msg.buf)-1] = '\0';
+	    msg.header.len += strlen(msg.buf)+1;
+	} else {
+	    snprintf(errtxt, sizeof(errtxt), "%s: %s request needs parameter.",
+		     __func__, PSP_printInfo(what));
+	    PSI_errlog(errtxt, 0);
+	    return -1;
+	}
+	break;
+    case PSP_INFO_TASKSIZE:
+    case PSP_INFO_NROFNODES:
+    case PSP_INFO_HWNUM:
+    case PSP_INFO_TASKRANK:
+	break;
+    default:
+	snprintf(errtxt, sizeof(errtxt),
+		 "%s: don't know how to handle '%s' request",
+		 __func__, PSP_printInfo(what));
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    if (PSI_sendMsg(&msg)<0) {
+	snprintf(errtxt, sizeof(errtxt), "%s(%s): write", __func__,
+		 PSP_printInfo(what));
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    if (receiveInfo(val, &size, verbose) == what && size) return 0;
+
+    return -1;
+}
+
+int PSI_infoString(PSnodes_ID_t node, PSP_Info_t what, const void *param,
+		   char *string, size_t size, int verbose)
+{
+    DDTypedBufferMsg_t msg = (DDTypedBufferMsg_t) {
+	.header = (DDMsg_t) {
+	    .type = PSP_CD_INFOREQUEST,
+	    .dest = PSC_getTID(node, 0),
+	    .sender = PSC_getMyTID(),
+	    .len = sizeof(msg.header)+sizeof(msg.type) },
+	.type = what,
+	.buf = { 0 } };
+
+    switch (what) {
+    case PSP_INFO_COUNTHEADER:
+    case PSP_INFO_COUNTSTATUS:
+    case PSP_INFO_HWNAME:
+	if (param) {
+	    *(int32_t *)msg.buf = *(const int32_t *)param;
+	    msg.header.len += sizeof(int32_t);
+	} else {
+	    snprintf(errtxt, sizeof(errtxt), "%s: %s request needs parameter.",
+		     __func__, PSP_printInfo(what));
+	    PSI_errlog(errtxt, 0);
+	    return -1;
+	}
+	break;
+    case PSP_INFO_RDPSTATUS:
+    case PSP_INFO_MCASTSTATUS:
+	if (param) {
+	    *(PSnodes_ID_t *)msg.buf = *(const PSnodes_ID_t *)param;
+	    msg.header.len += sizeof(PSnodes_ID_t);
+	} else {
+	    snprintf(errtxt, sizeof(errtxt), "%s: %s request needs parameter.",
+		     __func__, PSP_printInfo(what));
+	    PSI_errlog(errtxt, 0);
+	    return -1;
+	}
+	break;
+    case PSP_INFO_INSTDIR:
+    case PSP_INFO_DAEMONVER:
+	break;
+    default:
+	snprintf(errtxt, sizeof(errtxt),
+		 "%s: don't know how to handle '%s' request",
+		 __func__, PSP_printInfo(what));
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    if (PSI_sendMsg(&msg)<0) {
+	snprintf(errtxt, sizeof(errtxt), "%s(%s): write", __func__,
+		 PSP_printInfo(what));
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    if (receiveInfo(string, &size, verbose) == what && size) return 0;
+
+    return -1;
+}
+
+int PSI_infoTaskID(PSnodes_ID_t node, PSP_Info_t what, const void *param,
+		   PStask_ID_t *tid, int verbose)
+{
+    DDTypedBufferMsg_t msg = (DDTypedBufferMsg_t) {
+	.header = (DDMsg_t) {
+	    .type = PSP_CD_INFOREQUEST,
+	    .dest = PSC_getTID(node, 0),
+	    .sender = PSC_getMyTID(),
+	    .len = sizeof(msg.header)+sizeof(msg.type) },
+	.type = what,
+	.buf = { 0 } };
+    size_t size = sizeof(*tid);
+
+    switch (what) {
+    case PSP_INFO_PARENTTID:
+    case PSP_INFO_LOGGERTID:
+	break;
+    default:
+	snprintf(errtxt, sizeof(errtxt),
+		 "%s: don't know how to handle '%s' request",
+		 __func__, PSP_printInfo(what));
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    if (PSI_sendMsg(&msg)<0) {
+	snprintf(errtxt, sizeof(errtxt), "%s(%s): write", __func__,
+		 PSP_printInfo(what));
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    if (receiveInfo(tid, &size, verbose) == what && size) return 0;
+
+    return -1;
+}
+
+int PSI_infoNodeID(PSnodes_ID_t node, PSP_Info_t what, const void *param,
+		   PSnodes_ID_t *nid, int verbose)
+{
+    DDTypedBufferMsg_t msg = (DDTypedBufferMsg_t) {
+	.header = (DDMsg_t) {
+	    .type = PSP_CD_INFOREQUEST,
+	    .dest = PSC_getTID(node, 0),
+	    .sender = PSC_getMyTID(),
+	    .len = sizeof(msg.header)+sizeof(msg.type) },
+	.type = what,
+	.buf = { 0 } };
+    size_t size = sizeof(*nid);
+
+    switch (what) {
+    case PSP_INFO_RANKID:
+	if (param) {
+	    *(uint32_t *)msg.buf = *(const uint32_t *)param;
+	    msg.header.len += sizeof(uint32_t);
+	} else {
+	    snprintf(errtxt, sizeof(errtxt), "%s: %s request needs parameter.",
+		     __func__, PSP_printInfo(what));
+	    PSI_errlog(errtxt, 0);
+	    return -1;
+	}
+	break;
+    case PSP_INFO_HOST:
+	if (param) {
+	    *(uint32_t *)msg.buf = *(const uint32_t *)param;
+	    msg.header.len += sizeof(uint32_t);
+	} else {
+	    snprintf(errtxt, sizeof(errtxt), "%s: %s request needs parameter.",
+		     __func__, PSP_printInfo(what));
+	    PSI_errlog(errtxt, 0);
+	    return -1;
+	}
+	break;
+    default:
+	snprintf(errtxt, sizeof(errtxt),
+		 "%s: don't know how to handle '%s' request",
+		 __func__, PSP_printInfo(what));
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    if (PSI_sendMsg(&msg)<0) {
+	snprintf(errtxt, sizeof(errtxt), "%s(%s): write", __func__,
+		 PSP_printInfo(what));
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    if (receiveInfo(nid, &size, verbose) == what && size) return 0;
+
+    return -1;
+}
+
+int PSI_infoList(PSnodes_ID_t node, PSP_Info_t what, const void *param,
+		 void *buf, size_t size, int verbose)
+{
+    DDTypedBufferMsg_t msg = (DDTypedBufferMsg_t) {
+	.header = (DDMsg_t) {
+	    .type = PSP_CD_INFOREQUEST,
+	    .dest = PSC_getTID(node, 0),
+	    .sender = PSC_getMyTID(),
+	    .len = sizeof(msg.header)+sizeof(msg.type) },
+	.type = what,
+	.buf = { 0 } };
+    PSP_Info_t type;
+    size_t recvd = 0;
+
+    switch (what) {
+    case PSP_INFO_LIST_HOSTSTATUS:
+    case PSP_INFO_LIST_VIRTCPUS:
+    case PSP_INFO_LIST_PHYSCPUS:
+    case PSP_INFO_LIST_HWSTATUS:
+    case PSP_INFO_LIST_LOAD:
+    case PSP_INFO_LIST_ALLJOBS:
+    case PSP_INFO_LIST_NORMJOBS:
+    case PSP_INFO_LIST_ALLTASKS:
+    case PSP_INFO_LIST_NORMTASKS:
+	break;
+    default:
+	snprintf(errtxt, sizeof(errtxt),
+		 "%s: don't know how to handle '%s' request",
+		 __func__, PSP_printInfo(what));
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    if (PSI_sendMsg(&msg)<0) {
+	snprintf(errtxt, sizeof(errtxt), "%s(%s): write", __func__,
+		 PSP_printInfo(what));
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    do {
+	size_t chunk = size;
+	if (chunk) {
+	    type = receiveInfo(buf+recvd, &chunk, verbose);
+	} else {
+	    type = receiveInfo(NULL, &chunk, verbose);
+	}
+	if (chunk) {
+	    size -= chunk;
+	    recvd += chunk;
+	} else {
+	    size = 0;
+	}
+    } while (type == what);
+
+    if (type == PSP_INFO_LIST_END) return recvd;
+
+    return -1;
+}
+
+int PSI_infoOption(PSnodes_ID_t node, int num, PSP_Option_t option[],
+		   PSP_Optval_t value[], int verbose)
+{
+    DDOptionMsg_t msg;
+    int i;
+
+    if (num > DDOptionMsgMax) {
+	snprintf(errtxt, sizeof(errtxt), "%s: too many options", __func__);
+	PSI_errlog(errtxt, 0);
+	return -1;
+    }
+
+    msg.header = (DDMsg_t) {
+	.type = PSP_CD_GETOPTION,
+	.dest = PSC_getTID(node, 0),
+	.sender = PSC_getMyTID(),
+	.len = sizeof(msg) };
+
+    for (i=0; i<num; i++) {
+	msg.opt[i].option = option[i];
+    }
+    msg.count = num;
+
+    if (PSI_sendMsg(&msg)<0) {
+	snprintf(errtxt, sizeof(errtxt), "%s: write", __func__);
+	PSI_errexit(errtxt, errno);
+    }
+
+    if (PSI_recvMsg(&msg)<0) {
+	snprintf(errtxt, sizeof(errtxt), "%s: read", __func__);
+	PSI_errexit(errtxt, errno);
+    }
+
+    switch (msg.header.type) {
+    case PSP_CD_SETOPTION:
+	if (msg.count > num) {
+	    snprintf(errtxt, sizeof(errtxt),
+		     "%s: option-buffer to small.", __func__);
+	    PSI_errlog(errtxt, verbose ? 0 : 1);
+	    msg.count = num;
+	}
+
+	for (i=0; i<msg.count; i++) {
+	    value[i] = msg.opt[i].value;
+	}
+
+	return msg.count;
+    case PSP_CD_ERROR:
+    {
+	char* errstr = strerror(((DDErrorMsg_t*)&msg)->error);
+	snprintf(errtxt, sizeof(errtxt), "%s: error: %s",
+		 __func__,errstr ? errstr : "UNKNOWN");
+	PSI_errlog(errtxt, verbose ? 0 : 1);
+	break;
+    }
+    default:
+	snprintf(errtxt, sizeof(errtxt), "%s: unexpected msgtype '%s'",
+		 __func__, PSP_printMsg(msg.header.type));
+	PSI_errlog(errtxt, 0);
+    }
+
+    return -1;
+}
+
+char *PSI_printHWType(unsigned int hwType)
+{
+    int hwNum = 0;
+    static char txt[80], name[40];
+
+    txt[0] = '\0';
+
+    if (!hwType) snprintf(txt, sizeof(txt), "none ");
+
+    while (hwType) {
+	if (hwType & 1) {
+	    int err = PSI_infoString(-1, PSP_INFO_HWNAME,
+				     &hwNum, name, sizeof(name), 1);
+
+	    if (!err) {
+		snprintf(txt+strlen(txt), sizeof(txt)-strlen(txt),
+			 "%s ", name);
+	    } else {
+		snprintf(txt+strlen(txt), sizeof(txt)-strlen(txt), "unknown ");
+	    }
+	}
+
+	hwType >>= 1;
+	hwNum++;
+    }
+
+    txt[strlen(txt)-1] = '\0';
+
+    return txt;
+}
