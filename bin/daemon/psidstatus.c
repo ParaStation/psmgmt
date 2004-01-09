fd5404b071ee09e51ac6be9f920197e6d53cc1d5
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2004-01-09 16:13:12 +0000

    Implement the finalized interface. Some @todos remain.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@2838 34291a86-62e8-0310-8159-f060f8a2ee4e

--- bin/daemon/psidstatus.c
+++ bin/daemon/psidstatus.c
@@ -7,11 +7,11 @@
  * Copyright (C) ParTec AG Karlsruhe
  * All rights reserved.
  *
- * $Id: psidstatus.c,v 1.1 2003/12/19 15:12:08 eicker Exp $
+ * $Id: psidstatus.c,v 1.2 2004/01/09 16:13:12 eicker Exp $
  *
  */
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
-static char vcid[] __attribute__(( unused )) = "$Id: psidstatus.c,v 1.1 2003/12/19 15:12:08 eicker Exp $";
+static char vcid[] __attribute__(( unused )) = "$Id: psidstatus.c,v 1.2 2004/01/09 16:13:12 eicker Exp $";
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
 #include <stdio.h>
@@ -40,55 +40,23 @@ static char vcid[] __attribute__(( unused )) = "$Id: psidstatus.c,v 1.1 2003/12/
 #include "psidoption.h"
 #include "psidcomm.h"
 #include "psidrdp.h"
+#include "psidtimer.h"
 
 #include "psidstatus.h"
 
-static char errtxt[256];
+static char errtxt[256]; /**< General string to create error messages */
 
-static PSnodes_ID_t masterNode = 0;
-
-PSnodes_ID_t getMasterID(void)
-{
-    return masterNode;
-}
-
-void setMasterID(PSnodes_ID_t id)
-{
-    masterNode = id;
-}
-
-int amMasterNode(void)
-{
-    return (PSC_getMyID() == masterNode);
-}
-
-
+/** The jobs of the local node node */
 static PSID_Jobs_t myJobs = { .normal = 0, .total = 0 };
 
-void incJobs(int total, int normal)
-{
-    if (total) myJobs.total++;
-    if (normal) myJobs.normal++;
-
-    if (config->useMCast) {
-	incJobsMCast(PSC_getMyID(), total, normal);
-    } else {
-	// doRDPPing(); @todo
-    }
-}
-
-void decJobs(int total, int normal)
-{
-    if (total) myJobs.total--;
-    if (normal) myJobs.normal--;
-
-    if (config->useMCast) {
-	decJobsMCast(PSC_getMyID(), total, normal);
-    } else {
-	// doRDPPing(); @todo
-    }
-}
-
+/**
+ * @brief Get load information from kernel.
+ *
+ * Get load information from the kernel. The implementation is platform
+ * specific, since POSIX has no mechanism to retrieve this info.
+ *
+ * @return A @ref PSID_Load_t structure containing the load info.
+ */
 static PSID_Load_t getLoad(void)
 {
     PSID_Load_t load = {{0.0, 0.0, 0.0}};
@@ -119,22 +87,56 @@ static PSID_Load_t getLoad(void)
     return load;
 }
 
+/** Structure used to hold status information of all nodes on the master */
 typedef struct {
-    struct timeval lastping;  /**< Timestamp of last received ping */
+    struct timeval lastPing;  /**< Timestamp of last received ping */
     PSID_Jobs_t jobs;   /**< Number of jobs on the node */  
     PSID_Load_t load;   /**< Load parameters of node */
-    short placedJobs;
+    short missCounter;  /**< Number of consecutively missing status pings */
+    short placedJobs;   /**< @todo Move to psidpartition.c */
 } ClientStatus_t;
 
+/**
+ * Master's array holding status information of all nodes. Will be
+ * allocated on demand within @ref allocMasterSpace()
+ */
 static ClientStatus_t *clientStat = NULL;
 
-static void becomeMaster(void)
+/**
+ * @brief Allocate master space.
+ *
+ * Allocate space needed in order to fulfill all the tasks a master
+ * node is expected to handle. @ref clientStat will point to this space.
+ *
+ * Use @ref freeMasterSpace() to release the no longer needed space.
+ *
+ * @return No return value.
+ *
+ * @see freeMasterSpace()
+ */
+static void allocMasterSpace(void)
 {
+    PSnodes_ID_t node;
+
     clientStat = realloc(clientStat, PSC_getNrOfNodes() * sizeof(*clientStat));
-    setMasterID(PSC_getMyID());
+    for (node=0; node<PSC_getNrOfNodes(); node++) {
+	gettimeofday(&clientStat[node].lastPing, NULL);
+	clientStat[node].missCounter = 0;
+	clientStat[node].placedJobs = 0;
+    }
 }
 
-static void abandonMaster(void)
+/**
+ * @brief Free master space.
+ *
+ * Free space needed by master when master burdon is passed to next
+ * node. The space has to be allocated using @ref allocMasterSpace().
+ *
+ * @return No return value.
+ *
+ * @see allocMasterSpace()
+ */
+static void freeMasterSpace(void)
 {
     if (clientStat) {
 	free(clientStat);
@@ -142,7 +144,84 @@ static void abandonMaster(void)
     }
 }
 
-void sendRDPPing(void)
+/**
+ * Major timeout after which status ping are sent and @ref
+ * handleMasterTask() is called.
+ */
+static struct timeval StatusTimeout = { .tv_sec = 2, .tv_usec = 0 };
+
+/**
+ * Number of consecutive status pings allowed to miss before a node is
+ * declared to be dead.
+*/
+static const int DeadLimit = 5;
+
+/**
+ * @brief Master handling routine.
+ *
+ * All the stuff the master has to handle when @ref StatusTimeout is
+ * elapsed. This includes test if all expected status pings were
+ * received and ringing down nodes in order to prevent decomposition
+ * of the cluster into independent sub-clusters.
+ *
+ * @return No return value.
+ */
+static void handleMasterTasks(void)
+{
+    static PSnodes_ID_t next = 0;
+    PSnodes_ID_t node;
+    static int round = 0;
+    int nrDownNodes = 0;
+    struct timeval tv1, tv2;
+
+    gettimeofday(&tv2, NULL);
+    mytimersub(&tv2, StatusTimeout.tv_sec, StatusTimeout.tv_usec);
+    for (node=0; node<PSC_getNrOfNodes(); node++) {
+	if (PSnodes_isUp(node)) {
+	    if (timercmp(&clientStat[node].lastPing, &tv2, <)) {
+		/* no ping in the last 'round' */
+		snprintf(errtxt, sizeof(errtxt),
+			 "%s: Ping from node %d missing [%d]",
+			 __func__, node, clientStat[node].missCounter);
+		clientStat[node].missCounter++;
+		PSID_errlog(errtxt, 5);
+	    }
+	    if (clientStat[node].missCounter > DeadLimit) {
+		snprintf(errtxt, sizeof(errtxt),
+			 "%s: misscount exceeded to node %d", __func__, node);
+		PSID_errlog(errtxt, 1);
+		send_DAEMONCONNECT(node);
+	    }
+	} else {
+	    nrDownNodes++;
+	}
+    }
+
+    round %= 10;
+    if (!round) {
+	int count = (nrDownNodes > 10) ? 10 : nrDownNodes;
+	while (count) {
+	    next %= PSC_getNrOfNodes();
+	    if (!PSnodes_isUp(next)) {
+		send_DAEMONCONNECT(next);
+		count--;
+	    }
+	    next++;
+	}
+    }
+    round++;
+
+    return;
+}
+
+/**
+ * @brief Send status ping.
+ *
+ * Send a status ping 
+ *
+ * @return No return value.
+ */
+static void sendRDPPing(void)
 {
     DDBufferMsg_t msg = {
 	.header = {
@@ -150,7 +229,7 @@ void sendRDPPing(void)
 	    .sender = PSC_getMyTID(),
 	    .dest = PSC_getTID(getMasterID(), 0),
 	    .len = sizeof(msg.header) },
-	.buf = { 0 } };
+	.buf = {'\0'} };
     char *ptr = msg.buf;
 
     *(PSID_Jobs_t *)ptr = myJobs;
@@ -163,22 +242,40 @@ void sendRDPPing(void)
 
     if (getMasterID() == PSC_getMyID()) {
 	msg_LOAD(&msg);
+	handleMasterTasks();
     } else {
 	sendMsg(&msg);
     }
 }
 
-static struct timeval StatusTimeout = {2, 0}; /* sec, usec */
-
-static int timerID;
-
-static void becomeClient(void)
+void incJobs(int total, int normal)
 {
-    abandonMaster();
-    if (!Timer_isInitialized()) {
-	Timer_init(config->useSyslog);
+    snprintf(errtxt, sizeof(errtxt), "%s(%d,%d)", __func__, total, normal);
+    PSID_errlog(errtxt, 10);
+
+    if (total) myJobs.total++;
+    if (normal) myJobs.normal++;
+
+    if (config->useMCast) {
+	incJobsMCast(PSC_getMyID(), total, normal);
+    } else {
+	if (knowMaster()) sendRDPPing();
     }
-    timerID = Timer_register(&StatusTimeout, sendRDPPing); 
+}
+
+void decJobs(int total, int normal)
+{
+    snprintf(errtxt, sizeof(errtxt), "%s(%d,%d)", __func__, total, normal);
+    PSID_errlog(errtxt, 10);
+
+    if (total) myJobs.total--;
+    if (normal) myJobs.normal--;
+
+    if (config->useMCast) {
+	decJobsMCast(PSC_getMyID(), total, normal);
+    } else {
+	if (knowMaster()) sendRDPPing();
+    }
 }
 
 PSID_NodeStatus_t getStatus(PSnodes_ID_t node)
@@ -212,21 +309,81 @@ PSID_NodeStatus_t getStatus(PSnodes_ID_t node)
     return status;
 }
 
+/**
+ * ID of the timer used for status control. We need to store this in
+ * order to be able to release the timer again.
+ *
+ * Furthermore this is used for identifying, if the master is already
+ * known.
+ */
+static int timerID = -1;
+
+/** The actual master node. */
+static PSnodes_ID_t masterNode = 0;
+
+void declareMaster(PSnodes_ID_t newMaster)
+{
+    snprintf(errtxt, sizeof(errtxt), "%s(%d)", __func__, newMaster);
+    PSID_errlog(errtxt, 2);
+
+    if (knowMaster() && newMaster == getMasterID()) return;
+
+    if (knowMaster() && getMasterID() == PSC_getMyID()) freeMasterSpace();
+
+    if (newMaster == PSC_getMyID()) allocMasterSpace();
+
+    masterNode = newMaster;
+
+    if (config->useMCast) {
+	timerID = 0;
+	return;
+    }
+
+    if (!knowMaster()) {
+	if (!Timer_isInitialized()) {
+	    Timer_init(config->useSyslog);
+	}
+	timerID = Timer_register(&StatusTimeout, sendRDPPing); 
+	sendRDPPing();
+    }
+}
+
+int knowMaster(void)
+{
+    return (timerID != -1);
+}
+
+PSnodes_ID_t getMasterID(void)
+{
+    return masterNode;
+}
+
+void releaseStatusTimer(void)
+{
+    if (knowMaster()) Timer_remove(timerID);
+}
+
 /** Total number of nodes connected. Needed for license testing */
 static int totalNodes = 0;
 
 /** Total number of physical CPUs connected. Needed for license testing */
 static int totalCPUs = 0;
 
+/* Prototype forward declaration. */
+static int send_DEADNODE(PSnodes_ID_t deadnode);
 
-void declareNodeDead(PSnodes_ID_t id)
+void declareNodeDead(PSnodes_ID_t id, int sendDeadnode)
 {
     PStask_t *task;
 
-    if (PSnodes_isUp(id)) {
-	totalCPUs -= PSnodes_getPhysCPUs(id);
-	totalNodes--;
-    }
+    if (!PSnodes_isUp(id)) return;
+
+    snprintf(errtxt, sizeof(errtxt), "%s: node %d send DEADNODE %d",
+	     __func__, id, sendDeadnode);
+    PSID_errlog(errtxt, 2);
+
+    totalCPUs -= PSnodes_getPhysCPUs(id);
+    totalNodes--;
     PSnodes_bringDown(id);
     PSnodes_setPhysCPUs(id, 0);
     PSnodes_setVirtCPUs(id, 0);
@@ -260,16 +417,43 @@ void declareNodeDead(PSnodes_ID_t id)
 	task = task->next;
     }
 
-    snprintf(errtxt, sizeof(errtxt),
-	     "Lost connection to daemon of node %d", id);
-    PSID_errlog(errtxt, 2);
+    snprintf(errtxt, sizeof(errtxt), "%s: connection lost to node %d",
+	     __func__, id);
+    PSID_errlog(errtxt, 0);
+
+    if (id == getMasterID()) {
+	/* Dead node was master, find new one */
+	PSnodes_ID_t node = id;
+
+	while (node < PSC_getMyID()) {
+	    if (PSnodes_isUp(node)) break;
+	    node++;
+	}
+
+	snprintf(errtxt, sizeof(errtxt), "%s: new master %d", __func__, node);
+	PSID_errlog(errtxt, 2);
+
+	declareMaster(node);
+    }
+
+    if (config->useMCast) return;
+
+    if (knowMaster() && getMasterID() == PSC_getMyID() && sendDeadnode) {
+	send_DEADNODE(id);
+    }
 }
 
 /* External function. @todo */
 int shutdownNode(int phase);
 
+/* Prototype forward declaration */
+static int send_MASTERIS(PSnodes_ID_t dest);
+static int send_ACTIVENODES(PSnodes_ID_t dest);
+
 void declareNodeAlive(PSnodes_ID_t id, int physCPUs, int virtCPUs)
 {
+    int wasUp = PSnodes_isUp(id);
+
     snprintf(errtxt, sizeof(errtxt), "%s: node %d", __func__, id);
     PSID_errlog(errtxt, 2);
 
@@ -280,7 +464,7 @@ void declareNodeAlive(PSnodes_ID_t id, int physCPUs, int virtCPUs)
 	return;
     }
 
-    if (PSnodes_isUp(id)) {
+    if (wasUp) {
 	totalCPUs += physCPUs - PSnodes_getPhysCPUs(id);
     } else {
 	totalNodes++;
@@ -298,7 +482,7 @@ void declareNodeAlive(PSnodes_ID_t id, int physCPUs, int virtCPUs)
 	    shutdownNode(1);
 	    return;
 	} else {
-	    declareNodeDead(id);
+	    declareNodeDead(id, 0);
 	}
     }
     if (totalCPUs > lic_numval(&config->licEnv, LIC_CPUs, 0)) {
@@ -308,8 +492,28 @@ void declareNodeAlive(PSnodes_ID_t id, int physCPUs, int virtCPUs)
 	    shutdownNode(1);
 	    return;
 	} else {
-	    declareNodeDead(id);
+	    declareNodeDead(id, 0);
 	}
+    }
+
+    if ((!knowMaster() && id != PSC_getMyID()) || id < getMasterID()) {
+	/* New node will be master */
+	PSnodes_ID_t oldMaster = knowMaster() ? getMasterID() : PSC_getMyID();
+
+	snprintf(errtxt, sizeof(errtxt), "%s: new master %d", __func__, id);
+	PSID_errlog(errtxt, 2);
+
+	declareMaster(id);
+
+	if (config->useMCast) return;
+
+	if (oldMaster != PSC_getMyID()) send_MASTERIS(oldMaster);
+    }
+
+    if (config->useMCast) return;
+
+    if (getMasterID() == PSC_getMyID() && !wasUp) {
+	send_ACTIVENODES(id);
     }
 }
 
@@ -324,6 +528,9 @@ int send_DAEMONCONNECT(PSnodes_ID_t id)
 	.buf = {'\0'} };
     int32_t *CPUs = (int32_t *)msg.buf;
 
+    snprintf(errtxt, sizeof(errtxt), "%s(%d)", __func__, id);
+    PSID_errlog(errtxt, 10);
+    
     CPUs[0] = PSnodes_getPhysCPUs(PSC_getMyID());
     CPUs[1] = PSnodes_getVirtCPUs(PSC_getMyID());
     msg.header.len += 2 * sizeof(*CPUs);
@@ -381,33 +588,23 @@ void msg_DAEMONESTABLISHED(DDBufferMsg_t *msg)
     send_OPTIONS(id);
 }
 
-
-int send_MASTER_IS(PSnodes_ID_t dest)
+int send_DAEMONSHUTDOWN(void)
 {
-    DDBufferMsg_t msg = {
-	.header = {
-	    .type = PSP_DD_MASTER_IS,
-	    .sender = PSC_getMyTID(),
-	    .dest = PSC_getTID(dest, 0),
-	    .len = sizeof(msg.header) },
-	.buf = {0} };
+    DDMsg_t msg = {
+	.type = PSP_DD_DAEMONSHUTDOWN,
+	.sender = PSC_getMyTID(),
+	.dest = 0,
+	.len = sizeof(msg) };
 
-    *(PSnodes_ID_t *)msg.buf = getMasterID();
-    return sendMsg(&msg);
+    return broadcastMsg(&msg);
 }
 
-void msg_MASTER_IS(DDBufferMsg_t *msg)
+void msg_DAEMONSHUTDOWN(DDMsg_t *msg)
 {
-    PSnodes_ID_t newMaster = *(PSnodes_ID_t *)msg->buf;
-
-    if (newMaster != getMasterID()) {
-	if (newMaster < getMasterID()) {
-	    /* @todo */
-	    /* Try to connect new master */
-	} else {
-	    send_MASTER_IS(PSC_getID(msg->header.sender));
-	}
-    }
+    snprintf(errtxt, sizeof(errtxt), "%s(%d)",
+	     __func__, PSC_getID(msg->sender));
+    PSID_errlog(errtxt, 10);
+    declareNodeDead(PSC_getID(msg->sender), 0);
 }
 
 void msg_LOAD(DDBufferMsg_t *msg)
@@ -416,22 +613,168 @@ void msg_LOAD(DDBufferMsg_t *msg)
     PSnodes_ID_t client = PSC_getID(msg->header.sender);
 
     if (PSC_getMyID() != getMasterID()) {
-	send_MASTER_IS(PSC_getID(msg->header.sender));
+	send_MASTERIS(PSC_getID(msg->header.sender));
     } else {
 	clientStat[client].jobs = *(PSID_Jobs_t *)ptr;
 	ptr += sizeof(PSID_Jobs_t);
 
 	clientStat[client].load = *(PSID_Load_t *)ptr;
 	ptr += sizeof(PSID_Load_t);
+
+	gettimeofday(&clientStat[client].lastPing, NULL);
+	clientStat[client].missCounter = 0;
     }
 }
 
-void msg_DEAD_NODE(DDBufferMsg_t *msg)
+/**
+ * @brief Send a PSP_DD_MASTER_IS message.
+ *
+ * Send a PSP_DD_MASTER_IS message to node @a dest.
+ *
+ * @param dest ParaStation ID of the node to send the message to.
+ *
+ * @return On success, the number of bytes sent within the
+ * PSP_DD_MASTER_IS message is returned. If an error occured, -1 is
+ * returned and errno is set appropriately.
+ */
+static int send_MASTERIS(PSnodes_ID_t dest)
+{
+    DDBufferMsg_t msg = {
+	.header = {
+	    .type = PSP_DD_MASTER_IS,
+	    .sender = PSC_getMyTID(),
+	    .dest = PSC_getTID(dest, 0),
+	    .len = sizeof(msg.header) },
+	.buf = {'\0'} };
+
+    *(PSnodes_ID_t *)msg.buf = getMasterID();
+    return sendMsg(&msg);
+}
+
+void msg_MASTERIS(DDBufferMsg_t *msg)
+{
+    PSnodes_ID_t newMaster = *(PSnodes_ID_t *)msg->buf;
+
+    if (newMaster != getMasterID()) {
+	if (newMaster < getMasterID()) {
+	    send_DAEMONCONNECT(newMaster);
+	} else {
+	    send_MASTERIS(PSC_getID(msg->header.sender));
+	}
+    }
+}
+
+/**
+ * @brief Send a PSP_DD_ACTIVE_NODES message.
+ *
+ * Send a PSP_DD_ACTIVE_NODES message to node @a dest.
+ *
+ * @param dest ParaStation ID of the node to send the message to.
+ *
+ * @return On success, the number of bytes sent within the
+ * PSP_DD_ACTIVE_NODES message is returned. If an error occured, -1 is
+ * returned and errno is set appropriately.
+ */
+static int send_ACTIVENODES(PSnodes_ID_t dest)
+{
+    DDBufferMsg_t msg = {
+	.header = {
+	    .type = PSP_DD_ACTIVE_NODES,
+	    .sender = PSC_getMyTID(),
+	    .dest = PSC_getTID(dest, 0),
+	    .len = sizeof(msg.header) },
+	.buf = {'\0'} };
+
+    PSnodes_ID_t node, *nodeBuf = (PSnodes_ID_t *)msg.buf;
+    unsigned int idx = 0;
+    int total = 0;
+
+    if (dest == PSC_getMyID()) return 0;
+
+    for (node=0; node<PSC_getNrOfNodes(); node++) {
+	if (PSnodes_isUp(node)) {
+	    nodeBuf[idx] = node;
+	    idx++;
+	    if (idx == NODES_CHUNK) {
+		int ret;
+		msg.header.len += idx * sizeof(PSnodes_ID_t);
+		ret = sendMsg(&msg);
+		if (ret<0) {
+		    return ret;
+		} else {
+		    total += ret;
+		}
+		msg.header.len -= idx * sizeof(PSnodes_ID_t);
+		idx=0;
+	    }
+	}
+    }
+    if (idx) {
+	int ret;
+	msg.header.len += idx * sizeof(PSnodes_ID_t);
+	ret = sendMsg(&msg);
+	if (ret<0) {
+	    return ret;
+	} else {
+	    total += ret;
+	}
+    }
+
+    return total;
+}
+
+void msg_ACTIVENODES(DDBufferMsg_t *msg)
+{
+    int num = (msg->header.len - sizeof(msg->header)) / sizeof(PSnodes_ID_t);
+    PSnodes_ID_t *nodeBuf = (PSnodes_ID_t *)msg->buf;
+    int idx;
+
+    snprintf(errtxt, sizeof(errtxt), "%s: num is %d", __func__, num);
+    PSID_errlog(errtxt, 2);
+
+    for (idx=0; idx<num; idx++) {
+	snprintf(errtxt, sizeof(errtxt), "%s: %d. is %d",
+		 __func__, idx, nodeBuf[idx]);
+	PSID_errlog(errtxt, 10);
+	if (!PSnodes_isUp(nodeBuf[idx])) send_DAEMONCONNECT(nodeBuf[idx]);
+    }
+}
+
+/**
+ * @brief Broadcast a PSP_DD_DEAD_NODE message.
+ *
+ * Broadcast a PSP_DD_DEAD_NODE message to all active nodes.
+ *
+ * @return On success, the number of nodes the PSP_DD_DEAD_NODE
+ * message is sent to is returned, i.e. the value returned by the @ref
+ * broadcastMsg() call. If an error occured, -1 is returned and errno
+ * is set appropriately.
+ */
+static int send_DEADNODE(PSnodes_ID_t deadnode)
+{
+    DDBufferMsg_t msg = {
+	.header = {
+	    .type = PSP_DD_DEAD_NODE,
+	    .sender = PSC_getMyTID(),
+	    .dest = 0,
+	    .len = sizeof(msg.header) },
+	.buf = {'\0'} };
+    *(PSnodes_ID_t *)msg.buf = deadnode;
+
+    msg.header.len += sizeof(PSnodes_ID_t);
+
+    snprintf(errtxt, sizeof(errtxt), "%s: node %d", __func__, deadnode);
+    PSID_errlog(errtxt, 2);
+
+    return broadcastMsg(&msg);
+}
+
+void msg_DEADNODE(DDBufferMsg_t *msg)
 {
     PSnodes_ID_t deadNode = *(PSnodes_ID_t *)msg->buf;
 
-    send_MASTER_IS(deadNode);
-}
+    snprintf(errtxt, sizeof(errtxt), "%s(%d)", __func__, deadNode);
+    PSID_errlog(errtxt, 10);
 
-void msg_ACTIVE_NODES(DDBufferMsg_t *msg)
-{}
+    send_DAEMONCONNECT(deadNode);
+}
