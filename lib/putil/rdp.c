ParaStation/psmgmt

commit 83c7a3d06af00824ebc2363d3d4802a9ab3185c7

Bugfix: Really activate RDP's close timeout. Bugfix: Try to fix possible race-condition that might lead to annoying 'doACK:     strange things happen' messages
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7574 34291a86-62e8-0310-8159-f060f8a2ee4e

lib/putil/rdp.c
@@ -1418,11 +1418,11 @@ static int resequenceMsgQ(int node, int newExpected, int newSend)
 
     callback = !cp->window;
 
+    Timer_block(timerID, 1);
+
     cp->frameExpected = newExpected;     /* Accept initial seqno */
     cp->frameToSend = newSend;
     cp->ackExpected = newSend;
-
-    Timer_block(timerID, 1);
 
     list_for_each_safe(m, tmp, &cp->pendList) {
 	msgbuf_t *mp = list_entry(m, msgbuf_t, next);
@@ -2383,7 +2383,7 @@ void closeConnRDP(int node)
 
     closeConnection(node, 0, 1);
     gettimeofday(&tv, NULL);
-    timeradd(&tv, &CLOSED_TIMEOUT, &conntable[node].tmout);
+    timeradd(&tv, &CLOSED_TIMEOUT, &conntable[node].close);
 }
 
 int RDP_blockTimer(int block)

Makefile.version
@@ -1,2 +1,2 @@
 VERSION_$(PKG_NAME)=5.0.15
-RELEASE_$(PKG_NAME)=2
+RELEASE_$(PKG_NAME)=3

commit 8814672b49b3d56e6b4d981c30ba2e6a0b1b597e

Fix typo introduced in r7574.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7575 34291a86-62e8-0310-8159-f060f8a2ee4e

lib/putil/rdp.c
@@ -2383,7 +2383,7 @@ void closeConnRDP(int node)
 
     closeConnection(node, 0, 1);
     gettimeofday(&tv, NULL);
-    timeradd(&tv, &CLOSED_TIMEOUT, &conntable[node].close);
+    timeradd(&tv, &CLOSED_TIMEOUT, &conntable[node].closed);
 }
 
 int RDP_blockTimer(int block)