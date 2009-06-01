ParaStation/psmgmt

commit d0fe36ecc38fad070aa7fb031c0dd3129097a2a4

Enhancement: Propagate the setting of 'ulimit -c' to parallel jobs. This     should fix #206.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7602 34291a86-62e8-0310-8159-f060f8a2ee4e

lib/psi/psi.c
@@ -24,6 +24,7 @@ static char vcid[] __attribute__((used)) =
 #include <sys/un.h>
 #include <netinet/in.h>
 #include <sys/stat.h>
+#include <sys/resource.h>
 
 #include "pscommon.h"
 
@@ -709,7 +710,8 @@ void PSI_execLogger(const char *command)
 void PSI_propEnv(void)
 {
     extern char **environ;
-    char *envStr;
+    struct rlimit rlim;
+    char *envStr, valStr[64];
     int i;
 
     /* Propagate some environment variables */
@@ -743,6 +745,14 @@ void PSI_propEnv(void)
     if ((envStr = getenv("__PSI_NO_MEMBIND"))) {
 	setPSIEnv("__PSI_NO_MEMBIND", envStr, 1);
     }
+
+    getrlimit(RLIMIT_CORE, &rlim);
+    if (rlim.rlim_cur == RLIM_INFINITY) {
+	snprintf(valStr, sizeof(valStr), "infinity");
+    } else {
+	snprintf(valStr, sizeof(valStr), "%lx", rlim.rlim_cur);
+    }
+    setPSIEnv("__PSI_CORESIZE", envStr, 1);
 
     /* export all PSP_* vars to the ParaStation environment */
     for (i=0; environ[i]; i++) {

Makefile.version
@@ -1,2 +1,2 @@
 VERSION_$(PKG_NAME)=5.0.15
-RELEASE_$(PKG_NAME)=8
+RELEASE_$(PKG_NAME)=9

bin/daemon/psidspawn.c
@@ -555,7 +555,7 @@ static void execClient(PStask_t *task)
     /* logging is done via the forwarder thru stderr! */
     struct stat sb;
     int execFound = 0;
-    char *executable;
+    char *executable, *envStr;
 
     /* change the gid */
     if (setgid(task->gid)<0) {
@@ -607,6 +607,22 @@ static void execClient(PStask_t *task)
     if (setuid(task->uid)<0) {
 	fprintf(stderr, "%s: setuid: %s\n", __func__, get_strerror(errno));
 	exit(0);
+    }
+
+    /* restore core settings */
+    envStr = getenv("__PSI_CORESIZE");
+    if (envStr) {
+	struct rlimit rlim;
+	getrlimit(RLIMIT_CORE, &rlim);
+	if (!strcmp("infinity", envStr)) {
+	    rlim.rlim_cur = rlim.rlim_max;
+	} else {
+	    int ret = sscanf(envStr, "%lx", &rlim.rlim_cur);
+	    if (ret < 1) rlim.rlim_cur = 0;
+	    rlim.rlim_cur =
+		(rlim.rlim_max > rlim.rlim_cur) ? rlim.rlim_cur : rlim.rlim_max;
+	}
+	setrlimit(RLIMIT_CORE, &rlim);
     }
 
     /* change to the appropriate directory */

commit 2b0310a2bee5d18fc775d4d089f031344076ac65

Bugfix: Fix SEGFAULT introduced in r7602.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7604 34291a86-62e8-0310-8159-f060f8a2ee4e

lib/psi/psi.c
@@ -752,7 +752,7 @@ void PSI_propEnv(void)
     } else {
 	snprintf(valStr, sizeof(valStr), "%lx", rlim.rlim_cur);
     }
-    setPSIEnv("__PSI_CORESIZE", envStr, 1);
+    setPSIEnv("__PSI_CORESIZE", valStr, 1);
 
     /* export all PSP_* vars to the ParaStation environment */
     for (i=0; environ[i]; i++) {