ParaStation/psmgmt

commit bfd3ea57e5fd7bb67244613703785f1d282790c8

Enhancement: Implement daisy-chain broadcasts for PMI-broadcasts.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7613 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/logger/psilogger.c
@@ -89,7 +89,7 @@ int forw_verbose = 0;
 static int showUsage = 0;
 
 /** Number of maximum connected forwarders during runtime */
-int maxConnected;
+int maxConnected = 0;
 
 /** Set of fds, the logger listens to. This is mainly STDIN and daemonSock. */
 static fd_set myfds;
@@ -586,6 +586,48 @@ static void enterRawMode(void)
 }
 
 /**
+ * @brief Try to send answer to INITIALIZE message
+ *
+ * Try to send an answer to an INITIALIZE message to rank @a
+ * rank. This type of send is used in order to enable the daisy-chain
+ * broadcasts. Therefore, it is tested, if the both, predecessing and
+ * successing rank are already known. If one neighbor is still
+ * unknown, no message is send. Otherwise, an extended INITIALIZE
+ * answer is delivered containing information on the neighboring
+ * processes.
+ *
+ * @param rank Rank of the client the response is send to
+ *
+ * @return No return value.
+ */
+static void trySendInitAnswer(int rank)
+{
+    PStask_ID_t task, pred, succ;
+
+    if (rank < 0 || rank >= getNumKvsClients()) return;
+
+    pred = (rank) ? getClientTID(rank-1) : PSC_getMyTID();
+    task = getClientTID(rank);
+    succ = (rank == getNumKvsClients()-1) ? PSC_getMyTID():getClientTID(rank+1);
+
+    /* Check, if current request can be answered */
+    if (pred != -1 && succ != -1) {
+	char buf[sizeof(forw_verbose) + 2*sizeof(PStask_ID_t)], *ptr = buf;
+
+	*(int *)ptr = forw_verbose;
+	ptr += sizeof(int);
+	*(PStask_ID_t *)ptr = pred;
+	ptr += sizeof(PStask_ID_t);
+	*(PStask_ID_t *)ptr = succ;
+	ptr += sizeof(PStask_ID_t);
+
+	sendMsg(task, INITIALIZE, buf, sizeof(buf));
+	PSIlog_log(PSILOG_LOG_VERB, "%s: send answer to %s (%d)\n", __func__,
+		   PSC_printTID(task), rank);
+    }
+}
+
+/**
  * @brief Handle connection requests from new forwarders.
  *
  * Handles connection requests from new forwarder. In order to
@@ -599,18 +641,42 @@ static void enterRawMode(void)
  */
 static int newrequest(PSLog_Msg_t *msg)
 {
-    int ret=0;
+    int rank = msg->sender;
+    static int triggerOld = 0, kvsConnected = 0;
 
-    if (registerClient(msg->sender, msg->header.sender)) {
+    if (!registerClient(rank, msg->header.sender)) return 0;
+
+    maxConnected++;
+    PSIlog_log(PSILOG_LOG_VERB, "new connection from %s (%d)\n",
+	       PSC_printTID(msg->header.sender), rank);
+
+    if (msg->version < 2) triggerOld = 1;
+
+    if (rank < 0 || !enable_kvs) {
+	/* not part of kvs, answer immediately */
 	sendMsg(msg->header.sender, INITIALIZE,
 		(char *) &forw_verbose, sizeof(forw_verbose));
-	maxConnected++;
-	PSIlog_log(PSILOG_LOG_VERB, "new connection from %s (%d)\n",
-		   PSC_printTID(msg->header.sender), msg->sender);
-	ret = 1;
+    } else {
+
+	if (rank-1 > 0) trySendInitAnswer(rank - 1);
+	if (rank > 0) trySendInitAnswer(rank);
+	trySendInitAnswer(msg->sender + 1);
+
+	kvsConnected++;
     }
 
-    return ret;
+    if (enable_kvs && kvsConnected == getNumKvsClients()) {
+	/* All clients there, answer to rank 0 */
+	if (triggerOld) {
+	    sendMsg(msg->header.sender, INITIALIZE,
+		    (char *) &forw_verbose, sizeof(forw_verbose));
+	} else {
+	    trySendInitAnswer(0);
+	    switchDaisyChain(1);
+	}
+    }
+
+    return 1;
 }
 
 /**
@@ -1241,7 +1307,7 @@ int main( int argc, char**argv)
 	PSIlog_log(PSILOG_LOG_VERB, "Going to show resource usage.\n");
     }
 
-    PSLog_init(daemonSock, -1, 1);
+    PSLog_init(daemonSock, -1, 2);
 
     /* init the timer structure */
     if (!Timer_isInitialized()) {

Makefile.version
@@ -1,2 +1,2 @@
 VERSION_$(PKG_NAME)=5.0.16
-RELEASE_$(PKG_NAME)=0
+RELEASE_$(PKG_NAME)=1

bin/daemon/psidforwarder.c
@@ -119,7 +119,7 @@ AccountData accData;
  * Timeout for connecting and releasing logger. Might be overruled via
  * __PSI_LOGGER_TIMEOUT environment
  */
-int loggerTimeout = 10;
+int loggerTimeout = 60;
 
 /**
  * @brief Close socket to daemon.
@@ -733,10 +733,25 @@ static int connectLogger(PStask_ID_t tid)
 	errno = ECONNREFUSED;
 	return -1;
     } else {
+	char *ptr = msg.buf;
+
 	loggerTID = tid;
-	verbose = *(int *) msg.buf;
-	PSID_log(PSID_LOG_SPAWN, "%s(%s): Connected\n",
-		 __func__, PSC_printTID(tid));
+	verbose = *(int *)ptr;
+	ptr += sizeof(int);
+	PSID_log(PSID_LOG_SPAWN, "%s(%s): Connected", __func__,
+		 PSC_printTID(tid));
+
+	if (msg.header.len >=
+	    PSLog_headerSize + (int)(sizeof(int)+2*sizeof(PStask_ID_t))) {
+	    PStask_ID_t pred, succ;
+	    pred = *(PStask_ID_t *) ptr;
+	    ptr += sizeof(PStask_ID_t);
+	    succ = *(PStask_ID_t *) ptr;
+	    ptr += sizeof(PStask_ID_t);
+
+	    pmi_set_pred(pred);
+	    pmi_set_succ(succ);
+	}
     }
 
     return 0;
@@ -975,7 +990,7 @@ static int readFromLogger(void)
 		exit(0);
 		break;
 	    case KVS:
-		pmi_handleKvsRet(msg);
+		pmi_handleKvsRet(&msg);
 		break;
 	    case WINCH:
 		/* Logger detected change in window-size */
@@ -1749,7 +1764,7 @@ void PSID_forwarder(PStask_t *task, int daemonfd, int PMISocket,
     signal(SIGCHLD, sighandler);
     signal(SIGUSR1, sighandler);
 
-    PSLog_init(daemonSock, childTask->rank, 1);
+    PSLog_init(daemonSock, childTask->rank, 2);
 
     /* Make stdin nonblocking for us */
     flags = fcntl(stdinSock, F_GETFL);

bin/daemon/psidpmiprotocol.c
@@ -66,13 +66,20 @@ int updateMsgCount;
 char kvs_name_tmp[KVSNAME_MAX];
 /** The socket which is connected to the pmi client */
 SOCKET pmisock;
-/** The logger Task ID of the current job */
-PStask_ID_t loggertid;
+
+/** The logger task ID of the current job */
+static PStask_ID_t loggertid;
+
+/** The predecessor task ID of the current job */
+static PStask_ID_t predtid = -1;
+
+/** The successor task ID of the current job */
+static PStask_ID_t succtid = -1;
 
 /**
- * @brief Send a PSP_CD_KVS messages.
+ * @brief Send KVS message to logger.
  *
- * Send a PSP_CD_KVS messages to the logger to manipulte the global kvs.
+ * Send a KVS messages to the logger to manipulate the global kvs.
  *
  * @param msgbuffer The buffer which contains the kvs msg to send to
  * the logger.
@@ -87,6 +94,29 @@ static void sendKvstoLogger(char *msgbuffer)
 			  __func__, rank, msgbuffer);
     }
     PSLog_write(loggertid, KVS, msgbuffer, strlen(msgbuffer) +1);
+}
+
+/**
+ * @brief Send KVS messages to successor
+ *
+ * Send a KVS messages to the successor, i.e. the process with the
+ * next rank. If the current process is the on with the highest rank,
+ * the message will be send to the logger in order to signal
+ * successful delivery to all clients.
+ *
+ * @param msgbuffer The buffer which contains the kvs msg to send to
+ * the sucessor.
+ *
+ * @return No return value.
+ */
+static void sendKvstoSucc(char *msgbuffer)
+{
+    if (debug_kvs) {
+	PSIDfwd_printMsgf(STDERR,
+			  "%s: rank %i: %s\n",
+			  __func__, rank, msgbuffer);
+    }
+    PSLog_write(succtid, KVS, msgbuffer, strlen(msgbuffer) +1);
 }
 
 /**
@@ -1046,6 +1076,16 @@ int pmi_init(int pmisocket, PStask_ID_t loggertaskid, int pRank)
     return 0;
 }
 
+void pmi_set_pred(PStask_ID_t pred)
+{
+    predtid = pred;
+}
+
+void pmi_set_succ(PStask_ID_t succ)
+{
+    succtid = succ;
+}
+
 /**
  * @brief Extract PMI command.
  *
@@ -1181,7 +1221,7 @@ void pmi_finalize(void)
 *
 * @return No return value.
 */
-void pmi_handleKvsRet(PSLog_Msg_t msg)
+void pmi_handleKvsRet(PSLog_Msg_t *msg)
 {
     char cmd[VALLEN_MAX], reply[PMIU_MAXLINE];
     char *nextvalue, *saveptr, *value, vname[KEYLEN_MAX], kvsname[KEYLEN_MAX];
@@ -1190,11 +1230,11 @@ void pmi_handleKvsRet(PSLog_Msg_t msg)
 
     if (debug_kvs) {
 	PSIDfwd_printMsgf(STDERR, "%s: rank %i: %s\n",
-			  __func__, rank, msg.buf);
+			  __func__, rank, msg->buf);
     }
 
     /* extract cmd from msg */
-    if (!pmi_extract_cmd(msg.buf, cmd, sizeof(cmd)) || strlen(cmd) < 2) {
+    if (!pmi_extract_cmd(msg->buf, cmd, sizeof(cmd)) || strlen(cmd) < 2) {
 	PSIDfwd_printMsgf(STDERR, "%s: rank %i: received invalid kvs msg"
 			  " from logger\n", __func__, rank);
 	critErr();
@@ -1211,7 +1251,12 @@ void pmi_handleKvsRet(PSLog_Msg_t msg)
 
     /* update kvs after barrier_in */
     if (!strcmp(cmd, "kvs_update_cache")) {
-	nextvalue = strtok_r(msg.buf, delimiters, &saveptr);
+	char *msgCopy = NULL;
+	if (msg->header.sender == predtid && succtid != -1) {
+	    msgCopy = strdup(msg->buf);
+	}
+
+	nextvalue = strtok_r(msg->buf, delimiters, &saveptr);
 	nextvalue = strtok_r(NULL, delimiters, &saveptr);
 	while (nextvalue != NULL) {
 	    /* extract next key/value pair */
@@ -1245,18 +1290,36 @@ void pmi_handleKvsRet(PSLog_Msg_t msg)
 	    nextvalue = strtok_r( NULL, delimiters, &saveptr);
 	}
 	updateMsgCount++;
+	if (msg->header.sender == predtid && succtid != -1
+	    && msgCopy) {
+	    sendKvstoSucc(msgCopy);
+	    free(msgCopy);
+	}
 	return;
     }
 
     /* cache update finished */
     if (!strcmp(cmd, "kvs_update_cache_finish")) {
-	snprintf(reply, sizeof(reply), "cmd=kvs_update_cache_result mc=%i\n",
-		 updateMsgCount);
+	if (msg->header.sender == predtid && succtid != -1
+	    && succtid != loggertid) {
+	    /* forward to next client */
+	    sendKvstoSucc(msg->buf);
+	}
+	/* acknowledge to logger */
+	snprintf(reply, sizeof(reply),
+		 "cmd=kvs_update_cache_result mc=%i\n", updateMsgCount);
 	sendKvstoLogger(reply);
+
 	updateMsgCount = 0;
 	return;
     }
+    if (!strcmp(cmd, "barrier_out")) {
+	if (msg->header.sender == predtid && succtid != -1) {
+	    /* forward to next client */
+	    sendKvstoSucc(msg->buf);
+	}
+    }
 
     /* Forward msg from logger to client */
-    PMI_send(msg.buf);
+    PMI_send(msg->buf);
 }

bin/daemon/psidpmiprotocol.h
@@ -44,18 +44,48 @@ int pmi_parse_msg(char *msg);
  *
  * @param loggertaskid The task id of the logger.
  *
- * @param rank The rank of the pmi client.
+ * @param pRank The rank of the pmi client.
  *
  * @return Returns 0 on success and 1 on errors.
  */
 int pmi_init(int pmisocket, PStask_ID_t loggertaskid, int pRank);
 
 /**
+ * @brief Set predecessor task.
+ *
+ * Set the task ID of the current task's predecessor to @a pred. This
+ * implicitely enables daisy-chain broadcasts within PMI, if the
+ * successor process is disclosed at the same time.
+ *
+ * @param pred The task ID of the predecessor to be set.
+ *
+ * @return No return value.
+ *
+ * @see pmi_set_succ()
+ */
+void pmi_set_pred(PStask_ID_t pred);
+
+/**
+ * @brief Set successor task.
+ *
+ * Set the task ID of the current task's successor to @a succ. This
+ * implicitely enables daisy-chain broadcasts within PMI, if the
+ * predecessor process is disclosed at the same time.
+ *
+ * @param pred The task ID of the successor to be set.
+ *
+ * @return No return value.
+ *
+ * @see pmi_set_pred()
+ */
+void pmi_set_succ(PStask_ID_t succ);
+
+/**
  * @brief Forward or handle a kvs msg from logger.
  *
  * @return No return value.
  */
-void pmi_handleKvsRet(PSLog_Msg_t msg);
+void pmi_handleKvsRet(PSLog_Msg_t *msg);
 
 /**
  * @brief Send finalize ack to the pmi client, this is called from the

bin/logger/psiloggerkvs.c
@@ -32,37 +32,40 @@ static char vcid[] __attribute__((used)) =
 #include "psiloggerkvs.h"
 
 /** Array to store the forwarder TIDs joined to kvs. */
-PStask_ID_t *clientKvsTID;
+static PStask_ID_t *clientKvsTID;
 
 /** Array to store the received TIDs for tracking the barrier/cache updates. */
-PStask_ID_t *clientKvsTrackTID;
+static PStask_ID_t *clientKvsTrackTID;
 
 /** The actual size of clientKvsTID */
-int maxKvsClients = 64;
+static int maxKvsClients = 64;
 
 /** Number of clients joined to kvs */
-int noKvsClients = 0;
+static int noKvsClients = 0;
 
 /** The number of received kvs barrier_in */
-int kvsBarrierInCount = 0;
+static int kvsBarrierInCount = 0;
 
 /** The number of received kvs cache updates */
-int kvsCacheUpdateCount = 0;
+static int kvsCacheUpdateCount = 0;
 
 /** The number of kvs update msgs sends */
-int kvsUpdateMsgCount = 0;
+static int kvsUpdateMsgCount = 0;
 
 /** Outputs kvs debug msg if set */
-int debug_kvs = 0;
+static int debug_kvs = 0;
 
 /** Set the timeout of the barrier */
-int barrierTimeout = 0;
+static int barrierTimeout = 0;
 
 /** Id of the barrier timer */
-int timerid = -1;
+static int timerid = -1;
 
 /** track if we need to distribute an update */
-int kvsChanged = 0;
+static int kvsChanged = 0;
+
+/** flag enabling the daisy-chain broadcast */
+static int useDaisyChain =0;
 
 /**
  * @brief Wrapper for send kvs messages.
@@ -159,6 +162,11 @@ void initLoggerKvs(void)
     }
 }
 
+void switchDaisyChain(int val)
+{
+    useDaisyChain = val;
+}
+
 /**
  * @brief Send kvs message to all clients.
  *
@@ -179,8 +187,17 @@ static void sendMsgToKvsClients(char *msg)
 	return;
     }
 
-    for (i=0; i< maxKvsClients; i++) {
-	if (clientKvsTID[i] != -1) sendKvsMsg(clientKvsTID[i], msg);
+    if (useDaisyChain) {
+	if (clientKvsTID[0] != -1) {
+	    sendKvsMsg(clientKvsTID[0], msg);
+	} else {
+	    PSIlog_log(-1, "%s: daisy-chain enabled but unusable\n", __func__);
+	    return;
+	}
+    } else {
+	for (i=0; i< maxKvsClients; i++) {
+	    if (clientKvsTID[i] != -1) sendKvsMsg(clientKvsTID[i], msg);
+	}
     }
 }
 
@@ -758,6 +775,11 @@ static void handleKvsLeave(PSLog_Msg_t *msg)
     } else {
 	noKvsClients--;
     }
+}
+
+int getNumKvsClients(void)
+{
+    return noKvsClients;
 }
 
 /**

bin/logger/psiloggerkvs.h
@@ -39,4 +39,26 @@ void handleKvsMsg(PSLog_Msg_t *msg);
  */
 void initLoggerKvs(void);
 
+/**
+ * @brief Switch daisy-chain mode.
+ *
+ * Set the broadcast messages within kvs to daisy-chain mode, if @a
+ * val is different from 0.
+ *
+ * @param val If 1, daisy-chain mode will be switched on. Otherwise it
+ * will be left off.
+ *
+ * @return No return value.
+ */
+void switchDaisyChain(int val);
+
+/**
+ * @brief Number of kvs clients.
+ *
+ * Get the number of kvs-clients currently expected.
+ *
+ * @return The number of kvs-clients to be expected.
+ */
+int getNumKvsClients(void);
+
 #endif

commit 35e324df6f89f479514d84a01327284181410804

Fix one bug introduced in r7613. Improved logging.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7614 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/logger/psilogger.c
@@ -611,7 +611,7 @@ static void trySendInitAnswer(int rank)
     succ = (rank == getNumKvsClients()-1) ? PSC_getMyTID():getClientTID(rank+1);
 
     /* Check, if current request can be answered */
-    if (pred != -1 && succ != -1) {
+    if (task != -1 && pred != -1 && succ != -1) {
 	char buf[sizeof(forw_verbose) + 2*sizeof(PStask_ID_t)], *ptr = buf;
 
 	*(int *)ptr = forw_verbose;
@@ -647,7 +647,7 @@ static int newrequest(PSLog_Msg_t *msg)
     if (!registerClient(rank, msg->header.sender)) return 0;
 
     maxConnected++;
-    PSIlog_log(PSILOG_LOG_VERB, "new connection from %s (%d)\n",
+    PSIlog_log(PSILOG_LOG_VERB, "%s: new connection from %s (%d)\n", __func__,
 	       PSC_printTID(msg->header.sender), rank);
 
     if (msg->version < 2) triggerOld = 1;

bin/logger/psiloggerclient.c
@@ -337,7 +337,7 @@ int registerClient(int rank, PStask_ID_t tid)
 	nRecvClnts++;
     }
 
-    PSIlog_log(PSILOG_LOG_VERB, "new connection from %s (%d)\n",
+    PSIlog_log(PSILOG_LOG_VERB, "%s: new connection from %s (%d)\n", __func__,
 	       PSC_printTID(tid), rank);
 
     return 1;

lib/psi/psi.c
@@ -447,7 +447,8 @@ int PSI_recvMsg(DDMsg_t *msg, size_t size)
 	if (count >= expected) {
 	    if (msg->len > (int)size) {
 		errno = ENOBUFS;
-		PSI_warn(-1, errno, "%s", __func__);
+		PSI_warn(-1, errno, "%s: type %s", __func__,
+			 PSP_printMsg(msg->type));
 		n = -1;
 	    } else {
 		expected = msg->len;