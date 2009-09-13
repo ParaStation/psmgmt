ParaStation/psmgmt

commit 0ce455a961098e83e4eb9cc94474cc89f341a6d6

Replace PStask_t's double-linked list by list_t.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7741 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/daemon/psidtask.c
@@ -2,7 +2,7 @@
  *               ParaStation
  *
  * Copyright (C) 2002-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2008 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2009 ParTec Cluster Competence Center GmbH, Munich
  *
  * $Id$
  *
@@ -27,7 +27,7 @@ static char vcid[] __attribute__((used)) =
 
 #include "psidtask.h"
 
-PStask_t *managedTasks = NULL;
+LIST_HEAD(managedTasks);
 
 static void printList(PStask_sig_t *list)
 {
@@ -54,7 +54,7 @@ void PSID_setSignal(PStask_sig_t **siglist, PStask_ID_t tid, int signal)
 
     PSID_log(PSID_LOG_SIGNAL, "%s(%s, %d)\n",
 	     __func__, PSC_printTID(tid), signal);
-    
+
     if (PSID_getDebugMask() & PSID_LOG_SIGDBG) {
 	PSID_log(PSID_LOG_SIGDBG, "%s: signals before (in %p):",
 		 __func__, siglist);
@@ -226,70 +226,45 @@ int PSID_getSignalByTID(PStask_sig_t **siglist, PStask_ID_t tid)
 
 /****************** TAKSLIST MANIPULATING ROUTINES **********************/
 
-void PStasklist_delete(PStask_t **list)
+int PStasklist_enqueue(list_t *list, PStask_t *task)
 {
-    PStask_t *task;
+    PSID_log(PSID_LOG_TASK, "%s(%p,%s(%p))\n", __func__,
+	     list, PSC_printTID(task->tid), task);
 
-    PSID_log(PSID_LOG_TASK,
-	     "%s(%p[%lx])\n", __func__, list, *list ? (long)*list : -1);
-
-    while (*list) {
-	task = (*list);
-	(*list) = (*list)->next;
-	PStask_delete(task);
-    }
-}
-
-int PStasklist_enqueue(PStask_t **list, PStask_t *task)
-{
-    PSID_log(PSID_LOG_TASK, "%s(%p[%p],%s(%p))\n", __func__,
-	     list, *list, PSC_printTID(task->tid), task);
-
-    task->prev=NULL;
-    task->next = *list;
-    if (*list) (*list)->prev = task;
-    *list = task;
+    list_add_tail(list, &task->next);
 
     return 0;
 }
 
-PStask_t *PStasklist_dequeue(PStask_t **list, PStask_ID_t tid)
+void PStasklist_dequeue(PStask_t *task)
 {
-    PStask_t *task;
+    PSID_log(PSID_LOG_TASK, "%s(%p, %s)\n", __func__, task,
+	     task ? PSC_printTID(task->tid) : "");
 
-    PSID_log(PSID_LOG_TASK, "%s(%p[%p], %s)\n", __func__,
-	     list, *list, PSC_printTID(tid));
-
-    /* Don't use PStasklist_find here since task *is* deleted */
-    for (task=*list; task && task->tid!=tid; task=task->next);
-
-    if (task) {
-	if (task->next) task->next->prev = task->prev;
-	if (task->prev) {
-	    /* found in the middle of the list */
-	    task->prev->next = task->next;
-	} else {
-	    /* the task was the head of the list */
-	    *list = task->next;
-	}
-    }
-
-    return task;
+    if (!task || list_empty(&task->next)) return;
+    list_del_init(&task->next);
 }
 
-PStask_t *PStasklist_find(PStask_t *list, PStask_ID_t tid)
+PStask_t *PStasklist_find(list_t *list, PStask_ID_t tid)
 {
-    PStask_t *task;
+    list_t *t;
+    PStask_t *task = NULL;
 
     PSID_log(PSID_LOG_TASK, "%s(%p, %s)", __func__, list, PSC_printTID(tid));
 
-    for (task=list; task && task->tid!=tid; task=task->next);
+    list_for_each(t, list) {
+	PStask_t *tt = list_entry(t, PStask_t, next);
+	if (tt->tid == tid) {
+	    task = tt;
+	    break;
+	}
+    }
 
     if (task && task->deleted) {
 	PSID_log(PSID_LOG_TASK, " found but deleted\n");
 	return NULL;
     }
-    
+
     PSID_log(PSID_LOG_TASK, " is at %p\n", task);
     return task;
 }
@@ -300,7 +275,7 @@ void PStask_cleanup(PStask_ID_t tid)
 
     PSID_log(PSID_LOG_TASK, "%s(%s)\n", __func__, PSC_printTID(tid));
 
-    task = PStasklist_find(managedTasks, tid);
+    task = PStasklist_find(&managedTasks, tid);
     if (!task) {
 	PSID_log(-1, "%s: %s not in my tasklist\n",
 		 __func__, PSC_printTID(tid));
@@ -328,7 +303,7 @@ void PStask_cleanup(PStask_ID_t tid)
 
 	/* Detach from forwarder */
 	if (task->forwardertid) {
-	    PStask_t *forwarder = PStasklist_find(managedTasks,
+	    PStask_t *forwarder = PStasklist_find(&managedTasks,
 						  task->forwardertid);
 	    if (forwarder) {
 		PSID_removeSignal(&forwarder->childs, task->tid, -1);
@@ -349,7 +324,7 @@ void PStask_cleanup(PStask_ID_t tid)
 	    int sig = -1;
 
 	    while ((childTID = PSID_getSignal(&task->childs, &sig))) {
-		PStask_t *child = PStasklist_find(managedTasks, childTID);
+		PStask_t *child = PStasklist_find(&managedTasks, childTID);
 
 		if (child && child->fd == -1) {
 		    PSID_log(-1, "%s: forwarder kills child %s\n",

Makefile.version
@@ -1,2 +1,2 @@
 VERSION_$(PKG_NAME)=5.0.19
-RELEASE_$(PKG_NAME)=1
+RELEASE_$(PKG_NAME)=2

bin/daemon/psid.c
@@ -247,7 +247,7 @@ static void sighandler(int sig)
 
 	    tid = PSC_getTID(-1, pid);
 
-	    task = PStasklist_find(managedTasks, tid);
+	    task = PStasklist_find(&managedTasks, tid);
 	    if (task) {
 		if (!task->killat) {
 		    task->killat = time(NULL) + 10;
@@ -442,20 +442,15 @@ static void checkFileTable(fd_set *controlfds)
  */
 void checkObstinate(void)
 {
-    PStask_t *task=managedTasks;
     time_t now = time(NULL);
+    list_t *t, *tmp;
 
-    while (task) {
-	PStask_t *next=task->next;
+    list_for_each_safe(t, tmp, &managedTasks) {
+	PStask_t *task = list_entry(t, PStask_t, next);
 
 	if (task->deleted) {
-	    PStask_t *t = PStasklist_dequeue(&managedTasks, task->tid);
-	    if (t != task) {
-		PSID_log(-1, "%s: wrong task dequeued: %p(%s) != %p\n",
-			 __func__, task, PSC_printTID(task->tid), t);
-	    } else {
-		PStask_delete(task);
-	    }
+	    PStasklist_dequeue(task);
+	    PStask_delete(task);
 	} else if (task->killat && now > task->killat) {
 	    int ret;
 	    if (task->group != TG_LOGGER) {
@@ -473,7 +468,6 @@ void checkObstinate(void)
 		}
 	    }
 	}
-	task = next;
     }
 
     cleanupSpawnTasks();

bin/daemon/psidclient.c
@@ -170,7 +170,7 @@ int flushClientMsgs(int fd)
 
 int sendClient(DDMsg_t *msg)
 {
-    PStask_t *task = PStasklist_find(managedTasks, msg->dest);
+    PStask_t *task = PStasklist_find(&managedTasks, msg->dest);
     int fd, sent = 0;
 
     if (PSID_getDebugMask() & PSID_LOG_CLIENT) {
@@ -330,7 +330,7 @@ void deleteClient(int fd)
 
     if (tid==-1) return;
 
-    task = PStasklist_find(managedTasks, tid);
+    task = PStasklist_find(&managedTasks, tid);
     if (!task) {
 	PSID_log(-1, "%s: Task %s not found\n", __func__, PSC_printTID(tid));
 	return;
@@ -352,7 +352,7 @@ void deleteClient(int fd)
 
     /* Deregister TG_(PSC)SPAWNER from parent process */
     if (task->group == TG_SPAWNER || task->group == TG_PSCSPAWNER) {
-	PStask_t *parent = PStasklist_find(managedTasks, task->ptid);
+	PStask_t *parent = PStasklist_find(&managedTasks, task->ptid);
 
 	if (parent) {
 	    /* Remove dead spawner from list of childs */
@@ -449,13 +449,14 @@ void deleteClient(int fd)
 
 int killAllClients(int sig, int killAdminTasks)
 {
-    PStask_t *task;
+    list_t *t;
     int ret = 0;
 
     PSID_log(PSID_LOG_CLIENT, "%s(%d, %d)\n", __func__, sig, killAdminTasks);
 
     /* loop over all tasks */
-    for (task=managedTasks; task; task=task->next) {
+    list_for_each(t, &managedTasks) {
+	PStask_t *task = list_entry(t, PStask_t, next);
 	pid_t pid = PSC_getPID(task->tid);
 
 	if (task->deleted) continue;
@@ -542,11 +543,11 @@ static void msg_CLIENTCONNECT(DDBufferMsg_t *bufmsg)
      * first check if it is a reconnection
      * this can happen due to a exec call.
      */
-    task = PStasklist_find(managedTasks, tid);
+    task = PStasklist_find(&managedTasks, tid);
     if (!task && msg->group != TG_SPAWNER && msg->group != TG_PSCSPAWNER) {
 	PStask_ID_t pgtid = PSC_getTID(-1, getpgid(pid));
 
-	task = PStasklist_find(managedTasks, pgtid);
+	task = PStasklist_find(&managedTasks, pgtid);
 
 	if (task && (task->group == TG_LOGGER || task->group == TG_ADMIN)) {
 	    /*
@@ -567,7 +568,7 @@ static void msg_CLIENTCONNECT(DDBufferMsg_t *bufmsg)
 		PStasklist_enqueue(&managedTasks, child);
 
 		if (task->forwardertid) {
-		    PStask_t *forwarder = PStasklist_find(managedTasks,
+		    PStask_t *forwarder = PStasklist_find(&managedTasks,
 							  task->forwardertid);
 		    if (forwarder) {
 			/* Register new child to its forwarder */
@@ -628,7 +629,7 @@ static void msg_CLIENTCONNECT(DDBufferMsg_t *bufmsg)
 
 	    ptid = PSC_getTID(-1, msg->ppid);
 
-	    parent = PStasklist_find(managedTasks, ptid);
+	    parent = PStasklist_find(&managedTasks, ptid);
 
 	    if (parent) {
 		/* register the child */

bin/daemon/psidcomm.c
@@ -422,7 +422,7 @@ int PSID_handleMsg(DDBufferMsg_t *msg)
  */
 static void msg_SENDSTOP(DDMsg_t *msg)
 {
-    PStask_t *task = PStasklist_find(managedTasks, msg->dest);
+    PStask_t *task = PStasklist_find(&managedTasks, msg->dest);
 
     if (!task) return;
 
@@ -453,7 +453,7 @@ static void msg_SENDSTOP(DDMsg_t *msg)
  */
 static void msg_SENDCONT(DDMsg_t *msg)
 {
-    PStask_t *task = PStasklist_find(managedTasks, msg->dest);
+    PStask_t *task = PStasklist_find(&managedTasks, msg->dest);
 
     if (!task) return;
 

bin/daemon/psidinfo.c
@@ -90,7 +90,7 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 
 	    if (PSC_getID(inmsg->header.sender) == PSC_getMyID()) {
 		/* Test for correct protocol version */
-		PStask_t *requester = PStasklist_find(managedTasks,
+		PStask_t *requester = PStasklist_find(&managedTasks,
 						      inmsg->header.sender);
 		if (!requester) {
 		    PSID_log(-1, "%s: requester %s not found\n",
@@ -151,7 +151,7 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 	case PSP_INFO_LIST_TASKS:
 	    if (PSC_getPID(inmsg->header.dest)) {
 		/* request info for a special task */
-		PStask_t *task = PStasklist_find(managedTasks,
+		PStask_t *task = PStasklist_find(&managedTasks,
 						 inmsg->header.dest);
 		if (task) {
 		    PSP_taskInfo_t *taskinfo = (PSP_taskInfo_t *)msg.buf;
@@ -169,8 +169,9 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 		}
 	    } else {
 		/* request info for all tasks */
-		PStask_t *task;
-		for (task=managedTasks; task; task=task->next) {
+		list_t *t;
+		list_for_each(t, &managedTasks) {
+		    PStask_t *task = list_entry(t, PStask_t, next);
 		    PSP_taskInfo_t *taskinfo = (PSP_taskInfo_t *)msg.buf;
 		    if (task->deleted) continue;
 		    taskinfo->tid = task->tid;
@@ -197,8 +198,9 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 	{
 	    /* request info for all normal tasks */
 	    PSP_taskInfo_t *taskinfo = (PSP_taskInfo_t *)msg.buf;
-	    PStask_t *task;
-	    for (task=managedTasks; task; task=task->next) {
+	    list_t *t;
+	    list_for_each(t, &managedTasks) {
+		PStask_t *task = list_entry(t, PStask_t, next);
 		if (task->deleted) continue;
 		if ((PSP_Info_t) inmsg->type == PSP_INFO_LIST_NORMTASKS && (
 			task->group == TG_FORWARDER
@@ -277,7 +279,7 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 	    }
 
 	    if (PSC_getID(inmsg->header.sender) == PSC_getMyID()) {
-		req = PStasklist_find(managedTasks, inmsg->header.sender);
+		req = PStasklist_find(&managedTasks, inmsg->header.sender);
 
 		if (!req) {
 		    PSID_log(-1, "%s: requester %s not found\n",
@@ -378,7 +380,7 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 	{
 	    PStask_ID_t tid = PSC_getPID(inmsg->header.dest) ?
 		inmsg->header.dest : inmsg->header.sender;
-	    PStask_t *task = PStasklist_find(managedTasks, tid);
+	    PStask_t *task = PStasklist_find(&managedTasks, tid);
 	    if (task) {
 		if (task->ptid) {
 		    msg.header.type = inmsg->header.type;
@@ -424,7 +426,7 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 	{
 	    PStask_ID_t tid = PSC_getPID(inmsg->header.dest) ?
 		inmsg->header.dest : inmsg->header.sender;
-	    PStask_t *task=PStasklist_find(managedTasks, tid);
+	    PStask_t *task=PStasklist_find(&managedTasks, tid);
 
 	    if (!task) {
 		PSID_log(-1, "%s: task %s not found\n",
@@ -470,7 +472,7 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 	    PSnodes_ID_t node;
 
 	    if (PSC_getID(inmsg->header.sender) == PSC_getMyID()) {
-		requester = PStasklist_find(managedTasks,
+		requester = PStasklist_find(&managedTasks,
 					    inmsg->header.sender);
 
 		if (!requester) {
@@ -575,7 +577,7 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 	{
 	    PStask_ID_t target = PSC_getPID(inmsg->header.dest) ?
 		inmsg->header.dest : inmsg->header.sender;
-	    PStask_t *task = PStasklist_find(managedTasks, target);
+	    PStask_t *task = PStasklist_find(&managedTasks, target);
 
 	    if (!task) {
 		PSID_log(-1, "%s: task %s not found\n",
@@ -616,7 +618,7 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 	}
 	case PSP_INFO_CMDLINE:
 	{
-	    PStask_t *task = PStasklist_find(managedTasks, inmsg->header.dest);
+	    PStask_t *task = PStasklist_find(&managedTasks, inmsg->header.dest);
 	    if (task) {
 		int i;
 		for (i=0; i<task->argc; i++) {
@@ -640,8 +642,9 @@ static void msg_INFOREQUEST(DDTypedBufferMsg_t *inmsg)
 	{
 	    /* request info for all normal tasks */
 	    PSP_taskInfo_t *taskinfo = (PSP_taskInfo_t *)msg.buf;
-	    PStask_t *task;
-	    for (task=managedTasks; task; task=task->next) {
+	    list_t *t;
+	    list_for_each(t, &managedTasks) {
+		PStask_t *task = list_entry(t, PStask_t, next);
 		if (task->deleted) continue;
 		if ((PSP_Info_t) inmsg->type == PSP_INFO_QUEUE_NORMTASK && (
 			task->group == TG_FORWARDER

bin/daemon/psidpartition.c
@@ -1960,7 +1960,7 @@ static void sendAcctQueueMsg(PStask_t *task)
  */
 static void msg_CREATEPART(DDBufferMsg_t *inmsg)
 {
-    PStask_t *task = PStasklist_find(managedTasks, inmsg->header.sender);
+    PStask_t *task = PStasklist_find(&managedTasks, inmsg->header.sender);
 
     if (!task || (task && task->ptid)) {
 	PSID_log(-1, "%s: task %s not root process\n",
@@ -2154,7 +2154,7 @@ static void appendToNodelist(char *buf, PSpart_request_t *request)
  */
 static void msg_CREATEPARTNL(DDBufferMsg_t *inmsg)
 {
-    PStask_t *task = PStasklist_find(managedTasks, inmsg->header.sender);
+    PStask_t *task = PStasklist_find(&managedTasks, inmsg->header.sender);
 
     if (!task || (task && task->ptid)) {
 	PSID_log(-1, "%s: task %s not root process\n",
@@ -2341,7 +2341,7 @@ static void appendToSlotlist(DDBufferMsg_t *inmsg, PSpart_request_t *request)
  */
 static void msg_PROVIDEPART(DDBufferMsg_t *inmsg)
 {
-    PStask_t *task = PStasklist_find(managedTasks, inmsg->header.dest);
+    PStask_t *task = PStasklist_find(&managedTasks, inmsg->header.dest);
     PSpart_request_t *req;
     char *ptr = inmsg->buf;
 
@@ -2425,7 +2425,7 @@ static void msg_PROVIDEPART(DDBufferMsg_t *inmsg)
  */
 static void msg_PROVIDEPARTSL(DDBufferMsg_t *inmsg)
 {
-    PStask_t *task = PStasklist_find(managedTasks, inmsg->header.dest);
+    PStask_t *task = PStasklist_find(&managedTasks, inmsg->header.dest);
     PSpart_request_t *req;
 
     PSID_log(PSID_LOG_PART, "%s(%s)\n", __func__,
@@ -2513,7 +2513,7 @@ static void msg_GETNODES(DDBufferMsg_t *inmsg)
 {
     PStask_ID_t target = PSC_getPID(inmsg->header.dest) ?
 	inmsg->header.dest : inmsg->header.sender;
-    PStask_t *task = PStasklist_find(managedTasks, target);
+    PStask_t *task = PStasklist_find(&managedTasks, target);
     char *ptr = inmsg->buf;
     unsigned int num;
 
@@ -2639,7 +2639,7 @@ static void msg_GETRANKNODE(DDBufferMsg_t *inmsg)
 {
     PStask_ID_t target = PSC_getPID(inmsg->header.dest) ?
 	inmsg->header.dest : inmsg->header.sender;
-    PStask_t *task = PStasklist_find(managedTasks, target);
+    PStask_t *task = PStasklist_find(&managedTasks, target);
     char *ptr = inmsg->buf;
     int rank;
 
@@ -2746,7 +2746,7 @@ static void msg_NODESRES(DDBufferMsg_t *inmsg)
     if (PSC_getID(inmsg->header.dest) == PSC_getMyID()) {
 	int DaemonPSPver =
 	    PSIDnodes_getDaemonProtoVersion(PSC_getID(inmsg->header.sender));
-	PStask_t *task = PStasklist_find(managedTasks, inmsg->header.dest);
+	PStask_t *task = PStasklist_find(&managedTasks, inmsg->header.dest);
 	char *ptr = inmsg->buf;
 	int num = inmsg->header.len - sizeof(inmsg->header) - sizeof(int32_t);
 	int nextRank = *(int32_t *)ptr, requested = 0;
@@ -2857,9 +2857,10 @@ int send_GETTASKS(PSnodes_ID_t node)
 
 static void sendRequests(void)
 {
-    PStask_t *task;
+    list_t *t;
 
-    for (task=managedTasks; task; task=task->next) {
+    list_for_each(t, &managedTasks) {
+	PStask_t *task = list_entry(t, PStask_t, next);
 	if (task->deleted) continue;
 	if (task->request) {
 	    DDBufferMsg_t msg = {
@@ -2890,7 +2891,6 @@ static void sendRequests(void)
 
 static void sendExistingPartitions(PStask_ID_t dest)
 {
-    PStask_t *task;
     DDBufferMsg_t msg = {
 	.header = {
 	    .type = PSP_DD_PROVIDETASK,
@@ -2898,8 +2898,10 @@ static void sendExistingPartitions(PStask_ID_t dest)
 	    .dest = dest,
 	    .len = sizeof(msg.header) },
 	.buf = { '\0' }};
+    list_t *t;
 
-    for (task=managedTasks; task; task=task->next) {
+    list_for_each(t, &managedTasks) {
+	PStask_t *task = list_entry(t, PStask_t, next);
 	if (task->deleted) continue;
 	if (task->partition && task->partitionSize) {
 	    char *ptr = msg.buf;

bin/daemon/psidplugin.c
@@ -614,7 +614,7 @@ void PSID_sendPluginLists(PStask_ID_t dest)
  */
 static void msg_PLUGIN(DDTypedBufferMsg_t *inmsg)
 {
-    PStask_t *task = PStasklist_find(managedTasks, inmsg->header.sender);
+    PStask_t *task = PStasklist_find(&managedTasks, inmsg->header.sender);
     int destID = PSC_getID(inmsg->header.dest), ret = 0;
 
     PSID_log(PSID_LOG_PLUGIN, "%s(%s, %s)\n", __func__,

commit 0eaf443fd586403146ce725e560552982770139b

Bugfix: Fix bug introduced in r7741 by calling list_add_tail with wrong     order of arguments.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7787 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/daemon/psidtask.c
@@ -203,7 +203,7 @@ int PStasklist_enqueue(list_t *list, PStask_t *task)
     PSID_log(PSID_LOG_TASK, "%s(%p,%s(%p))\n", __func__,
 	     list, PSC_printTID(task->tid), task);
 
-    list_add_tail(list, &task->next);
+    list_add_tail(&task->next, list);
 
     return 0;
 }