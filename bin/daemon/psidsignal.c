ParaStation/psmgmt

commit a9c58f307d065c1b62ad9a04c003fc15697f1012

Bugfix: Prevent double free() on forwarder's child-list.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7794 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/daemon/psidsignal.c
@@ -62,15 +62,13 @@ int PSID_kill(pid_t pid, int sig, uid_t uid)
 	    if (!forwarder) {
 		PSID_log(PSID_LOG_SIGNAL, "%s: forwarder %s not found\n",
 			 __func__, PSC_printTID(child->forwardertid));
-	    } else {
-		/* Send signal to forwarder */
+	    } else if (forwarder->tid != -1) {
+		/* Send signal via forwarder */
 		PSLog_Msg_t msg;
 		char *ptr = msg.buf;
 
 		/* Make sure to listen to the forwarder */
-		if (forwarder->fd != -1) {
-		    FD_SET(forwarder->fd, &PSID_readfds);
-		}
+		FD_SET(forwarder->fd, &PSID_readfds);
 
 		msg.header.type = PSP_CC_MSG;
 		msg.header.sender = PSC_getMyTID();

bin/daemon/psidtask.c
@@ -298,6 +298,13 @@ void PStask_cleanup(PStask_ID_t tid)
 		PStask_sig_t *sig = list_entry(s, PStask_sig_t, next);
 		PStask_t *child = PStasklist_find(&managedTasks, sig->tid);
 
+		/* Remove from list before PSID_kill(). Might get
+		 * removed therein, too. Save to remove here since
+		 * each forwarder has just a single child, i.e. this
+		 * list is empty afterwards. */
+		list_del(&sig->next);
+		free(sig);
+
 		if (child && child->fd == -1) {
 		    PSID_log(-1, "%s: forwarder kills child %s\n",
 			     __func__, PSC_printTID(child->tid));
@@ -305,9 +312,6 @@ void PStask_cleanup(PStask_ID_t tid)
 		    PSID_kill(-PSC_getPID(child->tid), SIGKILL, child->uid);
 		    PStask_cleanup(child->tid);
 		}
-
-		list_del(&sig->next);
-		free(sig);
 	    }
 	}
 	task->removeIt = 1;

commit 744ed401ead9f3c024c8c959e0a74839ac0ca3da

Fix typo introduced in r7794.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7795 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/daemon/psidsignal.c
@@ -62,7 +62,7 @@ int PSID_kill(pid_t pid, int sig, uid_t uid)
 	    if (!forwarder) {
 		PSID_log(PSID_LOG_SIGNAL, "%s: forwarder %s not found\n",
 			 __func__, PSC_printTID(child->forwardertid));
-	    } else if (forwarder->tid != -1) {
+	    } else if (forwarder->fd != -1) {
 		/* Send signal via forwarder */
 		PSLog_Msg_t msg;
 		char *ptr = msg.buf;
@@ -296,7 +296,7 @@ void PSID_sendAllSignals(PStask_t *task)
 void PSID_sendSignalsToRelatives(PStask_t *task)
 {
     list_t *s;
-    
+
     if (task->ptid) {
 	PSID_sendSignal(task->ptid, task->uid, task->tid, -1, 0, 0);
 	PSID_log(PSID_LOG_SIGNAL, "%s(%s)", __func__, PSC_printTID(task->tid));