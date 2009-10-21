ParaStation/psmgmt

commit 3cb1e36db1754805b004e4d3982ec94a2bf9f088

Enhancement: Implement various script-hooks:  - script called during startup (#378)  - script called on master-node, if nodes goes down (#340)  - script called on master-node, if nodes comes up (extension of #340) Scripts can only be configured during startup, i.e. in the config-file Scripts configured can be requested via node list-directives in     psiadmin.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7875 34291a86-62e8-0310-8159-f060f8a2ee4e

Makefile.version
@@ -1,2 +1,2 @@
 VERSION_$(PKG_NAME)=5.0.22
-RELEASE_$(PKG_NAME)=1
+RELEASE_$(PKG_NAME)=2

bin/admin/commands.c
@@ -454,10 +454,46 @@ void PSIADM_StarttimeStat(char *nl)
 	    int err;
 
 	    printf("%4d\t", node);
-	    err = PSI_infoInt64(node, PSP_INFO_STARTTIME, NULL,	&secs, 0);
+	    err = PSI_infoInt64(node, PSP_INFO_STARTTIME, NULL, &secs, 0);
 	    if (!err) {
 		time_t startTime = (time_t) secs;
 		printf("%s", ctime(&startTime));
+	    }
+	} else {
+	    printf("%4d\tdown\n", node);
+	}
+    }
+}
+
+void PSIADM_ScriptStat(PSP_Info_t type, char *nl)
+{
+    PSnodes_ID_t node;
+    char scriptName[1500];
+    size_t scriptWidth = getWidth()-8;
+
+    if (! getHostStatus()) return;
+
+    printf("%4s\t%s\n", "Node", "Script");
+    for (node=0; node<PSC_getNrOfNodes(); node++) {
+	if (nl && !nl[node]) continue;
+
+	if (hostStatus.list[node]) {
+	    int err;
+
+	    printf("%4d\t", node);
+	    err = PSI_infoString(node, type, NULL, scriptName,
+				 sizeof(scriptName), 1);
+	    if (!err) {
+		size_t len = strlen(scriptName);
+		if (len) {
+		    if (len <= scriptWidth) {
+			printf("%s\n", scriptName);
+		    } else {
+			printf("...%s\n", scriptName+len-(scriptWidth-3));
+		    }
+		} else {
+		    printf("<none>\n");
+		}
 	    }
 	} else {
 	    printf("%4d\tdown\n", node);

bin/daemon/psidstatus.c
@@ -14,8 +14,11 @@ static char vcid[] __attribute__((used)) =
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
 
 /* Extra includes for load-determination */
 #ifdef __linux__
@@ -31,6 +34,7 @@ static char vcid[] __attribute__((used)) =
 #include "mcast.h"
 #include "rdp.h"
 #include "timer.h"
+#include "selector.h"
 
 #include "psidutil.h"
 #include "psidnodes.h"
@@ -44,6 +48,7 @@ static char vcid[] __attribute__((used)) =
 #include "psidaccount.h"
 #include "psidstate.h"
 #include "psidspawn.h"
+#include "psidscripts.h"
 
 #include "psidstatus.h"
 
@@ -508,6 +513,91 @@ void releaseStatusTimer(void)
 /* Prototype forward declaration. */
 static int send_DEADNODE(PSnodes_ID_t deadnode);
 
+typedef struct {
+    int id;
+    char *script;
+} stateChangeInfo_t;
+
+static int stateChangeEnv(void *info)
+{
+    int nID = -1;
+    in_addr_t nAddr;
+    char *nName;
+    char val[16];
+
+    if (info) {
+	stateChangeInfo_t *i = info;
+	nID = i->id;
+    }
+    if (nID < 0 || nID >= PSC_getNrOfNodes()) nID = -1;
+
+    snprintf(val, sizeof(val), "%d", nID);
+    setenv("NODE_ID", val, 1);
+
+    if (nID < 0) return 0;
+
+    /* identify and set hostname */
+    nAddr = PSIDnodes_getAddr(nID);
+    if (nAddr == INADDR_ANY) {
+	nName = "<unknown>";
+    } else {
+	struct hostent *hp = gethostbyaddr(&nAddr, sizeof(nAddr), AF_INET);
+
+	if (!hp) {
+	    nName = "<unknown>";
+	} else {
+	    nName = hp->h_name;
+	}
+    }
+    setenv("NODE_NAME", nName, 1);
+
+    return 0;
+}
+
+static int stateChangeCB(int fd, PSID_scriptCBInfo_t *cbInfo)
+{
+    int nID = -1, result = -13, iofd = -1;
+    char *sName = "<unknown>";
+
+    if (!cbInfo) {
+	PSID_log(-1, "%s: No extra info\n", __func__);
+    } else {
+	if (cbInfo->info) {
+	    stateChangeInfo_t *i = cbInfo->info;
+	    nID = i->id;
+	    sName = i->script;
+	    free(cbInfo->info);
+	}
+	iofd = cbInfo->iofd;
+	free(cbInfo);
+    }
+
+    PSID_readall(fd, &result, sizeof(result));
+    close(fd);
+    if (result) {
+	char line[128] = { '\0' };
+	if (iofd > -1) {
+	    int num = PSID_readall(iofd, line, sizeof(line));
+	    int eno = errno;
+	    close(iofd); /* Discard further output */
+	    if (num < 0) {
+		PSID_warn(-1, eno, "%s: read(iofd)", __func__);
+		line[0] = '\0';
+	    } else if (num == sizeof(line)) {
+		strcpy(&line[sizeof(line)-4], "...");
+	    } else {
+		line[num]='\0';
+	    }
+	}
+	PSID_log(-1, "%s: script '%s' returned %d: '%s'\n", __func__,
+		 sName, result, line);
+    }
+
+    Selector_remove(fd);
+
+    return 0;
+}
+
 void declareNodeDead(PSnodes_ID_t id, int sendDeadnode, int silent)
 {
     list_t *t;
@@ -574,7 +664,18 @@ void declareNodeDead(PSnodes_ID_t id, int sendDeadnode, int silent)
 
 	declareMaster(node);
     } else if (PSC_getMyID() == getMasterID()) {
+	int *idInfo = malloc(sizeof(*idInfo));
+	if (!idInfo) {
+	    PSID_warn(-1, errno, "%s", __func__);
+	} else {
+	    *idInfo = id;
+	}
+
 	cleanupRequests(id);
+	if (config->nodeDownScript && *config->nodeDownScript) {
+	    PSID_execScript(config->nodeDownScript, stateChangeEnv,
+			    stateChangeCB, &idInfo);
+	}
     }
 
     if (config->useMCast) return;
@@ -626,7 +727,18 @@ void declareNodeAlive(PSnodes_ID_t id, int physCPUs, int virtCPUs)
     }
 
     if (!config->useMCast && getMasterID() == PSC_getMyID() && !wasUp) {
+	int *idInfo = malloc(sizeof(*idInfo));
+	if (!idInfo) {
+	    PSID_warn(-1, errno, "%s", __func__);
+	} else {
+	    *idInfo = id;
+	}
+
 	send_ACTIVENODES(id);
+	if (config->nodeUpScript && *config->nodeUpScript) {
+	    PSID_execScript(config->nodeUpScript, stateChangeEnv,
+			    stateChangeCB, &idInfo);
+	}
     }
 
     if (getMasterID() == PSC_getMyID() && !wasUp) {

doc/parastation.conf
@@ -88,13 +88,6 @@ Hardware openib {
 Hardware ipath {
 }
 
-
-#
-# Number of connected Stations (including front-end)
-# You have to declare this before any Nodes
-#
-NrOfNodes	8
-
 #
 # Default HW-Type. All nodes have this HW (unless otherwise stated).
 # Possible values are names from the Hardware section and 'none'.
@@ -104,6 +97,60 @@ NrOfNodes	8
 # HWType myrinet
 #
 HWType	{ ethernet p4sock }
+
+#
+# This script is called during startup of the ParaStation
+# daemon. Depending on its return value the daemon continues startup
+# without any action (0), some output of the script is written to the
+# daemons log (-1) or the daemon stops immediately (-2).
+#
+# As a default no script is defined and, thus, nothing is called.
+#
+# startupScript	"scripts/psidStartup"
+
+#
+# This script is called by the currently elected master of the
+# ParaStation daemons every time a node becomes active from
+# ParaStation's point of view. I.e. the node's daemon connects to
+# the master daemon for the first time after being down.
+#
+# This might be used to pass this type of information into a
+# batch-system or some monitoring facility like the GridMonitor.
+#
+# While calling the script two additional arguments are passed in this
+# order:
+#
+# - The hostname of the node that appeared. The actual value is the IP
+# address implicitely defined in the Nodes-section of this file
+# resolved to the official name of the host using gethostbyaddr().
+#
+# - The ParaStation ID of the node that appeared.
+#
+# As a default no script is defined and, thus, nothing is called.
+#
+# nodeUpScript	"scripts/psidNodeUp"
+
+#
+# This script is called by the currently elected master of the
+# ParaStation daemons every time a node goes down from ParaStation's
+# point of view. I.e. the node's daemon disconnects from the master
+# daemon after being connected.
+#
+# This might be used to pass this type of information to a
+# batch-system or some monitoring facility like the GridMonitor.
+#
+# While calling the script two additional arguments are passed in this
+# order:
+#
+# - The hostname of the node that disappeared. The actual value is the
+# IP address implicitely defined in the Nodes-section of this file
+# resolved to the official name of the host using gethostbyaddr().
+#
+# - The ParaStation ID of the node that disappeared.
+#
+# As a default no script is defined and, thus, nothing is called.
+#
+# nodeDownScript	"scripts/psidNodeDown"
 
 #
 # Reserve node for a special user. 'any' allows all users to use the node.
@@ -188,6 +235,13 @@ HWType	{ ethernet p4sock }
 # This can also be set during run-time via psiadmin's 'set cpumap' directive.
 #
 # CPUmap	{ 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 }
+
+
+#
+# Number of connected Stations (including front-end)
+# You have to declare this before any Nodes
+#
+NrOfNodes	8
 
 #
 # List of all nodes

bin/Makefile.in
@@ -98,7 +98,7 @@ psid: psidutil.o psidtask.o psidspawn.o psidforwarder.o psidpmiprotocol.o
 psid: psidsignal.o psidmsgbuf.o psidrdp.o psidclient.o psidcomm.o
 psid: psidinfo.o psidoption.o psidpartition.o psidstatus.o psidhw.o
 psid: psidaccount.o psidnodes.o psidintel.o psidamd.o psidppc.o psidstate.o
-psid: psidplugin.o
+psid: psidplugin.o psidscripts.o
 psid: LOADLIBES:=$(LOADLIBES) -lm -lutil -lpslog -ldl
 ifeq ($(HAVE_LIBNUMA),yes)
 psid: LOADLIBES:=$(LOADLIBES) -lnuma
@@ -106,7 +106,7 @@ endif
 psid: LDFLAGS+=-rdynamic
 
 test_config: LOADLIBES:=$(LOADLIBES) -lm
-test_config: psidnodes.o
+test_config: psidnodes.o psidscripts.o
 
 # logger
 VPATH += $(SRCDIR)/logger

bin/admin/adminparser.c
@@ -446,6 +446,38 @@ static int listStarttimeCommand(char *token)
     return -1;
 }
 
+static int listScriptCommand(char *token)
+{
+    PSP_Info_t info;
+    char *nl_descr = parser_getString();
+    char *nl = defaultNL;
+
+    if (parser_getString()) goto error;
+
+    if (!strcasecmp(token, "startupscript")) {
+	info = PSP_INFO_STARTUPSCRIPT;
+    } else if (!strcasecmp(token, "nodeupscript")) {
+	info = PSP_INFO_NODEUPSCRIPT;
+    } else if (!strcasecmp(token, "nodedownscript")) {
+	info = PSP_INFO_NODEDOWNSCRIPT;
+    } else {
+	printf("Unknown info '%s' to list\n", token);
+	goto error;
+    }
+
+    if (nl_descr) {
+	nl = getNodeList(nl_descr);
+	if (!nl) return -1;
+    }
+
+    PSIADM_ScriptStat(info, nl);
+    return 0;
+
+ error:
+    printError(&listInfo);
+    return -1;
+}
+
 static int listRDPCommand(char *token)
 {
     char *nl_descr = parser_getString();
@@ -652,6 +684,9 @@ static keylist_t listList[] = {
     {"summary", listSummaryCommand},
     {"s", listSummaryCommand},
     {"starttime", listStarttimeCommand},
+    {"startupscript", listScriptCommand},
+    {"nodeupscript", listScriptCommand},
+    {"nodedownscript", listScriptCommand},
     {"up", listSomeCommand},
     {"versions", listVersionCommand},
     {NULL, listSpecialCommand}

bin/admin/commands.h
@@ -161,6 +161,35 @@ void PSIADM_SummaryStat(char *nl, int max);
 void PSIADM_StarttimeStat(char *nl);
 
 /**
+ * @brief Show node's script info.
+ *
+ * Show the ParaStation daemon's scripts of the nodes marked within
+ * the nodelist @a nl. Depending on the @a type argument, different
+ * information is listed:
+ *
+ * - PSP_INFO_STARTUPSCRIPT request the name of the script run during
+ * startup of the daemon in order to test local features and stati.
+ * This information is used to decide, if the local node is capable to
+ * take part in the cluster-action.
+ *
+ * - PSP_INFO_NODEUPSCRIPT request the name of the script called by
+ * the master daemon whenever a node becomes active in the concert of
+ * daemons within a cluster.
+ *
+ * - PSP_INFO_NODEDOWNSCRIPT request the name of the script called by
+ * the master daemon whenever a node disappeares from the concert of
+ * daemons within a cluster.
+ *
+ * @param type The type of scripts to display.
+ *
+ * @param nl The nodelist describing the nodes from which the
+ * information should be requested and put out.
+ *
+ * @return No return value.
+ */
+void PSIADM_ScriptStat(PSP_Info_t type, char *nl);
+
+/**
  * @brief Show some nodes.
  *
  * Show the nodes in the nodelist @a nl that are marked to

bin/admin/helpmsgs.c
@@ -548,7 +548,7 @@ static info_t listInfo = {
 	.arg = "{{[node] | count [hw <hw>] | {p|processes} [cnt <cnt>]"
 	" | {aproc|allprocesses} [cnt <cnt>] | {hardware|hw} | down | up"
 	" | load | rdp | mcast | plugins | summary [max <max>] | starttime"
-	" | versions}"
+	" | startupscript | nodeupscript | nodedownscript | versions}"
 	" <nodes> |"
 	" jobs [state {r[unning] | p[ending] | s[uspended]}] [slots] [<tid>]}"
     }},
@@ -597,6 +597,15 @@ static info_t listInfo = {
 	  " printed, too." },
 	{ .tag = "list starttime",
 	  .descr = "Show the daemon's start-time on the selected nodes." },
+	{ .tag = "list startupscript",
+	  .descr = "Show the daemon's script called during startup in order to"
+	  " test the local situation on the selected nodes." },
+	{ .tag = "list nodeupscript",
+	  .descr = "Show the daemon's script called on the master node whenever"
+	  " a daemon connects after being down before on the selected nodes." },
+	{ .tag = "list nodedownscript",
+	  .descr = "Show the daemon's script called on the master node whenever"
+	  " a daemon disconnects on the selected nodes." },
 	{ .tag = "list versions",
 	  .descr = "Show the daemon's version and the revision of the"
 	  " corresponding RPM on the selected nodes." },

commit 82c07f4ae0ee9e817dc89a938fef617efacb46bf

Bugfix: Make script-hooks triggered by node-status actually woring. This     fixed a bug introduced in r7875 Fix some glitches in psiadmin's output of 'list <script>'.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/mgmt@7878 34291a86-62e8-0310-8159-f060f8a2ee4e

Makefile.version
@@ -1,2 +1,2 @@
 VERSION_$(PKG_NAME)=5.0.22
-RELEASE_$(PKG_NAME)=2
+RELEASE_$(PKG_NAME)=3

bin/admin/commands.c
@@ -476,13 +476,10 @@ void PSIADM_ScriptStat(PSP_Info_t type, char *nl)
     printf("%4s\t%s\n", "Node", "Script");
     for (node=0; node<PSC_getNrOfNodes(); node++) {
 	if (nl && !nl[node]) continue;
-
+	printf("%4d\t", node);
 	if (hostStatus.list[node]) {
-	    int err;
-
-	    printf("%4d\t", node);
-	    err = PSI_infoString(node, type, NULL, scriptName,
-				 sizeof(scriptName), 1);
+	    int err = PSI_infoString(node, type, NULL, scriptName,
+				     sizeof(scriptName), 1);
 	    if (!err) {
 		size_t len = strlen(scriptName);
 		if (len) {
@@ -494,9 +491,11 @@ void PSIADM_ScriptStat(PSP_Info_t type, char *nl)
 		} else {
 		    printf("<none>\n");
 		}
+	    } else {
+		printf("\n");
 	    }
 	} else {
-	    printf("%4d\tdown\n", node);
+	    printf("down\n");
 	}
     }
 }

bin/daemon/psidstatus.c
@@ -566,7 +566,7 @@ static int stateChangeCB(int fd, PSID_scriptCBInfo_t *cbInfo)
 	    stateChangeInfo_t *i = cbInfo->info;
 	    nID = i->id;
 	    sName = i->script;
-	    free(cbInfo->info);
+	    free(i);
 	}
 	iofd = cbInfo->iofd;
 	free(cbInfo);
@@ -664,17 +664,17 @@ void declareNodeDead(PSnodes_ID_t id, int sendDeadnode, int silent)
 
 	declareMaster(node);
     } else if (PSC_getMyID() == getMasterID()) {
-	int *idInfo = malloc(sizeof(*idInfo));
-	if (!idInfo) {
-	    PSID_warn(-1, errno, "%s", __func__);
-	} else {
-	    *idInfo = id;
-	}
-
 	cleanupRequests(id);
 	if (config->nodeDownScript && *config->nodeDownScript) {
+	    stateChangeInfo_t *info = malloc(sizeof(*info));
+	    if (!info) {
+		PSID_warn(-1, errno, "%s", __func__);
+	    } else {
+		info->id = id;
+		info->script = config->nodeDownScript;
+	    }
 	    PSID_execScript(config->nodeDownScript, stateChangeEnv,
-			    stateChangeCB, &idInfo);
+			    stateChangeCB, info);
 	}
     }
 
@@ -727,17 +727,17 @@ void declareNodeAlive(PSnodes_ID_t id, int physCPUs, int virtCPUs)
     }
 
     if (!config->useMCast && getMasterID() == PSC_getMyID() && !wasUp) {
-	int *idInfo = malloc(sizeof(*idInfo));
-	if (!idInfo) {
-	    PSID_warn(-1, errno, "%s", __func__);
-	} else {
-	    *idInfo = id;
-	}
-
 	send_ACTIVENODES(id);
 	if (config->nodeUpScript && *config->nodeUpScript) {
+	    stateChangeInfo_t *info = malloc(sizeof(*info));
+	    if (!info) {
+		PSID_warn(-1, errno, "%s", __func__);
+	    } else {
+		info->id = id;
+		info->script = config->nodeUpScript;
+	    }
 	    PSID_execScript(config->nodeUpScript, stateChangeEnv,
-			    stateChangeCB, &idInfo);
+			    stateChangeCB, info);
 	}
     }
 

doc/parastation.conf
@@ -117,14 +117,15 @@ HWType	{ ethernet p4sock }
 # This might be used to pass this type of information into a
 # batch-system or some monitoring facility like the GridMonitor.
 #
-# While calling the script two additional arguments are passed in this
-# order:
+# While calling the script two additional arguments are passed within the
+# environment:
 #
-# - The hostname of the node that appeared. The actual value is the IP
-# address implicitely defined in the Nodes-section of this file
-# resolved to the official name of the host using gethostbyaddr().
+# - NODE_NAME: The hostname of the node that appeared. The actual
+# value is the IP address implicitely defined in the Nodes-section of
+# this file resolved to the official name of the host using
+# gethostbyaddr().
 #
-# - The ParaStation ID of the node that appeared.
+# - NODE_ID: The ParaStation ID of the node that appeared.
 #
 # As a default no script is defined and, thus, nothing is called.
 #
@@ -139,14 +140,15 @@ HWType	{ ethernet p4sock }
 # This might be used to pass this type of information to a
 # batch-system or some monitoring facility like the GridMonitor.
 #
-# While calling the script two additional arguments are passed in this
-# order:
+# While calling the script two additional arguments are passed within the
+# environment:
 #
-# - The hostname of the node that disappeared. The actual value is the
-# IP address implicitely defined in the Nodes-section of this file
-# resolved to the official name of the host using gethostbyaddr().
+# - NODE_NAME: The hostname of the node that disappeared. The actual
+# value is the IP address implicitely defined in the Nodes-section of
+# this file resolved to the official name of the host using
+# gethostbyaddr().
 #
-# - The ParaStation ID of the node that disappeared.
+# - NODE_ID: The ParaStation ID of the node that disappeared.
 #
 # As a default no script is defined and, thus, nothing is called.
 #