ParaStation/psmgmt

commit 4ad1fea05dda411cf990bebb6f65ba318abf6bd3

Complete correct initialization of clientStat.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8307 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/daemon/psidstatus.c
@@ -153,9 +153,10 @@ static void allocMasterSpace(void)
 
     for (node=0; node<PSC_getNrOfNodes(); node++) {
 	gettimeofday(&clientStat[node].lastPing, NULL);
-	clientStat[node].missCounter = 0;
 	clientStat[node].jobs = (PSID_Jobs_t) { .normal = 0, .total = 0 };
 	clientStat[node].mem = (PSID_Mem_t) { -1, -1 };
+	clientStat[node].missCounter = 0;
+	clientStat[node].wrongClient = 0;
     }
 }
 

Makefile.version
@@ -1,2 +1,2 @@
 VERSION_$(PKG_NAME)=5.0.28
-RELEASE_$(PKG_NAME)=1
+RELEASE_$(PKG_NAME)=2

commit 7223040d1bdb2b1b798876b27ffafa6b5a54edb0

Fix typo introduced in r8307.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8308 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/daemon/psidstatus.c
@@ -156,7 +156,7 @@ static void allocMasterSpace(void)
 	clientStat[node].jobs = (PSID_Jobs_t) { .normal = 0, .total = 0 };
 	clientStat[node].mem = (PSID_Mem_t) { -1, -1 };
 	clientStat[node].missCounter = 0;
-	clientStat[node].wrongClient = 0;
+	clientStat[node].wrongClients = 0;
     }
 }
 