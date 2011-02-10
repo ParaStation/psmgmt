0ffa0b4e1a56ac453f12baaf28f66aceb3e541f5
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2011-02-10 14:48:31 +0000

    Add some test-plugins for testing the new plugin-interface.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8377 34291a86-62e8-0310-8159-f060f8a2ee4e

---
+++ tests/plugins/plugin2.c
@@ -0,0 +1,103 @@
+/*
+ * Open ParaStation
+ *
+ * Copyright (C) 2011 ParTec Cluster Competence Center GmbH, Munich
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: psidplugin.c 8355 2010-10-26 16:31:28Z eicker $
+ *
+ */
+#include <stdlib.h>
+
+#include "psidutil.h"
+#include "psidplugin.h"
+#include "timer.h"
+
+#include "plugin.h"
+
+#define EXTENDED_API
+
+#ifdef EXTENDED_API
+int requiredAPI = 101;
+#else
+int requiredAPI = 100;
+#endif
+
+char name[] = "plugin2";
+
+int version = 100;
+
+plugin_dep_t dependencies[] = {
+    { "plugin3", 10 },
+    { "plugin4", 10 },
+    { "plugin5", 10 },
+    { NULL, 0 } };
+
+/* Flag suppressing some messages */
+char *silent = NULL;
+
+/* Flag suppressing all messages */
+char *quiet = NULL;
+
+#ifdef EXTENDED_API
+void initialize(void)
+{
+    if (!silent && !quiet) PSID_log(-1, "%s: %s()\n", name, __func__);
+}
+
+int myTimer = -1;
+
+void unload(void)
+{
+    if (!silent && !quiet) PSID_log(-1, "%s: %s()\n", name, __func__);
+    PSIDplugin_unload(name);
+}
+
+void finalize(void)
+{
+    struct timeval timeout = {7, 0};
+
+    if (!silent && !quiet) PSID_log(-1, "%s: %s()\n", name, __func__);
+
+    myTimer = Timer_register(&timeout, unload);
+    if (!silent||!quiet) PSID_log(-1, "%s: timer %d\n", name, myTimer);
+}
+
+void cleanup(void)
+{
+    if (!silent && !quiet) PSID_log(-1, "%s: %s()\n", name, __func__);
+
+    if (myTimer > -1) {
+	Timer_remove(myTimer);
+	if (!silent||!quiet) PSID_log(-1, "%s: timer %d del\n", name, myTimer);
+	myTimer = -1;
+    }
+}
+#endif
+
+__attribute__((constructor))
+void plugin_init(void)
+{
+    silent = getenv("PLUGIN_SILENT");
+    quiet = getenv("PLUGIN_QUIET");
+
+    if (!quiet) PSID_log(-1, "%s: %s()\n", name, __func__);
+}
+
+__attribute__((destructor))
+void plugin_fini(void)
+{
+    if (!quiet) PSID_log(-1, "%s: %s()\n", name, __func__);
+}
---
+++ tests/plugins/plugin3.c
@@ -0,0 +1,55 @@
+/*
+ * Open ParaStation
+ *
+ * Copyright (C) 2011 ParTec Cluster Competence Center GmbH, Munich
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: psidplugin.c 8355 2010-10-26 16:31:28Z eicker $
+ *
+ */
+#include <stdlib.h>
+
+#include "psidutil.h"
+
+#include "plugin.h"
+
+int requiredAPI = 99;
+
+int version = 200;
+
+char name[] = "plugin3";
+
+
+plugin_dep_t dependencies[] = {
+    { "plugin2", 0 },
+    { "plugin7", 0 },
+    { NULL, 0 } };
+
+/* Flag suppressing of all messages */
+char *quiet = NULL;
+
+__attribute__((constructor))
+void plugin_init(void)
+{
+    quiet = getenv("PLUGIN_QUIET");
+
+    if (!quiet) PSID_log(-1, "%s: %s()\n", name, __func__);
+}
+
+__attribute__((destructor))
+void plugin_fini(void)
+{
+    if (!quiet) PSID_log(-1, "%s: %s()\n", name, __func__);
+}
