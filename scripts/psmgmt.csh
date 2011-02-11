ParaStation/psmgmt

commit 7cb305e1623fe91fe11f786fae836c62d88ab1d5

Add /opt/parastation/sbin to root's PATH. This implements #893.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8368 34291a86-62e8-0310-8159-f060f8a2ee4e

scripts/psmgmt.csh
@@ -17,6 +17,9 @@ set _psdir="/opt/parastation"
 if ( -d "${_psdir}/bin" ) then
 
     set path = ( ${path} "${_psdir}/bin" )
+    if ( `id -ur` -eq 0 ) then
+	set path = ( ${path} "${_psdir}/sbin" )
+    endif
 
     if ( -d "${_psdir}/man" ) then
 	if ( ! ${?MANPATH} ) then

scripts/psmgmt.sh
@@ -17,6 +17,9 @@ _psdir="/opt/parastation"
 if test -d "${_psdir}/bin" ; then
 
     export PATH="${PATH}:${_psdir}/bin"
+    if test `id -ur` -eq 0; then
+	export PATH="${PATH}:${_psdir}/sbin"
+    fi
 
     if test -d "${_psdir}/man" ; then
 	if test -z "${MANPATH}" ; then

commit 8730b978b28241f0345dc3e163199b63751d20c7

Bugfix: Fix typo in csh-profile introduced in r8368.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8381 34291a86-62e8-0310-8159-f060f8a2ee4e

scripts/psmgmt.csh
@@ -17,7 +17,7 @@ set _psdir="/opt/parastation"
 if ( -d "${_psdir}/bin" ) then
 
     set path = ( ${path} "${_psdir}/bin" )
-    if ( `id -ur` -eq 0 ) then
+    if ( `id -ur` == 0 ) then
 	set path = ( ${path} "${_psdir}/sbin" )
     endif
 