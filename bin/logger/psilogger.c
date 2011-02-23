ParaStation/psmgmt

commit 4ea55d31154d7f2a1f7839d23abd2c07fe348411

Bugfix: Improve logging with timestamps. This should fix #719.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8365 34291a86-62e8-0310-8159-f060f8a2ee4e

include/logging.h
@@ -1,7 +1,7 @@
 /*
  *               ParaStation
  *
- * Copyright (C) 2005-2009 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2010 ParTec Cluster Competence Center GmbH, Munich
  *
  * $Id$
  *
@@ -42,6 +42,8 @@ typedef struct {
 		      logger_setTag()/logger_getTag() or logger_init(). */
     char* trail;   /**< Remnants from the last messages that did not
 		      had a closing '\n' character. */
+    int trailSize; /**< Maximum size to be currently stored in trail */
+    char trailUsed; /**< Flag if trail is currently in use */
     char timeFlag; /**< Flag if current time shall be given within tag */
 } logger_t;
 

lib/pscommon/logging.c
@@ -66,6 +66,8 @@ logger_t* logger_init(char* tag, FILE* logfile)
     logger->tag = NULL;
     logger_setTag(logger, tag);
     logger->trail = NULL;
+    logger->trailSize = 0;
+    logger->trailUsed = 0;
     logger->timeFlag = 0;
 
     return logger;
@@ -111,13 +113,13 @@ static inline char *getTimeStr(logger_t *logger)
  * logger_warn() and @ref logger_exit() actually printing the message.
  *
  * The message defined by @a format and @a ap will be spiffed up with
- * @a logger's tag and put out to the destination also defined within
- * @a logger.
+ * @a logger's tag and some timestamp and put out to the destination
+ * also defined within @a logger.
  *
  * The message is only actually put out if @a format contains a
- * trailing newline character. Otherwise it will be stored within @a
- * logger and put in front of further messages sent via this special
- * logger.
+ * newline character. Any trails left after the last newline character
+ * will be stored within @a logger and put in front of further
+ * messages sent via this special logger.
  *
  * This function does @b no keys/mask handling, i.e. this has to be
  * done within the wrapper functions.
@@ -140,27 +142,27 @@ static inline char *getTimeStr(logger_t *logger)
  */
 static void do_print(logger_t* logger, const char* format, va_list ap)
 {
-    static char *prefix = NULL, *text = NULL, *line = NULL;
-    static int prfxlen = 0, txtlen = 0, linelen = 0;
+    static char *prefix = NULL, *text = NULL;
+    static int prfxlen = 0, txtlen = 0;
     va_list aq;
-    char *tag;
+    char *tag, *timeStr, *c;
     int len;
 
     if (!logger) return;
     tag = logger->tag;
 
-    if (!logger->trail) {
-	char *timeStr = getTimeStr(logger);
-	len = snprintf(prefix, prfxlen, "%s%s%s", tag ? tag : "", timeStr,
-		       (tag || logger->timeFlag) ? ": " : "");
-	if (len >= prfxlen) {
-	    prfxlen = len + 80; /* Some extra space */
-	    prefix = (char*)realloc(prefix, prfxlen);
-	    sprintf(prefix, "%s%s%s", tag ? tag : "", timeStr,
-		    (tag || logger->timeFlag) ? ": " : "");
-	}
+    /* Prepare prefix string */
+    timeStr = getTimeStr(logger);
+    len = snprintf(prefix, prfxlen, "%s%s%s", tag ? tag : "", timeStr,
+		   (tag || logger->timeFlag) ? ": " : "");
+    if (len >= prfxlen) {
+	prfxlen = len + 80; /* Some extra space */
+	prefix = (char*)realloc(prefix, prfxlen);
+	sprintf(prefix, "%s%s%s", tag ? tag : "", timeStr,
+		(tag || logger->timeFlag) ? ": " : "");
     }
 
+    /* Create actual output */
     va_copy(aq, ap);
     len = vsnprintf(text, txtlen, format, ap);
     if (len >= txtlen) {
@@ -170,37 +172,40 @@ static void do_print(logger_t* logger, const char* format, va_list ap)
     }
     va_end(aq);
 
-    if (logger->trail) {
-	len = snprintf(line, linelen, "%s%s", logger->trail, text);
-	if (len >= linelen) {
-	    linelen = len + 80; /* Some extra space */
-	    line = (char*)realloc(line, linelen);
-	    sprintf(line, "%s%s", logger->trail, text);
+    c = text;
+
+    while (c && *c) {
+	char *s = logger->trailUsed ? logger->trail : prefix;
+	char *r = strchr(c, '\n');
+
+	if (r) {
+	    *r = '\0';
+	    r++;
 	}
-    } else {
-	len = snprintf(line, linelen, "%s%s", prefix, text);
-	if (len >= linelen) {
-	    linelen = len + 80; /* Some extra space */
-	    line = (char*)realloc(line, linelen);
-	    sprintf(line, "%s%s", prefix, text);
+
+	if (r) {
+	    /* got newline, lets do the output */
+	    if (logger->logfile) {
+		fprintf(logger->logfile, "%s%s\n", s, c);
+	    } else {
+		syslog(LOG_ERR, "%s%s", s, c);
+	    }
+	    logger->trailUsed = 0;
+	} else {
+	    /* no newline, append to trail */
+	    len = snprintf(logger->trail, logger->trailSize, "%s%s", s, c);
+	    if (len >= logger->trailSize) {
+		logger->trailSize = len + 80; /* Some extra space */
+		logger->trail = realloc(logger->trail, logger->trailSize);
+		sprintf(logger->trail, "%s%s", s, c);
+	    }
+	    logger->trailUsed = 1;
 	}
+
+	c = r;
     }
 
-    if (logger->logfile) {
-	fprintf(logger->logfile, "%s", logger->trail ? text : line);
-	fflush(logger->logfile);
-    }
-
-    if (text[strlen(text)-1] == '\n') {
-	if (!logger->logfile) syslog(LOG_ERR, "%s", line);
-	if (logger->trail) {
-	    free(logger->trail);
-	    logger->trail = NULL;
-	}
-    } else {
-	if (logger->trail) free(logger->trail);
-	logger->trail = strdup(line);
-    }
+    if (logger->logfile) fflush(logger->logfile);
 }
 
 void logger_print(logger_t* logger, int32_t key, const char* format, ...)

commit deb953b7b83781533079f239070f0aeea02f2ab3

Bugfix: Output of very long lines might get crippled. Fix bug introduced     with r8365. Enhancement: Nicer output of long line with and without prefixes.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8392 34291a86-62e8-0310-8159-f060f8a2ee4e

Makefile.version
@@ -1,2 +1,2 @@
 VERSION_$(PKG_NAME)=5.0.30
-RELEASE_$(PKG_NAME)=1
+RELEASE_$(PKG_NAME)=2

bin/logger/psilogger.c
@@ -2,7 +2,7 @@
  *               ParaStation
  *
  * Copyright (C) 1999-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2010 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2011 ParTec Cluster Competence Center GmbH, Munich
  *
  * $Id$
  *
@@ -861,6 +861,7 @@ static void handleUSAGEMsg(PSLog_Msg_t *msg)
  */
 static void handleOutMsg(PSLog_Msg_t *msg, int outfd)
 {
+    static int lastSender = lastSenderMagic, nlAtEnd = 1;
     PSIlog_log(PSILOG_LOG_VERB, "Got %d bytes from %s\n",
 	       msg->header.len - PSLog_headerSize,
 	       PSC_printTID(msg->header.sender));
@@ -869,7 +870,6 @@ static void handleOutMsg(PSLog_Msg_t *msg, int outfd)
 	/* collect all ouput */
 	cacheOutput(msg, outfd);
     } else if (prependSource) {
-	static int lastSender = lastSenderMagic, nlAtEnd = 1;
 	char prefix[30];
 	char *buf = msg->buf;
 	size_t count = msg->header.len - PSLog_headerSize;
@@ -920,15 +920,28 @@ static void handleOutMsg(PSLog_Msg_t *msg, int outfd)
     } else {
 	switch (outfd) {
 	case STDOUT_FILENO:
+	    if (msg->sender != lastSender && !nlAtEnd) {
+		PSIlog_stdout(-1, "\n");
+	    }
 	    PSIlog_stdout(-1, "%.*s",
 			  msg->header.len-PSLog_headerSize, msg->buf);
 	    break;
 	case STDERR_FILENO:
+	    if (msg->sender != lastSender && !nlAtEnd) {
+		PSIlog_stdout(-1, "\n");
+	    }
 	    PSIlog_stderr(-1, "%.*s",
 			  msg->header.len-PSLog_headerSize, msg->buf);
 	    break;
 	default:
 	    PSIlog_log(-1, "%s: unknown outfd %d\n", __func__, outfd);
+	}
+	if (msg->buf[msg->header.len-PSLog_headerSize-1] == '\n') {
+	    lastSender = lastSenderMagic;
+	    nlAtEnd = 1;
+	} else {
+	    lastSender = msg->sender;
+	    nlAtEnd = 0;
 	}
     }
 }

include/logging.h
@@ -1,7 +1,7 @@
 /*
  *               ParaStation
  *
- * Copyright (C) 2005-2010 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2011 ParTec Cluster Competence Center GmbH, Munich
  *
  * $Id$
  *
@@ -42,8 +42,8 @@ typedef struct {
 		      logger_setTag()/logger_getTag() or logger_init(). */
     char* trail;   /**< Remnants from the last messages that did not
 		      had a closing '\n' character. */
-    int trailSize; /**< Maximum size to be currently stored in trail */
-    char trailUsed; /**< Flag if trail is currently in use */
+    size_t trailSize; /**< Maximum size to be currently stored in trail */
+    size_t trailUsed; /**< Number of character currently stored in trail */
     char timeFlag; /**< Flag if current time shall be given within tag */
 } logger_t;
 

lib/pscommon/logging.c
@@ -1,7 +1,7 @@
 /*
  *               ParaStation
  *
- * Copyright (C) 2005-2010 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2011 ParTec Cluster Competence Center GmbH, Munich
  *
  * $Id$
  *
@@ -143,10 +143,9 @@ static inline char *getTimeStr(logger_t *logger)
 static void do_print(logger_t* logger, const char* format, va_list ap)
 {
     static char *prefix = NULL, *text = NULL;
-    static int prfxlen = 0, txtlen = 0;
+    static size_t prfxlen = 0, txtlen = 0, len;
     va_list aq;
     char *tag, *timeStr, *c;
-    int len;
 
     if (!logger) return;
     tag = logger->tag;
@@ -175,7 +174,6 @@ static void do_print(logger_t* logger, const char* format, va_list ap)
     c = text;
 
     while (c && *c) {
-	char *s = logger->trailUsed ? logger->trail : prefix;
 	char *r = strchr(c, '\n');
 
 	if (r) {
@@ -185,6 +183,7 @@ static void do_print(logger_t* logger, const char* format, va_list ap)
 
 	if (r) {
 	    /* got newline, lets do the output */
+	    char *s = logger->trailUsed ? logger->trail : prefix;
 	    if (logger->logfile) {
 		fprintf(logger->logfile, "%s%s\n", s, c);
 	    } else {
@@ -193,13 +192,19 @@ static void do_print(logger_t* logger, const char* format, va_list ap)
 	    logger->trailUsed = 0;
 	} else {
 	    /* no newline, append to trail */
-	    len = snprintf(logger->trail, logger->trailSize, "%s%s", s, c);
-	    if (len >= logger->trailSize) {
-		logger->trailSize = len + 80; /* Some extra space */
+	    len = (!logger->trailUsed && prefix) ? strlen(prefix) : 0;
+	    len += strlen(c);
+	    if (logger->trailUsed + len >= logger->trailSize) {
+		logger->trailSize =
+		    logger->trailUsed + len + 80; /* Some extra space */
 		logger->trail = realloc(logger->trail, logger->trailSize);
-		sprintf(logger->trail, "%s%s", s, c);
 	    }
-	    logger->trailUsed = 1;
+	    if (!logger->trailUsed && prefix) {
+		/* some prefix to be put into trail */
+		logger->trailUsed = sprintf(logger->trail, "%s", prefix);
+	    }
+	    logger->trailUsed +=
+		sprintf(logger->trail + logger->trailUsed, "%s", c);
 	}
 
 	c = r;