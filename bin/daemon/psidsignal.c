eb83297dbc9475d62a4c9820511d9e6b6f5c41df
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2013-01-08 21:22:45 +0000

    Enhancement: Only do PStask_gcSig() with expensive blocking of signals,     if required.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8703 34291a86-62e8-0310-8159-f060f8a2ee4e

--- bin/daemon/psidsignal.c
+++ bin/daemon/psidsignal.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2003-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2012 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2013 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -1247,6 +1247,8 @@ static void drop_RELEASE(DDBufferMsg_t *msg)
 static void signalGC(void)
 {
     int blocked, blockedRDP;
+
+    if (!PStask_gcSigRequired()) return;
 
     blocked = PSID_blockSig(1, SIGCHLD);
     blockedRDP = RDP_blockTimer(1);
