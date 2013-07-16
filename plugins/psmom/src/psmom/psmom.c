f1b33f5394a2cdb0ed1361505831edc8265b7827
rauh <rauh@34291a86-62e8-0310-8159-f060f8a2ee4e>
2013-07-16 14:36:10 +0000

    pushed psmom version
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8827 34291a86-62e8-0310-8159-f060f8a2ee4e

--- plugins/psmom/src/psmom/psmom.c
+++ plugins/psmom/src/psmom/psmom.c
@@ -125,7 +125,7 @@ handlerFunc_t oldSpawnReqHandler = NULL;
 
 /** psid plugin requirements */
 char name[] = "psmom";
-int version = 56;
+int version = 57;
 int requiredAPI = 109;
 plugin_dep_t dependencies[2];
 
