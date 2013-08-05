e1a55cd253facda0c5775ff8d428217e2bf41634
rauh <rauh@34291a86-62e8-0310-8159-f060f8a2ee4e>
2013-08-05 12:14:06 +0000

    Enhancement: removed unnecessary prototyps.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8855 34291a86-62e8-0310-8159-f060f8a2ee4e

--- plugins/pspmi/pmikvs.h
+++ plugins/pspmi/pmikvs.h
@@ -21,44 +21,4 @@
 /* file handle for memory debug output */
 extern FILE *memoryDebug;
 
-/**
- * @brief Show a key value pair.
- *
- * @param key The key of the kv-pair to query. If NULL than all kv-pairs should
- * be returned.
- *
- * @return Returns a dynamically allocated string holding the
- * requested kv-pairs.
- */
-char *show(char *key);
-
-/**
- * @brief Display a help message for the psmom kvs.
- *
- * @return Returns the dynamically allocated help message.
- */
-char *help(void);
-
-/**
- * @brief Set a key-value pair.
- *
- * @param key The key to set.
- *
- * @param value The value to set.
- *
- * @return Returns a pointer a message describing the success.
- */
-char *set(char *key, char *value);
-
-/**
- * @brief Remove a key-value pair.
- *
- * @param key The key as index to unset.
- *
- * @param value The value to unset.
- *
- * @return Returns a pointer a message describing the success.
- */
-char *unset(char *key);
-
 #endif
