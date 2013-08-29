ParaStation/psmgmt

commit e89bb3b5d2a88eb4e7f585c5e0ecfbd230993e74

Enhancement: Avoid directory clash of /var/account (#1930). Enhancement: Let the psaccounter started by psid run as root. Enhancement: Let ordinary users only get their own accounting     records using psaccounter. Adminusers may receive accounting     data from everyone..
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8746 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/daemon/psidaccount.c
@@ -22,6 +22,8 @@ static char vcid[] __attribute__((used)) =
 #include "psidcomm.h"
 #include "psidutil.h"
 #include "psidoption.h"
+#include "psidtask.h"
+#include "psidnodes.h"
 
 #include "psidaccount.h"
 
@@ -53,7 +55,7 @@ void PSID_addAcct(PStask_ID_t acctr)
     acct = malloc(sizeof(*acct));
     acct->acct = acctr;
     list_add_tail(&acct->next, &PSID_accounters);
-};
+}
 
 void PSID_remAcct(PStask_ID_t acctr)
 {
@@ -133,10 +135,51 @@ void PSID_msgACCOUNT(DDBufferMsg_t *msg)
 	     PSC_printTID(msg->header.sender));
 
     if (msg->header.dest == PSC_getMyTID()) {
-	/* message for me, let's forward to all known accounters */
+	DDTypedBufferMsg_t *tmsg = (DDTypedBufferMsg_t *) msg;
 	list_t *pos, *tmp;
+	PStask_t *task;
+	char *ptr = tmsg->buf;
+	uid_t uid;
+	static int lastStartUID = -1;
+
+	/* skip logger TID and rank to get to UID */
+	ptr += sizeof(PStask_ID_t);
+	ptr += sizeof(int32_t);
+	uid = *(uid_t *)ptr;
+
+	/* save uid for next incoming slot msg */
+	if (tmsg->type == PSP_ACCOUNT_START) {
+	    lastStartUID = uid;
+	}
+
+	/* no uid in msg, used saved one */
+	if (tmsg->type == PSP_ACCOUNT_SLOTS) {
+	    uid = lastStartUID;
+	}
+
+	/* message for me, let's forward to all authorized accounters */
+
 	list_for_each_safe(pos, tmp, &PSID_accounters) {
 	    PSID_acct_t *acct = list_entry(pos, PSID_acct_t, next);
+
+	    if (!(task = PStasklist_find(&managedTasks, acct->acct))) {
+		PSID_log(PSID_LOG_VERB, "%s: task for accounter '%s' not"
+			    " found\n", __func__, PSC_printTID(acct->acct));
+		continue;
+	    }
+
+	    /* Let accounter running as adminuser receive all accounting
+	     * messages. Unprivileged users may only see their own
+	     * accounting data.
+	     */
+	    if (task->uid != 0 && task->uid != uid &&
+		!PSIDnodes_testGUID(PSC_getMyID(), PSIDNODES_ADMUSER,
+			       (PSIDnodes_guid_t){.u=task->uid}) &&
+		!PSIDnodes_testGUID(PSC_getMyID(), PSIDNODES_ADMGROUP,
+			       (PSIDnodes_guid_t){.g=task->gid})) {
+		continue;
+	    }
+
 	    msg->header.dest = acct->acct;
 	    if (sendMsg(msg) == -1 && errno != EWOULDBLOCK) {
 		PSID_warn(-1, errno, "%s: sendMsg()", __func__);

bin/Makefile.in
@@ -18,7 +18,7 @@
 #
 # ParaStation
 #
-# Copyright (C) 2012 ParTec Cluster Competence Center GmbH, Munich
+# Copyright (C) 2013 ParTec Cluster Competence Center GmbH, Munich
 #
 # This file may be distributed under the terms of the Q Public License
 # as defined in the file LICENSE.QPL included in the packaging of this
@@ -701,7 +701,6 @@ mostlyclean-compile:
 	-rm -f daemon/psidoption.$(OBJEXT)
 	-rm -f daemon/psidpartition.$(OBJEXT)
 	-rm -f daemon/psidplugin.$(OBJEXT)
-	-rm -f daemon/psidpmiprotocol.$(OBJEXT)
 	-rm -f daemon/psidppc.$(OBJEXT)
 	-rm -f daemon/psidrdp.$(OBJEXT)
 	-rm -f daemon/psidscripts.$(OBJEXT)

bin/accounter/psaccounter.c
@@ -61,7 +61,7 @@ static char vcid[] __attribute__ ((used)) =
 #define MAX_USERNAME_LEN 64
 #define MAX_GROUPNAME_LEN 64
 #define EXEC_HOST_SIZE 1024
-#define DEFAULT_LOG_DIR LOCALSTATEDIR "/account"
+#define DEFAULT_LOG_DIR LOCALSTATEDIR "/log/psaccounter"
 #define ERROR_WAIT_TIME	60
 
 /* children struct */

dist/psmgmt.spec.templ
@@ -151,7 +151,7 @@ exit 0
 %{_sysconfdir}/init.d/parastation
 %config(noreplace) /etc/xinetd.d/psidstarter
 %config(noreplace) /etc/parastation.conf
-%attr(770,root,nobody) %{_localstatedir}/account
+%attr(770,root,root) %{_localstatedir}/log/psaccounter
 
 %files devel
 %defattr(-,root,root,-)

doc/Makefile.am
@@ -24,6 +24,6 @@ parastation.conf.tmpl: $(srcdir)/parastation.conf
 CLEANFILES = parastation.conf.tmpl
 
 install-exec-local:
-	$(INSTALL) -d $(DESTDIR)$(localstatedir)/account
+	$(INSTALL) -d $(DESTDIR)$(localstatedir)/log/psaccounter
 
 EXTRA_DIST = MessageTypes

doc/Makefile.in
@@ -536,7 +536,7 @@ parastation.conf.tmpl: $(srcdir)/parastation.conf
 	$(INSTALL) $^ $@
 
 install-exec-local:
-	$(INSTALL) -d $(DESTDIR)$(localstatedir)/account
+	$(INSTALL) -d $(DESTDIR)$(localstatedir)/log/psaccounter
 
 # Tell versions [3.59,3.63) of GNU make to not export all variables.
 # Otherwise a system limit (for SysV at least) may be exceeded.

lib/Makefile.in
@@ -18,7 +18,7 @@
 #
 # ParaStation
 #
-# Copyright (C) 2012 ParTec Cluster Competence Center GmbH, Munich
+# Copyright (C) 2013 ParTec Cluster Competence Center GmbH, Munich
 #
 # This file may be distributed under the terms of the Q Public License
 # as defined in the file LICENSE.QPL included in the packaging of this

commit 06f1b239f326dc7362c199aa08e0ab6b0c53bef0

Bugfix: Fix accounting again. This was broken by r8746.     In fact this breaks ACCOUNTING_SLOTS messages, too. Will be fixed     within one of the next commits.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8875 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/daemon/psidaccount.c
@@ -129,69 +129,98 @@ void send_acct_OPTIONS(PStask_ID_t dest, int all)
     }
 }
 
-void PSID_msgACCOUNT(DDBufferMsg_t *msg)
+/**
+ * @brief Forward PSP_CD_ACCOUNT message locally.
+ *
+ * Forward the message @a msg of type PSP_CD_ACCOUNT to a local
+ * accounter. Beforehand permissions of the accounter to receive this
+ * specific accounting message will be double-checked. I.e. only
+ * accounters owned by root, an admin user or a member of the admin
+ * group are allowed receive all accounting message. All other users
+ * will only receive messages concerning their own jobs.
+ *
+ * @param msg Pointer to the message to forward.
+ *
+ * @return No return value.
+ */
+static void localForward_ACCOUNT(DDTypedBufferMsg_t *msg)
+{
+    PStask_t *task = PStasklist_find(&managedTasks, msg->header.dest);
+    char *ptr = msg->buf;
+    uid_t uid;
+
+    /* skip logger TID */
+    ptr += sizeof(PStask_ID_t);
+
+    if (msg->type != PSP_ACCOUNT_SLOTS) {
+	/* skip rank to get to UID */
+	ptr += sizeof(int32_t);
+    }
+    uid = *(uid_t *)ptr;
+
+    if (!task) {
+	PSID_log(-1, "%s: '%s' not found\n", __func__,
+		 PSC_printTID(msg->header.dest));
+	return;
+    }
+
+    /* Unprivileged users may only see their own accounting data. */
+    if (task->uid != 0 && task->uid != uid &&
+	!PSIDnodes_testGUID(PSC_getMyID(), PSIDNODES_ADMUSER,
+			    (PSIDnodes_guid_t){.u=task->uid}) &&
+	!PSIDnodes_testGUID(PSC_getMyID(), PSIDNODES_ADMGROUP,
+			    (PSIDnodes_guid_t){.g=task->gid})) {
+	return;
+    }
+
+    if (sendMsg(msg) == -1 && errno != EWOULDBLOCK) {
+	PSID_warn(-1, errno, "%s: sendMsg()", __func__);
+    }
+}
+
+/**
+ * @brief Handle PSP_CD_ACCOUNT message.
+ *
+ * Handle the message @a msg of type PSP_CD_ACCOUNT. If the message is
+ * destined to the local daemon, it will be forwarded to all registered
+ * accounter tasks. I.e. the message will be multiplexed if more than
+ * one accounter is registered through the corresponding @ref
+ * PSID_addAcct() calls.
+ *
+ * Delivery of messages to local accounters will be filtered by @ref
+ * localForward_ACCOUNT().
+ *
+ * @param msg Pointer to the message to handle.
+ *
+ * @return No return value.
+ */
+static void msg_ACCOUNT(DDTypedBufferMsg_t *msg)
 {
     PSID_log(PSID_LOG_VERB, "%s: from %s\n", __func__,
 	     PSC_printTID(msg->header.sender));
 
     if (msg->header.dest == PSC_getMyTID()) {
-	DDTypedBufferMsg_t *tmsg = (DDTypedBufferMsg_t *) msg;
-	list_t *pos, *tmp;
-	PStask_t *task;
-	char *ptr = tmsg->buf;
-	uid_t uid;
-	static int lastStartUID = -1;
-
-	/* skip logger TID and rank to get to UID */
-	ptr += sizeof(PStask_ID_t);
-	ptr += sizeof(int32_t);
-	uid = *(uid_t *)ptr;
-
-	/* save uid for next incoming slot msg */
-	if (tmsg->type == PSP_ACCOUNT_START) {
-	    lastStartUID = uid;
-	}
-
-	/* no uid in msg, used saved one */
-	if (tmsg->type == PSP_ACCOUNT_SLOTS) {
-	    uid = lastStartUID;
-	}
-
 	/* message for me, let's forward to all authorized accounters */
+	list_t *pos, *tmp;
 
 	list_for_each_safe(pos, tmp, &PSID_accounters) {
 	    PSID_acct_t *acct = list_entry(pos, PSID_acct_t, next);
-
-	    if (!(task = PStasklist_find(&managedTasks, acct->acct))) {
-		PSID_log(PSID_LOG_VERB, "%s: task for accounter '%s' not"
-			    " found\n", __func__, PSC_printTID(acct->acct));
-		continue;
-	    }
-
-	    /* Let accounter running as adminuser receive all accounting
-	     * messages. Unprivileged users may only see their own
-	     * accounting data.
-	     */
-	    if (task->uid != 0 && task->uid != uid &&
-		!PSIDnodes_testGUID(PSC_getMyID(), PSIDNODES_ADMUSER,
-			       (PSIDnodes_guid_t){.u=task->uid}) &&
-		!PSIDnodes_testGUID(PSC_getMyID(), PSIDNODES_ADMGROUP,
-			       (PSIDnodes_guid_t){.g=task->gid})) {
-		continue;
-	    }
-
 	    msg->header.dest = acct->acct;
-	    if (sendMsg(msg) == -1 && errno != EWOULDBLOCK) {
-		PSID_warn(-1, errno, "%s: sendMsg()", __func__);
+
+	    if (PSC_getID(acct->acct) == PSC_getMyID()) {
+		/* local accounter */
+		localForward_ACCOUNT(msg);
+	    } else {
+		if (sendMsg(msg) == -1 && errno != EWOULDBLOCK) {
+		    PSID_warn(-1, errno, "%s: sendMsg()", __func__);
+		}
 	    }
 	}
 	/* Restore original dest to allow re-use of msg */
 	msg->header.dest = PSC_getMyTID();
     } else if (PSC_getPID(msg->header.dest)) {
 	/* forward to accounter */
-	if (sendMsg(msg) == -1 && errno != EWOULDBLOCK) {
-	    PSID_warn(-1, errno, "%s: sendMsg()", __func__);
-	}
+	localForward_ACCOUNT(msg);
     }
 }
 
@@ -209,5 +238,5 @@ void initAccount(void)
 {
     PSID_log(PSID_LOG_VERB, "%s()\n", __func__);
 
-    PSID_registerMsg(PSP_CD_ACCOUNT, PSID_msgACCOUNT);
+    PSID_registerMsg(PSP_CD_ACCOUNT, (handlerFunc_t) msg_ACCOUNT);
 }

bin/daemon/psidaccount.h
@@ -104,21 +104,6 @@ void send_acct_OPTIONS(PStask_ID_t dest, int all);
  */
 int PSID_getNumAcct(void);
 
-/**
- * @brief Handle PSP_CD_ACCOUNT message.
- *
- * Handle the message @a msg of type PSP_CD_ACCOUNT. If the message is
- * destined to the local daemon it will be forwarded to all registered
- * accounter tasks. I.e. the message will be multiplexed if more than
- * one accounter is registered through the corresponding @ref
- * PSID_addAcct() calls.
- *
- * @param msg Pointer to the message to handle.
- *
- * @return No return value.
- */
-void PSID_msgACCOUNT(DDBufferMsg_t *msg);
-
 #ifdef __cplusplus
 }/* extern "C" */
 #endif