abe957839cb577986f72a8e4d503e8e8aaeeaa96
eicker <eicker@34291a86-62e8-0310-8159-f060f8a2ee4e>
2013-08-29 17:37:57 +0000

    Fix even one more copyright statement
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8874 34291a86-62e8-0310-8159-f060f8a2ee4e

--- include/env.h
+++ include/env.h
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2003 ParTec AG, Karlsruhe
- * Copyright (C) 2005 - 2013 Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2013 Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
