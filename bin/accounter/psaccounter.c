ParaStation/psmgmt

commit 68e3f2c967a272f1813d61abf8d568d495a287f9

Enhancement: Allow more than 32 cores per CPU. The new limit is 1024,     nevertheless the protocoll foresees future extensions.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8878 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/accounter/psaccounter.c
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2006-2011 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2006-2013 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -64,13 +64,6 @@ static char vcid[] __attribute__ ((used)) =
 #define DEFAULT_LOG_DIR LOCALSTATEDIR "/log/psaccounter"
 #define ERROR_WAIT_TIME	60
 
-/* children struct */
-typedef struct {
-    PStask_ID_t pid;
-    PStask_ID_t forwarderpid;
-    char *cmd;
-} Children_t;
-
 /* job structure */
 typedef struct {
     char *jobname;
@@ -79,7 +72,7 @@ typedef struct {
     char user[MAX_USERNAME_LEN];
     char group[MAX_GROUPNAME_LEN];
     PStask_ID_t logger;
-    PStask_ID_t submitHostID;
+    PSnodes_ID_t submitHostID;
     size_t exec_hosts_size;
     size_t countSlotMsg;
     size_t taskSize;
@@ -101,7 +94,6 @@ typedef struct {
     float cput;
     struct rusage rusage;
     struct timeval walltime;
-    struct Children_t *children;
     uint64_t maxrss;
     uint64_t maxvsize;
     uint64_t avgrss;
@@ -380,9 +372,10 @@ static void dumpJobs(struct t_node *leaf)
     if (leaf) {
 	if (leaf->job) {
 	    job = leaf->job;
-	    alog("job logger:%i child_started:%zi partition_size:%i user:%s "
-		 "group:%s cmd=%s exec_hosts=%s submitHost:%s\n", job->logger,
-		 job->procStartCount, job->partitionSize, job->user,
+	    alog("job logger: %s child_started:%zi partition_size:%i user:%s "
+		 "group:%s cmd=%s exec_hosts=%s submitHost:%s\n",
+		 PSC_printTID(job->logger), job->procStartCount,
+		 job->partitionSize, job->user,
 		 job->group, job->jobname, job->exec_hosts,
 		 accNodes[job->submitHostID].hostname);
 	}
@@ -516,8 +509,8 @@ static Job_t *handleComHeader(char **newptr, const char *func, int *rank)
 	    exit(EXIT_FAILURE);
 	}
 	if (debug & 0x020) {
-	    alog("%s (%s): got new job, logger pid:%i from function: %s\n",
-		 __func__, func, PSC_getPID(job->logger), func);
+	    alog("%s (%s): got new job, logger: %s from function: %s\n",
+		 __func__, func, PSC_printTID(job->logger), func);
 	}
     }
 
@@ -610,7 +603,7 @@ static void printAccEndMsg(char *chead, PStask_ID_t key)
     int procs;
 
     if (!job) {
-	alog("%s: couldn`t find job:%i\n", __func__, key);
+	alog("%s: couldn`t find job: %s\n", __func__, PSC_printTID(key));
 	return;
     }
 
@@ -749,7 +742,7 @@ static void printAccEndMsg(char *chead, PStask_ID_t key)
 	    whour, wmin, wsec, procs);
 
     if (!deleteJob(key)) {
-	alog("%s: error deleting job:%i\n", __func__, key);
+	alog("%s: error deleting job: %s\n", __func__, PSC_printTID(key));
     }
 }
 
@@ -771,7 +764,7 @@ static void timer_handler()
     }
 
     if (!(job = findJob(tid))) {
-	alog("%s: couldn't find job:%i\n", __func__, tid);
+	alog("%s: couldn't find job: %s\n", __func__, PSC_printTID(tid));
     } else {
 	time_t atime;
 	struct tm *ptm;
@@ -815,11 +808,9 @@ static void timer_handler()
  *
  * Handle the message @a msg of type PSP_ACCOUNT_QUEUE.
  *
+ * @param msgptr Pointer to the message to handle.
+ *
  * @param chead Header of the log entry.
- *
- * @param ptr Pointer to the message to handle.
- *
- * @param key The job id (equals the taskID of the logger).
  *
  * @return No return value.
  */
@@ -908,7 +899,7 @@ static void handleAccDeleteMsg(char *chead, PStask_ID_t logger)
     Job_t *job = findJob(logger);
 
     if (!job) {
-	alog("%s: couldn`t find job:%i\n", __func__, logger);
+	alog("%s: couldn`t find job: %s\n", __func__, PSC_printTID(logger));
 	return;
     }
 
@@ -919,7 +910,7 @@ static void handleAccDeleteMsg(char *chead, PStask_ID_t logger)
 
     /* delete job structure */
     if (!deleteJob(logger)) {
-	alog("%s: couldn`t delete job:%i\n", __func__, logger);
+	alog("%s: couldn`t delete job: %s\n", __func__, PSC_printTID(logger));
 	return;
     }
 }
@@ -1000,8 +991,8 @@ static void handleAccEndMsg(char *msgptr, char *chead, PStask_ID_t sender)
 	    if (!finddJob(job->logger)) {
 		insertdJob(job->logger);
 		if(debug & 0x010) {
-		    alog("%s: waiting for all children to exit, job:%i\n",
-			 __func__, job->logger);
+		    alog("%s: waiting for all children to exit, job: %s\n",
+			 __func__, PSC_printTID(job->logger));
 		}
 	    }
 	} else {
@@ -1183,7 +1174,7 @@ static void handleAccSlotsMsg(char *chead, DDTypedBufferMsg_t * msg)
 
     PStask_ID_t logger;
     unsigned int numSlots, slot;
-    size_t slotSize;
+    size_t nBytes, myBytes = PSCPU_bytesForCPUs(PSCPU_MAX);
     char sep[2] = "";
     Job_t *job;
 
@@ -1194,19 +1185,25 @@ static void handleAccSlotsMsg(char *chead, DDTypedBufferMsg_t * msg)
     job = findJob(logger);
 
     if (!job) {
-	alog("%s: couldn't find job:%i\n", __func__, logger);
+	alog("%s: couldn't find job: %s\n", __func__, PSC_printTID(logger));
 	return;
     }
 
-    /* number of Slots */
-    numSlots = *(uint32_t *) ptr;
-    ptr += sizeof(uint32_t);
+    /* skip child's uid */
+    ptr += sizeof(uid_t);
 
-    slotSize = (msg->header.len - sizeof(msg->header) - sizeof(msg->type)
-		- sizeof(PStask_ID_t) - sizeof(uint32_t)) / numSlots;
+    /* number of slots */
+    numSlots = *(uint16_t *)ptr;
+    ptr += sizeof(uint16_t);
 
-    /* - nodeID */
-    slotSize -= sizeof(PSnodes_ID_t);
+    /* size of CPUset part */
+    nBytes = *(uint16_t *)ptr;
+    ptr += sizeof(uint16_t);
+
+    if (nBytes > myBytes) {
+	alog("%s got too many CPUs: %zd > %zd for %s\n", __func__,
+	     nBytes*8, myBytes*8, PSC_printTID(logger));
+    }
 
     for (slot = 0; slot < numSlots; slot++) {
 	int bufleft;
@@ -1216,23 +1213,18 @@ static void handleAccSlotsMsg(char *chead, DDTypedBufferMsg_t * msg)
 	int16_t cpuID;
 
 	/* node_id */
-	if ((nodeID = *(PSnodes_ID_t *) ptr) >= nrOfNodes) {
-	    alog("%s got invalid nodeID:%i nrOfNodes:%i logger:%i\n", __func__,
-		 nodeID, nrOfNodes, logger);
+	nodeID = *(PSnodes_ID_t *)ptr;
+	ptr += sizeof(PSnodes_ID_t);
+	if (nodeID >= nrOfNodes) {
+	    alog("%s got invalid nodeID:%i nrOfNodes:%i logger: %s\n", __func__,
+		 nodeID, nrOfNodes, PSC_printTID(logger));
 	    return;
 	}
 	ptr += sizeof(PSnodes_ID_t);
 
-
-	switch (slotSize) {
-	case sizeof(PSCPU_set_t):
-	    /* cpuset of node */
-	    memcpy(CPUset, ptr, sizeof(PSCPU_set_t));
-	    ptr += sizeof(PSCPU_set_t);
-	    break;
-	default:
-	    protocolError("unknown slotSize", __func__);
-	}
+	PSCPU_clrAll(CPUset);
+	PSCPU_inject(CPUset, ptr, nBytes);
+	ptr += nBytes;
 
 	/* find cpu we are running on */
 	cpuID = PSCPU_first(CPUset, PSCPU_MAX);
@@ -1339,7 +1331,7 @@ static void handleAcctMsg(DDTypedBufferMsg_t * msg)
 	     PSC_getPID(logger));
 
     if (debug & 0x040) {
-	alog("%s: received new acc msg: type:%s, sender:%d, logger:%d\n",
+	alog("%s: received new acc msg: type:%s, sender: %s, logger:%s\n",
 	     __func__,
 	     msg->type == PSP_ACCOUNT_QUEUE ? "Queue" :
 	     msg->type == PSP_ACCOUNT_START ? "Start" :
@@ -1348,7 +1340,7 @@ static void handleAcctMsg(DDTypedBufferMsg_t * msg)
 	     msg->type == PSP_ACCOUNT_CHILD ? "Child" :
 	     msg->type == PSP_ACCOUNT_LOG ? "Log" :
 	     msg->type == PSP_ACCOUNT_END ? "End" : "?",
-	     PSC_getPID(sender), PSC_getPID(logger));
+	     PSC_printTID(sender), PSC_printTID(logger));
     }
 
     switch (msg->type) {

bin/admin/commands.c
@@ -1215,8 +1215,9 @@ void PSIADM_JobStat(PStask_ID_t task, PSpart_list_t opt)
     req = PSpart_newReq();
 
     while ((recvd=PSI_infoQueueNext(what, buf, sizeof(buf), 1)) > 0) {
-	static PSpart_slot_t *slotBuf = NULL;
-	static size_t slotBufSize = 0;
+	static PSpart_slot_t *slots = NULL;
+	static char *slotBuf = NULL;
+	static size_t slotsSize = 0, slotBufSize = 0;
 
 	PStask_ID_t tid;
 	PSpart_list_t flags;
@@ -1239,28 +1240,37 @@ void PSIADM_JobStat(PStask_ID_t task, PSpart_list_t opt)
 
 	if (req->num) {
 	    size_t itemSize;
-	    if (masterPSPversion < 334) {
-		itemSize = sizeof(PSnodes_ID_t);
-	    } else if (masterDaemonPSPversion < 401) {
-		itemSize = sizeof(PSpart_oldSlot_t);
-	    } else {
-		itemSize = sizeof(PSpart_slot_t);
-	    }
 
-	    if (req->num * sizeof(*slotBuf) > slotBufSize) {
-		slotBufSize = 2 * req->num * sizeof(*slotBuf);
+	    if (req->num * sizeof(PSpart_slot_t) > slotBufSize) {
+		slotBufSize = 2 * req->num * sizeof(PSpart_slot_t);
 		slotBuf = realloc(slotBuf, slotBufSize);
 	    }
+	    if (req->num * sizeof(*slots) > slotsSize) {
+		slotsSize = 2 * req->num * sizeof(*slots);
+		slots = realloc(slots, slotsSize);
+	    }
 
-	    if (!slotBuf) {
+	    if (!slotBuf || !slots) {
 		printf("No memory\n");
 		break;
 	    }
 
 	    recvd=PSI_infoQueueNext(what, slotBuf, slotBufSize, 1);
 
+	    if (masterPSPversion < 334) {
+		itemSize = sizeof(PSnodes_ID_t);
+	    } else if (masterDaemonPSPversion < 401) {
+		itemSize = sizeof(PSpart_oldSlot_t);
+	    } else if (masterPSPversion < 408) {
+		itemSize = PSCPU_bytesForCPUs(32);
+	    } else {
+		itemSize = *(uint16_t *)slotBuf;
+		recvd -= sizeof(uint16_t);
+	    }
+
 	    if ((unsigned int)recvd != req->num * itemSize) {
-		printf("Message lost\n");
+		printf("Message lost. Suppress node-list\n");
+		req->num = 0;
 	    }
 	}
 
@@ -1285,7 +1295,28 @@ void PSIADM_JobStat(PStask_ID_t task, PSpart_list_t opt)
 		    printOldSlots(req->num, (PSpart_oldSlot_t *) slotBuf,
 				  width-47, 47);
 		} else {
-		    printSlots(req->num, slotBuf, width-47, 47);
+		    size_t nBytes, myBytes = PSCPU_bytesForCPUs(PSCPU_MAX);
+		    char *ptr = slotBuf;
+		    int n;
+		    if (masterPSPversion < 408) {
+			nBytes = PSCPU_bytesForCPUs(32);
+		    } else {
+			nBytes = *(uint16_t *)ptr;
+			ptr +=  sizeof(uint16_t);
+		    }
+		    if (nBytes > myBytes) {
+			printf("warning: slots might be truncated");
+		    }
+		    if (getenv("PSIADMIN_DEBUG")) printf("[%zd]", nBytes);
+		    for (n = 0; n < req->num; n++) {
+			slots[n].node = *(PSnodes_ID_t *)ptr;
+			ptr += sizeof(PSnodes_ID_t);
+
+			PSCPU_clrAll(slots[n].CPUset);
+			PSCPU_inject(slots[n].CPUset, ptr, nBytes);
+			ptr += nBytes;
+		    }
+		    printSlots(req->num, slots, width-47, 47);
 		}
 		printf("\n");
 	    } else if (masterDaemonPSPversion < 407) {

bin/daemon/psidpartition.c
@@ -1670,9 +1670,11 @@ static int sendNodelist(PSpart_request_t *request, DDBufferMsg_t *msg)
 
 /**
  * Chunk-size for PSP_DD_PROVIDEPARTSL and PSP_DD_PROVIDETASKSL
- * messages
+ * messages.
+ *
+ * This definition is only for compatibility with older version.
  */
-#define SLOTS_CHUNK (int)(1024 / sizeof(PSpart_slot_t))
+#define SLOTS_CHUNK 128
 
 /**
  * @brief Send a list of slots.
@@ -1685,9 +1687,9 @@ static int sendNodelist(PSpart_request_t *request, DDBufferMsg_t *msg)
  * length (in the .len field) has to correctly represent the messages
  * preset content.
  *
- * In order to send the list of slots, it is split into chunks of @ref
- * SLOTS_CHUNK entries. Each chunk is copied into the message and send
- * separately to its destination.
+ * In order to send the list of slots, it is split into chunks. Each
+ * chunk is copied into the message and send separately to its
+ * destination.
  *
  * @param slots The list of slots to send.
  *
@@ -1706,6 +1708,8 @@ static int sendSlotlist(PSpart_slot_t *slots, int num, DDBufferMsg_t *msg)
     int destPSPver = PSIDnodes_getProtoV(PSC_getID(msg->header.dest));
     int destDmnPSPver = PSIDnodes_getDmnProtoV(PSC_getID(msg->header.dest));
     int bufOffset = msg->header.len - sizeof(msg->header);
+    unsigned short maxCPUs = 0;
+    int slotsChunk, n;
 
     PSID_log(PSID_LOG_PART, "%s(%s)\n", __func__,
 	     PSC_printTID(msg->header.dest));
@@ -1715,9 +1719,27 @@ static int sendSlotlist(PSpart_slot_t *slots, int num, DDBufferMsg_t *msg)
 	return -1;
     }
 
+    /* Determine maximum number of CPUs */
+    for (n = 0; n < num; n++) {
+	unsigned short cpus = PSIDnodes_getVirtCPUs(slots[n].node);
+	if (cpus > maxCPUs) maxCPUs = cpus;
+    }
+    if (!maxCPUs) {
+	PSID_log(-1, "%s: No remote CPUs\n", __func__);
+	return -1;
+    }
+
+    if (destPSPver < 334) {
+	slotsChunk = SLOTS_CHUNK;
+    } else if (destDmnPSPver < 408) {
+	slotsChunk = SLOTS_CHUNK;
+    } else {
+	slotsChunk = 1024/(sizeof(PSnodes_ID_t)+PSCPU_bytesForCPUs(maxCPUs));
+    }
+
     while (offset < num && PSIDnodes_isUp(PSC_getID(msg->header.dest))) {
 	PSpart_slot_t *mySlots = slots+offset;
-	int chunk = (num-offset > SLOTS_CHUNK) ? SLOTS_CHUNK : num-offset;
+	int chunk = (num-offset > slotsChunk) ? slotsChunk : num-offset;
 	char *ptr = msg->buf + bufOffset;
 	msg->header.len = sizeof(msg->header) + bufOffset;
 
@@ -1728,24 +1750,40 @@ static int sendSlotlist(PSpart_slot_t *slots, int num, DDBufferMsg_t *msg)
 	if (destPSPver < 334) {
 	    /* Map to nodelist for compatibility reasons */
 	    PSnodes_ID_t *nodeBuf = (PSnodes_ID_t *)ptr;
-	    int n;
-	    for (n=0; n<chunk; n++) nodeBuf[n] = mySlots[n].node;
+	    for (n = 0; n < chunk; n++) nodeBuf[n] = mySlots[n].node;
 	    msg->header.len += chunk * sizeof(*nodeBuf);
 	} else if (destDmnPSPver < 401) {
 	    /* Map to old slotlist for compatibility reasons */
 	    PSpart_oldSlot_t *oldSlots = (PSpart_oldSlot_t *)ptr;
-	    int n;
-	    for (n=0; n<chunk; n++) {
+	    for (n = 0; n < chunk; n++) {
 		PSnodes_ID_t node = oldSlots[n].node = mySlots[n].node;
 		oldSlots[n].cpu = PSCPU_first(mySlots[n].CPUset,
 					      PSIDnodes_getPhysCPUs(node));
 	    }
 	    msg->header.len += chunk * sizeof(*oldSlots);
 	} else {
-	    memcpy(ptr, mySlots, chunk * sizeof(*slots));
-	    //ptr += chunk * sizeof(*slots);
-	    msg->header.len += chunk * sizeof(*slots);
+	    size_t nBytes;
+
+	    if (destDmnPSPver < 408) {
+		nBytes = PSCPU_bytesForCPUs(32);
+	    } else {
+		nBytes = PSCPU_bytesForCPUs(maxCPUs);
+		*(uint16_t *)ptr = nBytes;
+		ptr += sizeof(uint16_t);
+		msg->header.len += sizeof(uint16_t);
+	    }
+
+	    for (n = 0; n < chunk; n++) {
+		*(PSnodes_ID_t *)ptr = mySlots[n].node;
+		ptr += sizeof(PSnodes_ID_t);
+		msg->header.len += sizeof(PSnodes_ID_t);
+
+		PSCPU_extract(ptr, mySlots[n].CPUset, nBytes);
+		ptr += nBytes;
+		msg->header.len += nBytes;
+	    }
 	}
+
 	offset += chunk;
 	PSID_log(PSID_LOG_PART, "%s: send chunk of %d (size %d)\n", __func__,
 		 chunk, msg->header.len);
@@ -2467,20 +2505,20 @@ static void appendToSlotlist(DDBufferMsg_t *inmsg, PSpart_request_t *request)
     int dmnPSPver = PSIDnodes_getDmnProtoV(PSC_getID(inmsg->header.sender));
     PSpart_slot_t *slots = request->slots + request->sizeGot;
     char *ptr = inmsg->buf;
-    int chunk = *(int16_t *)ptr;
+    int chunk = *(int16_t *)ptr, n;
     ptr += sizeof(int16_t);
+
+    PSID_log(PSID_LOG_PART, "%s(%s)\n", __func__, PSC_printTID(request->tid));
 
     if (PSPver < 334) {
 	PSnodes_ID_t *nodeBuf = (PSnodes_ID_t *)ptr;
-	int n;
-	for (n=0; n<chunk; n++) {
+	for (n = 0; n < chunk; n++) {
 	    slots[n].node = nodeBuf[n];
 	    PSCPU_setAll(slots[n].CPUset);
 	}
     } else if (dmnPSPver < 401) {
 	PSpart_oldSlot_t *oldSlots = (PSpart_oldSlot_t *)ptr;
-	int n;
-	for (n=0; n<chunk; n++) {
+	for (n = 0; n < chunk; n++) {
 	    slots[n].node = oldSlots[n].node;
 	    if (oldSlots[n].cpu == -1) {
 		PSCPU_setAll(slots[n].CPUset);
@@ -2489,7 +2527,28 @@ static void appendToSlotlist(DDBufferMsg_t *inmsg, PSpart_request_t *request)
 	    }
 	}
     } else {
-	memcpy(slots, ptr, chunk * sizeof(*slots));
+	size_t nBytes, myBytes = PSCPU_bytesForCPUs(PSCPU_MAX);
+	if (dmnPSPver < 408) {
+	    nBytes = PSCPU_bytesForCPUs(32);
+	} else {
+	    nBytes = *(uint16_t *)ptr;
+	    ptr += sizeof(uint16_t);
+	}
+
+	if (nBytes > myBytes) {
+	    PSID_log(-1, "%s(%s): too many CPUs: %zd > %zd\n", __func__,
+		     PSC_printTID(request->tid), nBytes*8, myBytes*8);
+	}
+	PSID_log(PSID_LOG_PART, "%s: Using %zd bytes\n", __func__, nBytes);
+
+	for (n = 0; n < chunk; n++) {
+	    slots[n].node = *(PSnodes_ID_t *)ptr;
+	    ptr += sizeof(PSnodes_ID_t);
+
+	    PSCPU_clrAll(slots[n].CPUset);
+	    PSCPU_inject(slots[n].CPUset, ptr, nBytes);
+	    ptr += nBytes;
+	}
     }
     request->sizeGot += chunk;
 }
@@ -2875,10 +2934,20 @@ static void msg_GETNODES(DDBufferMsg_t *inmsg)
 	    //ptr = (char *)&oldSlots[num];
 	    msg.header.len += num * sizeof(*oldSlots);
 	} else if (dmnPSPver < 402) {
+	    unsigned int n;
+	    size_t nBytes = PSCPU_bytesForCPUs(32);
+
 	    if (num > SLOTS_CHUNK) goto error;
-	    memcpy(ptr, slots, num * sizeof(*slots));
-	    //ptr += num * sizeof(*slots);
-	    msg.header.len += num * sizeof(*slots);
+
+	    for (n = 0; n < num; n++) {
+		*(PSnodes_ID_t *)ptr = slots[n].node;
+		ptr += sizeof(PSnodes_ID_t);
+		msg.header.len += sizeof(PSnodes_ID_t);
+
+		PSCPU_extract(ptr, slots[n].CPUset, nBytes);
+		ptr += nBytes;
+		msg.header.len += nBytes;
+	    }
 	} else {
 	    *(int16_t *)ptr = num;
 	    //ptr += sizeof(int16_t);
@@ -2979,20 +3048,25 @@ static void msg_GETRANKNODE(DDBufferMsg_t *inmsg)
 	ptr += sizeof(int32_t);
 	msg.header.len += sizeof(int32_t);
 
-	if (dmnPSPver >= 402) {
+	if (dmnPSPver < 402) {
+	    size_t nBytes = PSCPU_bytesForCPUs(32);
+
+	    *(PSnodes_ID_t *)ptr = slot->node;
+	    ptr += sizeof(PSnodes_ID_t);
+	    msg.header.len += sizeof(PSnodes_ID_t);
+
+	    PSCPU_extract(ptr, slot->CPUset, nBytes);
+	    //ptr += nBytes;
+	    msg.header.len += nBytes;
+
+	    sendMsg(&msg);
+	} else {
 	    *(int16_t *)ptr = 1; /* requested */
-	    ptr += sizeof(int16_t);
+	    //ptr += sizeof(int16_t);
 	    msg.header.len += sizeof(int16_t);
-	    *(int16_t *)ptr = 1; /* num */
-	    ptr += sizeof(int16_t);
-	    msg.header.len += sizeof(int16_t);
+
+	    sendSlotlist(slot, 1, &msg);
 	}
-
-	memcpy(ptr, slot, sizeof(*slot));
-	//ptr += sizeof(*slot);
-	msg.header.len += sizeof(*slot);
-
-	sendMsg(&msg);
 
 	return;
     }
@@ -3033,8 +3107,10 @@ static void msg_NODESRES(DDBufferMsg_t *inmsg)
 	PStask_t *task = PStasklist_find(&managedTasks, inmsg->header.dest);
 	char *ptr = inmsg->buf;
 	int num = inmsg->header.len - sizeof(inmsg->header) - sizeof(int32_t);
-	int nextRank = *(int32_t *)ptr, requested = 0;
+	int nextRank, requested = 0, n;
 	PSpart_slot_t *slots;
+
+	nextRank = *(int32_t *)ptr;
 	ptr += sizeof(int32_t);
 
 	PSID_log(PSID_LOG_PART, "%s(%s)\n", __func__,
@@ -3049,7 +3125,7 @@ static void msg_NODESRES(DDBufferMsg_t *inmsg)
 	if (dmnPSPver < 401) {
 	    num /= sizeof(PSpart_oldSlot_t);
 	} else if (dmnPSPver < 402) {
-	    num /= sizeof(PSpart_slot_t);
+	    num /= PSCPU_bytesForCPUs(32);
 	} else {
 	    requested = *(int16_t *)ptr;
 	    ptr += sizeof(int16_t);
@@ -3075,8 +3151,7 @@ static void msg_NODESRES(DDBufferMsg_t *inmsg)
 
 	if (dmnPSPver < 401) {
 	    PSpart_oldSlot_t *oldSlots = (PSpart_oldSlot_t *)ptr;
-	    int n;
-	    for (n=0; n<num; n++) {
+	    for (n = 0; n < num; n++) {
 		slots[n].node = oldSlots[n].node;
 		if (oldSlots[n].cpu == -1) {
 		    PSCPU_setAll(slots[n].CPUset);
@@ -3084,10 +3159,28 @@ static void msg_NODESRES(DDBufferMsg_t *inmsg)
 		    PSCPU_setCPU(slots[n].CPUset, oldSlots[n].cpu);
 		}
 	    }
-	} else if (dmnPSPver < 402) {
-	    memcpy(slots, ptr, num * sizeof(*slots));
 	} else {
-	    memcpy(slots, ptr, num * sizeof(*slots));
+	    size_t nBytes, myBytes = PSCPU_bytesForCPUs(PSCPU_MAX);
+	    if (dmnPSPver < 408) {
+		nBytes = PSCPU_bytesForCPUs(32);
+	    } else {
+		nBytes = *(uint16_t *)ptr;
+		ptr += sizeof(uint16_t);
+	    }
+
+	    if (nBytes > myBytes) {
+		PSID_log(-1, "%s(%s): too many CPUs: %zd > %zd\n", __func__,
+			 PSC_printTID(inmsg->header.dest), nBytes*8, myBytes*8);
+	    }
+
+	    for (n = 0; n < num; n++) {
+		slots[n].node = *(PSnodes_ID_t *)ptr;
+		ptr += sizeof(PSnodes_ID_t);
+
+		PSCPU_clrAll(slots[n].CPUset);
+		PSCPU_inject(slots[n].CPUset, ptr, nBytes);
+		ptr += nBytes;
+	    }
 	}
 	task->spawnNum += num;
 
@@ -3096,12 +3189,11 @@ static void msg_NODESRES(DDBufferMsg_t *inmsg)
 	    PSpart_slot_t *slots = task->spawnNodes + nextRank;
 	    PSnodes_ID_t *nodeBuf = (PSnodes_ID_t *)(inmsg->buf
 						     + sizeof(int32_t));
-	    int n;
 
 	    inmsg->header.type = PSP_CD_NODESRES;
 	    inmsg->header.len = sizeof(inmsg->header) + sizeof(int32_t);
 
-	    for (n=0; n<requested; n++) nodeBuf[n] = slots[n].node;
+	    for (n = 0; n < requested; n++) nodeBuf[n] = slots[n].node;
 	    inmsg->header.len += requested * sizeof(*nodeBuf);
 	} else {
 	    return;
@@ -3486,7 +3578,14 @@ static void sendReqList(PStask_ID_t dest, list_t *queue, PSpart_list_t opt)
 	msg.header.len -= len;
 
 	if (num) {
-	    int offset = 0;
+	    int offset = 0, n;
+	    unsigned short maxCPUs = 0;
+
+	    /* Determine maximum number of CPUs */
+	    for (n = 0; n < num; n++) {
+		unsigned short cpus = PSIDnodes_getVirtCPUs(req->slots[n].node);
+		if (cpus > maxCPUs) maxCPUs = cpus;
+	    }
 
 	    msg.type = PSP_INFO_QUEUE_SEP;
 	    if (sendMsg(&msg) == -1 && errno != EWOULDBLOCK) {
@@ -3498,27 +3597,49 @@ static void sendReqList(PStask_ID_t dest, list_t *queue, PSpart_list_t opt)
 	    while (offset < num) {
 		int chunk =
 		    (num-offset > SLOTS_CHUNK) ? SLOTS_CHUNK : num-offset;
+		int n;
 		PSpart_slot_t *slots = req->slots+offset;
+
 		ptr = msg.buf;
 
 		if (PSPver < 334) {
 		    PSnodes_ID_t *nodeBuf = (PSnodes_ID_t *)ptr;
-		    int n;
-		    for (n=0; n<chunk; n++) nodeBuf[n] = slots[n].node;
+		    for (n = 0; n < chunk; n++) nodeBuf[n] = slots[n].node;
 		    len = chunk * sizeof(*nodeBuf);
 		} else if (dmnPSPver < 401) {
 		    PSpart_oldSlot_t *oldSlots = (PSpart_oldSlot_t *)ptr;
-		    int n;
-		    for (n=0; n<chunk; n++) {
+		    for (n = 0; n < chunk; n++) {
 			PSnodes_ID_t node = oldSlots[n].node = slots[n].node;
 			oldSlots[n].cpu = PSCPU_first(slots[n].CPUset,
 						PSIDnodes_getPhysCPUs(node));
 		    }
 		    len = chunk * sizeof(*oldSlots);
 		} else {
-		    memcpy(ptr, slots, chunk * sizeof(*slots));
-		    len = chunk * sizeof(*slots);
+		    size_t nBytes;
+		    len = 0;
+
+		    if (dmnPSPver < 408) {
+			nBytes = PSCPU_bytesForCPUs(32);
+		    } else {
+			nBytes = PSCPU_bytesForCPUs(maxCPUs);
+			if (!offset) {
+			    *(uint16_t *)ptr = nBytes;
+			    ptr += sizeof(uint16_t);
+			    len += sizeof(uint16_t);
+			}
+		    }
+
+		    for (n = 0; n < chunk; n++) {
+			*(PSnodes_ID_t *)ptr = slots[n].node;
+			ptr += sizeof(PSnodes_ID_t);
+			len += sizeof(PSnodes_ID_t);
+
+			PSCPU_extract(ptr, slots[n].CPUset, nBytes);
+			ptr += nBytes;
+			len += nBytes;
+		    }
 		}
+
 		offset += chunk;
 
 		msg.header.len += len;

commit 57eefbcc1c27cef211acb17a3f1fd1939b5402a1

Bugfix: Fix problems introduced in r8878 Bugfix: Fix psiadmin's output for 'list jobs'
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8879 34291a86-62e8-0310-8159-f060f8a2ee4e

bin/accounter/psaccounter.c
@@ -1220,7 +1220,6 @@ static void handleAccSlotsMsg(char *chead, DDTypedBufferMsg_t * msg)
 		 nodeID, nrOfNodes, PSC_printTID(logger));
 	    return;
 	}
-	ptr += sizeof(PSnodes_ID_t);
 
 	PSCPU_clrAll(CPUset);
 	PSCPU_inject(CPUset, ptr, nBytes);

bin/admin/commands.c
@@ -1261,11 +1261,14 @@ void PSIADM_JobStat(PStask_ID_t task, PSpart_list_t opt)
 		itemSize = sizeof(PSnodes_ID_t);
 	    } else if (masterDaemonPSPversion < 401) {
 		itemSize = sizeof(PSpart_oldSlot_t);
-	    } else if (masterPSPversion < 408) {
-		itemSize = PSCPU_bytesForCPUs(32);
 	    } else {
-		itemSize = *(uint16_t *)slotBuf;
-		recvd -= sizeof(uint16_t);
+		itemSize = sizeof(PSnodes_ID_t);
+		if (masterDaemonPSPversion < 408) {
+		    itemSize += PSCPU_bytesForCPUs(32);
+		} else {
+		    itemSize += *(uint16_t *)slotBuf;
+		    recvd -= sizeof(uint16_t);
+		}
 	    }
 
 	    if ((unsigned int)recvd != req->num * itemSize) {
@@ -1277,7 +1280,7 @@ void PSIADM_JobStat(PStask_ID_t task, PSpart_list_t opt)
 	if (!task || rootTID == tid) {
 	    if (!found) {
 		printf("%22s %5s %5s %5s %5s %-*s\n",
-		       "RootTaskId", "State", "Size", "UID ", "GID ",
+		       "RootTaskId", "State", "Size", " UID", " GID",
 		       width-47, req->num ? "Target Slots" : "Starttime");
 	    }
 
@@ -1298,7 +1301,7 @@ void PSIADM_JobStat(PStask_ID_t task, PSpart_list_t opt)
 		    size_t nBytes, myBytes = PSCPU_bytesForCPUs(PSCPU_MAX);
 		    char *ptr = slotBuf;
 		    int n;
-		    if (masterPSPversion < 408) {
+		    if (masterDaemonPSPversion < 408) {
 			nBytes = PSCPU_bytesForCPUs(32);
 		    } else {
 			nBytes = *(uint16_t *)ptr;
@@ -1307,7 +1310,6 @@ void PSIADM_JobStat(PStask_ID_t task, PSpart_list_t opt)
 		    if (nBytes > myBytes) {
 			printf("warning: slots might be truncated");
 		    }
-		    if (getenv("PSIADMIN_DEBUG")) printf("[%zd]", nBytes);
 		    for (n = 0; n < req->num; n++) {
 			slots[n].node = *(PSnodes_ID_t *)ptr;
 			ptr += sizeof(PSnodes_ID_t);

bin/daemon/psidpartition.c
@@ -2539,7 +2539,6 @@ static void appendToSlotlist(DDBufferMsg_t *inmsg, PSpart_request_t *request)
 	    PSID_log(-1, "%s(%s): too many CPUs: %zd > %zd\n", __func__,
 		     PSC_printTID(request->tid), nBytes*8, myBytes*8);
 	}
-	PSID_log(PSID_LOG_PART, "%s: Using %zd bytes\n", __func__, nBytes);
 
 	for (n = 0; n < chunk; n++) {
 	    slots[n].node = *(PSnodes_ID_t *)ptr;