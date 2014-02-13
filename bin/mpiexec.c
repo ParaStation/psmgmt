b005d9e31b1d8e97f07004b413340ba862a2f0a3
eicker <eicker@21b3a009-d9f9-44df-8eaf-6b729cfaa075>
2014-02-13 20:30:23 +0000

    Fix whitespace formatting.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8900 21b3a009-d9f9-44df-8eaf-6b729cfaa075

--- bin/mpiexec.c
+++ bin/mpiexec.c
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2007-2013 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2007-2014 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -567,8 +567,9 @@ static void createSpawner(int argc, char *argv[], int np, int admin)
 	    PSI_infoList(-1, PSP_INFO_LIST_PARTITION, NULL,
 			 nodeList, pSize*sizeof(*nodeList), 0);
 	    startNode = (getenv("__MPIEXEC_DIST_START") ?
-			    getNodeIDbyIndex(np, 1) :  PSC_getMyID());
-	    setPSIEnv("__MPIEXEC_DIST_START", getenv("__MPIEXEC_DIST_START"), 1);
+			    getNodeIDbyIndex(np, 1) : PSC_getMyID());
+	    setPSIEnv("__MPIEXEC_DIST_START",
+		      getenv("__MPIEXEC_DIST_START"), 1);
 	} else {
 	    getFirstNodeID(&startNode);
 	}
