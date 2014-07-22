f00d23565a90375d246d8a9a96f0cab9bbdbd19b
Dorian Krause <d.krause@fz-juelich.de>
2014-07-22 14:38:31 +0200

    Add hostname test

---
+++ plugins/psslurm/regtest/tests/hostname/eval.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import traceback
+import re
+
+RETVAL = 0
+
+def Assert(x):
+	global RETVAL
+
+	if not x:
+		sys.stderr.write("Test failure:\n")
+		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
+		RETVAL = 1
+
+for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
+	P = p.upper()
+
+	Assert("0:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P])
+	Assert("COMPLETED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P])
+
+	try:
+		out = open(os.environ["PSTEST_SCONTROL_%s_STD_OUT" % P]).read()
+	except Exception as e:
+		Assert(not str(e))
+
+	# TODO This is Juropa 3 specific!
+	Assert(7 == len(out))
+	Assert(None != re.match(r'j3c.*', out))
+
+sys.exit(RETVAL)
+
