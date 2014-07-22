be277b83028bdab40f3465277393b3fbb75a52f9
Dorian Krause <d.krause@fz-juelich.de>
2014-07-22 14:40:07 +0200

    Add prolog-epilog-rel test

---
+++ plugins/psslurm/regtest/tests/prolog-epilog-rel/eval.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import traceback
+import re
+import pprint
+
+RETVAL = 0
+
+def Assert(x, msg = None):
+	global RETVAL
+
+	if not x:
+		if msg:
+			sys.stderr.write("Test failure ('%s'):\n" % msg)
+		else:
+			sys.stderr.write("Test failure:\n")
+		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
+		RETVAL = 1
+
+for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
+	P = p.upper()
+
+	Assert("0:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P], p)
+	Assert("COMPLETED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P], p)
+
+	jobid = os.environ["PSTEST_SCONTROL_%s_JOB_ID" % P]
+
+	print(jobid)
+	print("output/prolog-%s.txt" % jobid)
+
+	out = ""
+	try:
+		out = open("output/prolog-%s.txt" % jobid).read()
+	except Exception as e:
+		Assert(1 == 0, p + ": " + str(e))
+	Assert("OK\n" == out, p)
+
+	out = ""
+	try:
+		out = open("output/epilog-%s.txt" % jobid).read()
+	except Exception as e:
+		Assert(1 == 0, p + ": " + str(e))
+	Assert("OK\n" == out, p)
+
+sys.exit(RETVAL)
+
