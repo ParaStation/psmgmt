49554ccf9d92c28e023dcb3858c5bf0845eee948
Dorian Krause <d.krause@fz-juelich.de>
2014-07-22 16:01:50 +0200

    Add stdin-fwd-5 test

---
+++ plugins/psslurm/regtest/tests/stdin-fwd-5/submit.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+import select
+import re
+
+
+cmd = ["srun", "-N", "2", "-n", "64", "-t", "2", "-p", os.environ["PSTEST_PARTITION"]]
+if "" != os.environ["PSTEST_RESERVATION"]:
+	cmd += ["--reservation", os.environ["PSTEST_RESERVATION"]]
+cmd += ["-i", "5", "./read.py"]
+
+p = subprocess.Popen(cmd, \
+                     stdout = subprocess.PIPE, \
+                     stderr = subprocess.PIPE, \
+                     stdin  = subprocess.PIPE)
+
+interact = 0
+
+stdout = ""
+stderr = ""
+
+while 1:
+	x = p.poll()
+	if None != x:
+		sys.stdout.write(stdout)
+		sys.stderr.write(stderr)
+		sys.exit(x)
+
+	ready, _, _ = select.select([p.stdout, p.stderr], [], [], 1)
+	for x in ready:
+		line = x.readline()
+		if "" != line.strip():
+			if re.match(r'.*allocated resources.*', line):
+				p.stdin.write("OK\n")
+				interact = 0
+
+		if p.stdout == x:
+			stdout += line
+		if p.stderr == x:
+			stderr += line	
+
