8093086e1faf3df363878a53ac455fec40a1e09d
Dorian Krause <d.krause@fz-juelich.de>
2014-07-23 10:28:28 +0200

    Add ticket-68 test

---
+++ plugins/psslurm/regtest/tests/ticket-68/eval.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import traceback
+import re
+import pprint
+
+RETVAL = 0
+
+def Assert(x, msg = None):
+	global RETVAL
+
+	if not x:
+		if msg:
+			sys.stderr.write("Test failure ('%s'):\n" % msg)
+		else:
+			sys.stderr.write("Test failure:\n")
+		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
+		RETVAL = 1
+
+stdout = {}
+
+for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
+	P = p.upper()
+
+	Assert("0:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P], p)
+	Assert("COMPLETED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P], p)
+
+	Assert(os.path.isfile("output/slurm-%s.out" % os.environ["PSTEST_SCONTROL_%s_JOB_ID" % P]), p)
+	Assert(not os.path.isfile("slurm-%s.out" % os.environ["PSTEST_SCONTROL_%s_JOB_ID" % P]), p)
+
+sys.exit(RETVAL)
+
