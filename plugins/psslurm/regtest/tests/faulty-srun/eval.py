acd2abf9ed9cd276ad90c0c89c0d03c3adbb34dd
Dorian Krause <d.krause@fz-juelich.de>
2014-07-23 11:44:32 +0200

    Add faulty-srun test

---
+++ plugins/psslurm/regtest/tests/faulty-srun/eval.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import traceback
+import re
+import pprint
+
+RETVAL = 0
+
+def Assert(x, msg = None):
+	global RETVAL
+
+	if not x:
+		if msg:
+			sys.stderr.write("Test failure ('%s'):\n" % msg)
+		else:
+			sys.stderr.write("Test failure:\n")
+		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
+		RETVAL = 1
+
+stdout = {}
+
+for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
+	P = p.upper()
+
+	Assert("2:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P], p)
+	Assert("FAILED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P], p)
+
+sys.exit(RETVAL)
+
