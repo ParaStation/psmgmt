6aa98f536563309e25228a0bb285ce06fd6cb4b5
Dorian Krause <d.krause@fz-juelich.de>
2014-07-23 14:56:16 +0200

    Add ticket-20 commit

---
+++ plugins/psslurm/regtest/tests/ticket-20/eval.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import traceback
+import re
+import pprint
+
+RETVAL = 0
+
+def Assert(x, msg = None):
+	global RETVAL
+
+	if not x:
+		if msg:
+			sys.stderr.write("Test failure ('%s'):\n" % msg)
+		else:
+			sys.stderr.write("Test failure:\n")
+		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
+		RETVAL = 1
+
+for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
+	P = p.upper()
+
+	Assert("0:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P], p)
+	Assert("COMPLETED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P], p)
+
+	try:
+		out = open(os.environ["PSTEST_SCONTROL_%s_STD_OUT" % P]).read()
+	except Exception as e:
+		Assert(1 == 0, p + ": " + str(e))
+
+	lines = [x for x in map(lambda z: z.strip(), out.split("\n")) if len(x) > 0 and not re.match(r'.*error.*', x)]
+	Assert(1 == len(lines), p)
+
+	time = int(lines[0])
+	Assert(time < 120, p + ": time = %d" % time)
+
+sys.exit(RETVAL)
+
---
+++ plugins/psslurm/regtest/tests/ticket-20/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+srun -n 1 -t 1 python ./loop.py
+
