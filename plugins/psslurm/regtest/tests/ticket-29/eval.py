20ea9dc161cbf35b96fdb38d1b6e9ca0f613115e
Dorian Krause <d.krause@fz-juelich.de>
2014-07-23 11:30:35 +0200

    Add ticket-29 test

---
+++ plugins/psslurm/regtest/tests/ticket-29/eval.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import traceback
+import re
+import pprint
+
+RETVAL = 0
+
+def Assert(x, msg = None):
+	global RETVAL
+
+	if not x:
+		if msg:
+			sys.stderr.write("Test failure ('%s'):\n" % msg)
+		else:
+			sys.stderr.write("Test failure:\n")
+		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
+		RETVAL = 1
+
+stdout = {}
+
+for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
+	P = p.upper()
+
+	Assert("1:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P], p)
+	Assert("FAILED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P], p)
+
+	try:
+		out = open(os.environ["PSTEST_SCONTROL_%s_STD_OUT" % P]).read()
+	except Exception as e:
+		Assert(1 == 0, p + ": " + str(e))
+
+	print(p + ": '%s'" % out)
+	Assert(not re.match(r'.*ARRAY.*', out), p)
+
+sys.exit(RETVAL)
+
