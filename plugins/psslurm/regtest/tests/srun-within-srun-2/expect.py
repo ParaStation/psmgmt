9a33ee559a093f772cafc7f078a8323aeecbaf65
Dorian Krause <d.krause@fz-juelich.de>
2014-07-24 09:23:41 +0200

    Add srun-within-srun-2 test

---
+++ plugins/psslurm/regtest/tests/srun-within-srun-2/expect.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+cmd = "srun -N 1 -t 1 -p %s " % os.environ["PSTEST_PARTITION"]
+if "" != os.environ["PSTEST_RESERVATION"]:
+	cmd += "--reservation %s " % os.environ["PSTEST_RESERVATION"]
+cmd += "--pty /bin/bash"
+
+stdin = """
+set timeout -1
+
+spawn %s
+
+expect -re "queued and waiting for resources"
+expect -re "has been allocated resources"
+expect -re "bash" { send "srun -n 1 hostname\n" }
+expect -re "bash" { send "echo $?\n" }
+expect -re "bash" { send "exit\n" }
+expect eof
+
+catch wait result
+exit [lindex $result 3]
+""" % cmd
+
+p = subprocess.Popen(["/usr/bin/expect", "-"], stdin = subprocess.PIPE)
+p.communicate(stdin)
+
+sys.exit(p.wait())
+
