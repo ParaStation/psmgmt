3851e68b6d36f932d9bb8efe121a1b2b0e05e55a
Dorian Krause <d.krause@fz-juelich.de>
2014-07-24 16:01:35 +0200

    tests: Improve error reporting

--- plugins/psslurm/regtest/tests/srun-env/eval.py
+++ plugins/psslurm/regtest/tests/srun-env/eval.py
@@ -26,8 +26,8 @@ env = {}
 for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
 	P = p.upper()
 
-	Assert("0:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P])
-	Assert("COMPLETED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P])
+	Assert("0:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P], p)
+	Assert("COMPLETED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P], p)
 
 	try:
 		out = open(os.environ["PSTEST_SCONTROL_%s_STD_OUT" % P]).read()
