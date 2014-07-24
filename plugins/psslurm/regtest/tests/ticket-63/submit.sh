8ba5096780122bc81f5c5b0943bbeca2cfae23ae
Dorian Krause <d.krause@fz-juelich.de>
2014-07-24 16:22:37 +0200

    tests/ticket-63: Bug fixes

--- plugins/psslurm/regtest/tests/ticket-63/submit.sh
+++ plugins/psslurm/regtest/tests/ticket-63/submit.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+RES=""
+if ! [[ "x" == "x${PSTEST_RESERVATION}" ]]; then
+	RES="--reservation ${PSTEST_RESERVATION}"
+fi
+
 ulimit -u
 
 ulimit -u 4096
 ulimit -u
-srun -N 1 -n 1 --propagate=NPROC ./ulimit.sh
+srun -N 1 -n 1 --propagate=NPROC -p ${PSTEST_PARTITION} ${RES} ./ulimit.sh
 
 ulimit -u 2048
 ulimit -u
-srun -N 1 -n 1 --propagate=NPROC ./ulimit.sh 
+srun -N 1 -n 1 --propagate=NPROC -p ${PSTEST_PARTITION} ${RES} ./ulimit.sh
 
