34e36e8138ea3693d2ac4e7dbdeabdd667b963e4
Dorian Krause <d.krause@fz-juelich.de>
2014-07-24 16:22:37 +0200

    tests/ticket-63: Bug fixes

--- plugins/psslurm/regtest/tests/ticket-63/eval.py
+++ plugins/psslurm/regtest/tests/ticket-63/eval.py
@@ -31,7 +31,11 @@ for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
 	except Exception as e:
 		Assert(1 == 0, p + ": " + str(e))
 
-	numbers = [int(x) for x in map(lambda x: x.strip(), out.split("\n")) if len(x) > 0]
+	try:
+		numbers = [int(x) for x in map(lambda x: x.strip(), out.split("\n")) if len(x) > 0]
+	except Exception as e:
+		Assert(1 == 0, p + ": " + str(e))
+
 	Assert(5 == len(numbers))
 	Assert(numbers[0] != numbers[1], p)	# If this test fails we need to change the numbers in submit.sh
 	Assert(numbers[1] == numbers[2], p)
