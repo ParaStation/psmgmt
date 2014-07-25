def12704fcd11d60ca5857ad163f592df0f5d735
Dorian Krause <d.krause@fz-juelich.de>
2014-07-25 12:46:38 +0200

    Add test disable-status

---
+++ plugins/psslurm/regtest/tests/disable-status/eval.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import traceback
+import re
+import pprint
+
+RETVAL = 0
+
+def Assert(x, msg = None):
+	global RETVAL
+
+	if not x:
+		if msg:
+			sys.stderr.write("Test failure ('%s'):\n" % msg)
+		else:
+			sys.stderr.write("Test failure:\n")
+		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
+		RETVAL = 1
+
+pprint.pprint(os.environ, indent = 1)
+
+for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
+	P = p.upper()
+
+	Assert("0" == os.environ["PSTEST_FPROC_%s_EXIT_CODE" % P], p)
+
+	try:
+		out = open("%s/slurm-%s.out" % (os.environ["PSTEST_OUTDIR"], p)).read()
+	except Exception as e:
+		Assert(1 == 0, p + ": " + str(e))
+	
+	lines = [x for x in map(lambda z: z.strip(), out.split("\n")) if len(x) > 0]
+	Assert("SIGINT" == lines[-1])
+
+sys.exit(RETVAL)
+
