bc252c08a3b6c196cd10135a237c678bb5795cd3
Dorian Krause <d.krause@fz-juelich.de>
2014-07-25 07:25:25 +0200

    Add test ticket-24

---
+++ plugins/psslurm/regtest/tests/ticket-24/submit.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+import select
+import time
+import re
+import signal
+
+start = time.time()
+
+cmd = ["srun", "-n", "1", "-t", "2", "-p", os.environ["PSTEST_PARTITION"]]
+if "" != os.environ["PSTEST_RESERVATION"]:
+	cmd += ["--reservation", os.environ["PSTEST_RESERVATION"]]
+cmd += ["./loop.sh"]
+
+p = subprocess.Popen(cmd, \
+                     stdout = subprocess.PIPE,
+                     stderr = subprocess.PIPE)
+
+while 1:
+	x = p.poll()
+	if None != x:
+		sys.exit(x)
+	
+	ready, _, _ = select.select([p.stdout, p.stderr], [], [], 1)
+	for x in ready:
+		line = x.readline()
+		if "" != line.strip():
+			if re.match(r'.*allocated resources.*', line):
+				time.sleep(1)
+				for i in range(10):
+					p.send_signal(signal.SIGINT)
+					time.sleep(0.1)
+		
+			sys.stdout.write(("%.2f:\t" % (time.time() - start)) + line)
+
