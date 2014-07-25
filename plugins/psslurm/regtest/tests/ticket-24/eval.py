063fc3964059b8c6e0d129e4d686824a515e83b4
Dorian Krause <d.krause@fz-juelich.de>
2014-07-25 07:25:25 +0200

    Add test ticket-24

---
+++ plugins/psslurm/regtest/tests/ticket-24/eval.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import traceback
+import re
+import pprint
+
+RETVAL = 0
+
+def Assert(x, msg = None):
+	global RETVAL
+
+	if not x:
+		if msg:
+			sys.stderr.write("Test failure ('%s'):\n" % msg)
+		else:
+			sys.stderr.write("Test failure:\n")
+		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
+		RETVAL = 1
+
+for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
+	P = p.upper()
+
+# Since we cannot assess whether the problem created problems on the compute
+# nodes we just let the test pass. It is still a useful test in the suite, though,
+# as it we can check the system health after running the test suite.
+
+sys.exit(RETVAL)
+
