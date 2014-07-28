a6a98533fe5bb7a2debd0a8d7e87d53b778a7162
Dorian Krause <d.krause@fz-juelich.de>
2014-07-28 07:41:20 +0200

    Add cpu_bind and mem_bind tests

---
+++ plugins/psslurm/regtest/tests/cpu_bind-cores/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+JOB_NAME=$(scontrol show job ${SLURM_JOB_ID} | head -n 1 | awk '{print $2}' | sed 's/Name=//g')
+
+gcc -DCPU_MASK=1 prog.c -o output-${JOB_NAME}/prog.exe 
+srun -n 2 --cpu_bind=cores output-${JOB_NAME}/prog.exe
+
---
+++ plugins/psslurm/regtest/tests/cpu_bind-threads/eval.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import traceback
+import re
+import pprint
+
+RETVAL = 0
+
+def Assert(x, msg = None):
+	global RETVAL
+
+	if not x:
+		if msg:
+			sys.stderr.write("Test failure ('%s'):\n" % msg)
+		else:
+			sys.stderr.write("Test failure:\n")
+		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
+		RETVAL = 1
+
+pprint.pprint(os.environ, indent = 1)
+
+for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
+	P = p.upper()
+
+	Assert("0:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P], p)
+	Assert("COMPLETED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P], p)
+
+	try:
+		out = open(os.environ["PSTEST_SCONTROL_%s_STD_OUT" % P]).read()
+	except Exception as e:
+		Assert(1 == 0, p + ": " + str(e))
+
+	try:
+		lines = [x for x in map(lambda z: z.strip(), out.split("\n")) if len(x) > 0]
+		Assert(len(lines) == 2, p)
+
+		count = 0
+		for line in lines:
+			tmp = line.split()
+			if 0 == int(tmp[0]):
+				Assert(1 == int(tmp[1], base = 16), p)
+				count += 1
+			if 1 == int(tmp[0]):
+				Assert(256 == int(tmp[1], base = 16), p)
+				count += 1
+		Assert(len(lines) == count, p)
+	except Exception as e:
+		Assert(1 == 0, p + ": " + str(e))
+
+sys.exit(RETVAL)
