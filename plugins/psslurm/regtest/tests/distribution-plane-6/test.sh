dc50fb74857a3c63562d3da9ac9c2bf32886b254
Dorian Krause <d.krause@fz-juelich.de>
2014-07-29 10:17:51 +0200

    Reduce the number of distribution-plane tests

--- plugins/psslurm/regtest/tests/distribution-plane-6/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-JOB_NAME=$(scontrol show job ${SLURM_JOB_ID} | head -n 1 | awk '{print $2}' | sed 's/Name=//g')
-
-gcc prog.c -o output-${JOB_NAME}/prog.exe
-srun -n 64 -m plane=6 output-${JOB_NAME}/prog.exe
-
