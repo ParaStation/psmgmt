b2176028fbf37404b8c5ee121ee2a08ebf760397
Dorian Krause <d.krause@fz-juelich.de>
2014-07-31 10:39:28 +0200

    tests/multiprog: Use new testsuite module

--- plugins/psslurm/regtest/tests/multiprog/eval.py
+++ plugins/psslurm/regtest/tests/multiprog/eval.py
@@ -2,43 +2,27 @@
 
 import sys
 import os
-import traceback
 import re
-import pprint
 
-RETVAL = 0
+sys.path.append("/".join(os.path.abspath(os.path.dirname(sys.argv[0])).split('/')[0:-2] + ["lib"]))
+from testsuite import *
 
-def Assert(x, msg = None):
-	global RETVAL
+helper.pretty_print_env()
 
-	if not x:
-		if msg:
-			sys.stderr.write("Test failure ('%s'):\n" % msg)
-		else:
-			sys.stderr.write("Test failure:\n")
-		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
-		RETVAL = 1
+for p in helper.partitions():
+	helper.check_job_completed_ok(p)
 
-for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
-	P = p.upper()
+	lines = [x for x in helper.job_stdout_lines(p) if x != "Submitted batch job %s" % helper.job_id(p)]
+	test.check(6 == len(lines))
 
-	Assert("0:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P], p)
-	Assert("COMPLETED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P], p)
-
-	try:
-		out = open(os.environ["PSTEST_SCONTROL_%s_STD_OUT" % P]).read()
-	except Exception as e:
-		Assert(1 == 0, p + ": " + str(e))
-
-	lines = [x for x in map(lambda x: x.strip(), out.split("\n")) if len(x) > 0]
-	Assert(6 == len(lines))
 	for line in lines:
-		Assert(re.match(r'1:.*', line) or \
+		test.check(re.match(r'1:.*', line) or \
 		       re.match(r'2:.*', line) or \
 		       "3: task:3"   == line or \
 		       "4: task:4"   == line or \
 		       "0: offset:0" == line or \
 		       "5: offset:1" == line, p)
 
-sys.exit(RETVAL)
+
+test.quit()
 
