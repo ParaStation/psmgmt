2836bb37d3582c186f234b66d26da1295048ee65
Dorian Krause <d.krause@fz-juelich.de>
2014-07-31 08:09:45 +0200

    Add testsuite Python module
    The testsuite Python module is intended to streamline the existing
    tests and reduce boiler-plate code.

---
+++ plugins/psslurm/regtest/lib/testsuite/helper.py
@@ -0,0 +1,99 @@
+
+import os
+import re
+
+import testsuite.test as test
+
+
+#
+# Get the list of partitions
+def partitions():
+	return [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]
+
+#
+# Access the environment without terminating the test if something goes wrong
+def save_env_access(part, key):
+	try:
+		tmp = os.environ[key % part.upper()]
+	except Exception as e:
+		test.check(1 == 0, part + ": " + str(e))
+		return ""
+
+	return tmp
+
+#
+# Get the job exit code for the specified partition
+def job_exit_code(part):
+	return save_env_access(part, "PSTEST_SCONTROL_%s_EXIT_CODE")
+
+#
+# Get the job state for the specified partition
+def job_state(part):
+	return save_env_access(part, "PSTEST_SCONTROL_%s_JOB_STATE")
+
+#
+# Get the job id.
+def job_id(part):
+	return save_env_access(part, "PSTEST_SCONTROL_%s_JOB_ID")
+
+#
+# Get the job node list. In order for this to function the host names of the compute
+# nodes must conform to some standards. The name prefix (which is common for all compute
+# nodes) must start and end with a letter and may contain numbers only in the middle part.
+def job_node_list(part):
+	def expand1(matchobj):
+		tmp0, tmp1 = matchobj.group(0).split('-')
+
+		assert(len(tmp0) == len(tmp1))
+		fmt = "%%0%dd" % len(tmp0)
+
+		return ",".join([fmt % z for z in range(int(tmp0), int(tmp1)+1)])
+
+	def expand2(matchobj):
+		tmp0, tmp1 = matchobj.group(0).replace(']', '').split('[')
+		
+		return ",".join([tmp0 + x for x in tmp1.split(',')])
+
+	nodelist = save_env_access(part, "PSTEST_SCONTROL_%s_NODE_LIST")
+
+	if not re.match(r'[a-zA-Z][a-zA-Z0-9]*[a-zA-Z][0-9]+.*', nodelist):
+		test.check(1 == 0, part + ": Cannot handle node naming scheme")
+		return []
+
+	return re.sub(r'[a-zA-Z][a-zA-Z0-9]*[a-zA-Z]([0-9]*\[[0-9,]+\])', expand2, \
+	              re.sub(r'([0-9]+)-([0-9]+)', expand1, nodelist)).split(',')
+
+#
+# Check that the job completed with zero exit code. This is a commonly used
+# test so it makes sense to provide a convience wrapper.
+def check_job_completed_ok(part):
+ 	test.check("COMPLETED" == job_state(part), part)
+ 	test.check("0:0"       == job_exit_code(part), part)
+
+#
+# Retrieve the stdout/stderr of a job as an array of stripped lines.
+def job_output_lines(part, key):
+	try:
+		tmp = open(os.environ[key % part.upper()]).read()
+	except Exception as e:
+		test.check(1 == 0, part + ": " + str(e))
+		return []
+
+	return [x for x in map(lambda z: z.strip(), tmp.split("\n")) if len(x) > 0]
+
+#
+# Get the standard output of the job splitted into lines.
+def job_stdout_lines(part):
+	return job_output_lines(part, "PSTEST_SCONTROL_%s_STD_OUT")
+
+#
+# Get the standard error of the job splitted into lines.
+def job_stderr_lines(part):
+	return job_output_lines(part, "PSTEST_SCONTROL_%s_STD_ERR")
+
+#
+# Print the environment to stdout 
+def pretty_print_env():
+	print("{\n " + ",\n ".join(["'%s': '%s'" % (str(k), str(v)) for k, v in os.environ.iteritems()]) + "\n}")
+
+
---
+++ plugins/psslurm/regtest/lib/testsuite/test.py
@@ -0,0 +1,40 @@
+
+import traceback
+import sys
+
+
+RETVAL = 0
+
+#
+# Mark test as failed.
+def fail():
+	RETVAL = 1
+
+#
+# End of each test
+def quit():
+	sys.exit(RETVAL)
+
+#
+# Report the outcome of a check to stdout or stderr (depending on the
+# outcome).
+def _check_report_outcome(fo, prefix, msg, stack):
+	if msg:
+		prefix += " ('%s'):\n" % msg
+	else:
+		prefix += ":\n"
+
+	fo.write(prefix)
+	map(lambda x: fo.write("\t" + x.strip() + "\n"), stack)
+
+#
+# Check that condition x evaluates to True.
+def check(x, msg = None):
+	stack = traceback.format_stack()
+
+	if x:
+		_check_report_outcome(sys.stdout, "Test success", msg, stack)
+	else:
+		fail()
+		_check_report_outcome(sys.stderr, "Test failure", msg, stack)
+
