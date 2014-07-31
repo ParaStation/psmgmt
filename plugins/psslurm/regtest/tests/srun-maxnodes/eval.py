153c73108892032c077a91368b62388e91e947b7
Dorian Krause <d.krause@fz-juelich.de>
2014-07-31 13:17:08 +0200

    tests/srun-maxnodes: Use new testsuite module

--- plugins/psslurm/regtest/tests/srun-maxnodes/eval.py
+++ plugins/psslurm/regtest/tests/srun-maxnodes/eval.py
@@ -2,44 +2,23 @@
 
 import sys
 import os
-import traceback
-import re
-import pprint
 
-RETVAL = 0
+sys.path.append("/".join(os.path.abspath(os.path.dirname(sys.argv[0])).split('/')[0:-2] + ["lib"]))
+from testsuite import *
 
-def Assert(x, msg = None):
-	global RETVAL
+helper.pretty_print_env()
 
-	if not x:
-		if msg:
-			sys.stderr.write("Test failure ('%s'):\n" % msg)
-		else:
-			sys.stderr.write("Test failure:\n")
-		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
-		RETVAL = 1
+for p in helper.partitions():
+	helper.check_job_completed_ok(p)
 
-for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
-	P = p.upper()
+	lines = [x for x in helper.job_stdout_lines(p) if x != "Submitted batch job %s" % helper.job_id(p)]
 
-	Assert("0:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P], p)
-	Assert("COMPLETED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P], p)
+	test.check(4 == len(lines), p)
+	test.check(2 == len([x for x in lines if x == lines[0]]))
+	test.check(2 == len([x for x in lines if x == lines[1]]))
+	test.check(2 == len([x for x in lines if x == lines[2]]))
+	test.check(2 == len([x for x in lines if x == lines[3]]))
 
-	try:
-		out = open(os.environ["PSTEST_SCONTROL_%s_STD_OUT" % P]).read()
-	except Exception as e:
-		Assert(1 == 0, p + ": " + str(e))
 
-	try:
-		lines = [x for x in map(lambda z: z.strip(), out.split("\n")) if len(x) > 0]
+test.quit()
 
-		Assert(4 == len(lines), p)
-		Assert(2 == len([x for x in lines if x == lines[0]]))
-		Assert(2 == len([x for x in lines if x == lines[1]]))
-		Assert(2 == len([x for x in lines if x == lines[2]]))
-		Assert(2 == len([x for x in lines if x == lines[3]]))
-
-	except Exception as e:
-		Assert(1 == 0, p + ": " + str(e))
-
-sys.exit(RETVAL)
