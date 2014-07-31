9ee255ba09a8771287eb20a092a71f849f5ebd6c
Dorian Krause <d.krause@fz-juelich.de>
2014-07-31 16:25:07 +0200

    tests/ticket-32: Use new testsuite module

--- plugins/psslurm/regtest/tests/ticket-32/eval.py
+++ plugins/psslurm/regtest/tests/ticket-32/eval.py
@@ -2,41 +2,22 @@
 
 import sys
 import os
-import traceback
 import re
-import pprint
 
-RETVAL = 0
+sys.path.append("/".join(os.path.abspath(os.path.dirname(sys.argv[0])).split('/')[0:-2] + ["lib"]))
+from testsuite import *
 
-def Assert(x, msg = None):
-	global RETVAL
+helper.pretty_print_env()
 
-	if not x:
-		if msg:
-			sys.stderr.write("Test failure ('%s'):\n" % msg)
-		else:
-			sys.stderr.write("Test failure:\n")
-		map(lambda x: sys.stderr.write("\t" + x.strip() + "\n"), traceback.format_stack())
-		RETVAL = 1
+for p in helper.partitions():
+	test.check("FAILED" == helper.job_state(p), p)
+	test.check("16:0"   == helper.job_exit_code(p), p)
 
-pprint.pprint(os.environ, indent = 1)
-
-for p in [x.strip() for x in os.environ["PSTEST_PARTITIONS"].split()]:
-	P = p.upper()
-
-	Assert("16:0" == os.environ["PSTEST_SCONTROL_%s_EXIT_CODE" % P], p)
-	Assert("FAILED" == os.environ["PSTEST_SCONTROL_%s_JOB_STATE" % P], p)
-
-	try:
-		out = open(os.environ["PSTEST_SCONTROL_%s_STD_ERR" % P]).read()
-	except Exception as e:
-		Assert(1 == 0, p + ": " + str(e))
-
-	lines   = [x for x in map(lambda z: z.strip(), out.split("\n")) if len(x) > 0]
+	lines = helper.job_stderr_lines(p)
 
 	for i in range(16):
-		Assert(1 == len([x for x in lines if re.match(r'srun: error:.*Exited with exit code %d$' % (i + 1), x)]), p) 
+		test.check(1 == len([x for x in lines if re.match(r'srun: error:.*Exited with exit code %d$' % (i + 1), x)]), p) 
 
 
-sys.exit(RETVAL)
+test.quit()
 
