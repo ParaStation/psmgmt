5ed506379a1b070d0e6f5d6ea167af90804a0eef
Dorian Krause <d.krause@fz-juelich.de>
2014-09-23 08:11:56 +0200

    Add new tests signal-2 and signal-3

---
+++ plugins/psslurm/regtest/tests/signal-3/eval.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+
+sys.path.append("/".join(os.path.abspath(os.path.dirname(sys.argv[0])).split('/')[0:-2] + ["lib"]))
+from testsuite import *
+
+helper.pretty_print_env()
+
+for p in helper.partitions():
+	test.check("TIMEOUT" == helper.job_state(p), p)
+	test.check("0:1"     == helper.job_exit_code(p), p)
+
+	lines = [x for x in helper.job_stdout_lines(p) if x != "Submitted batch job %s" % helper.job_id(p) and \
+	                                                  not re.match(r'.*error.*', x)]
+
+	test.check(1 == len(lines), p)
+
+	time = int(lines[0])
+	test.check(time < 120, p + ": time = %d" % time)
+
+
+test.quit()
+
