fdb4f0d5b39822e388aac4738bd40820f5321456
Dorian Krause <d.krause@fz-juelich.de>
2014-09-24 08:25:12 +0200

    Add test for ticket 108 based on Slurm testsuite

---
+++ plugins/psslurm/regtest/tests/ticket-108/run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+
+testsuite = "/".join(os.getcwd().split("/")[:-2] + ["expect"])
+if "SLURM_TESTSUITE" in os.environ.keys():
+	testsuite = os.environ["SLURM_TESTSUITE"]
+
+os.environ["SBATCH_PARTITION"] = os.environ["PSTEST_PARTITION"]
+os.environ["SALLOC_PARTITION"] = os.environ["PSTEST_PARTITION"]
+os.environ["SLURM_PARTITION"]  = os.environ["PSTEST_PARTITION"]
+
+if "" != os.environ["PSTEST_RESERVATION"]:
+	os.environ["SBATCH_RESERVATION"] = os.environ["PSTEST_RESERVATION"]
+	os.environ["SALLOC_RESERVATION"] = os.environ["PSTEST_RESERVATION"]
+	os.environ["SLURM_RESERVATION"]  = os.environ["PSTEST_RESERVATION"]
+
+for x in ["globals", "test7.9", "test7.9.prog.c"]:
+	os.symlink(testsuite + "/" + x, os.environ["PSTEST_OUTDIR"] + "/" + x)
+
+p = subprocess.Popen([os.environ["PSTEST_OUTDIR"] + "/test7.9"], \
+                     cwd = os.environ["PSTEST_OUTDIR"])
+
+sys.exit(p.wait())
+
