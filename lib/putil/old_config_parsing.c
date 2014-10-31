268d1da6829c9dc6d9a306305d4f687f3d8336a7
eicker <eicker@21b3a009-d9f9-44df-8eaf-6b729cfaa075>
2014-10-31 12:12:54 +0000

    Bugfix: Fix setting of CFLAGS while building rpm distored by r8970. Fix copyright statements.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@8974 21b3a009-d9f9-44df-8eaf-6b729cfaa075

--- lib/putil/old_config_parsing.c
+++ lib/putil/old_config_parsing.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2003-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2012 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2014 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
