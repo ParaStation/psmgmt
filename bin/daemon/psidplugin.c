625de988cd1e4808bdd48e675183c0b1dc0775ee
eicker <eicker@21b3a009-d9f9-44df-8eaf-6b729cfaa075>
2015-02-20 12:06:59 +0000

    Enhancement: Push plugin API to 112 for dynamic resource allocation.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9108 21b3a009-d9f9-44df-8eaf-6b729cfaa075

--- bin/daemon/psidplugin.c
+++ bin/daemon/psidplugin.c
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2009-2014 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2009-2015 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -110,8 +110,11 @@ static LIST_HEAD(pluginList);
  *	PSIDHOOK_FRWRD_CLIENT_STAT
  *
  * 111: added PSID_cancelCB()
+ *
+ * 112: dynamic resource allocation via PSIDHOOK_XTND_PART_DYNAMIC,
+ *      PSIDHOOK_RELS_PART_DYNAMIC and PSIDpart_extendRes()
  */
-static int pluginAPIVersion = 111;
+static int pluginAPIVersion = 112;
 
 
 /** Grace period between finalize and unload on forcefully unloads */
