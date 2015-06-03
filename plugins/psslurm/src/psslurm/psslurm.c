33be457b0d3a95ccf56a5ef0821b7649a86d1e2e
Michael Rauh <rauh@par-tec.com>
2015-06-03 16:56:59 +0200

    New psslurm prototype version 29.

--- plugins/psslurm/src/psslurm/psslurm.c
+++ plugins/psslurm/src/psslurm/psslurm.c
@@ -77,7 +77,7 @@ handlerFunc_t oldCCErrorHandler = NULL;
 
 /** psid plugin requirements */
 char name[] = "psslurm";
-int version = 28;
+int version = 29;
 int requiredAPI = 112;
 plugin_dep_t dependencies[5];
 
