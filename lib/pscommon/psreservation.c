ParaStation/psmgmt

commit cc8b09cd3b63e836b4df8677f18c225b7cb304ea

Enhancement: Enable to count released resources within a reservation.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9165 21b3a009-d9f9-44df-8eaf-6b729cfaa075

lib/pscommon/psreservation.c
@@ -120,6 +120,7 @@ PSrsrvtn_t *PSrsrvtn_get(void)
     rp->nSlots = 0;
     rp->slots = NULL;
     rp->nextSlot = 0;
+    rp->relSlot = 0;
     rp->checked = 0;
     rp->dynSent = 0;
     rp->state = RES_USED;
@@ -143,6 +144,7 @@ void PSrsrvtn_put(PSrsrvtn_t *rp)
     rp->nSlots = 0;
     rp->slots = NULL;
     rp->nextSlot = 0;
+    rp->relSlot = 0;
     rp->checked = 0;
     rp->dynSent = 0;
     list_add_tail(&rp->next, &resFreeList);
@@ -207,6 +209,7 @@ static void freeChunk(res_chunk_t *chunk)
 	new->slots = old->slots;
 	old->slots = NULL;
 	new->nextSlot = old->nextSlot;
+	new->relSlot = old->relSlot;
 	new->checked = old->checked;
 	new->dynSent = old->dynSent;
 

include/psreservation.h
@@ -58,6 +58,7 @@ typedef struct {
     int nSlots;               /**< Number of slots in @ref slots */
     PSpart_slot_t *slots;     /**< Slots forming the reservation */
     int nextSlot;             /**< Number of next slot to use */
+    int relSlots;             /**< Number of slots already released */
     char checked;             /**< Was checked to be completable */
     char dynSent;             /**< Dynamic request was sent */
     PSrsrvtn_state_t state;   /**< flag internal state of structure */

commit 56c65adc98e212b70ced46c336593174bcc8d4a6

Bugfix: Fix typo introduced in r9165
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9169 21b3a009-d9f9-44df-8eaf-6b729cfaa075

lib/pscommon/psreservation.c
@@ -120,7 +120,7 @@ PSrsrvtn_t *PSrsrvtn_get(void)
     rp->nSlots = 0;
     rp->slots = NULL;
     rp->nextSlot = 0;
-    rp->relSlot = 0;
+    rp->relSlots = 0;
     rp->checked = 0;
     rp->dynSent = 0;
     rp->state = RES_USED;
@@ -144,7 +144,7 @@ void PSrsrvtn_put(PSrsrvtn_t *rp)
     rp->nSlots = 0;
     rp->slots = NULL;
     rp->nextSlot = 0;
-    rp->relSlot = 0;
+    rp->relSlots = 0;
     rp->checked = 0;
     rp->dynSent = 0;
     list_add_tail(&rp->next, &resFreeList);
@@ -209,7 +209,7 @@ static void freeChunk(res_chunk_t *chunk)
 	new->slots = old->slots;
 	old->slots = NULL;
 	new->nextSlot = old->nextSlot;
-	new->relSlot = old->relSlot;
+	new->relSlots = old->relSlots;
 	new->checked = old->checked;
 	new->dynSent = old->dynSent;
 