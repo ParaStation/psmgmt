32d70abe807922123ddd3a3d65af815049bb2edd
Stephan Krempel <krempel@par-tec.com>
2015-07-15 13:27:20 +0200

    New psslurm prototype version 41.

--- plugins/psslurm/src/psslurm/psslurm.c
+++ plugins/psslurm/src/psslurm/psslurm.c
@@ -77,7 +77,7 @@ handlerFunc_t oldCCErrorHandler = NULL;
 
 /** psid plugin requirements */
 char name[] = "psslurm";
-int version = 39;
+int version = 41;
 int requiredAPI = 112;
 plugin_dep_t dependencies[5];
 
