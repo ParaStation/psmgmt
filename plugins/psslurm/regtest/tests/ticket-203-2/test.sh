a20905d8e04bac46925e7d58e8840c70674e530f
Dorian Krause <d.krause@fz-juelich.de>
2015-07-31 16:04:00 +0200

    Add tests for ticket 203

---
+++ plugins/psslurm/regtest/tests/ticket-203-2/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+JOB_NAME=$(scontrol show job ${SLURM_JOB_ID} | head -n 1 | awk '{print $2}' | sed 's/Name=//g')
+
+gcc -fopenmp prog.c -o output-${JOB_NAME}/prog.exe 
+
+(
+	ulimit -t 10
+	env OMP_NUM_THREADS=8 srun -n 2 --cpu_bind=none output-${JOB_NAME}/prog.exe
+)
+
