18535c38b85178038c3d71f9fcba9ac2ff2d3157
Michael Rauh <rauh@par-tec.com>
2015-08-21 12:53:03 +0200

    Bugfix: prevent socket timeouts for rpc:6009 in slurmctld.

--- plugins/psslurm/src/psslurm/psslurmproto.c
+++ plugins/psslurm/src/psslurm/psslurmproto.c
@@ -1578,6 +1578,7 @@ static void handleKillReq(Slurm_Msg_t *sMsg, uint32_t jobid,
 	handleTerminateJob(sMsg, job, SIGTERM);
     } else {
 	signalStepsByJobid(jobid, SIGTERM);
+	goto SEND_SUCCESS;
     }
 
     return;
