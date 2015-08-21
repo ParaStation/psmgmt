49c3e1e9b155364c99c5a4deccd7eceb560e8920
Michael Rauh <rauh@par-tec.com>
2015-08-21 16:05:59 +0200

    New psslurm prototype version 48.

--- plugins/psslurm/src/psslurm/psslurm.c
+++ plugins/psslurm/src/psslurm/psslurm.c
@@ -78,7 +78,7 @@ handlerFunc_t oldSpawnHandler = NULL;
 
 /** psid plugin requirements */
 char name[] = "psslurm";
-int version = 44;
+int version = 48;
 int requiredAPI = 112;
 plugin_dep_t dependencies[5];
 
