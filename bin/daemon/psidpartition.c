ParaStation/psmgmt

commit 2d91d7272034264d96ad21972ed88800750bd15f

Bugfix: Fix memory corruption if realloc() actually moves data.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9202 21b3a009-d9f9-44df-8eaf-6b729cfaa075

bin/daemon/psidpartition.c
@@ -4160,6 +4160,8 @@ static int PSIDpart_getReservation(PSrsrvtn_t *res)
 	    PSID_blockSIGCHLD(blockedCHLD);
 	    res->slots = NULL;
 	    return -1;
+	} else {
+	    res-slots = s;
 	}
     }
 

commit 8fb1943b573235b36d512d3e3a74d5cc95411527

Fix typo preventing compilation introduced in r9202.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9204 21b3a009-d9f9-44df-8eaf-6b729cfaa075

bin/daemon/psidpartition.c
@@ -4161,7 +4161,7 @@ static int PSIDpart_getReservation(PSrsrvtn_t *res)
 	    res->slots = NULL;
 	    return -1;
 	} else {
-	    res-slots = s;
+	    res->slots = s;
 	}
     }
 