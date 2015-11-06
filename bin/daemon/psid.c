ParaStation/psmgmt

commit 2d91d7272034264d96ad21972ed88800750bd15f

Bugfix: Fix memory corruption if realloc() actually moves data.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9202 21b3a009-d9f9-44df-8eaf-6b729cfaa075

bin/daemon/psidpartition.c
@@ -4160,6 +4160,8 @@ static int PSIDpart_getReservation(PSrsrvtn_t *res)
 	    PSID_blockSIGCHLD(blockedCHLD);
 	    res->slots = NULL;
 	    return -1;
+	} else {
+	    res-slots = s;
 	}
     }
 

commit 522279b73e235b4fb9309414661cf6268d7239cc

Bugfix: fixed memory corruption in task pro/epilogue. Bugifx: Discontinue the blocking of SIGUSR2 after fork(). This was     triggered by r9205. Bugfix: Fixed various problems detected by valgrind. Bugfix: init pelogue data correct. Bugfix: fixed problems reported by scan-build. Bugfix: fixed segfault in step forwarder callback (jrc:#3862). Corrected indenting.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9206 21b3a009-d9f9-44df-8eaf-6b729cfaa075

Enhancement: Make psid's behavior under SIGUSR2 bombardment nicer. This
    shall help with jrt:#3694

git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9205 21b3a009-d9f9-44df-8eaf-6b729cfaa075

Fix typo preventing compilation introduced in r9202.

git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9204 21b3a009-d9f9-44df-8eaf-6b729cfaa075

Bugfix: Fix memory corruption if realloc() actually moves data.

git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9202 21b3a009-d9f9-44df-8eaf-6b729cfaa075

bin/daemon/psid.c
@@ -269,15 +269,15 @@ static void printMallocInfo(void)
 
     mi = mallinfo();
 
-    PSID_log(-1, "%s:\n", __func__);
-    PSID_log(-1, "arena    %d\n", mi.arena);
-    PSID_log(-1, "ordblks  %d\n", mi.ordblks);
-    PSID_log(-1, "hblks    %d\n", mi.hblks);
-    PSID_log(-1, "hblkhd   %d\n", mi.hblkhd);
-    PSID_log(-1, "uordblks %d\n", mi.uordblks);
-    PSID_log(-1, "fordblks %d\n", mi.fordblks);
-    PSID_log(-1, "keepcost %d\n", mi.keepcost);
-    PSID_log(-1, "====================\n");
+    PSID_log(PSID_LOG_RESET, "%s:\n", __func__);
+    PSID_log(PSID_LOG_RESET, "arena    %d\n", mi.arena);
+    PSID_log(PSID_LOG_RESET, "ordblks  %d\n", mi.ordblks);
+    PSID_log(PSID_LOG_RESET, "hblks    %d\n", mi.hblks);
+    PSID_log(PSID_LOG_RESET, "hblkhd   %d\n", mi.hblkhd);
+    PSID_log(PSID_LOG_RESET, "uordblks %d\n", mi.uordblks);
+    PSID_log(PSID_LOG_RESET, "fordblks %d\n", mi.fordblks);
+    PSID_log(PSID_LOG_RESET, "keepcost %d\n", mi.keepcost);
+    PSID_log(PSID_LOG_RESET, "====================\n");
 }
 
 /**
@@ -430,6 +430,24 @@ static void initSignalFD()
     if ((Selector_register(signalFD, handleSignalFD, NULL)) == -1) {
 	PSID_exit(errno, "Error registering signalfd");
     }
+}
+
+/**
+ * @brief Handling of SIGUSR2
+ *
+ * Handling of SIGUSR2 is suppressed most of the time in order to
+ * prevent receiving it in a critical situation, i.e. while in
+ * malloc() and friends. This has to be prevented since SIGUSR2's only
+ * purpose is to call malloc_trim() which will block if called from
+ * within malloc() and friends.
+ *
+ * It is save to handle SIGURS2 from within the main-loop. Thus, this
+ * function should be registered via @ref PSID_registerLoopAct().
+ */
+static void handleSIGUSR2(void)
+{
+    PSID_blockSig(0, SIGUSR2);
+    PSID_blockSig(1, SIGUSR2);
 }
 
 /**
@@ -608,7 +626,9 @@ int main(int argc, const char *argv[])
        }
     }
 
+    PSID_blockSig(1,SIGUSR2);
     initSigHandlers();
+    PSID_registerLoopAct(handleSIGUSR2);
 
 #define _PATH_TTY "/dev/tty"
     /* First disconnect from the old controlling tty. */

bin/daemon/psidpartition.c
@@ -4156,6 +4156,8 @@ static int PSIDpart_getReservation(PSrsrvtn_t *res)
 	    free(res->slots);
 	    res->slots = NULL;
 	    return -1;
+	} else {
+	    res->slots = s;
 	}
     }
 

bin/daemon/psidspawn.c
@@ -1444,7 +1444,6 @@ static void sendAcctChild(PStask_t *task)
 	    char dots[]="...";
 	    PSP_putTypedMsgBuf(&msg, __func__, "dots", dots, strlen(dots));
 	    offset = len-MAXARGV0+3;
-	    len = MAXARGV0-3;
 	}
 	PSP_putTypedMsgBuf(&msg, __func__, "name", task->argv[0] + offset,
 			   strlen(task->argv[0] + offset));

bin/daemon/psidtask.c
@@ -472,7 +472,6 @@ void PStask_cleanup(PStask_ID_t tid)
 	    }
 
 	    RDP_blockTimer(blockedRDP);
-
 	}
     }
 

bin/daemon/psidutil.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 1999-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2014 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2015 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -130,6 +130,8 @@ void PSID_resetSigs(void)
 #else
     signal(SIGSTKFLT,SIG_DFL);
 #endif
+
+    PSID_blockSig(0, SIGUSR2);
 }
 
 /* Reading and basic handling of the configuration */

bin/mpiexec.c
@@ -239,6 +239,7 @@ static void *umalloc(size_t size, const char *func)
 {
     void *ptr;
 
+    if (!size) return NULL;
     if (!(ptr = malloc(size))) {
 	fprintf(stderr, "%s: memory allocation failed\n", func);
 	exit(EXIT_FAILURE);

plugins/pelogue/peloguecomm.c
@@ -306,6 +306,8 @@ static void handlePELogueStart(DDTypedBufferMsg_t *msg,
        .buf = {'\0'} };
 
     data = umalloc(sizeof(PElogue_Data_t));
+    data->exit = 0;
+    data->prologue = prologue;
 
     /* get plugin */
     data->plugin = getStringM(&ptr);

plugins/psaccount/psaccountinter.c
@@ -311,7 +311,7 @@ void forwardAccountMsg(DDTypedBufferMsg_t *msg, int type, PStask_ID_t logger)
     char *ptr;
 
     /* copy the msg */
-    fmsg = umalloc(msg->header.len);
+    fmsg = umalloc(sizeof(DDTypedBufferMsg_t));
     memcpy(fmsg, msg, msg->header.len);
 
     /* prepare to forward */
@@ -323,11 +323,16 @@ void forwardAccountMsg(DDTypedBufferMsg_t *msg, int type, PStask_ID_t logger)
 
     /* add TaskID of child for start message */
     if (type == PSP_ACCOUNT_FORWARD_START) {
+	fmsg->header.len = sizeof(fmsg->header);
 	ptr = fmsg->buf;
 	ptr += sizeof(PStask_ID_t);
+	fmsg->header.len += sizeof(PStask_ID_t);
 	ptr += sizeof(int32_t);
+	fmsg->header.len += sizeof(int32_t);
 	ptr += sizeof(uid_t);
+	fmsg->header.len += sizeof(uid_t);
 	ptr += sizeof(gid_t);
+	fmsg->header.len += sizeof(gid_t);
 
 	*( PStask_ID_t *)ptr = msg->header.sender;
 	//ptr += sizeof(PStask_ID_t);

plugins/psslurm/src/psslurm/psslurmenv.c
@@ -459,7 +459,6 @@ static char * genCPUbindString(Step_t *step) {
 
     if (step->cpuBindType & (CPU_BIND_MAP | CPU_BIND_MASK)) {
 	strcpy(string+len, step->cpuBind);
-	len += strlen(step->cpuBind);
     }
 
     return string;
@@ -502,7 +501,6 @@ static char * genMemBindString(Step_t *step) {
 
     if (step->memBindType & (MEM_BIND_MAP | MEM_BIND_MASK)) {
 	strcpy(string+len, step->memBind);
-	len += strlen(step->memBind);
     }
 
     return string;

plugins/psslurm/src/psslurm/psslurmforwarder.c
@@ -179,8 +179,8 @@ static int stepCallback(int32_t exit_status, char *errMsg,
 
     if (step->taskEpilog && *(step->taskEpilog) != '\0') {
 	/* run task epilogue now for each task in this step */
-	return startTaskEpilogues(step);
-
+	if (!(startTaskEpilogues(step))) step->fwdata = NULL;
+	return 0;
     } else if ((alloc = findAlloc(step->jobid)) &&
 	alloc->terminate &&
 	alloc->nodes[0] == PSC_getMyID()) {
@@ -361,18 +361,18 @@ int handleExecClientUser(void *data)
 		putenv(strdup(task->environ[i]));
 	    }
 	}
-    }
-
-    ptr = task->environ[count++];
-    while (ptr) {
-	if (!(strncmp(ptr, "SLURM_STEPID=", 13))) {
-	    sscanf(ptr+13, "%u", &stepid);
-	}
-	if (!(strncmp(ptr, "SLURM_JOBID=", 12))) {
-	    sscanf(ptr+12, "%u", &jobid);
-	}
 
 	ptr = task->environ[count++];
+	while (ptr) {
+	    if (!(strncmp(ptr, "SLURM_STEPID=", 13))) {
+		sscanf(ptr+13, "%u", &stepid);
+	    }
+	    if (!(strncmp(ptr, "SLURM_JOBID=", 12))) {
+		sscanf(ptr+12, "%u", &jobid);
+	    }
+
+	    ptr = task->environ[count++];
+	}
     }
 
     if ((step = findStepById(jobid, stepid))) {

plugins/psslurm/src/psslurm/psslurmio.c
@@ -631,7 +631,6 @@ char *replaceSymbols(uint32_t jobid, uint32_t stepid, char *hostname,
 	strn2Buf(ptr, len, &buf, &bufSize);
 
 	/* zero padding */
-	symNumLen = 0;
 	snprintf(symLen, sizeof(symLen), "%%u");
 	while (symNum[0] >= 48 && symNum[0] <=57) symNum++;
 	if ((symNumLen = symNum - symbol) >0) {

plugins/psslurm/src/psslurm/psslurmjob.c
@@ -293,6 +293,8 @@ BCast_t *addBCast(int socket)
     bcast->fileName = NULL;
     bcast->block = NULL;
     bcast->fwdata = NULL;
+    bcast->sigLen = 0;
+    initSlurmMsg(&bcast->msg);
 
     list_add_tail(&(bcast->list), &BCastList.list);
 

plugins/psslurm/src/psslurm/psslurmpelogue.c
@@ -393,6 +393,7 @@ int startTaskEpilogues(Step_t *step)
 	return 0;
     }
 
+    step->fwdata = fwdata;
     return 1;
 }
 

plugins/psslurm/src/psslurm/psslurmpin.c
@@ -266,7 +266,7 @@ static void getBindMapFromString(PSCPU_set_t *CPUset, uint16_t cpuBindType,
 	next = strtok_r(NULL, delimiters, &saveptr);
     }
 
-    if (!myent) {
+    if (!myent && numents) {
 	myent = entarray[local_tid % numents];
     }
 
@@ -274,14 +274,11 @@ static void getBindMapFromString(PSCPU_set_t *CPUset, uint16_t cpuBindType,
 	PSCPU_setAll(*CPUset); //XXX other result in error case?
 	if (cpuBindType & CPU_BIND_MASK) {
 	    mlog("%s: invalid cpu mask string '%s'\n", __func__, ents);
-	}
-	else if (cpuBindType & CPU_BIND_MAP) {
+	} else if (cpuBindType & CPU_BIND_MAP) {
 	    mlog("%s: invalid cpu map string '%s'\n", __func__, ents);
-	}
-	else if (cpuBindType & CPU_BIND_LDMASK) {
+	} else if (cpuBindType & CPU_BIND_LDMASK) {
 	    mlog("%s: invalid socket mask string '%s'\n", __func__, ents);
-	}
-	else if (cpuBindType & CPU_BIND_LDMAP) {
+	} else if (cpuBindType & CPU_BIND_LDMAP) {
 	    mlog("%s: invalid socket map string '%s'\n", __func__, ents);
 	}
 	goto cleanup;
@@ -294,9 +291,7 @@ static void getBindMapFromString(PSCPU_set_t *CPUset, uint16_t cpuBindType,
 	mdbg(PSSLURM_LOG_PART, "%s: (bind_mask) node '%i' local task '%i' "
 		"cpumaskstr '%s' cpumask '%s'\n", __func__, nodeid, local_tid,
 		myent, PSCPU_print(*CPUset));
-    }
-    else if (cpuBindType & CPU_BIND_MAP) {
-	mycpu = 0;
+    } else if (cpuBindType & CPU_BIND_MAP) {
 	if (strncmp(myent, "0x", 2) == 0) {
 	    mycpu = strtoul (myent+2, &endptr, 16);
 	} else {
@@ -304,24 +299,20 @@ static void getBindMapFromString(PSCPU_set_t *CPUset, uint16_t cpuBindType,
 	}
 	if (*endptr == '\0') {
 	    PSCPU_setCPU(*CPUset, mycpu);
-	}
-	else {
+	} else {
 	    PSCPU_setAll(*CPUset); //XXX other result in error case?
 	    mlog("%s: invalid cpu map '%s'\n", __func__, myent);
 	}
 	mdbg(PSSLURM_LOG_PART, "%s: (bind_map) node '%i' local task '%i'"
 		" cpustr '%s' cpu '%i'\n", __func__, nodeid, local_tid, myent,
 		mycpu);
-    }
-    else if (cpuBindType & CPU_BIND_LDMASK) {
+    } else if (cpuBindType & CPU_BIND_LDMASK) {
 	parseSocketMask(CPUset, socketCount, coresPerSocket, cpuCount,
 			hwThreads, myent);
 	mdbg(PSSLURM_LOG_PART, "%s: (bind_ldmask) node '%i' local task '%i' "
 		"socketmaskstr '%s' cpumask '%s'\n", __func__, nodeid, local_tid,
 		myent, PSCPU_print(*CPUset));
-    }
-    else if (cpuBindType & CPU_BIND_LDMAP) {
-	mysock = 0;
+    } else if (cpuBindType & CPU_BIND_LDMAP) {
 	if (strncmp(myent, "0x", 2) == 0) {
 	    mysock = strtoul (myent+2, &endptr, 16);
 	} else {
@@ -330,8 +321,7 @@ static void getBindMapFromString(PSCPU_set_t *CPUset, uint16_t cpuBindType,
 	if (*endptr == '\0') {
 	    pinToSocket(CPUset, socketCount, coresPerSocket, cpuCount,
 			hwThreads, mysock);
-	}
-	else {
+	} else {
 	    PSCPU_setAll(*CPUset); //XXX other result in error case?
 	    mlog("%s: invalid socket map '%s'\n", __func__, myent);
 	}
@@ -513,7 +503,6 @@ void getSocketBinding(PSCPU_set_t *CPUset, uint8_t *coreMap,
 
 	    /* we need another socket to hold the threads */
 	    socketsUsed++;
-	    usedSocket = currentSocket;
 	}
 
         /* bind to all hw threads (allowed) of current core */
@@ -1186,7 +1175,7 @@ void doMemBind(Step_t *step, PStask_t *task)
 	next = strtok_r(NULL, delimiters, &saveptr);
     }
 
-    if (!myent) {
+    if (!myent && numents) {
 	myent = entarray[local_tid % numents];
     }
 
@@ -1202,7 +1191,6 @@ void doMemBind(Step_t *step, PStask_t *task)
     }
 
     if (step->memBindType & MEM_BIND_MAP) {
-	mynode = 0;
 
 	if (strncmp(myent, "0x", 2) == 0) {
 	    mynode = strtoul (myent+2, &endptr, 16);

plugins/psslurm/src/psslurm/psslurmproto.c
@@ -784,14 +784,14 @@ static void handleSignalJob(Slurm_Msg_t *sMsg)
 
     job = findJobById(jobid);
     step = findStepByJobid(jobid);
-    uid = step ? step->uid : 0;
-    uid = job ? job->uid : 0;
 
     if (!job && !step) {
 	mlog("%s: job '%u' to signal not found\n", __func__, jobid);
 	sendSlurmRC(sMsg, ESLURM_INVALID_JOB_ID);
 	return;
     }
+
+    uid = step ? step->uid : job->uid;
 
     /* check permissions */
     if (!(checkAuthorizedUser(sMsg->head.uid, uid))) {
@@ -2202,6 +2202,7 @@ int addSlurmAccData(uint8_t accType, pid_t childPid, PStask_ID_t loggerTID,
 void sendStepExit(Step_t *step, int exit_status)
 {
     PS_DataBuffer_t body = { .buf = NULL };
+    pid_t childPid;
 
     addUint32ToMsg(step->jobid, &body);
     addUint32ToMsg(step->stepid, &body);
@@ -2218,7 +2219,8 @@ void sendStepExit(Step_t *step, int exit_status)
     }
 
     /* account data */
-    addSlurmAccData(step->accType, 0, PSC_getTID(-1, step->fwdata->childPid),
+    childPid = (step->fwdata) ? step->fwdata->childPid : 1;
+    addSlurmAccData(step->accType, 0, PSC_getTID(-1, childPid),
 		    &body, step->nodes, step->nrOfNodes);
 
     mlog("%s: sending REQUEST_STEP_COMPLETE to slurmctld: exit '%u'\n",

plugins/psslurm/src/psslurm/psslurmpscomm.c
@@ -1196,16 +1196,14 @@ static void handleCC_INIT_Msg(PSLog_Msg_t *msg)
 		}
 	    }
 	}
-    }
-
-    else if (msg->sender >= 0) {
+    } else if (msg->sender >= 0) {
 
 	if ((step = findStepByLogger(msg->header.dest))) {
 
 	    if (PSC_getMyID() == PSC_getID(msg->header.sender)) {
 
-		if ((task = findTaskByForwarder(&step->tasks.list, msg->header.sender))) {
-
+		if ((task = findTaskByForwarder(&step->tasks.list,
+						    msg->header.sender))) {
 		    verboseCpuPinningOutput(step, task);
 		}
 	    }
@@ -1213,6 +1211,7 @@ static void handleCC_INIT_Msg(PSLog_Msg_t *msg)
     }
 }
 
+/*
 static void closeClientIO(PSLog_Msg_t *msg)
 {
     Step_t *step = NULL;
@@ -1237,6 +1236,7 @@ static void closeClientIO(PSLog_Msg_t *msg)
     printChildMessage(step, NULL, 0, STDOUT, msg->sender);
     printChildMessage(step, NULL, 0, STDERR, msg->sender);
 }
+*/
 
 static void handleCC_STDIN_Msg(PSLog_Msg_t *msg)
 {
@@ -1396,7 +1396,7 @@ void handleCCMsg(PSLog_Msg_t *msg)
 
 void handleCCError(PSLog_Msg_t *msg)
 {
-    closeClientIO(msg);
+    //closeClientIO(msg);
     if (oldCCErrorHandler) oldCCErrorHandler((DDBufferMsg_t *) msg);
 }
 