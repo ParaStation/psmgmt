9703a8897e8000162981aeab8f92330e4c85dc3a
Dorian Krause <d.krause@fz-juelich.de>
2015-12-10 15:49:31 +0100

    Update mem_bind tests
    Update the mem_bind tests (use --cpu_bind=sockets) and
    fix a bug in mem_bind_local.

--- plugins/psslurm/regtest/tests/mem_bind-default/test.sh
+++ plugins/psslurm/regtest/tests/mem_bind-default/test.sh
@@ -3,5 +3,5 @@
 JOB_NAME=$(scontrol show job ${SLURM_JOB_ID} | head -n 1 | awk '{print $2}' | sed 's/Name=//g')
 
 gcc -DMEM_MASK=1 prog.c -o output-${JOB_NAME}/prog.exe -lnuma
-srun -n 2  output-${JOB_NAME}/prog.exe
+srun -n 2  --cpu_bind=socket output-${JOB_NAME}/prog.exe
 
