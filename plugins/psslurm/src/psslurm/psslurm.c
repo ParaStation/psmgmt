5dea1fe00f8945008fd5bd3bfcda3a3bdf1e5aa8
Michael Rauh <rauh@par-tec.com>
2016-02-01 18:42:24 +0100

    New psslurm version 84.

--- plugins/psslurm/src/psslurm/psslurm.c
+++ plugins/psslurm/src/psslurm/psslurm.c
@@ -82,7 +82,7 @@ handlerFunc_t oldSpawnHandler = NULL;
 
 /** psid plugin requirements */
 char name[] = "psslurm";
-int version = 82;
+int version = 84;
 int requiredAPI = 112;
 plugin_dep_t dependencies[6];
 
