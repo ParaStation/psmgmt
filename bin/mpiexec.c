711e6d7dff97a02473bfd3a3ec406e5c42c11f65
Michael Rauh <rauh@par-tec.com>
2016-03-08 10:22:00 +0100

    Unset __PSI_EXPORTS in mpiexec to reduce environment.

--- bin/mpiexec.c
+++ bin/mpiexec.c
@@ -1119,6 +1119,7 @@ static void setupCommonEnv(int np)
     /* *hack* let MPI processes fail if no pspmi plugin is loaded */
     setPSIEnv("PMI_FD", "10000", 1);
     setPSIEnv("PMI_PORT", "10000", 1);
+    unsetPSIEnv("__PSI_EXPORTS");
 }
 
 /**
