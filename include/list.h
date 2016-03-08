c4b663a7f3ee7d95e30adb8aec9b1e9288fa6bca
moschny <moschny@21b3a009-d9f9-44df-8eaf-6b729cfaa075>
2016-03-08 15:19:23 +0000

    Add annotation for iwyu tool.
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9327 21b3a009-d9f9-44df-8eaf-6b729cfaa075

--- include/list.h
+++ include/list.h
@@ -22,7 +22,7 @@
  * using the generic single-entry routines.
  */
 
-#include "list_t.h"
+#include "list_t.h" /* IWYU pragma: export */
 
 #define LIST_HEAD_INIT(name) (list_t) { &(name), &(name) }
 
