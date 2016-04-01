5159e3cc3b380bf98e810d77c063873f308100a5
eicker <eicker@21b3a009-d9f9-44df-8eaf-6b729cfaa075>
2016-04-01 19:19:31 +0000

    Bugfix: Printing PSCPU_set_t via PSCPU_print() might be broken (psc:#243)
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9331 21b3a009-d9f9-44df-8eaf-6b729cfaa075

--- lib/pscommon/pscpu.c
+++ lib/pscommon/pscpu.c
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2007-2015 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2007-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -124,9 +124,9 @@ char *PSCPU_print_part(PSCPU_set_t set, size_t num)
     unsigned int i;
 
     snprintf(setStr, sizeof(setStr), "0x");
-    for (i=num; i>0; i--) {
+    for (i=(num+1)/2; i>0; i--) {
 	snprintf(setStr+strlen(setStr), sizeof(setStr)-strlen(setStr),
-		 "%hx", set[i-1]);
+		 "%04hx", set[i-1]);
     }
 
     return setStr;
