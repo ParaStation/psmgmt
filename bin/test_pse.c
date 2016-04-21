ParaStation/psmgmt

commit cd367801d3605b9303fba6ffd21fa47b5e882dd9

We need to PSE_initialize() before parsing the cmdline (PSI_resolveHWList()     requires connection to psid to work)
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9090 21b3a009-d9f9-44df-8eaf-6b729cfaa075

bin/mpiexec.c
@@ -1895,7 +1895,6 @@ static void setupEnvironment(int verbose)
     setupPSCOMEnv(verbose);
     /* Both *before* PSE_initialize() for environment propagation */
 
-    PSE_initialize();
     rank = PSE_getRank();
 
     /* be only verbose if we are the logger */
@@ -3193,6 +3192,9 @@ int main(int argc, char *argv[], char** envp)
     if ((envstr = getenv("PSI_TPP"))) {
 	envtpp = strtol(envstr, NULL, 0);
     }
+
+    /* Initialzie daemon connection */
+    PSE_initialize();
 
     /* parse command line options */
     parseCmdOptions(argc, argv);

commit b235b8908213e562817021202b66670460f16448

Bugfix: Fix mpiexec's propagation of single environments via the -e     option. This broke in r9090. To fix it environment propagation had     to be detached from PSI / PSE initialization.
git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9353 21b3a009-d9f9-44df-8eaf-6b729cfaa075

bin/gmspawner.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2003-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2014 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -558,12 +558,6 @@ int main(int argc, const char *argv[])
 	printf("\b\n\n");
     }
 
-    /*
-     * Besides initializing the PSI stuff, this furthermore propagates
-     * some environment variables. Thus use this one instead of
-     * PSI_initClient().
-     */
-
     /* init PSI */
     if (!PSI_initClient(TG_GMSPAWNER)) {
 	fprintf(stderr, "Initialization of PSI failed.");
@@ -579,6 +573,8 @@ int main(int argc, const char *argv[])
 
     /* Propagate some environment variables */
     PSI_propEnv();
+    PSI_propEnvList("PSI_EXPORTS");
+    PSI_propEnvList("__PSI_EXPORTS");
 
     srandom(time(NULL));
     magic = random()%9999999;

bin/mpiexec.c
@@ -1902,15 +1902,12 @@ static void setupPSIDEnv(int verbose)
  */
 static void setupEnvironment(int verbose)
 {
-    int rank;
+    int rank = PSE_getRank();
 
     /* setup environment depending on psid/logger */
     setupPSIDEnv(verbose);
     /* setup environment depending on pscom library */
     setupPSCOMEnv(verbose);
-    /* Both *before* PSE_initialize() for environment propagation */
-
-    rank = PSE_getRank();
 
     /* be only verbose if we are the logger */
     if (rank != -1) verbose = 0;
@@ -3268,6 +3265,11 @@ int main(int argc, char *argv[], char** envp)
 /*     PSI_RemoteArgs(filter_argc-dup_argc, &filter_argv[dup_argc],
  *     &dup_argc, &dup_argv); */
 /*     @todo Enable PSI_RARG_PRE correctly !! */
+
+    /* Now actually Propagate parts of the environment */
+    PSI_propEnv();
+    PSI_propEnvList("PSI_EXPORTS");
+    PSI_propEnvList("__PSI_EXPORTS");
 
     /* create spawner process and switch to logger */
     createSpawner(argc, argv, np, admin);

bin/mpirun_chgm.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2003-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2012 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -306,14 +306,14 @@ int main(int argc, const char *argv[])
     /* Don't irritate the user with logger messages */
     setenv("PSI_NOMSGLOGGERDONE", "", 1);
 
-    /*
-     * Besides initializing the PSI stuff, this furthermore propagates
-     * some environment variables. Thus use this one instead of
-     * PSI_initClient().
-     */
     PSE_initialize();
 
     rank = PSE_getRank();
+
+    /* Propagate some environment variables */
+    PSI_propEnv();
+    PSI_propEnvList("PSI_EXPORTS");
+    PSI_propEnvList("__PSI_EXPORTS");
 
     if (rank != -1) {
 	fprintf(stderr, "%s: never act as client process.\n", argv[dup_argc]);

bin/mpirun_chp4.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2003-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2012 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -248,14 +248,14 @@ int main(int argc, const char *argv[])
     /* Don't irritate the user with logger messages */
     setenv("PSI_NOMSGLOGGERDONE", "", 1);
 
-    /*
-     * Besides initializing the PSI stuff, this furthermore propagetes
-     * some environment variables. Thus use this one instead of
-     * PSI_initClient().
-     */
     PSE_initialize();
 
     rank = PSE_getRank();
+
+    /* Propagate some additional environment variables */
+    PSI_propEnv();
+    PSI_propEnvList("PSI_EXPORTS");
+    PSI_propEnvList("__PSI_EXPORTS");
 
     if (rank != -1) {
 	fprintf(stderr, "%s: never act as client process.\n", argv[dup_argc]);

bin/mpirun_openib.c
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2007-2014 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2007-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -101,7 +101,13 @@ static void createSpawner(int argc, char *argv[], int np)
     }
 
     PSE_initialize();
+
     rank = PSE_getRank();
+
+    /* Propagate some environment variables */
+    PSI_propEnv();
+    PSI_propEnvList("PSI_EXPORTS");
+    PSI_propEnvList("__PSI_EXPORTS");
 
     if (rank == -1) {
 	PSnodes_ID_t *nds;

bin/psispawn.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2003-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2011 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -122,6 +122,8 @@ int main(int argc, char *argv[])
 	    setPSIEnv("TERM", env_str, 1);
 	}
     }
+    PSI_propEnvList("PSI_EXPORTS");
+    PSI_propEnvList("__PSI_EXPORTS");
 
     /* spawning the process */
     {

bin/psmstart.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2002-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2014 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -31,6 +31,7 @@ static char vcid[] __attribute__((used)) =
 #include <popt.h>
 
 #include <pse.h>
+#include <psi.h>
 
 /*
  * Print version info
@@ -232,8 +233,12 @@ int main(int argc, const char *argv[])
     optCon = NULL;
     free(dup_argv);
 
-    /* at least handling of envlist has to be done before PSE_initialize() */
     PSE_initialize();
+
+    /* Propagate some environment variables */
+    PSI_propEnv();
+    PSI_propEnvList("PSI_EXPORTS");
+    PSI_propEnvList("__PSI_EXPORTS");
 
     if (login) {
 	struct passwd *passwd;

bin/pssh.c
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2006-2015 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2006-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -31,6 +31,7 @@ static char vcid[] __attribute__((used)) =
 #include <popt.h>
 
 #include <pse.h>
+#include <psi.h>
 #include <psiinfo.h>
 #include <psienv.h>
 #include <pscommon.h>
@@ -239,8 +240,12 @@ int main(int argc, const char *argv[])
 	free(val);
     }
 
-    /* at least handling of envlist has to be done before PSE_initialize() */
     PSE_initialize();
+
+    /* Propagate some environment variables */
+    PSI_propEnv();
+    PSI_propEnvList("PSI_EXPORTS");
+    PSI_propEnvList("__PSI_EXPORTS");
 
     if (PSE_getRank() != -1)
 	fprintf(stderr, "Wrong rank! Spawned by another process?\n");

include/psi.h
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 1999-2003 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2013 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -255,6 +255,22 @@ void PSI_execLogger(const char *command);
  * @return No return value.
  */
 void PSI_propEnv(void);
+
+/**
+ * @brief Propagate list of environments
+ *
+ * Propagate a list of environment variables to the next level of
+ * spawning. For that, the environment variable @a listName containing
+ * a comma-separated list of environment variable names is
+ * analyzed. Each environment mentioned in this list is put into the
+ * PSI_environment
+ *
+ * @param listName Name of the environment containing a
+ * comma-separated list of environments to be propagated.
+ *
+ * @return No return value.
+ */
+void PSI_propEnvList(char *listName);
 
 /**
  * @brief Get the fd which is connected to the local daemon.

lib/pse/pse.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 1999-2003 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2014 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -101,9 +101,6 @@ void PSE_initialize(void)
 
     logger_print(logger, PSE_LOG_VERB, "[%d] My TID is %s\n",
 		 PSE_getRank(), PSC_printTID(PSC_getMyTID()));
-
-    /* Propagate some environment variables */
-    PSI_propEnv();
 
     /* Get masterNode/masterPort from environment (if available) */
     envStr = getenv("__PSI_MASTERNODE");

lib/psi/psi.c
@@ -275,21 +275,7 @@ static int connectDaemon(PStask_group_t taskGroup, int tryStart)
     return 0;
 }
 
-/**
- * @brief Propagate list of environments
- *
- * Propagate a list of environment variables to the next level of
- * spawning. For that, the environment variable @a listName containing
- * a comma-separated list of environment variable names is
- * analyzed. Each environment mentioned in this list is put into the
- * PSI_environment
- *
- * @param listName Name of the environment containing a
- * comma-separated list of environments to be propagated.
- *
- * @return No return value.
- */
-static void propEnvList(char *listName)
+void PSI_propEnvList(char *listName)
 {
     char *envStr = getenv(listName);
 
@@ -363,9 +349,6 @@ int PSI_initClient(PStask_group_t taskGroup)
 	}
 	return 0;
     }
-
-    propEnvList("PSI_EXPORTS");
-    propEnvList("__PSI_EXPORTS");
 
     return 1;
 }

bin/test_pse.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2001-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2013 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2016 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -12,7 +12,7 @@
  * \file
  * test_pse: ParaStation PSE test program
  *
- * $Id$ 
+ * $Id$
  *
  * \author
  * Jens Hauke <hauke@par-tec.com>
@@ -26,7 +26,8 @@
 
 #include <popt.h>
 
-#include "pse.h"
+#include <pse.h>
+#include <psi.h>
 
 static int arg_np;
 
@@ -42,6 +43,11 @@ static void run(int argc, char *argv[], int np)
     PSE_initialize();
 
     rank = PSE_getRank();
+
+    /* Propagate some environment variables */
+    PSI_propEnv();
+    PSI_propEnvList("PSI_EXPORTS");
+    PSI_propEnvList("__PSI_EXPORTS");
 
     if (rank == -1){
 	/* I am the logger */
@@ -78,7 +84,7 @@ int main(int argc, char *argv[])
     int rc;
 
     struct poptOption optionsTable[] = {
-        { "np", '\0', POPT_ARG_INT | POPT_ARGFLAG_ONEDASH,
+	{ "np", '\0', POPT_ARG_INT | POPT_ARGFLAG_ONEDASH,
 	  &arg_np, 0, "number of processes to start", "num"},
 	POPT_AUTOHELP
 	{ NULL, '\0', 0, NULL, 0, NULL, NULL}