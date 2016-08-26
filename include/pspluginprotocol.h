2f4d9610c72abb88d574421e52599ed41c37de6c
eicker <eicker@21b3a009-d9f9-44df-8eaf-6b729cfaa075>
2016-08-26 08:48:44 +0000

    Merge pspluginprotocol.h from psslurm
    git-svn-id: http://svn.ipd.uka.de/repos/ParaStation/trunk/psmgmt@9419 21b3a009-d9f9-44df-8eaf-6b729cfaa075

---
+++ include/pspluginprotocol.h
@@ -0,0 +1,21 @@
+/*
+ * ParaStation
+ *
+ * Copyright (C) 2013-2016 ParTec Cluster Competence Center GmbH, Munich
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined in the file LICENSE.QPL included in the packaging of this
+ * file.
+ */
+
+#ifndef __PSPLUGIN_PROTOCOL
+#define __PSPLUGIN_PROTOCOL
+
+/** Various message types used by plugins */
+#define PSP_CC_PLUG_PSMOM	    0x0200  /**< psmom message */
+#define PSP_CC_PLUG_ACCOUNT	    0x0201  /**< psaccount message */
+#define PSP_CC_PLUG_PELOGUE	    0x0202  /**< pelogue message */
+#define PSP_CC_PLUG_PSSLURM	    0x0203  /**< psslurm message */
+#define PSP_CC_PLUG_PSEXEC	    0x0204  /**< psexec message */
+
+#endif  /* __PSPLUGIN_PROTOCOL */
