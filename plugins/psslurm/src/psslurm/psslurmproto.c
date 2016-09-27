ParaStation/psmgmt

commit 90ca9c21eef55d81318ff8040f87858d658b48bb

Bugfix: first received step does not have to be step 0 (PCT:#230).

plugins/psslurm/src/psslurm/psslurmproto.c
@@ -516,6 +516,8 @@ void handleLaunchTasks(Slurm_Msg_t *sMsg)
 	alloc = addAllocation(step->jobid, step->cred->jobNumHosts,
 		step->cred->jobHostlist, &step->env,
 		&step->spankenv, step->uid, step->gid, step->username);
+	/* first received step does *not* have to be step 0 */
+	alloc->state = JOB_PROLOGUE;
     }
 
     if ((acctType = getConfValueC(&SlurmConfig, "JobAcctGatherType"))) {

commit 5ebd6d51bce6b16e5199077b37afbb71c484faab

Bugfix: fix bug introduced in 90ca9c21.

plugins/psslurm/src/psslurm/psslurmproto.c
@@ -513,11 +513,13 @@ void handleLaunchTasks(Slurm_Msg_t *sMsg)
 
     /* add allocation */
     if (!(job = findJobById(step->jobid))) {
-	alloc = addAllocation(step->jobid, step->cred->jobNumHosts,
-		step->cred->jobHostlist, &step->env,
-		&step->spankenv, step->uid, step->gid, step->username);
-	/* first received step does *not* have to be step 0 */
-	alloc->state = JOB_PROLOGUE;
+	if (!(alloc = findAlloc(step->jobid))) {
+	    alloc = addAllocation(step->jobid, step->cred->jobNumHosts,
+		    step->cred->jobHostlist, &step->env,
+		    &step->spankenv, step->uid, step->gid, step->username);
+	    /* first received step does *not* have to be step 0 */
+	    alloc->state = JOB_PROLOGUE;
+	}
     }
 
     if ((acctType = getConfValueC(&SlurmConfig, "JobAcctGatherType"))) {