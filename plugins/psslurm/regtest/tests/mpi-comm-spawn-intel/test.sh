6482f2a779508c7b33982e95e303508a0eeba75d
Michael Rauh <rauh@par-tec.com>
2016-09-28 11:45:59 +0200

    Update regression test suite:
        commit 9fa0291250ecabaed283a7c1a50cab02ea929eca
        Author: Dorian Krause <d.krause@fz-juelich.de>
        Date:   Wed Sep 21 18:45:35 2016 +0200
    
    	Add new tests for ticket 251
    
        commit 15abdfd61671f25de978aa4ac337ef7f6c9e921c
        Author: Dorian Krause <d.krause@fz-juelich.de>
        Date:   Wed Sep 21 18:41:10 2016 +0200
    
    	Add new pmi-{rank,size} tests
    
        commit 7b3dbc49a04ea511d2939a617feef3fdbec89dae
        Author: Dorian Krause <d.krause@fz-juelich.de>
        Date:   Mon Sep 12 09:28:45 2016 +0200
    
    	Update test mpi-comm-spawn-intel
    
    	The module structure on JUROPA3 has
    	changed recently.

--- plugins/psslurm/regtest/tests/mpi-comm-spawn-intel/test.sh
+++ plugins/psslurm/regtest/tests/mpi-comm-spawn-intel/test.sh
@@ -3,7 +3,7 @@
 JOB_NAME=$(scontrol show job ${SLURM_JOB_ID} | head -n 1 | awk '{print $2}' | sed 's/Name=//g')
 
 # Juropa-3 specific :(
-module load iimpi
+module load Intel IntelMPI
 mpiicc prog.c -o output-${JOB_NAME}/prog.exe
 
 export I_MPI_FABRICS=tcp
