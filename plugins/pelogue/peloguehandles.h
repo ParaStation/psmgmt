9af59855eba6205b2ab65c97d75f4b3199bd6ff2
Norbert Eicker <n.eicker@fz-juelich.de>
2017-01-02 09:27:45 +0100

    Fix typo

--- plugins/pelogue/peloguehandles.h
+++ plugins/pelogue/peloguehandles.h
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2014-2016 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2014-2017 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -30,7 +30,7 @@ psPelogueDelPluginConfig_t *psPelogueDelPluginConfig;
 
 psPelogueAddJob_t *psPelogueAddJob;
 psPelogueStartPE_t *psPelogueStartPE;
-sPelogueSignalPE_t *psPelogueSignalPE;
+psPelogueSignalPE_t *psPelogueSignalPE;
 psPelogueDeleteJob_t *psPelogueDeleteJob;
 
 #endif  /* __PELOGUE_HANDLES */
