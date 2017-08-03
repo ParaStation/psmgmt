9056e8e89257cd9ff96455d828154790a391ba3b
Norbert Eicker <n.eicker@fz-juelich.de>
2017-08-03 07:58:26 +0200

    Improve fixes done in 75f3605c

--- lib/putil/selector.c
+++ lib/putil/selector.c
@@ -2,17 +2,12 @@
  * ParaStation
  *
  * Copyright (C) 2003-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2016 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2017 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
  * file.
  */
-#ifndef DOXYGEN_SHOULD_SKIP_THIS
-static char vcid[] __attribute__((used)) =
-    "$Id$";
-#endif /* DOXYGEN_SHOULD_SKIP_THIS */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -376,6 +371,7 @@ int Selector_setMax(int max)
     if (!newSelectors) {
 	logger_warn(logger, -1, ENOMEM, "%s", __func__);
 	errno = ENOMEM;
+	maxSelectorFD = oldMax;
 	return -1;
     }
     selectors = newSelectors;
