5c41279049aabbe7dea9f6793ca7925c44ebfc54
Michael Rauh <rauh@par-tec.com>
2017-10-26 15:55:56 +0200

    Show active and registered jobs in pelogue show query

--- plugins/pelogue/peloguekvs.c
+++ plugins/pelogue/peloguekvs.c
@@ -9,13 +9,20 @@
  */
 #include "pluginmalloc.h"
 #include "peloguechild.h"
+#include "peloguejob.h"
 
 #include "plugin.h"
 
 char *show(char *key)
 {
+    char line[256];
     char *buf = NULL;
     size_t bufSize = 0;
+
+    snprintf(line, sizeof(line), "active jobs: %i\n", countActiveJobs());
+    str2Buf(line, &buf, &bufSize);
+    snprintf(line, sizeof(line), "registered jobs: %i\n", countRegJobs());
+    str2Buf(line, &buf, &bufSize);
 
     return printChildStatistics(buf, &bufSize);
 }
