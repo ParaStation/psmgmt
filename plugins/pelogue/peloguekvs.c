f7fbc37ed9b4755dc4fa3d0a7dff52c2f871d72c
Norbert Eicker <n.eicker@fz-juelich.de>
2018-01-16 19:44:59 +0100

    Fix missing include

--- plugins/pelogue/peloguekvs.c
+++ plugins/pelogue/peloguekvs.c
@@ -1,13 +1,14 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2011-2017 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2011-2018 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
  * file.
  */
 #include <string.h>
+#include <stdio.h>
 
 #include "pluginmalloc.h"
 #include "peloguechild.h"
