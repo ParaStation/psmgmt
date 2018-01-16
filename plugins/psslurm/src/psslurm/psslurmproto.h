556345db877f51c522801f689bd56ec5554feb71
Norbert Eicker <n.eicker@fz-juelich.de>
2018-01-16 19:46:08 +0100

    Fix missing includes

--- plugins/psslurm/src/psslurm/psslurmproto.h
+++ plugins/psslurm/src/psslurm/psslurmproto.h
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2014-2017 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2014-2018 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -9,6 +9,9 @@
  */
 #ifndef __PSSLURM_PROTO
 #define __PSSLURM_PROTO
+
+#include <stdbool.h>
+#include <stdint.h>
 
 #include "psslurmtypes.h"
 #include "psslurmjob.h"
