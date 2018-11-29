94582bc2bd2badfca4d31e1995287010605a06e9
Michael Rauh <rauh@par-tec.com>
2018-11-29 10:52:51 +0100

    Add function traverseHostList() to replace expandHostList() (#2747)

--- include/pshostlist.h
+++ include/pshostlist.h
@@ -14,6 +14,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "psnodes.h"
+
 /**
  * @brief Create list from range
  *
@@ -64,5 +66,82 @@ bool range2List(char *prefix, char *range, char **list, size_t *size,
  * returned.
  */
 char *expandHostList(char *hostlist, uint32_t *count);
+
+/**
+ * @brief Host-list visitor function
+ *
+ * Visitor function used by @ref traverseHostList() in order to visit
+ * each host of a given host-list.
+ *
+ * The parameters are as follows: @a host points to the host to
+ * visit. @a info points to the additional information passed to @ref
+ * traverseHostList() in order to be forwarded to each host.
+ *
+ * If the visitor function returns false the traversal will be
+ * interrupted and @ref traverseHostList() will return to its calling
+ * function.
+ */
+typedef bool HL_Visitor_t(char *host, void *info);
+
+/**
+ * @brief Traverse every host of a host-list
+ *
+ * Traverse all hosts of a given @a hostlist by calling @a visitor for each
+ * host. In addition to a pointer to the current host @a info is
+ * passed as additional information to @a visitor.
+ *
+ * @param hostlist The host-list to parse and traverse
+ *
+ * @param visitor Visitor function to be called for each host
+ *
+ * @param info Additional information to be passed to @a visitor while
+ * visiting the hosts
+ *
+ * @return If the host-list cannot be parsed or a visitor returns false,
+ * traversal will be stopped and false is returned. If parsing was successful
+ * and no visitor returned false during the traversal true is returned.
+ */
+bool traverseHostList(char *hostlist, HL_Visitor_t visitor, void *info);
+
+/**
+ * @brief Host resolve function
+ *
+ * Resolve function used by @ref convHLtoPSnodes() in order to resolve
+ * a ParaStation node ID for a give host. This enables the use of @ref
+ * convHLtoPSnodes() within and outside of the main psid process.
+ * Plugins running within the main psid process can use @ref getNodeIDbyName()
+ * as resolve function. Other processes may use @ref PSI_resolveNodeID() as
+ * resolve function.
+ *
+ * The resolve function should return -1 if the host lookup failed. Otherwise
+ * the ParaStation node ID is returned.
+ */
+typedef PSnodes_ID_t ResolveFunc_t(const char *host);
+
+/**
+ * @brief Convert a host-list to ParaStation node IDs
+ *
+ * Traverse all hosts of a given @a hostlist by calling the
+ * resolve function @a resolveNID for each host in the list. Each
+ * host has to be unique and may only appear once in the host-list.
+ * The corresponding ParaStation node ID for every host resolved
+ * by @a resolveNID is saved in @a nodes. The number of resolved
+ * hosts is saved in @a nrOfNodes. The node array @a nodes
+ * is allocated using malloc() and should be freed by the caller
+ * after use.
+ *
+ * @param hostlist The host-list to convert
+ *
+ * @param resolveNID Function to resolve a single hostname
+ *
+ * @param nodes Array of ParaStation node IDs holding the result
+ *
+ * @param nrOfNodes Number of nodes in node-list
+ *
+ * @return Returns true if every host in the host-list could be converted to
+ * a representing ParaStation node ID. Otherwise false is returned.
+ */
+bool convHLtoPSnodes(char *hostlist, ResolveFunc_t resolveNID,
+		     PSnodes_ID_t **nodes, uint32_t *nrOfNodes);
 
 #endif   /* __PSHOSTLIST_H */
--- lib/pscommon/pshostlist.c
+++ lib/pscommon/pshostlist.c
@@ -18,6 +18,14 @@
 
 #define STR_MALLOC_SIZE 512
 
+/** Used by @ref convHLtoPSnodes() to pass information to the visitor */
+typedef struct {
+    uint32_t nrOfNodes;		/**< number of resolved nodes */
+    uint32_t size;		/**< actual size of @a nodes array */
+    PSnodes_ID_t *nodes;	/**< all resovled node IDs */
+    ResolveFunc_t *resolveNID;	/**< resolver function to be used */
+} PSnodeList_t;
+
 static char *str2Buf(char *strSave, char **buffer, size_t *bufSize)
 {
     size_t lenBuf;
@@ -168,4 +176,195 @@ expandError:
     if (prefix) free(prefix);
     if (expHL) free(expHL);
     return NULL;
+}
+
+/**
+ * @brief Visit every node of a host-range
+ *
+ * Expand a host-range and visit each host calling the @a visitor function.
+ * The @a range is a describing string of the form "<first>[-<last>]".
+ * <first> must be smaller or equal to <last>. Each host visited is
+ * preceded by * @a prefix.
+ *
+ * @param prefix Prefix repented to each host visited
+ *
+ * @param range Range describing hosts visited "<first>[-<last>]"
+ *
+ * @param visitor Visitor function to be called for each host
+ *
+ * @param info Additional information to be passed to @a visitor while
+ * visiting the hosts
+ *
+ * @return Returns true on success otherwise false.
+ */
+static bool visitHostRange(char *prefix, char *range, HL_Visitor_t visitor,
+			   void *info)
+{
+    char *sep = strchr(range, '-');
+    unsigned int i, min, max;
+    int pad;
+    char tmp[1024];
+
+    if (!sep) {
+	snprintf(tmp, sizeof(tmp), "%s%s", prefix ? prefix : "", range);
+	if (!visitor(tmp, info)) return false;
+	return true;
+    }
+
+    if (sscanf(range, "%u%n-%u", &min, &pad, &max) != 2) {
+	PSC_log(-1, "%s: invalid range '%s'\n", __func__, range);
+	return false;
+    }
+
+    if (min>max) {
+	PSC_log(-1, "%s: invalid range '%s'\n", __func__, range);
+	return false;
+    }
+
+    for (i=min; i<=max; i++) {
+	snprintf(tmp, sizeof(tmp), "%s%0*u", prefix ? prefix : "", pad, i);
+	if (!visitor(tmp, info)) return false;
+    }
+
+    return true;
+}
+
+bool traverseHostList(char *hostlist, HL_Visitor_t visitor, void *info)
+{
+    const char delimiters[] =", \n";
+    char *saveptr, *range, *prefix = NULL;
+    bool isOpen = false;
+
+    char *duphl = strdup(hostlist);
+    if (!duphl) PSC_exit(errno, "%s: strdup(hostlist)", __func__);
+
+    char *next = strtok_r(duphl, delimiters, &saveptr);
+    while (next) {
+	char *openBrk = strchr(next, '[');
+	char *closeBrk = strchr(next, ']');
+
+	if (openBrk && !closeBrk) {
+	    if (isOpen) {
+		PSC_log(-1, "%s: error two open bracket found\n", __func__);
+		goto expandError;
+	    }
+
+	    range = openBrk +1;
+	    *openBrk = '\0';
+	    prefix = strdup(next);
+	    if (!prefix) PSC_exit(errno, "%s: strdup(next)", __func__);
+
+	    if (!visitHostRange(prefix, range, visitor, info)) {
+		goto expandError;
+	    }
+	    isOpen = true;
+	} else if (openBrk && closeBrk) {
+	    if (isOpen) {
+		PSC_log(-1, "%s: error two open bracket found\n", __func__);
+		goto expandError;
+	    }
+
+	    range = openBrk +1;
+	    *closeBrk = *openBrk = '\0';
+
+	    if (!visitHostRange(next, range, visitor, info)) {
+		goto expandError;
+	    }
+	} else if (closeBrk) {
+	    if (!isOpen) {
+		PSC_log(-1, "%s: error no open bracket found\n", __func__);
+		goto expandError;
+	    }
+	    if (!prefix) {
+		PSC_log(-1, "%s: error invalid prefix\n", __func__);
+		goto expandError;
+	    }
+
+	    *closeBrk = '\0';
+	    if (!visitHostRange(prefix, next, visitor, info)) {
+		goto expandError;
+	    }
+
+	    if (prefix) free(prefix);
+	    prefix = NULL;
+	    isOpen = false;
+	} else if (isOpen) {
+	    if (!prefix) {
+		PSC_log(-1, "%s: error invalid prefix\n", __func__);
+		goto expandError;
+	    }
+	    if (!visitHostRange(prefix, next, visitor, info)) {
+		goto expandError;
+	    }
+	} else {
+	    visitor(next, info);
+	}
+	next = strtok_r(NULL, delimiters, &saveptr);
+    }
+
+    free(duphl);
+    free(prefix);
+    return true;
+
+expandError:
+    free(duphl);
+    free(prefix);
+    return false;
+}
+
+/**
+ * @brief Convert a single host to ParaStation node ID
+ *
+ * @param host The host to convert
+ *
+ * @param info Pointer holding a PSnodeList_t structure holding
+ * the result
+ *
+ * @return Returns true on success otherwise false
+ */
+static bool hostToPSnode(char *host, void *info)
+{
+    PSnodeList_t *psNL = info;
+
+    if (psNL->size == psNL->nrOfNodes + 1) {
+	PSC_log(-1, "%s: no space left (max %i)\n", __func__, psNL->size);
+	return false;
+    }
+
+    PSnodes_ID_t nodeID = psNL->resolveNID(host);
+    if (nodeID == -1) {
+	PSC_log(-1, "%s: no PS node ID for host %s\n", __func__, host);
+	return false;
+    }
+    psNL->nodes[psNL->nrOfNodes++] = nodeID;
+
+    return true;
+}
+
+bool convHLtoPSnodes(char *hostlist, ResolveFunc_t resolveNID,
+		     PSnodes_ID_t **nodes, uint32_t *nrOfNodes)
+{
+    PSnodeList_t psNL = {
+	.nrOfNodes = 0,
+	.size = PSC_getNrOfNodes(),
+	.resolveNID = resolveNID };
+
+    *nrOfNodes = 0;
+    *nodes = NULL;
+
+    psNL.nodes = malloc(psNL.size * sizeof(*psNL.nodes));
+    if (!psNL.nodes) {
+	PSC_log(-1, "%s: no memory for nodelist\n", __func__);
+	return false;
+    }
+
+    if (!traverseHostList(hostlist, hostToPSnode, &psNL)) {
+	free(psNL.nodes);
+	return false;
+    }
+
+    *nodes = psNL.nodes;
+    *nrOfNodes = psNL.nrOfNodes;
+
+    return true;
 }
