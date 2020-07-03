019475e336d8777e609f580c1bc371992c4de18d
Norbert Eicker <n.eicker@fz-juelich.de>
2020-07-03 11:02:04 +0200

    Improve rejection logging

--- bin/daemon/psidutil.c
+++ bin/daemon/psidutil.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 1999-2004 ParTec AG, Karlsruhe
- * Copyright (C) 2005-2019 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2005-2020 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -468,16 +468,18 @@ bool PSID_checkPrivilege(PStask_ID_t sender)
     PStask_t *senderTask = PStasklist_find(&managedTasks, sender);
 
     if (!senderTask) {
-	PSID_log(-1, "%s: sender not found in managed tasks\n", __func__);
+	PSID_log(-1, "%s: sender %s not among managed tasks\n", __func__,
+		 PSC_printTID(sender));
 	return false;
     }
 
     if (senderTask->uid && senderTask->gid
-	    && !PSIDnodes_testGUID(PSC_getMyID(), PSIDNODES_ADMUSER,
-				   (PSIDnodes_guid_t){.u=senderTask->uid})
-	    && !PSIDnodes_testGUID(PSC_getMyID(), PSIDNODES_ADMGROUP,
-				   (PSIDnodes_guid_t){.g=senderTask->gid})) {
-	    PSID_log(-1, "%s: sender not privileged\n", __func__);
+	&& !PSIDnodes_testGUID(PSC_getMyID(), PSIDNODES_ADMUSER,
+			       (PSIDnodes_guid_t){.u=senderTask->uid})
+	&& !PSIDnodes_testGUID(PSC_getMyID(), PSIDNODES_ADMGROUP,
+			       (PSIDnodes_guid_t){.g=senderTask->gid})) {
+	PSID_log(-1, "%s: sender %s not privileged\n", __func__,
+		 PSC_printTID(sender));
 	return false;
     }
 
