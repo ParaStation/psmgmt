b88ee27e50cb223f7248d4cdf3819bf5ec6c6587
Michael Rauh <rauh@par-tec.com>
2020-11-13 13:30:55 +0100

    Rework jail.sh to use modules for cgroup controllers
     - introduce jail-main.sh to replace jail.sh and jail-term.sh

---
+++ plugins/jail/jail-main.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# ParaStation
+#
+# Copyright (C) 2018-2020 ParTec Cluster Competence Center GmbH, Munich
+#
+# This file may be distributed under the terms of the Q Public License
+# as defined in the file LICENSE.QPL included in the packaging of this
+# file.
+#
+#
+# Script to be executed by ParaStation's jail plugin each time a
+# process will be jailed into the cgroup. Nevertheless, this
+# functionality is independent of the actual cgroup plugin.
+#
+# This script is executed with the same permissions as the ParaStation
+# daemon psid, i.e. typically with root permission! Thus, special care
+# has to be taken when changing this script.
+#
+# This script will be called by the jail plugin via system() and get
+# the process ID of the process to be jailed as an argument.
+
+CommandName=${0##*/}
+CommandPath=${0%/*}
+CHILD=${1}
+SCRIPT=${CommandName%%.*}
+JOB_CPUS="$__PSSLURM_JOB_CORE_BITMAP"
+PSPAM_USER="$__PSPAM_ADD_USER"
+
+unset LANGUAGE
+export LANG=C
+
+source $CommandPath/jail-functions.inc
+source $CommandPath/jail-config.inc
+
+exec 2>>$LOG_FILE 1>&2
+
+for modName in ${MODULES//,/$IFS}; do
+	MODULE="$CommandPath/$SCRIPT-$modName.inc"
+	[ -r $MODULE ] || {
+		mlog "$SCRIPT module $MODULE not found"
+		continue
+	}
+	BASE="$CGROUP_BASE/$modName"
+	dlog "Calling module $MODULE for child $CHILD"
+	source $MODULE
+done
+
+exit 0
