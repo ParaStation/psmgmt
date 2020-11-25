59805b4333e6a713b513114491030e9956328d25
Norbert Eicker <n.eicker@fz-juelich.de>
2020-11-25 13:17:16 +0100

    Cleanup

--- lib/putil/psparamspace.c
+++ lib/putil/psparamspace.c
@@ -7,11 +7,6 @@
  * as defined in the file LICENSE.QPL included in the packaging of this
  * file.
  */
-#ifndef DOXYGEN_SHOULD_SKIP_THIS
-static char vcid[] __attribute__((used)) =
-    "$Id$";
-#endif /* DOXYGEN_SHOULD_SKIP_THIS */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
