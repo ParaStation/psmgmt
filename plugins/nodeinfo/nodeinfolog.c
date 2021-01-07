0867e8924b0aeefd87551045497a2532d1750a3b
Norbert Eicker <n.eicker@fz-juelich.de>
2021-01-07 19:33:04 +0100

    Introduce finalizeNodeInfoLogger() and reflect the pluginlogger

--- plugins/nodeinfo/nodeinfolog.c
+++ plugins/nodeinfo/nodeinfolog.c
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2020 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2020-2021 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -20,7 +20,14 @@ void initNodeInfoLogger(char *name)
     initPluginLogger(name, NULL);
 }
 
-void maskNodeInfoLogger(int32_t mask)
+void maskNodeInfoLogger(uint32_t mask)
 {
     logger_setMask(nodeInfoLogger, mask);
+    maskPluginLogger(mask);
+}
+
+void finalizeNodeInfoLogger(void)
+{
+    finalizePluginLogger();
+    logger_finalize(nodeInfoLogger);
 }
--- plugins/nodeinfo/nodeinfolog.h
+++ plugins/nodeinfo/nodeinfolog.h
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2020 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2020-2021 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -25,8 +25,20 @@ typedef enum {
     NODEINFO_LOG_VERBOSE = 0x00001, /**< Be verbose */
 } NodeInfo_log_types_t;
 
+/*
+ * Further log-types inherited from pluginlogger in libplugincommon:
+ * typedef enum {
+ *     PLUGIN_LOG_VERBOSE  = 0x000010,   /\**< Other verbose stuff *\/
+ *     PLUGIN_LOG_MALLOC   = 0x000020,   /\**< Log memory allocation *\/
+ *     PLUGIN_LOG_FW       = 0x000040,   /\**< Verbose forwarder *\/
+ * } PSPlugin_log_types_t;
+ */
+
 /**
  * @brief Init the logger facility
+ *
+ * Initialize all logging facilities utilized by the plugin. This
+ * includes the one of libplugincommon.
  *
  * @return No return value
  */
@@ -37,6 +49,16 @@ void initNodeInfoLogger(char *name);
  *
  * @return No return value
  */
-void maskNodeInfoLogger(int32_t mask);
+void maskNodeInfoLogger(uint32_t mask);
+
+/**
+ * @brief Finalize the logger facility
+ *
+ * Finalize all logging facilities utilized by the plugin. This
+ * includes the one of libplugincommon.
+ *
+ * @return No return value
+ */
+void finalizeNodeInfoLogger(void);
 
 #endif  /* __NODEINFO_LOG_H */
