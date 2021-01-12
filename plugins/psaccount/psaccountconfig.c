21dea359899963f58d430fea024811ddb4d9babb
Norbert Eicker <n.eicker@fz-juelich.de>
2021-01-12 12:55:39 +0100

    Remove obsolete parameter TIME_JOBSTART_POLL from definition

--- plugins/psaccount/psaccountconfig.c
+++ plugins/psaccount/psaccountconfig.c
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2012-2020 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2012-2021 ParTec Cluster Competence Center GmbH, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -15,8 +15,6 @@
 
 const ConfDef_t confDef[] =
 {
-    { "SAFE_ACC_UPDATES", true,	"flag", "1",
-      "Obsolete! Was: Constantly forward accounting updates" },
     { "TIME_JOBSTART_POLL", true, "num", "1",
       "Poll interval in seconds at the beginning of a job" },
     { "TIME_JOBSTART_WAIT", true, "num", "1",
