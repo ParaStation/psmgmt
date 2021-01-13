eaacf24aca3ff547462bc86a4bd735b74d5db727
Norbert Eicker <n.eicker@fz-juelich.de>
2021-01-13 01:14:03 +0100

    Add config parameter POLL_INTERVAL to psaccount

--- plugins/psaccount/psaccountconfig.c
+++ plugins/psaccount/psaccountconfig.c
@@ -15,6 +15,8 @@
 
 const ConfDef_t confDef[] =
 {
+    { "POLL_INTERVAL", true, "num", "30",
+      "General poll interval in seconds" },
     { "TIME_JOBSTART_POLL", true, "num", "1",
       "Poll interval in seconds at the beginning of a job" },
     { "TIME_JOBSTART_WAIT", true, "num", "1",
