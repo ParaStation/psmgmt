75e421e5b62fd7dac3c86df708729cad182f7c84
Michael Rauh <rauh@par-tec.com>
2021-02-23 17:56:03 +0100

    Added missing header includes to x11spank

--- plugins/psslurm/src/spank/x11spank.c
+++ plugins/psslurm/src/spank/x11spank.c
@@ -8,11 +8,14 @@
 
 #include <arpa/inet.h>
 #include <sys/types.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <sys/un.h>
+#include <signal.h>
 
 #include <slurm/spank.h>
 
