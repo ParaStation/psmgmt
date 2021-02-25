444648b37c190778e22e42293c6a59c6363a7e95
Michael Rauh <rauh@par-tec.com>
2021-02-25 16:51:23 +0100

    Add psgw route plugin "plugin03.py"

--- plugins/psgw/route/plugin03.py
+++ plugins/psgw/route/plugin03.py
@@ -1,11 +1,13 @@
+import itertools
+
 #
 # Route function: Given the numerical Ids of nodes in partition A and B, the function
 # returns a tuple (error, numeral of gateway)
 def routeConnectionS(sizePartA, sizePartB, numGwd, numeralNodeA, numeralNodeB):
-  numRelNodeA = numeralNodeA - numeralNodeA
-  numRelNodeB = (numeralNodeB - numeralNodeA) % sizePartB
-  numRelGw = (numRelNodeA + numRelNodeB) % numGwd
-  numeralGw = (numeralNodeA + numRelGw) % numGwd
+  A = [x for x in range(sizePartA)]
+  B = [x for x in range(sizePartB)]
+  P = list(itertools.product(A,B))
+  numeralGw = P.index((numeralNodeA, numeralNodeB)) % numGwd
 
   return None, numeralGw
 
