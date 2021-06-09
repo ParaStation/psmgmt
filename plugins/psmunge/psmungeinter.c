391fd51802332d6bda85e530b0f2c60eb4d4a7c5
Norbert Eicker <n.eicker@fz-juelich.de>
2021-06-09 12:11:59 +0200

    Simplify logic

--- plugins/psmunge/psmungeinter.c
+++ plugins/psmunge/psmungeinter.c
@@ -2,6 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2016-2021 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2021 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -181,13 +182,10 @@ bool initMunge(void)
     if (!initDefaultContext()) return false;
 
     /* test munge functionality */
-    if (!psMungeEncode(&cred) || !psMungeDecode(cred, &uid, &gid)) {
-	free(cred);
-	return false;
-    }
-
+    bool ret = true;
+    if (!psMungeEncode(&cred) || !psMungeDecode(cred, &uid, &gid)) ret = false;
     free(cred);
-    return true;
+    return ret;
 }
 
 void finalizeMunge(void)
