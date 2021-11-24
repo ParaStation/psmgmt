dd0da009d0f1e4205d8236b73564aaeedfc5dce3
Norbert Eicker <n.eicker@fz-juelich.de>
2021-11-24 23:06:57 +0100

    Do not update *val if getFromBuf() failed

--- plugins/psslurm/src/psslurm/psslurmpack.c
+++ plugins/psslurm/src/psslurm/psslurmpack.c
@@ -64,17 +64,17 @@ static char *__getBitString(char **ptr, const char *func, const int line)
     }
     return bitStr;
 }
-
 #define getBitString(ptr) __getBitString(ptr, __func__, __LINE__)
 
 static bool __getBool(char **ptr, bool *val, const char *func, const int line)
 {
     uint8_t tmp;
-    bool ret = getFromBuf(ptr, &tmp, PSDATA_UINT8, sizeof(tmp), func, line);
-    *val = tmp;
-    return ret;
+    if (getFromBuf(ptr, &tmp, PSDATA_UINT8, sizeof(tmp), func, line) && val) {
+	*val = tmp;
+	return true;
+    }
+    return false;
 }
-
 #define getBool(ptr, val) __getBool(ptr, val, __func__, __LINE__)
 
 static void packOldStepID(uint32_t stepid, PS_SendDB_t *data)
