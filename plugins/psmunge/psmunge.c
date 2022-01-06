c6ab7511a688d512e28d5b6ce5ffb26e57f83724
Norbert Eicker <n.eicker@fz-juelich.de>
2022-01-06 14:24:42 +0100

    IWYU cleanup for psmunge plugin

--- plugins/psmunge/psmunge.c
+++ plugins/psmunge/psmunge.c
@@ -2,15 +2,16 @@
  * ParaStation
  *
  * Copyright (C) 2014-2021 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2022 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
  * file.
  */
-
+#include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
 
+#include "logging.h"
 #include "plugin.h"
 
 #include "psmungeinter.h"
--- plugins/psmunge/psmungelog.c
+++ plugins/psmunge/psmungelog.c
@@ -2,15 +2,15 @@
  * ParaStation
  *
  * Copyright (C) 2014-2021 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2022 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
  * file.
  */
+#include "psmungelog.h"
 
 #include "pluginlog.h"
-
-#include "psmungelog.h"
 
 logger_t *psmungelogger = NULL;
 
