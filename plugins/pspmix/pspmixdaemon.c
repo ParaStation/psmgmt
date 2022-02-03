bad8584ce41f415cad64a6da7c061aa709308ede
Norbert Eicker <n.eicker@fz-juelich.de>
2022-02-03 13:49:53 +0100

    Bugfix: only take healthy jobservers into account

--- plugins/pspmix/pspmixdaemon.c
+++ plugins/pspmix/pspmixdaemon.c
@@ -191,24 +191,23 @@ static int forwardPspmixFwMsg(PSLog_Msg_t *tmpmsg, ForwarderData_t *fw)
     DDTypedBufferMsg_t *msg = (DDTypedBufferMsg_t *)vmsg;
 
     if (msg->type == PSPMIX_CLIENT_INIT) {
-	PspmixJobserver_t *server = NULL;
-
-	list_t *j;
-	list_for_each(j, &pmixJobservers) {
-	    PspmixJobserver_t *js = list_entry(j, PspmixJobserver_t, next);
-	    if (js->fwdata->tid == msg->header.sender)
-		server = js;
-	}
-	if (server) {
-	    if (!server->used) {
-		mlog("%s: Job with logger tid %s started to use PMIx.\n",
-			__func__, PSC_printTID(server->loggertid));
+	bool found = false;
+	list_t *s;
+	list_for_each(s, &pmixJobservers) {
+	    PspmixJobserver_t *server = list_entry(s, PspmixJobserver_t, next);
+	    if (server->fwdata && server->fwdata->tid == msg->header.sender
+		&& server->timerId < 0) {
+		if (!server->used) {
+		    mlog("%s: Job with logger tid %s started to use PMIx.\n",
+			 __func__, PSC_printTID(server->loggertid));
+		}
+		server->used = true;
+		found = true;
+		break;
 	    }
-	    server->used = true;
-	} else {
-	    mlog("%s: Couldn't find sending jobserver %s\n", __func__,
-		    PSC_printTID(msg->header.sender));
 	}
+	if (!found) mlog("%s: Couldn't find sending jobserver %s\n", __func__,
+			 PSC_printTID(msg->header.sender));
     }
 
     forwardPspmixMsg(vmsg);
