68285bbe73d193ee6e3a448e2db29a9a569a8dd5
Norbert Eicker <n.eicker@fz-juelich.de>
2022-05-19 17:25:07 +0200

    Major rework adding more ways to test PMI implemenation

--- tests/PMI/reproducer.c
+++ tests/PMI/reproducer.c
@@ -1,75 +1,97 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "pmi.h"
 
-#define NUM_PUTS 2
+#define NUM_PUTS 4
+
+#define VAL_LEN 256
+
+#define NUM_ROUNDS 4
+
+int name_len_max, key_len_max, value_len_max;
+
+bool doKVSTest(char *KVSname, int rank, int size, char *valStr)
+{
+    char key[key_len_max];
+    for (int i = 0; i < NUM_PUTS; i++) {
+	snprintf(key, sizeof(key), "string-%d-%d", rank, i);
+	int rc = PMI_KVS_Put(KVSname, key, valStr);
+	printf("[%d] i/rc are %d/%d\n", rank, i, rc);
+    }
+
+    PMI_KVS_Commit(KVSname);
+
+    PMI_Barrier();
+
+    for (int i = 0; i < size; i++) {
+	for (int j = 0; j < NUM_PUTS; j++) {
+	    char kvsVal[value_len_max];
+	    snprintf(key, sizeof(key), "string-%d-%d", i, j);
+
+	    int rc = PMI_KVS_Get(KVSname, key, kvsVal, value_len_max);
+
+	    if (rc != PMI_SUCCESS || strncmp(kvsVal, valStr, value_len_max)) {
+		printf("[%d] Failed (rank = %d put = %d value = %s rc = %d)\n",
+		       rank, i, j, kvsVal, rc);
+		fflush(stdout);
+
+		return false;
+	    }
+	}
+    }
+
+    return true;
+}
 
 int main(void)
 {
-    int i, j;
-    int rank, size, has_parent;
-    int name_len_max, key_len_max, value_len_max;
-    char *kvs_name = NULL, *key = NULL, *value = NULL;
-    char *kvs_val = NULL;
+    int has_parent;
+    int rc = PMI_Init(&has_parent);
+    if (rc != PMI_SUCCESS) return -1;
 
-    PMI_Init(&has_parent);
+    int rank;
     PMI_Get_rank(&rank);
+    int size;
     PMI_Get_size(&size);
+
+    printf("[%d] size %d %sparent\n", rank, size, has_parent ? "" : "no ");
 
     PMI_KVS_Get_name_length_max(&name_len_max);
     PMI_KVS_Get_key_length_max(&key_len_max);
     PMI_KVS_Get_value_length_max(&value_len_max);
 
     //value_len_max = 1000; // @todo
-    if (!rank) printf("name/key/value max are %d/%d/%d\n", name_len_max,
-		      key_len_max, value_len_max);
+    //if (!rank) printf("name/key/value max are %d/%d/%d\n", name_len_max,
+    //key_len_max, value_len_max); // @todo
+    printf("[%d] name/key/value max are %d/%d/%d\n", rank, name_len_max,
+	   key_len_max, value_len_max);
 
-    key = (char *) malloc(key_len_max * sizeof(char));
-    value = (char *) malloc(value_len_max * sizeof(char));
-    kvs_name = (char *) malloc(name_len_max * sizeof(char));
-    kvs_val = (char *) malloc(value_len_max * sizeof(char));
-
+    char kvs_name[name_len_max];
     PMI_KVS_Get_my_name(kvs_name, name_len_max);
 
-    for (i = 0; i < value_len_max - 1; i++) {
-	value[i] = 'a';
-    }
-    value[value_len_max - 1] = '\0';
+    char value[value_len_max];
+    int len = (VAL_LEN) ? VAL_LEN : value_len_max;
+    for (int i = 0; i < len - 1; i++) value[i] = 33 + i %(127-33);
+    value[len - 1] = '\0';
 
-    for (i = 0; i < NUM_PUTS; i++) {
-	snprintf(key, key_len_max, "string-%d-%d", rank, i);
-	int rc = PMI_KVS_Put(kvs_name, key, value);
-	printf("[%d] i/rc are %d/%d\n", rank, i, rc);
-    }
+    // printf("[%d] val is '%s'\n", rank, value);
 
-    PMI_KVS_Commit(kvs_name);
+    bool result = true;
+    for (int i = 0; i < NUM_ROUNDS && result; i++) {
+	result = doKVSTest(kvs_name, rank, size, value);
 
-    PMI_Barrier();
-
-    for (i = 0; i < size; i++) {
-	for (j = 0; j < NUM_PUTS; j++) {
-	    snprintf(key, key_len_max, "string-%d-%d", i, j);
-
-	    int rc = PMI_KVS_Get(kvs_name, key, kvs_val, value_len_max);
-
-	    if (rc != PMI_SUCCESS || strncmp(kvs_val, value, value_len_max)) {
-		printf("[%d] Failed (i = %d j = %d value = %s rc = %d)\n",
-		       rank, i, j, kvs_val, rc);
-		fflush(stdout);
-
-		return -1;
-
-	    }
-	}
+	if (result) printf("[%d] Round %d: Pass\n", rank, i);
     }
 
-    printf("[%d] Pass\n", rank);
-    fflush(stdout);
+    if (result) {
+	printf("[%d] Pass\n", rank);
+	fflush(stdout);
+    }
 
-    free(key);
-    free(value);
-    free(kvs_name);
+    PMI_Finalize();
 
     return 0;
 }
