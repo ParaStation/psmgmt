05f6f645750ddc6170e24a8d340708bc065737dc
Michael Rauh <rauh@par-tec.com>
2022-09-02 16:52:03 +0200

    psaccount: Add rapl counters from sys filesystem to energy monitor script

--- plugins/psaccount/scripts/monitor_energy.sh
+++ plugins/psaccount/scripts/monitor_energy.sh
@@ -5,10 +5,21 @@
 # power:num energy:num
 #
 # power = current power usage
-# energy = used energy
+# energy = used energy in joules
 #
 # An optional environment variable ENERGY_TYPE may specify which
 # facility to use for energy monitoring.
 #
+if [ "$ENERGY_TYPE" == "acct_gather_energy/rapl" ]; then
+    # read rapl energy values from /sys filesystem
+    power=0; energy=0
+    for next in /sys/class/powercap/intel-rapl/intel-rapl\:*/energy_uj; do
+	val=$(cat $next)
+	# convert micro joule to joule and add all packages
+	energy=$(($energy + $val/1000000))
+    done
+else
+    power=0; energy=0
+fi
 
 echo power:$power energy:$energy
