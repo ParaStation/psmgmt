efd0555fac64fe8d64a6a3df0316cacf218544a3
Michael Rauh <rauh@par-tec.com>
2022-10-14 16:10:24 +0200

    Document optional ipmi account variables in monitor script

--- plugins/psaccount/scripts/monitor_energy.sh
+++ plugins/psaccount/scripts/monitor_energy.sh
@@ -10,6 +10,9 @@
 # An optional environment variable ENERGY_TYPE may specify which
 # facility to use for energy monitoring.
 #
+# Additional environment variables may be available dependent on
+# the different energy types
+
 if [ "$ENERGY_TYPE" == "acct_gather_energy/rapl" ]; then
     # read rapl energy values from /sys filesystem
     power=0; energy=0
@@ -18,6 +21,14 @@ if [ "$ENERGY_TYPE" == "acct_gather_energy/rapl" ]; then
 	# convert micro joule to joule and add all packages
 	energy=$(($energy + $val/1000000))
     done
+
+elif [ "$ENERGY_TYPE" == "acct_gather_energy/ipmi" ]; then
+    # read ipmi data
+    # optional environment variables include
+    # IPMI_FREQUENCY, IPMI_ADJUSTMENT, IPMI_POWER_SENSORS,
+    # IPMI_USERNAME, IPMI_PASSWORD
+
+    power=0; energy=0
 else
     power=0; energy=0
 fi
