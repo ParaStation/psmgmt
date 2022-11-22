cf779bdb3f33f5fa4d422b76ba8e406ba5bbacf4
Norbert Eicker <n.eicker@fz-juelich.de>
2022-11-23 00:04:43 +0100

    Enhance documenation of the Timer module

--- include/timer.h
+++ include/timer.h
@@ -24,27 +24,25 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-/**
- * The minimum timeout handled by this module in milli-seconds.
- */
+/** The minimum timeout handled by this module in milli-seconds */
 #define MIN_TIMEOUT_MSEC 100
 
 /**
- * @brief Initializes the Timer module.
+ * @brief Initializes the Timer module
  *
- * Initialization of the Timer machinery. If any timer is registered,
+ * Initialization of the Timer machinery. If any timer was registered,
  * it will be removed.
  *
- * @param logfile File to use for logging. If NULL, syslog(3) is used.
+ * @param logfile File to use for logging; if NULL, syslog(3) is used
  *
- * @return No return value.
+ * @return No return value
  *
  * @see syslog(3)
  */
 void Timer_init(FILE* logfile);
 
 /**
- * @brief Test if the Timer module is initialized.
+ * @brief Test if the Timer module is initialized
  *
  * Test if the Timer module is initialized, i.e. if Timer_init() was called
  * before.
@@ -62,24 +60,24 @@ typedef enum {
 } Timer_log_key_t;
 
 /**
- * @brief Query the debug-mask.
+ * @brief Query the Timer module's debug-mask
  *
  * Get the debug-mask of the Timer module.
  *
- * @return The actual debug-mask is returned.
+ * @return The actual debug-mask is returned
  *
  * @see Timer_setDebugMask()
  */
 int32_t Timer_getDebugMask(void);
 
 /**
- * @brief Set the debug-mask.
+ * @brief Set the Timer module's debug-mask
  *
  * Set the debug-mask of the Timer module. @a mask is a bit-wise OR of
  * the different keys defined within @ref Timer_log_key_t. If the
  * respective bit is set within @a mask, the log-messages marked with
  * the corresponding bits are put out to the selected channel
- * (i.e. stderr of syslog() as defined within @ref
+ * (i.e. stderr or syslog() as defined within @ref
  * Timer_init()). Accordingly a @mask of -1 means to put out all
  * messages defined.
  *
@@ -91,9 +89,9 @@ int32_t Timer_getDebugMask(void);
  *
  * At the time the Timer module only produces critical errors yet!
  *
- * @param mask The debug-mask to set.
+ * @param mask Debug-mask to set
  *
- * @return No return value.
+ * @return No return value
  *
  * @see Timer_getDebugMask(), Timer_log_key_t
  */
@@ -102,49 +100,42 @@ void Timer_setDebugMask(int32_t mask);
 /**
  * @brief Register a new timer
  *
- * Registration of a new timer. The @a timeoutHandler will be called
- * after @a timeout has elapsed. Afterwards, e.g. for removing using
- * @ref Timer_remove(), the timer will be identified by its unique ID
- * which is returned by this function.
+ * Register a new timer to call @a timeoutHandler after @a timeout has
+ * elapsed. To apply further actions to this timer identify it by its
+ * unique ID which is returned by this function.
  *
+ * @param timeout Amount of time to elapse before @a timeoutHandler is
+ * called again
  *
- * @param timeout The amount of time, after which the @a timeoutHandler is
- * called again.
+ * @param timeoutHandler Function to call when the @a timeout has elapsed
  *
- * @param timeoutHandler If @a timeout has elapsed, this function is called.
- *
- *
- * @return On success, the unique ID of the newly registered timer is
- * returned. This Id is a positive number i.e. not including 0. On
- * error, -1 is returned.
+ * @return On success, the unique ID of the new timer is returned, or
+ * -1 on failure; the ID is a positive number i.e. not including 0
  */
 int Timer_register(struct timeval* timeout, void (*timeoutHandler)(void));
 
 /**
  * @brief Register a new enhanced timer
  *
- * Registration of a new enhanced timer. The @a timeoutHandler will be
- * called after @a timeout has elapsed. Afterward, e.g. for removing
- * using @ref Timer_remove(), the timer will be identified by its
- * unique ID which is returned by this function.
+ * Register a new enhanced timer to call @a timeoutHandler after @a
+ * timeout has elapsed. To apply further actions to this timer
+ * identify it by its unique ID which is returned by this function.
  *
- * In contrast to Timer_register() the @a timeoutHandler registered
- * with this function gets the unique ID used to identify the timer
- * and @a info passed when the timer is elapsed.
+ * In contrast to @ref Timer_register() the @a timeoutHandler
+ * registered with this function gets its unique ID and @a info passed
+ * as arguments the timer has elapsed.
  *
+ * @param timeout Amount of time to elapse before @a timeoutHandler is
+ * called again
  *
- * @param timeout The amount of time, after which the @a timeoutHandler is
- * called again.
- *
- * @param timeoutHandler If @a timeout has elapsed, this handler is called.
+ * @param timeoutHandler Function to call when the @a timeout has elapsed
  *
  * @param info Pointer to additional information passed to @a
- * timeoutHandler in case of an elapsed timer.
+ * timeoutHandler in case of an elapsed timer
  *
  *
- * @return On success, the unique ID of the newly registered timer is
- * returned. This Id is a positive number i.e. not including 0. On
- * error, -1 is returned.
+ * @return On success, the unique ID of the new timer is returned, or
+ * -1 on failure; the ID is a positive number i.e. not including 0
  */
 int Timer_registerEnhanced(struct timeval* timeout,
 			   void (*timeoutHandler)(int, void *), void *info);
@@ -156,15 +147,15 @@ int Timer_registerEnhanced(struct timeval* timeout,
  *
  * @param id Unique ID used to identify the timer
  *
- * @return On success 0 is returned; or -1 in case of failure
+ * @return On success, 0 is returned, or -1 in case of failure
  */
 int Timer_remove(int id);
 
 /**
  * @brief Block a timer
  *
- * Block or unblock a registered timer. The timer will be identified by its
- * corresponding unique ID @a id.
+ * Block or unblock a registered timer. The timer will be identified
+ * by its unique ID @a id.
  *
  * @attention Calling this function from within the timer-handler does
  * not work as expected. This is due to the fact that the specific
@@ -180,7 +171,7 @@ int Timer_remove(int id);
  * facility's signal-handler. Here still all registered timers are
  * controlled for expiry and correspondingly marked, even if not
  * called immediately. The actual call will be carried out as soon as
- * the signal is unblocked. Thus, removing a timer via @ref
+ * the timer is unblocked. Thus, removing a timer via @ref
  * Timer_remove() usually saves resources compared to blocking a timer
  * for an undefined period.
  *
@@ -201,9 +192,21 @@ int Timer_block(int id, bool block);
  * Restart the timer identified by its unique ID @a id. This
  * guarantees that a new full timer period starts now.
  *
+ * @attention Be aware of the fact that timer periods here are defined
+ * differently than for POSIX per-process timers. While POSIX
+ * per-process timers guarantee to not expire before the defined
+ * period but slightly after, this timer might expire slightly before
+ * the period but never after. The amount of time it might expire
+ * early is given by the shortest period currently active. This
+ * actually defines the system timer to use and might be requested via
+ * @ref Timer_getActPeriod(). Accordingly, the new full period depends
+ * on how far from the next elapsed system timer this function is
+ * called. Nevertheless, it is guaranteed that no timer will elapse
+ * more than the shortest period earlier than its own period.
+ *
  * @param id Unique ID used to identify the timer
  *
- * @return On success 0 is returned; or -1 in case of failure
+ * @return On success, 0 is returned, or -1 in case of failure
  */
 int Timer_restart(int id);
 
@@ -221,14 +224,16 @@ int Timer_restart(int id);
  *
  * Since the actual work is done within this function, it is crucial
  * to call it on a regular basis in order to guarantee the handling of
- * the timers. Ideal points are immediately around central select()
- * calls or somewhere within a main loop of a program.
+ * the timers. Ideal points are immediately around central @ref
+ * select() or @ref epoll_wait() calls or somewhere within a main loop
+ * of a program.
  *
  * As an example, the Selector facility uses Timers in order to
  * provide certain functionality. Here @ref Timer_handleSignals() is
- * called every time before going into the central select(). Be aware
- * of the fact that this select() is interrupted on a regular basis
- * due to signals sent to the process because of elapsed timers.
+ * called every time before going into the central @ref select() or
+ * @ref epoll_wait(). This is supported by the fact that these @ref
+ * select() or @ref epoll_wait() calls are interrupted regularly due
+ * to signals sent to the process because of elapsed timers.
  *
  * @return No return value.
  */
