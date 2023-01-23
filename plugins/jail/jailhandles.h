cb759063a36bbe616c7c0bacaed5ba53c0b521b6
Michael Rauh <rauh@par-tec.com>
2023-01-23 13:23:15 +0100

    Add jail handle to be used in psslurm

---
+++ plugins/jail/jailhandles.h
@@ -0,0 +1,18 @@
+/*
+ * ParaStation
+ *
+ * Copyright (C) 2023 ParTec Cluster Competence Center GmbH, Munich
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined in the file LICENSE.QPL included in the packaging of this
+ * file.
+ */
+
+#ifndef __JAIL__HANDLES
+#define __JAIL__HANDLES
+
+#include "jailtypes.h"
+
+jailGetScripts_t *jailGetScripts;
+
+#endif  /* __JAIL__HANDLES */
