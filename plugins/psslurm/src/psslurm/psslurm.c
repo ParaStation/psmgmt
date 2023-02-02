1d040c2f23004fce79865b2c80d9a06c6b7560b9
Michael Rauh <rauh@par-tec.com>
2023-02-02 11:01:48 +0100

    Don't load psslurm of jail function handles are not avaiable

--- plugins/psslurm/src/psslurm/psslurm.c
+++ plugins/psslurm/src/psslurm/psslurm.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2014-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021-2022 ParTec AG, Munich
+ * Copyright (C) 2021-2023 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -541,7 +541,10 @@ static bool regJailHandles(void)
     void *pluginHandle = PSIDplugin_getHandle("jail");
 
     jailGetScripts = dlsym(pluginHandle, "jailGetScripts");
-    if (!jailGetScripts) flog("loading jailGetScripts() failed\n");
+    if (!jailGetScripts) {
+	flog("loading jailGetScripts() failed\n");
+	return false;
+    }
 
     return true;
 }
