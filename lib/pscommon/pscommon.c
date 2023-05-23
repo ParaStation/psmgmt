c28375db61cfb8babd96dc74252a801a5c4b54d2
Norbert Eicker <n.eicker@fz-juelich.de>
2023-05-23 23:34:04 +0200

    Do not spoil the namespace

--- lib/pscommon/pscommon.c
+++ lib/pscommon/pscommon.c
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2002-2004 ParTec AG, Karlsruhe
  * Copyright (C) 2005-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021-2022 ParTec AG, Munich
+ * Copyright (C) 2021-2023 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -586,7 +586,7 @@ static LIST_HEAD(localIPs);
  *
  * @return No return value
  */
-void getLocalIPs(void)
+static void getLocalIPs(void)
 {
     int skfd, n;
     struct ifconf ifc = { .ifc_buf = NULL } ;
