99ad39f8f636fcce5b0d1a1c4807cedd231a3820
Michael Rauh <rauh@par-tec.com>
2023-06-01 15:47:20 +0200

    Make jail user environment variable more generic

--- plugins/pspam/pspam/pspamcomm.c
+++ plugins/pspam/pspam/pspamcomm.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2017-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021-2022 ParTec AG, Munich
+ * Copyright (C) 2021-2023 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -43,7 +43,7 @@ static int jailChild(void *info)
 {
     Session_t *session = info;
 
-    setenv("__PSPAM_ADD_USER", session->user, 1);
+    setenv("__PSJAIL_ADD_USER_TO_CGROUP", session->user, 1);
     return PSIDhook_call(PSIDHOOK_JAIL_CHILD, &session->pid);
 }
 
