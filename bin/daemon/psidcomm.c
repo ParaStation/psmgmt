dfba299e34530f4553b7deb18100c36b1dfca2fa
Norbert Eicker <n.eicker@fz-juelich.de>
2023-06-13 22:32:35 +0200

    Suppress warnings of master pings to dynamic nodes

--- bin/daemon/psidcomm.c
+++ bin/daemon/psidcomm.c
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2003-2004 ParTec AG, Karlsruhe
  * Copyright (C) 2005-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021-2022 ParTec AG, Munich
+ * Copyright (C) 2021-2023 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -191,13 +191,19 @@ ssize_t sendMsg(void *amsg)
 	    PSID_dropMsg((DDBufferMsg_t *)msg);
 	    if (msg->type == PSP_CD_SENDSTOP || msg->type == PSP_CD_SENDCONT
 		|| msg->type == PSP_CC_MSG || msg->type == PSP_CC_ERROR) {
-		/* suppress message unless explicitely requested */
+		/* suppress message unless explicitly requested */
 		key = PSID_LOG_COMM;
 	    }
 	}
+	if (isRDP && msg->type == PSP_DD_DAEMONCONNECT && eno == EHOSTUNREACH
+	    && PSIDnodes_getAddr(PSC_getID(msg->dest)) == INADDR_NONE) {
+	    /* the message tries to reach an unconnected dynamic node */
+	    /* suppress message unless explicitly requested */
+	    key = PSID_LOG_STATUS;
+	}
 
 	if (eno == EWOULDBLOCK) {
-	    /* suppress message unless explicitely requested */
+	    /* suppress message unless explicitly requested */
 	    key = PSID_LOG_COMM;
 	}
 
