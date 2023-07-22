e46f015b02cf0984e4930ca41e62b7d4a7604c40
Norbert Eicker <n.eicker@fz-juelich.de>
2023-07-22 18:07:15 +0200

    Bugfix: let doGetMsgBuf() warn even if header.len is 0

--- lib/pscommon/psprotocol.c
+++ lib/pscommon/psprotocol.c
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2002-2004 ParTec AG, Karlsruhe
  * Copyright (C) 2005-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021-2022 ParTec AG, Munich
+ * Copyright (C) 2021-2023 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -272,19 +272,17 @@ static bool doGetMsgBuf(DDBufferMsg_t *msg, size_t *used, const char *callName,
 			const char *caller, const char *dataName, void *data,
 			size_t size, bool typed, bool try)
 {
-    size_t avail, u;
-
     if (!msg || !used || !data) {
 	PSC_log(-1, "%s: no '%s' provided for '%s' in %s()\n", callName,
 		msg ? (used ? "data" : "used") : "msg", dataName, caller);
 	return false;
     }
 
-    u = *used;
+    size_t u = *used;
     if (typed) u += offsetof(DDTypedBufferMsg_t, buf) - sizeof(msg->header);
 
-    avail = msg->header.len - sizeof(msg->header);
-    if (size > avail - u) {
+    ssize_t totAvail = msg->header.len - sizeof(msg->header);
+    if ((ssize_t)(size + u) > totAvail) {
 	PSC_log(try ? PSC_LOG_VERB : -1,
 		"%s: insufficient data for '%s' in %s()\n", callName, dataName,
 		caller);
