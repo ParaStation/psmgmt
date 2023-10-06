a02fceeeeb571a55d0c6a40dde7e51147f00f89e
Norbert Eicker <n.eicker@fz-juelich.de>
2023-10-07 00:50:51 +0200

    Move psid to sbin

--- plugins/dynIP/doc/AWS_ParallelCluster/ComputeNode/80-setup_psid.sh
+++ plugins/dynIP/doc/AWS_ParallelCluster/ComputeNode/80-setup_psid.sh
@@ -5,5 +5,5 @@ sysctl kernel.pid_max=32768
 cat << EOF > /etc/systemd/system/psid.service.d/override.conf
 [Service]
 ExecStart=
-ExecStart=/opt/parastation/bin/psid -d 0x28000100
+ExecStart=/opt/parastation/sbin/psid -d 0x28000100
 EOF
