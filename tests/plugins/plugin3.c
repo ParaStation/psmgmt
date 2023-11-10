0211e6366f99bf9d5cf5b3c0faedd29e07f5e068
Norbert Eicker <n.eicker@fz-juelich.de>
2023-11-10 19:22:54 +0100

    Copyright fixes

--- tests/plugins/plugin3.c
+++ tests/plugins/plugin3.c
@@ -2,6 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2012 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2023 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
