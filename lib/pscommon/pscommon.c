38e3df6edf497ab3921c68cd2b904934e6052cef
Norbert Eicker <n.eicker@fz-juelich.de>
2024-01-07 20:07:00 +0100

    Prevent memory leak when pscommon's logger is re-initialized

--- lib/pscommon/pscommon.c
+++ lib/pscommon/pscommon.c
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2002-2004 ParTec AG, Karlsruhe
  * Copyright (C) 2005-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021-2023 ParTec AG, Munich
+ * Copyright (C) 2021-2024 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -40,6 +40,7 @@ static PStask_ID_t myTID = -1;
 /* Wrapper functions for logging */
 void PSC_initLog(FILE* logfile)
 {
+    if (PSC_logger) logger_finalize(PSC_logger);
     PSC_logger = logger_init("PSC", logfile);
     if (!PSC_logger) {
 	if (logfile) {
