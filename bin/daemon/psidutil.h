09164eaf2f9048bfc5e8fdfe14bfcfe82457943e
Norbert Eicker <n.eicker@fz-juelich.de>
2024-02-14 00:34:38 +0100

    Omit unnecessary checks

--- bin/daemon/psidutil.h
+++ bin/daemon/psidutil.h
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 1999-2004 ParTec AG, Karlsruhe
  * Copyright (C) 2005-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021-2023 ParTec AG, Munich
+ * Copyright (C) 2021-2024 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -88,15 +88,13 @@ void PSID_setDebugMask(int32_t mask);
  *
  * @see logger_print()
  */
-#define PSID_log(...) if (PSID_logger) logger_print(PSID_logger, -1, __VA_ARGS__)
+#define PSID_log(...) logger_print(PSID_logger, -1, __VA_ARGS__)
 
-#define PSID_dbg(...) if (PSID_logger) logger_print(PSID_logger, __VA_ARGS__)
+#define PSID_dbg(...) logger_print(PSID_logger, __VA_ARGS__)
 
-#define PSID_flog(...) if (PSID_logger) logger_funcprint(PSID_logger, __func__,	\
-							 -1, __VA_ARGS__)
+#define PSID_flog(...) logger_funcprint(PSID_logger, __func__, -1, __VA_ARGS__)
 
-#define PSID_fdbg(...) if (PSID_logger) logger_funcprint(PSID_logger, __func__,	\
-							 __VA_ARGS__)
+#define PSID_fdbg(...) logger_funcprint(PSID_logger, __func__, __VA_ARGS__)
 
 /**
  * Print a warn messages via PSID's logging facility @a PSID_logger.
@@ -105,7 +103,7 @@ void PSID_setDebugMask(int32_t mask);
  *
  * @see logger_warn()
  */
-#define PSID_warn(...) if (PSID_logger)	logger_warn(PSID_logger, __VA_ARGS__)
+#define PSID_warn(...) logger_warn(PSID_logger, __VA_ARGS__)
 
 /**
  * Print a warn messages via PSID's logging facility @a PSID_logger
