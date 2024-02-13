7176ca27ae8be6936e3341afa18bd24a7a7f66b7
Norbert Eicker <n.eicker@fz-juelich.de>
2024-02-13 21:47:36 +0100

    Fix format error unveiled by use of logger_funcprint()

--- plugins/psgw/psgwres.c
+++ plugins/psgw/psgwres.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2018-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021-2023 ParTec AG, Munich
+ * Copyright (C) 2021-2024 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -220,7 +220,7 @@ void __cancelReq(PSGW_Req_t *req, char *reason, const char *func)
     writeErrorFile(req, reason, NULL, true);
 
     mlog("%s: %s", func, reason);
-    flog("canceling request for jobid %u\n", req->jobid);
+    flog("canceling request for jobid %s\n", req->jobid);
 
     /* kill routing script */
     if (req->routePID != -1) {
