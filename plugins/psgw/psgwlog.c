1c8c55cf23c2b107da56e7521f3680d35cc02014
Norbert Eicker <n.eicker@fz-juelich.de>
2024-02-13 21:46:02 +0100

    Utilize logger_funcprint() in psgw plugin
     - Do not use and remove limited local implementation

--- plugins/psgw/psgwlog.c
+++ plugins/psgw/psgwlog.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2018-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021 ParTec AG, Munich
+ * Copyright (C) 2021-2024 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -10,11 +10,7 @@
  */
 #include "psgwlog.h"
 
-#include <stdarg.h>
-
 #include "pluginlog.h"
-
-#define MAX_FLOG_SIZE 4096
 
 logger_t *psgwlogger = NULL;
 
@@ -27,24 +23,4 @@ void initLogger(char *name, FILE *logfile)
 void maskLogger(int32_t mask)
 {
     logger_setMask(psgwlogger, mask);
-}
-
-void __flog(const char *func, int32_t key, char *format, ...)
-{
-    static char buf[MAX_FLOG_SIZE];
-    char *fmt = format;
-    va_list ap;
-    size_t len;
-
-    if ((key != -1) && !(psgwlogger->mask & key)) return;
-
-    len = snprintf(NULL, 0, "%s: %s", func, format);
-    if (len+1 <= sizeof(buf)) {
-	snprintf(buf, sizeof(buf), "%s: %s", func, format);
-	fmt = buf;
-    }
-
-    va_start(ap, format);
-    logger_vprint(psgwlogger, -1, fmt, ap);
-    va_end(ap);
 }
--- plugins/psgw/psgwlog.h
+++ plugins/psgw/psgwlog.h
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2018-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2022 ParTec AG, Munich
+ * Copyright (C) 2022-2024 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -18,28 +18,12 @@
 
 extern logger_t *psgwlogger;
 
-#define mlog(...) if (psgwlogger) logger_print(psgwlogger, -1, __VA_ARGS__)
-#define mwarn(...) if (psgwlogger) logger_warn(psgwlogger, -1, __VA_ARGS__)
-#define mdbg(...) if (psgwlogger) logger_print(psgwlogger, __VA_ARGS__)
+#define mlog(...) logger_print(psgwlogger, -1, __VA_ARGS__)
+#define mwarn(...) logger_warn(psgwlogger, -1, __VA_ARGS__)
+#define mdbg(...) logger_print(psgwlogger, __VA_ARGS__)
 
-/**
- * @brief Print a log message with function prefix
- *
- * Print a log message and add a function name as prefix. The maximal message
- * length is MAX_FLOG_SIZE including the added function name. If the message
- * to print is larger then MAX_FLOG_SIZE it will be printed without prefix.
- *
- * @param func The function name to use as prefix
- *
- * @param format The format to be used in order to produce output. The
- * syntax of this parameter is according to the one defined for the
- * printf() family of functions from the C standard. This string will
- * also define the further parameters to be expected.
- */
-void __flog(const char *func, int32_t key, char *format, ...);
-
-#define flog(...) __flog(__func__, -1, __VA_ARGS__)
-#define fdbg(key, ...) __flog(__func__, key, __VA_ARGS__)
+#define flog(...) logger_funcprint(psgwlogger, __func__, -1, __VA_ARGS__)
+#define fdbg(key, ...) logger_funcprint(psgwlogger, __func__, key, __VA_ARGS__)
 
 /** Various types of logging levels for more verbose logging */
 typedef enum {
