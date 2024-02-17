11a927d81d5144743303125ed5493d97ec0320a2
Norbert Eicker <n.eicker@fz-juelich.de>
2024-02-17 16:41:12 +0100

    Streamlining

--- lib/plugincommon/pluginspawn.c
+++ lib/plugincommon/pluginspawn.c
@@ -17,11 +17,9 @@
 
 #include "pluginmalloc.h"
 
-SpawnRequest_t *initSpawnRequest(int num) {
-
-    SpawnRequest_t *req;
-
-    req = umalloc(sizeof(*req));
+SpawnRequest_t *initSpawnRequest(int num)
+{
+    SpawnRequest_t *req = umalloc(sizeof(*req));
     if (!req) return NULL;
 
     req->num = num;
@@ -35,34 +33,30 @@ SpawnRequest_t *initSpawnRequest(int num) {
     return req;
 }
 
-SpawnRequest_t *copySpawnRequest(SpawnRequest_t *req) {
-
-    SpawnRequest_t *ret;
-    SingleSpawn_t *old, *new;
-    int i, j;
-
+SpawnRequest_t *copySpawnRequest(SpawnRequest_t *req)
+{
     if (!req) return NULL;
 
-    ret = initSpawnRequest(req->num);
+    SpawnRequest_t *ret = initSpawnRequest(req->num);
     if (!ret) return NULL;
 
-    for (i=0; i < req->num; i++) {
-	old = &(req->spawns[i]);
-	new = &(ret->spawns[i]);
+    for (int i = 0; i < req->num; i++) {
+	SingleSpawn_t *old = &(req->spawns[i]);
+	SingleSpawn_t *new = &(ret->spawns[i]);
 
 	new->np = old->np;
 
 	if (old->argv) {
 	    new->argc = old->argc;
 	    new->argv = umalloc(new->argc * sizeof(*new->argv));
-	    for (j = 0; j < old->argc; j++) {
+	    for (int j = 0; j < old->argc; j++) {
 		new->argv[j] = ustrdup(old->argv[j]);
 	    }
 	}
 	if (old->preputv) {
 	    new->preputc = old->preputc;
 	    new->preputv = umalloc(new->preputc * sizeof(*new->preputv));
-	    for (j = 0; j < old->preputc; j++) {
+	    for (int j = 0; j < old->preputc; j++) {
 		new->preputv[j].key = ustrdup(old->preputv[j].key);
 		new->preputv[j].value = ustrdup(old->preputv[j].value);
 	    }
@@ -70,7 +64,7 @@ SpawnRequest_t *copySpawnRequest(SpawnRequest_t *req) {
 	if (old->infov) {
 	    new->infoc = old->infoc;
 	    new->infov = umalloc(new->infoc * sizeof(*new->infov));
-	    for (j = 0; j < old->infoc; j++) {
+	    for (int j = 0; j < old->infoc; j++) {
 		new->infov[j].key = ustrdup(old->infov[j].key);
 		new->infov[j].value = ustrdup(old->infov[j].value);
 	    }
@@ -86,26 +80,24 @@ SpawnRequest_t *copySpawnRequest(SpawnRequest_t *req) {
     return ret;
 }
 
-void freeSpawnRequest(SpawnRequest_t *req) {
-
-    int i, j;
-
-    for (i=0; i < req->num; i++) {
+void freeSpawnRequest(SpawnRequest_t *req)
+{
+    for (int i = 0; i < req->num; i++) {
 	if (req->spawns[i].argv) {
-	    for (j = 0; j < req->spawns[i].argc; j++) {
+	    for (int j = 0; j < req->spawns[i].argc; j++) {
 		ufree(req->spawns[i].argv[j]);
 	    }
 	    ufree(req->spawns[i].argv);
 	}
 	if (req->spawns[i].preputv) {
-	    for (j = 0; j < req->spawns[i].preputc; j++) {
+	    for (int j = 0; j < req->spawns[i].preputc; j++) {
 		ufree(req->spawns[i].preputv[j].key);
 		ufree(req->spawns[i].preputv[j].value);
 	    }
 	    ufree(req->spawns[i].preputv);
 	}
 	if (req->spawns[i].infov) {
-	    for (j = 0; j < req->spawns[i].infoc; j++) {
+	    for (int j = 0; j < req->spawns[i].infoc; j++) {
 		ufree(req->spawns[i].infov[j].key);
 		ufree(req->spawns[i].infov[j].value);
 	    }
