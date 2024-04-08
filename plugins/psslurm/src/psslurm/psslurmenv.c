f5fdcaebaa4d17bb56913a51adab791a7bcddbf3
Norbert Eicker <n.eicker@fz-juelich.de>
2024-04-08 12:07:09 +0200

    Streamlining

--- plugins/psslurm/src/psslurm/psslurmenv.c
+++ plugins/psslurm/src/psslurm/psslurmenv.c
@@ -611,17 +611,15 @@ static void setGResJobEnv(list_t *gresList, env_t env)
 	    char name[GPU_VARIABLE_MAXLEN+strlen(prefix)+1];
 	    for (size_t i = 0; gpu_variables[i]; i++) {
 		/* set variable if not already set by the user */
-		if (!envGet(env, gpu_variables[i])) {
-		    snprintf(name, sizeof(name), "%s%s", prefix,
-			    gpu_variables[i]);
-		    /* append some spaces to help step code to detect whether
-		     * the user has changed the variable in his job script */
-		    char *val = umalloc(strList.strLen + 6);
-		    sprintf(val, "%s     ", strList.buf);
-		    envSet(env, gpu_variables[i], val);
-		    envSet(env, name, val);
-		    ufree(val);
-		}
+		if (envGet(env, gpu_variables[i])) continue;
+		snprintf(name, sizeof(name), "%s%s", prefix, gpu_variables[i]);
+		/* append some spaces to help step code to detect whether
+		 * the user has changed the variable in his job script */
+		char *val = umalloc(strList.strLen + 6);
+		sprintf(val, "%s     ", strList.buf);
+		envSet(env, gpu_variables[i], val);
+		envSet(env, name, val);
+		ufree(val);
 	    }
 
 	    freeStrBuf(&strList);
