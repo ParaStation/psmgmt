8faeb7e19a8da2cd0e40f297b8092ca90ddf2017
Norbert Eicker <n.eicker@fz-juelich.de>
2024-06-26 16:03:43 +0200

    Use srun's colon syntax to re-spawn multiple apps

--- plugins/psslurm/src/psslurm/psslurmspawn.c
+++ plugins/psslurm/src/psslurm/psslurmspawn.c
@@ -63,10 +63,8 @@ static void addSpawnPreputToEnv(int preputc, KVP_t *preputv, env_t env)
     }
 }
 
-static size_t fillCmdForSingleSpawn(SpawnRequest_t *req, PStask_t *task)
+static size_t fillWithSrun(SpawnRequest_t *req, PStask_t *task)
 {
-    if (req->num != 1) return 0; // ensure to only handle single spawns
-
     const char *srun = getConfValueC(Config, "SRUN_BINARY");
     if (!srun) {
 	flog("no SRUN_BINARY provided\n");
@@ -94,166 +92,78 @@ static size_t fillCmdForSingleSpawn(SpawnRequest_t *req, PStask_t *task)
 	strvAdd(argV, "--cpu-bind=none");
     }
 
-    SingleSpawn_t *spawn = &(req->spawns[0]);
-
-    /* set the number of processes to spawn */
-    strvAdd(argV, "-n");                  // --ntasks=
-    snprintf(buffer, sizeof(buffer), "%d", spawn->np);
-    strvAdd(argV, buffer);
-
-    /* extract info values and keys
-     *
-     * These info variables are implementation dependend and can
-     * be used for e.g. process placement. All unsupported values
-     * will be silently ignored.
-     *
-     * ParaStation will support:
-     *
-     *  - wdir: The working directory of the spawned processes
-     *  - arch/nodetype: The type of nodes to be used
-     *  - path: The directory were to search for the executable
-     *  - tpp: Threads per process
-     *
-     * TODO:
-     *
-     *  - soft:
-     *	    - if not all processes could be started the spawn is still
-     *		considered successful
-     *	    - ignore negative values and >maxproc
-     *	    - format = list of triplets
-     *		+ list 1,3,4,5,8
-     *		+ triplets a:b:c where a:b range c= multiplicator
-     *		+ 0:8:2 means 0,2,4,6,8
-     *		+ 0:8:2,7 means 0,2,4,6,7,8
-     *		+ 0:2 means 0,1,2
-     */
-    for (int i = 0; i < spawn->infoc; i++) {
-	KVP_t *info = &(spawn->infov[i]);
-
-	if (strcmp(info->key, "wdir") == 0) {
-	    strvAdd(argV, "-D");          // --chdir=
-	    strvAdd(argV, info->value);
-	} else if (strcmp(info->key, "host") == 0) {
-	    strvAdd(argV, "-w");          // --nodelist=
-	    strvAdd(argV, info->value);
-	} else if (strcmp(info->key, "parricide") == 0) {
-	    if (!strcmp(info->value, "disabled")) {
-		task->noParricide = true;
-	    }
-	} else {
-	    flog("info key '%s' not supported\n", info->key);
-	}
-    }
-
-    char *envArg = strdup("--export=ALL");
-    for (char **env = envGetArray(spawn->env); env && *env; env++) {
-	char *eq = strchr(*env, '=');
-	if (!eq || eq == *env) continue;
-
-	/* append environment to argument */
-	char *tmpArg = PSC_concat(envArg, ",", *env);
-	if (!tmpArg) continue;
-	free(envArg);
-	envArg = tmpArg;
-
-	/* unset the current environment since it would have precedence */
-	*eq = '\0';
-	unsetenv(*env);
-	*eq = '=';
-    }
-    strvLink(argV, envArg);
-
-    for (int a = 0; a < spawn->argc; a++) strvAdd(argV, spawn->argv[a]);
-
-    task->argV = argV;
-
-    return spawn->np;
-}
-
-static int fillCmdForMultiSpawn(SpawnRequest_t *req, PStask_t *task)
-{
-    const char *srun = getConfValueC(Config, "SRUN_BINARY");
-    if (!srun) {
-	flog("no SRUN_BINARY provided\n");
-	return 0;
-    }
-
-    /* create multi-prog file */
-    char filebuf[64];
-    sprintf(filebuf, "/tmp/psslurm-spawn.%d.XXXXXX", getpid());
-    int fd = mkstemp(filebuf);
-    if (fd < 0) {
-	fwarn(errno, "failed to create temporary multi-prog file %s", filebuf);
-	return 0;
-    }
-    FILE *fs = fdopen(fd, "w");
-    if (!fs) {
-	fwarn(errno, "no stream for multi-prog file %s", filebuf);
-	return 0;
-    }
-
-    int numApps = req->num;
-    flog("writing %d applications to multi-prog file '%s'\n", numApps, filebuf);
-
     size_t nTasks = 0;
-    for (int i = 0; i < numApps; i++) {
-	SingleSpawn_t *spawn = &(req->spawns[i]);
+    for (int s = 0; s < req->num; s++) {
+	SingleSpawn_t *spawn = &(req->spawns[s]);
 
-	/* TODO: handle info (slurm ignores too) */
-	if (spawn->infoc > 0) {
-	    flog("spawn info not supported for spawn multiple binaries.\n");
+	if (s) {
+	    /* separate the next het group */
+	    strvAdd(argV, ":");
+	    /* reset environment propagation */
+	    strvAdd(argV, "--export=NONE");
 	}
 
-	if (spawn->np == 1) {
-	    fprintf(fs, "%zd", nTasks);
-	} else {
-	    fprintf(fs, "%zd-%zd", nTasks, nTasks + spawn->np - 1);
+	/* set the number of processes to spawn */
+	strvAdd(argV, "-n");                  // --ntasks=
+	snprintf(buffer, sizeof(buffer), "%d", spawn->np);
+	strvAdd(argV, buffer);
+
+	/* extract info values and keys
+	 *
+	 * These info variables are implementation dependend and can
+	 * be used for e.g. process placement. All unsupported values
+	 * will be silently ignored.
+	 *
+	 * ParaStation will support:
+	 *
+	 *  - wdir: The working directory of the spawned processes
+	 *  - parricide: The type of nodes to be used
+	 *  - path: The directory were to search for the executable
+	 *  - tpp: Threads per process
+	 *
+	 */
+	for (int i = 0; i < spawn->infoc; i++) {
+	    KVP_t *info = &(spawn->infov[i]);
+
+	    if (strcmp(info->key, "wdir") == 0) {
+		if (!s) {
+		    strvAdd(argV, "-D");          // --chdir=
+		    strvAdd(argV, info->value);
+		}
+	    } else if (strcmp(info->key, "host") == 0) {
+		strvAdd(argV, "-w");          // --nodelist=
+		strvAdd(argV, info->value);
+	    } else if (strcmp(info->key, "parricide") == 0) {
+		if (!strcmp(info->value, "disabled")) {
+		    task->noParricide = true;
+		}
+	    } else {
+		flog("info key '%s' not supported\n", info->key);
+	    }
 	}
 
-	if (envSize(spawn->env)) fprintf(fs, " env");
+	char *envArg = strdup("--export=ALL");
 	for (char **env = envGetArray(spawn->env); env && *env; env++) {
 	    char *eq = strchr(*env, '=');
 	    if (!eq || eq == *env) continue;
 
-	    /* prepend environment to app */
-	    fprintf(fs, " %s", *env);
-	}
+	    /* append environment to argument */
+	    char *tmpArg = PSC_concat(envArg, ",", *env);
+	    if (!tmpArg) continue;
+	    free(envArg);
+	    envArg = tmpArg;
 
-	for (int j = 0; j < spawn->argc; j++) fprintf(fs, " %s", spawn->argv[j]);
-	fprintf(fs, "\n");
+	    /* unset the current environment since it would have precedence */
+	    *eq = '\0';
+	    unsetenv(*env);
+	    *eq = '=';
+	}
+	strvLink(argV, envArg);
+
+	for (int a = 0; a < spawn->argc; a++) strvAdd(argV, spawn->argv[a]);
 
 	nTasks += spawn->np;
     }
-    fclose(fs);
-
-    strv_t argV = strvNew(NULL);
-    strvAdd(argV, srun);
-
-    /* ensure srun will not hang and wait for resources */
-    strvAdd(argV, "--immediate=5");
-
-    /* always use exact the resources requested */
-    strvAdd(argV, "--exact");
-
-    /* this is stupid but needed for best slurm compatibility
-       actually this removes our default rank binding from the spawned
-       processes which is needed, since slurm does none cpu binding by
-       default and inherits that to the spawned processes */
-    if (!(step->cpuBindType & ( CPU_BIND_NONE | CPU_BIND_TO_BOARDS
-				| CPU_BIND_MAP | CPU_BIND_MASK | CPU_BIND_LDMAP
-				| CPU_BIND_LDMASK | CPU_BIND_TO_SOCKETS
-				| CPU_BIND_TO_LDOMS | CPU_BIND_LDRANK
-				| CPU_BIND_RANK | CPU_BIND_TO_THREADS ))) {
-	strvAdd(argV, "--cpu-bind=none");
-    }
-
-    /* set the number of processes to spawn */
-    strvAdd(argV, "-n");                  // --ntasks=
-    snprintf(buffer, sizeof(buffer), "%zd", nTasks);
-    strvAdd(argV, buffer);
-
-    strvAdd(argV, "--multi-prog");
-    strvAdd(argV, filebuf);
 
     task->argV = argV;
 
@@ -311,12 +221,7 @@ int fillSpawnTaskWithSrun(SpawnRequest_t *req, int usize, PStask_t *task)
     SingleSpawn_t *spawn = &(req->spawns[0]);
     addSpawnPreputToEnv(spawn->preputc, spawn->preputv, task->env);
 
-    size_t jobSize;
-    if (req->num == 1) {
-	jobSize = fillCmdForSingleSpawn(req, task);
-    } else {
-	jobSize = fillCmdForMultiSpawn(req, task);
-    }
+    size_t jobSize = fillWithSrun(req, task);
 
     /* update environment */
     pmitype = envGet(task->env, "PSSLURM_PMI_TYPE");
