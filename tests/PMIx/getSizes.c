0cd9f7d90e9e265e6ff7ed0fe45b887a60142c4a
Norbert Eicker <n.eicker@fz-juelich.de>
2024-07-04 10:36:06 +0200

    Enhance PMIx/getSizes test

--- tests/PMIx/getSizes.c
+++ tests/PMIx/getSizes.c
@@ -2,9 +2,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define GET_INFO(name)				\
-    if (PMIX_SUCCESS != (rc = PMIx_Get(&scope, name, NULL, 0, &val))) { \
+    if (PMIX_SUCCESS != (rc = PMIx_Get(&scope, name, info, ninfo, &val))) { \
 	printf("Get "#name" failed: %s\n", PMIx_Error_string(rc));	\
 	exit(1);							\
     }									\
@@ -13,9 +14,7 @@
 int main(void)
 {
     pmix_status_t rc;
-    pmix_value_t *val;
     pmix_proc_t proc;
-
     if (PMIX_SUCCESS != (rc = PMIx_Init(&proc, NULL, 0))) {
 	printf("[%s:%u]: PMIx_Init failed: %s\n", proc.nspace, proc.rank,
 	       PMIx_Error_string(rc));
@@ -29,10 +28,26 @@ int main(void)
     PMIX_LOAD_PROCID(&scope, proc.nspace, PMIX_RANK_WILDCARD);
 
     /* get various information */
+    pmix_value_t *val;
+    pmix_info_t *info = NULL;
+    size_t ninfo = 0;
+
     GET_INFO(PMIX_UNIV_SIZE);
     GET_INFO(PMIX_JOB_SIZE);
-    GET_INFO(PMIX_APP_SIZE);
 
+    PMIX_LOAD_PROCID(&scope, proc.nspace, proc.rank);
+    GET_INFO(PMIX_APPNUM);
+
+    PMIX_LOAD_PROCID(&scope, proc.nspace, PMIX_RANK_WILDCARD);
+    ninfo = 2;
+    PMIX_INFO_CREATE(info, ninfo);
+    PMIX_INFO_LOAD(&(info[0]), PMIX_APP_INFO, NULL, PMIX_BOOL);
+    PMIX_INFO_LOAD(&(info[1]), PMIX_APPNUM, &val->data.uint32, PMIX_UINT32);
+    GET_INFO(PMIX_APP_SIZE);
+    PMIX_INFO_FREE(info, ninfo);
+    ninfo = 0;
+
+    sleep(1);
     if (PMIX_SUCCESS != (rc = PMIx_Finalize(NULL, 0))) {
 	printf("[%s:%u]: PMIx_Finalize failed: %s\n", proc.nspace, proc.rank,
 	       PMIx_Error_string(rc));
