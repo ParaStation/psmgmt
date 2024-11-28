443a98b71825f9c9ad7249c206f02cf7543abc7a
Michael Rauh <rauh@par-tec.com>
2024-11-28 16:26:51 +0100

    Allow to control the psaccount monitor scripts using psiadmin

--- plugins/psaccount/psaccountkvs.c
+++ plugins/psaccount/psaccountkvs.c
@@ -26,6 +26,8 @@
 #include "psaccountjob.h"
 #include "psaccountlog.h"
 #include "psaccounttypes.h"
+#include "psaccountinterconnect.h"
+#include "psaccountfilesystem.h"
 
 FILE *memoryDebug = NULL;
 
@@ -71,7 +73,6 @@ static char *showEnergy(void)
 
 char *show(char *key)
 {
-
     if (!key) {
 	strbuf_t buf = strbufNew(NULL);
 	strbufAdd(buf, "use key [clients|dclients|jobs|config|energy]\n");
@@ -96,6 +97,71 @@ char *show(char *key)
     strbuf_t buf = strbufNew(NULL);
     strbufAdd(buf, "invalid key, use [clients|dclients|jobs|config|energy]\n");
     return strbufSteal(buf);
+}
+
+/** define monitor start function */
+typedef bool monStart_t(void);
+/** define monitor stop function */
+typedef void monStop_t(void);
+
+/**
+ * @brief Control a monitor script
+ *
+ * @param buf Message buffer to return result to user
+ *
+ * @param startFunc Monitor script start function
+ *
+ * @param stopFunc Monitor script stop function
+ *
+ * @param name Name of the monitor script
+ *
+ * @param cmd Command to execute
+ */
+static void ctlScript(strbuf_t buf, monStart_t *startFunc, monStop_t *stopFunc,
+		      char *name, char *cmd)
+{
+    if (!strcmp(cmd, "start")) {
+	bool ret = startFunc();
+	if (ret) {
+	    strbufAdd(buf, "\nStarted '");
+	    strbufAdd(buf, name);
+	    strbufAdd(buf, "' monitor script\n'");
+	} else {
+	    strbufAdd(buf, "\nStarting '");
+	    strbufAdd(buf, name);
+	    strbufAdd(buf, "' monitor script failed\n'");
+	}
+    } else if (!strcmp(cmd, "stop")) {
+	stopFunc();
+	strbufAdd(buf, "\nStopped '");
+	strbufAdd(buf, name);
+	strbufAdd(buf, "' monitor script\n'");
+    } else {
+	strbufAdd(buf, "\nInvalid command '");
+	strbufAdd(buf, cmd);
+	strbufAdd(buf, "', use start or stop'");
+    }
+}
+
+/** define monitor control environment function */
+typedef bool monCtlEnv_t(psAccountCtl_t, const char *);
+
+static void setScriptEnv(strbuf_t buf, monCtlEnv_t *envCtl, char *name,
+			 psAccountCtl_t action, const char *envStr)
+{
+    if (envCtl(action, envStr)) {
+	strbufAdd(buf, "\n");
+	strbufAdd(buf, (action == PSACCOUNT_SCRIPT_ENV_SET ? "Set" : "Unset"));
+	strbufAdd(buf, " environment '");
+	strbufAdd(buf, envStr);
+	strbufAdd(buf, "' for '");
+	strbufAdd(buf, name);
+	strbufAdd(buf, "' monitor script\n'");
+    } else {
+	strbufAdd(buf, "\nFailed to ");
+	(strbufAdd(buf, action == PSACCOUNT_SCRIPT_ENV_SET ? "set" : "unset"));
+	strbufAdd(buf, " environment\n");
+    }
 }
 
 char *set(char *key, char *val)
@@ -149,6 +215,33 @@ char *set(char *key, char *val)
 	    strbufAdd(buf, val);
 	    strbufAdd(buf, "' for writing failed\n");
 	}
+    } else if (!strcmp(key, "ctlEnergy")) {
+	ctlScript(buf, &Energy_startScript, &Energy_stopScript,
+		  "energy", val);
+    } else if (!strcmp(key, "ctlFilesystem")) {
+	ctlScript(buf, &FS_startScript, FS_stopScript, "file-system",
+		  val);
+    } else if (!strcmp(key, "ctlInterconnect")) {
+	ctlScript(buf, &IC_startScript, &IC_stopScript, "interconnect",
+		  val);
+    } else if (!strcmp(key, "setEnergyEnv")) {
+	setScriptEnv(buf, &Energy_ctlEnv, "energy", PSACCOUNT_SCRIPT_ENV_SET,
+		     val);
+    } else if (!strcmp(key, "setFS_Env")) {
+	setScriptEnv(buf, &FS_ctlEnv, "file-system", PSACCOUNT_SCRIPT_ENV_SET,
+		     val);
+    } else if (!strcmp(key, "setIC_Env")) {
+	setScriptEnv(buf, &IC_ctlEnv, "interconnect", PSACCOUNT_SCRIPT_ENV_SET,
+		     val);
+    } else if (!strcmp(key, "unsetEnergyEnv")) {
+	setScriptEnv(buf, &Energy_ctlEnv, "energy", PSACCOUNT_SCRIPT_ENV_UNSET,
+		     val);
+    } else if (!strcmp(key, "unsetFS_Env")) {
+	setScriptEnv(buf, &FS_ctlEnv, "file-system", PSACCOUNT_SCRIPT_ENV_UNSET,
+		     val);
+    } else if (!strcmp(key, "unsetIC_Env")) {
+	setScriptEnv(buf, &IC_ctlEnv, "interconnect",
+		     PSACCOUNT_SCRIPT_ENV_UNSET, val);
     } else {
 	strbufAdd(buf, "\nInvalid key '");
 	strbufAdd(buf, key);
@@ -202,7 +295,12 @@ char *help(char *key)
 		 confDef[i].name, type, confDef[i].desc);
 	strbufAdd(buf, line);
     }
-    strbufAdd(buf, "\nuse show [clients|dclients|jobs|config]\n");
+    strbufAdd(buf, "\nuse show [clients|dclients|jobs|config]\n\n");
+    strbufAdd(buf, "\nset [ctlEnergy|ctlFilesystem|ctlInterconnect] ");
+    strbufAdd(buf, "[start|stop] to control monitor scripts\n");
+    strbufAdd(buf, "\nset [setEnergyEnv|setIC_Env|setFS_Env] name=val\n");
+    strbufAdd(buf, "set [unsetEnergyEnv|unsetIC_Env|unsetFS_Env] name\n");
+    strbufAdd(buf, "to change environment for monitor scripts\n");
 
     return strbufSteal(buf);
 }
