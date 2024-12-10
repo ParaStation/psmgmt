eae66ea87219ffc5e4cf6318822531c0b4ff1edc
Norbert Eicker <n.eicker@fz-juelich.de>
2024-12-10 16:55:04 +0100

    Add some missing copyright statements

--- include/kvscommon.h
+++ include/kvscommon.h
@@ -2,6 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2007-2019 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2024 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
