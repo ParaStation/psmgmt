3f41bb8f97ccb659a61c5f04dfe3e522e459ca2f
Norbert Eicker <n.eicker@fz-juelich.de>
2025-01-29 23:19:28 +0100

    IWYU fixes

--- plugins/psslurm/src/psslurm/psslurmaccount.c
+++ plugins/psslurm/src/psslurm/psslurmaccount.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2019 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2022-2024 ParTec AG, Munich
+ * Copyright (C) 2022-2025 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -12,12 +12,10 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 #include "pluginconfig.h"
 #include "pluginmalloc.h"
 #include "psaccounthandles.h"
-#include "pscommon.h"
 
 #include "slurmcommon.h"
 #include "psslurmlog.h"
