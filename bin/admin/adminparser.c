00ec1a188e6e116281c59a6630b13e64e56d7ac1
Norbert Eicker <n.eicker@fz-juelich.de>
2025-02-10 13:42:39 +0100

    Update visible copyrights in psiadmin

--- bin/admin/adminparser.c
+++ bin/admin/adminparser.c
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2003-2004 ParTec AG, Karlsruhe
  * Copyright (C) 2005-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021-2024 ParTec AG, Munich
+ * Copyright (C) 2021-2025 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -2431,7 +2431,7 @@ static int versionCommand(char *token)
     printf(" Copyright (C) 1996-2004 ParTec AG, Karlsruhe\n");
     printf(" Copyright (C) 2005-2021 ParTec Cluster Competence Center GmbH,"
 	   " Munich\n");
-    printf(" Copyright (C) 2021-2024 ParTec AG, Munich\n");
+    printf(" Copyright (C) 2021-2025 ParTec AG, Munich\n");
     printf("\n");
     printf("PSProtocol: %d\t(with a %sgeneous setup)\n", PSProtocolVersion,
 	   PSI_mixedProto() ? "hetero" : "homo");
