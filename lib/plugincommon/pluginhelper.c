053f00d907b788720f89e89f271f580376df5bca
Michael Rauh <rauh@par-tec.com>
2025-07-29 11:08:35 +0200

    Ensure mkDir() does not operate on an empty path

--- lib/plugincommon/pluginhelper.c
+++ lib/plugincommon/pluginhelper.c
@@ -2,7 +2,7 @@
  * ParaStation
  *
  * Copyright (C) 2012-2021 ParTec Cluster Competence Center GmbH, Munich
- * Copyright (C) 2021-2024 ParTec AG, Munich
+ * Copyright (C) 2021-2025 ParTec AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -98,7 +98,7 @@ static bool doCreateDir(const char *dir, mode_t mode, uid_t uid, gid_t gid)
 
 bool mkDir(const char *path, mode_t mode, uid_t uid, gid_t gid)
 {
-    if (!path) {
+    if (!path || !*path) {
 	pluginflog("invalid directory\n");
 	return false;
     }
